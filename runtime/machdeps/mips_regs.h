11820d8a49f0388cc274dd3c8892f45ba32db62f
Fergus Henderson <fjh@cs.mu.oz.au>
1993-10-22 19:06:18 +0000

    A very minor cosmetic change.

--- runtime/machdeps/mips_regs.h
+++ runtime/machdeps/mips_regs.h
@@ -9,14 +9,14 @@
    At the moment we're only using the callee-save registers.
    We should modify this to optionally use the caller-save registers. */
 
-reg 	Word	mr0 __asm("s0");
-reg	Word	mr1 __asm("s1");
-reg	Word	mr2 __asm("s2");
-reg	Word	mr3 __asm("s3");
-reg	Word	mr4 __asm("s4");
-reg	Word	mr5 __asm("s5");
-reg	Word	mr6 __asm("s6");
-reg	Word	mr7 __asm("s7");
+reg 	Word	mr0 __asm__("s0");
+reg	Word	mr1 __asm__("s1");
+reg	Word	mr2 __asm__("s2");
+reg	Word	mr3 __asm__("s3");
+reg	Word	mr4 __asm__("s4");
+reg	Word	mr5 __asm__("s5");
+reg	Word	mr6 __asm__("s6");
+reg	Word	mr7 __asm__("s7");
 
 #define NUM_REAL_REGS 8
 
