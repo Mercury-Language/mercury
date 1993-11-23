eae0ea1f6efc2c04049c2e1f30af047ce7fd49fc
Fergus Henderson <fjh@cs.mu.oz.au>
1993-11-23 23:53:49 +0000

    Add Tom's recent changes.

--- library/string.m
+++ library/string.m
@@ -24,4 +24,37 @@ char_to_string(Char,String) :-
 	length(String,1),
 	name(Char,String).	
 
+:- pred string__int_to_str(int, string).
+:- mode string__int_to_str(input, output).
+
+string__int_to_str(N, Str) :-
+	(if
+		N < 0
+	then
+		N1 is 0 - N,
+		string__int_to_str(N1, Str1),
+		string__append("-", Str1, Str)
+	else if
+		N < 10
+	then
+		digit_to_string(N,Str)
+	else
+		N10 is N mod 10,
+		N1 is N // 10,
+		digit_to_string(N10, Digit),
+		string__int_to_str(N1, Str1),
+		string__append(Str1, Digit, Str)
+	).
+
+digit_to_string(0, "0").
+digit_to_string(1, "1").
+digit_to_string(2, "2").
+digit_to_string(3, "3").
+digit_to_string(4, "4").
+digit_to_string(5, "5").
+digit_to_string(6, "6").
+digit_to_string(7, "7").
+digit_to_string(8, "8").
+digit_to_string(9, "9").
+
 :- end_module string.
