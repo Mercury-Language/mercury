085420b1410c39b121066a7c49274a00103ac1ae
Fergus Henderson <fjh@cs.mu.oz.au>
1993-12-08 13:00:23 +0000

    Did some more work on hlds.nl.

--- compiler/make_hlds.m
+++ compiler/make_hlds.m
@@ -2,6 +2,13 @@
 :- module make_hlds.
 :- import prog_io, hlds.
 
+% This module converts from the parse tree structure which is
+% read in by prog_io.nl, into the simplified high level data structure
+% defined in hlds.nl.  In the parse tree, the program is represented
+% as a list of items; we insert each item into the appropriate symbol
+% table, and report any duplicate definition errors.  We also
+% transform 
+
 %-----------------------------------------------------------------------------%
 :- pred parse_tree_to_hlds(prog_io:program, hlds:module_info).
 :- mode parse_tree_to_hlds(input, output).
@@ -51,9 +58,9 @@ add_item(module_defn(_VarSet, _ModuleDefn), Module, Module) -->
 %-----------------------------------------------------------------------------%
 
 module_add_inst_defn(Module0, VarSet, InstDefn, Cond, Module) -->
-	Module0 = module(Name, Preds, Types, Insts0, Modes),
+	{ Module0 = module(Name, Preds, Types, Insts0, Modes) },
 	insts_add(Insts0, VarSet, InstDefn, Cond, Insts),
-	Module =  module(Name, Preds, Types, Insts, Modes).
+	{ Module =  module(Name, Preds, Types, Insts, Modes) }.
 
 insts_add(Insts0, VarSet, inst_defn(Name, Args, Body), Cond, Insts) -->
 	{ length(Args, Arity),
@@ -70,38 +77,44 @@ insts_add(Insts0, VarSet, inst_defn(Name, Args, Body), Cond, Insts) -->
 			multiple_def_error(Name, Arity, "inst")
 		)
 	else
-		map__insert(Insts0, Name - Arity, I, Insts)
+		{ map__insert(Insts0, Name - Arity, I, Insts) }
 	).
+
+inst_is_compat(I1, I2) :-
+	I1 = I2.
 
 %-----------------------------------------------------------------------------%
 
 module_add_mode_defn(Module0, VarSet, TypeDefn, Cond, Module) -->
-	Module0 = module(Name, Preds, Types, Insts, Modes0),
+	{ Module0 = module(Name, Preds, Types, Insts, Modes0) },
 	modes_add(Modes0, VarSet, ModeDefn, Cond, Modes),
-	Module =  module(Name, Preds, Types, Insts, Modes).
+	{ Module =  module(Name, Preds, Types, Insts, Modes) }.
 
 modes_add(Modes0, VarSet, mode_defn(Name, Args, Body), Cond, Modes) -->
-	length(Args, Arity),
-	I = hlds__mode_defn(VarSet, Args, Body, Cond),
+	{ length(Args, Arity),
+	  I = hlds__mode_defn(VarSet, Args, Body, Cond) },
 	(if some [I2]
-		map__search(Modes0, Name - Arity, I2)
+		{ map__search(Modes0, Name - Arity, I2) }
 	then
-		Modes = Modes0,
+		{ Modes = Modes0 },
 		(if mode_is_compat(I, I2) then
 			duplicate_def_warning(Name, Arity, "mode")
 		else
 			multiple_def_error(Name, Arity, "mode")
 		)
 	else
-		map__insert(Modes0, Name - Arity, I, Modes)
+		{ map__insert(Modes0, Name - Arity, I, Modes) }
 	).
+
+mode_is_compat(M1, M2) :-
+	M1 = M2.
 
 %-----------------------------------------------------------------------------%
 
 module_add_type_defn(Module0, VarSet, TypeDefn, Cond, Module) -->
-	Module0 = module(Name, Preds, Types0, Insts, Modes),
+	{ Module0 = module(Name, Preds, Types0, Insts, Modes) },
 	types_add(Types0, VarSet, TypeDefn, Cond, Types),
-	Module =  module(Name, Preds, Types, Insts, Modes).
+	{ Module =  module(Name, Preds, Types, Insts, Modes) }.
 
 type_name_args(du_type(Name, Args, Body), Name, Args, du_type(Body)).
 type_name_args(uu_type(Name, Args, Body), Name, Args, uu_type(Body)).
@@ -126,22 +139,49 @@ types_add(Types0, VarSet, TypeDefn, Cond, Types) -->
 		{ map__insert(Types0, Name - Arity, I, Types) }
 	).
 
+type_is_compat(T1, T2) :-
+	T1 = T2.
+
 %-----------------------------------------------------------------------------%
 
-module_add_pred(Module0, VarSet, PredName, TypesAndModes, Cond, Module) --> ...
-	Module0 = module(Name, Preds0, Types, Insts, Modes),
-	preds_add(Preds0, VarSet, PredName, TypesAndModes, Cond, Preds),
-	Module = module(Name, Preds, Types, Insts, Modes).
+module_add_pred(Module0, VarSet, PredName, TypesAndModes, Cond, Module) -->
+	{ Module0 = module(Name, Preds0, Types, Insts, Modes) },
+	{ split_types_and_modes(TypesAndModes, Types, MaybeModes) },
+	preds_add(Preds0, VarSet, PredName, Types, Cond, Preds),
+	{ Module1 = module(Name, Preds, Types, Insts, Modes) },
+	(if some [Modes]
+		{ MaybeModes = yes(Modes) }
+	then
+		module_add_modes(Module1, VarSet, PredName, Modes, Cond, Module)
+	else
+		{ Module = Module1 }
+	).
 
-preds_add(Preds0, VarSet, Name, TypesAndModes, Cond, Preds) -->
-	{ length(TypesAndModes, Arity),
-	  I = predicate(VarSet, Args, Body, Cond) },
+split_types_and_modes(TypesAndModes, Types, MaybeModes) :-
+	split_types_and_modes_2(TypesAndModes, Types, Modes, no, R),
+	(if [R = yes] then
+		{ MaybeModes = yes(Modes) }
+	else
+		MaybeModes = no
+	).
+
+split_types_and_modes_2([], [], [], R, R).
+split_types_and_modes_2([TM|TMs], [T|Ts], [M|Ms], R0, R) :-
+	split_type_and_mode(TM, T, M),
+	split_types_and_modes_2(TMs, Ts, Ms, R0, R).
+
+split_type_and_mode(type_only(T), T, free -> free, _, yes).
+split_type_and_mode(type_and_mode(T,M), T, M, R, R).
+
+preds_add(Preds0, VarSet, Name, Types, Cond, Preds) -->
+	{ length(Types, Arity),
+	  I = predicate(VarSet, Types, Cond, ) },
 	(if some [I2]
-		{ map__search(Types0, Name - Arity, I2) }
+		{ map__search(Preds0, Name - Arity, I2) }
 	then
 		{ Types = Types0 },
 		(if 
-			{ type_is_compat(I, I2) }
+			{ pred_is_compat(I, I2) }
 		then
 			duplicate_def_warning(Name, Arity, "pred")
 		else
@@ -153,7 +193,10 @@ preds_add(Preds0, VarSet, Name, TypesAndModes, Cond, Preds) -->
 
 %-----------------------------------------------------------------------------%
 
-module_add_mode(Module0, VarSet, PredName, Modes, Cond, Module) --> ...
+module_add_mode(Module0, VarSet, PredName, Modes, Cond, Module) -->
+	{ Module0 = module(Name, Preds0, Types, Insts, Modes) },
+	preds_add(Preds0, VarSet, PredName, Modes, Cond, Preds),
+	{ Module = module(Name, Preds, Types, Insts, Modes) }.
 
 module_add_clause(Module0, VarSet, PredName, Args, Body, Module) --> ...
 
