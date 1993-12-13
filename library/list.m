cf59b66d64bece486d7e8189bb76095ef5edd3b7
Thomas Conway <conway@cs.mu.oz.au>
1993-12-13 06:21:19 +0000

    Added a condense predicate: condense: list(list(T)) -> list(T)

--- library/list.m
+++ library/list.m
@@ -8,4 +8,12 @@
 :- pred member(T, list(T), list(T)).
 :- pred member(T, list(T)).
 :- pred length(list(T), integer).
+:- pred condense(list(list(T)),list(T)).
+
+:- implementation.
+
+condense([], []).
+condense([L|Ls], R) :-
+	condense(Ls, R1),
+	append(L, R1, R).
 
