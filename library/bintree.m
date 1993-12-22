5f151f0d5147647475bd02eae881c61baa733ad6
Thomas Conway <conway@cs.mu.oz.au>
1993-12-22 23:51:49 +0000

    Partly debugged - insert and search work.

--- library/bintree.m
+++ library/bintree.m
@@ -51,10 +51,56 @@ bintree__search(tree(K0 - V0, Left, Right), K, V) :-
 		V0 = V
 	else if
 		Result = (<)
-	them
+	then
 		bintree__search(Right, K, V)
 	else
 		bintree__search(Left, K, V)
 	).
 
+:- pred bintree__delete(bintree(K,V), K, bintree(K,V)).
+:- mode bintree__delete(input, input, output).
+
+bintree__delete(empty, K, empty).
+bintree__delete(tree(K0 - V0, Left, Right), K, Tree) :-
+	compare(Result, K0, K),
+	(if
+		Result = (=)
+	then
+		bintree__fixup(Left, Right, Tree)
+	else if
+		Result = (<)
+	then
+		bintree_delete(Right, K, Tree1),
+		Tree = tree(K0 - V0, Left, Tree1)
+	else
+		bintree_delete(Left, K, Tree1),
+		Tree = tree(K0 - V0, Tree1, Right)
+	).
+
+bintree__fixup(empty, Right, Right).
+bintree__fixup(tree(Node, Left, Right0), Right, Tree) :-
+	bintree__fixup(Right0, Right, Right1),
+	Tree = tree(Node, Left, Right1).
+
+:- pred bintree__from_list(list(pair(K,V)), bintree(K,V)).
+:- mode bintree__from_list(input, output).
+
+bintree__from_list(List, Tree) :-
+	bintree__from_list_2(List, empty, Tree).
+
+bintree__from_list_2([], Tree, Tree).
+bintree__from_list_2([K - V | List], Tree0, Tree) :-
+	bintree__insert(Tree0, K, V, Tree1),
+	bintree__from_list_2(List, Tree1, Tree).
+
+:- pred bintree__to_list(bintree(K,V), list(pair(K,V))).
+:- mode bintree__to_list(input, output).
+
+bintree__to_list(Tree, List) :-
+	bintree__to_list_2(Tree, [], List).
+
+bintree__to_list_2(empty, List, List).
+bintree__to_list_2(tree(Node, Left, Right), List0, List) :-
+	bintree__to_list_2(Right, List0, List1),
+	bintree__to_list_2(Left, [Node | List1], List).
 
