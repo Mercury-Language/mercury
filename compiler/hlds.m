031426ef9e82a0f201ea79da8eb7f7c57dd994b3
Thomas Conway <conway@cs.mu.oz.au>
1994-01-06 04:47:49 +0000

    A few little fixes.

--- compiler/hlds.m
+++ compiler/hlds.m
@@ -141,7 +141,7 @@
 				% into case statements by the determinism
 				% analysis.
 				% Variable, functor-args-goal, followvars
-			;	switch(var_id, list(case), vars)
+			;	switch(var_id, list(case), XXX)
 
 				% Initially only the two terms are filled
 				% in.  Mode analysis fills in the last
