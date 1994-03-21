a4492a380232b4f1a52126ac48adc8da62550b00
Fergus Henderson <fjh@cs.mu.oz.au>
1994-03-21 18:59:25 +0000

    Added lots of comments; added a couple of new predicates.
    set.nl:
    	Added lots of comments; added a couple of new predicates.

--- library/set.m
+++ library/set.m
@@ -1,41 +1,106 @@
 %--------------------------------------------------------------------------%
 %--------------------------------------------------------------------------%
 
-:- module set.
+% File: set.nl.
+% Main author: conway.
+% (As usual, hacked a little by fjh ;-).
 
+% This file contains a `set' ADT.
+% Sets are implemented here as unsorted lists.
+% XXX Are they allowed to contain duplicates?
+% -- If so, there is a bug in set__delete, and the code for set__union
+%    should probably be replaced by a call to append.
+% -- If not, there is a bug in either the code for set__insert,
+%    or at least in the type declaration (the pred declaration
+%    for set__insert should have a `where' clause and be one of Lee's types).
+
+%--------------------------------------------------------------------------%
+
+:- module set.
 :- interface.
+:- import_module list.
 
 :- type set(T).
+
+	% `set__init(Set)' is true iff `Set' is an empty set.
 
 :- pred set__init(set(T)).
 :- mode set__init(out) is det.
 
+	% `set__equal(SetA, SetB)' is true iff
+	% `SetA' and `SetB' contain the same elements.
+
 :- pred set__equal(set(T), set(T)).
 :- mode set__equal(in, in) is semidet.
+
+	% `set__subset(SetA, SetB)' is true iff `SetA' is a subset of `SetB'.
 
 :- pred set__subset(set(T), set(T)).
 :- mode set__subset(in, in).
 
+	% `set__superset(SetA, SetB)' is true iff `SetA' is a
+	% superset of `SetB'.
+
 :- pred set__superset(set(T), set(T)).
 :- mode set__superset(in, in).
 
-% The mode for set__member should be more general to
-% allow for matching within set__member ie matching on
-% top level functors or whatever.
+	% `set_member(X, Set)' is true iff `X' is a member of `Set'.
 
 :- pred set__member(T, set(T)).
 :- mode set__member(in, in) is det.
 :- mode set__member(out, in) is nondet.
 
+	% `set__insert(Set0, X, Set)' is true iff `Set' is the union of
+	% `Set0' and the set containing only `X'.
+
 :- pred set__insert(set(T), T, set(T)).
 :- mode set__insert(in, in, out) is det.
+
+	% `set__insert_list(Set0, Xs, Set)' is true iff `Set' is the union of
+	% `Set0' and the set containing only the members of `Xs'.
+
+:- pred set__insert_list(set(T), T, set(T)).
+:- mode set__insert_list(in, in, out) is det.
+
+	% `set__delete(Set0, X, Set)' is true iff `Set0' contains `X',
+	% and `Set' is the relative complement of `Set0' and the set
+	% containing only `X', i.e.  if `Set' is the set which contains
+	% all the elements of `Set0' except `X'.
 
 :- pred set__delete(set(T), T, set(T)).
 :- mode set__delete(in, out, out) is nondet.
 :- mode set__delete(in, in, out) is semidet.
 
+	% `set__delete_list(Set0, Xs, Set)' is true iff `Set0' contains
+	% every member of `Xs', and `Set' is the relative complement of
+	% `Set0' and the set containing only the members of `Xs'.
+
+:- pred set__delete_list(set(T), list(T), set(T)).
+:- mode set__delete_list(in, in, out) is semidet.
+
+	% `set__remove(Set0, X, Set)' is true iff `Set' is the relative
+	% complement of `Set0' and the set containing only `X', i.e.
+	% if `Set' is the set which contains all the elements of `Set0'
+	% except `X'.
+
+:- pred set__remove(set(T), T, set(T)).
+:- mode set__remove(in, in, out) is det.
+
+	% `set__remove_list(Set0, Xs, Set)' is true iff `Set' is the relative
+	% complement of `Set0' and the set containing only the members of
+	% `Xs'.
+
+:- pred set__remove_list(set(T), list(T), set(T)).
+:- mode set__remove_list(in, in, out) is det.
+
+	% `set_union(SetA, SetB, Set)' is true iff `Set' is the union of
+	% `SetA' and `SetB'.
+
 :- pred set__union(set(T), set(T), set(T)).
 :- mode set__union(in, in, out) is det.
+
+	% `set_intersect(SetA, SetB, Set)' is true iff `Set' is the
+	% intersection of `SetA' and `SetB'.
 
 :- pred set__intersect(set(T), set(T), set(T)).
 :- mode set__intersect(in, in, out).
@@ -44,56 +109,79 @@
 
 :- import_module list.
 
-:- type set(T)          ==      list(T).
+:- type set(T)		  ==	  list(T).
 
 set__init([]).
 
-set__equal([], []).
-set__equal([E|S0], S1) :-
-    set__delete(S1, E, S2),
-    set__equal(S0, S2).
+set__equal(S1, S2) :-
+	set__subset(S1, S2),
+	set__subset(S2, S1).
 
 set__subset([], S).
 set__subset([E|S0], S1) :-
-    set__member(E, S1),
-    set__subset(S0, S1).
+	set__member(E, S1),
+	set__subset(S0, S1).
 
 set__superset(S0, S1) :-
-    set__subset(S1, S0).
+	set__subset(S1, S0).
 
 set__member(E, S) :-
-    member(E, S).
+	member(E, S).
 
 set__insert(S0, E, [E|S0]).
+	% XXX are sets allowed to contain duplicates??
+
+:- set__delete_list(_, Xs, _) when Xs.
+
+set__delete_list(S, [], S).
+set__delete_list(S0, [X | Xs], S) :-
+	set__delete(S0, X, S1),
+	set__delete_list(S1, Xs, S).
 
 set__delete(S0, E, S) :-
-    delete(S0, E, S).
+	delete(S0, E, S).
+
+:- set__remove_list(_, Xs, _) when Xs.
+
+set__remove_list(S, [], S).
+set__remove_list(S0, [X | Xs], S) :-
+	set__remove(S0, X, S1),
+	set__remove_list(S1, Xs, S).
+
+set__remove([], _, []).
+set__remove([E|Es], X, S) :-
+	( E = X ->
+		Es1 = Es
+	;
+		Es1 = [E|Es]
+	),
+	set__remove(Es1, X, S).
 
 set__union([], S, S).
 set__union([E|S0], S1, S) :-
-    (
-	member(E, S1)
-    ->
-	S2 = S1
-    ;
-	S2 = [E|S1]
-    ),
-    set__union(S0, S2, S).
+	(
+		member(E, S1)
+	->
+		S2 = S1
+	;
+		S2 = [E|S1]
+	),
+	set__union(S0, S2, S).
 
 set__intersect(S0, S1, S) :-
-    set__intersect_2(S0, S1, [], S).
+	set__intersect_2(S0, S1, [], S).
 
 :- pred set__intersect_2(set(T), set(T), set(T), set(T)).
 :- mode set__intersect_2(in, in, in, out).
 
 set__intersect_2([], S1, S, S).
 set__intersect_2([E|S0], S1, S2, S) :-
-    (
-	member(E, S1)
-    ->
-	S3 = [E|S2]
-    ;
-	S3 = S2
-    ),
-    set__intersect_2(S0, S1, S3, S).
+	(
+		member(E, S1)
+	->
+		S3 = [E|S2]
+	;
+		S3 = S2
+	),
+	set__intersect_2(S0, S1, S3, S).
 
