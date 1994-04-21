7adc13d1b5114bbb23a8afc70a71ce76842a49eb
Fergus Henderson <fjh@cs.mu.oz.au>
1994-04-21 07:38:31 +0000

    Bug fixes.

--- scripts/mint.sh
+++ scripts/mint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [  $# != 1 ]
 then
-	echo "usage: `basename $0` module-name" 1 >& 2
+	echo "usage: `basename $0` module-name" 1>&2
 	exit 1
 fi
 
@@ -9,7 +9,7 @@ file="@LIBDIR@/$1.int"
 
 if [ -r $file ]
 then
-	$(PAGER:-more) $file
+	${PAGER:-more} $file
 else
 	echo "`basename $0`: no interface file for $1." 1>&2
 fi
