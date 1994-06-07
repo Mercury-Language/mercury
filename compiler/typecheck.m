654efd52050552292454a7c40a123444473ed1ae
Fergus Henderson <fjh@cs.mu.oz.au>
1994-06-07 02:40:03 +0000

    Split the checking for undefined types in typecheck.nl into
    typecheck.nl, undef_types.nl, Makefile, doit.nl:
    	Split the checking for undefined types in typecheck.nl into
    	a separate module undef_types.nl.

--- compiler/typecheck.m
+++ compiler/typecheck.m
@@ -135,6 +135,7 @@
 :- import_module varset, term, prog_util, type_util.
 :- import_module term_io, prog_out, hlds_out, mercury_to_mercury.
 :- import_module options, getopt, globals.
+:- import_module undef_types.
 
 %-----------------------------------------------------------------------------%
 
@@ -1467,160 +1468,6 @@ check_circular_types_2([TypeId | TypeIds], Types0, Types) -->
 
 JUNK ****/
 	
-
-%-----------------------------------------------------------------------------%
-%-----------------------------------------------------------------------------%
-
-	% Check for any possible undefined types.
-	% Should we add a definition for undefined types?
-
-:- pred check_undefined_types(module_info, module_info, io__state, io__state).
-:- mode check_undefined_types(in, out, di, uo).
-check_undefined_types(Module, Module) -->
-	{ module_info_types(Module, TypeDefns) },
-	{ map__keys(TypeDefns, TypeIds) },
-	find_undef_type_bodies(TypeIds, TypeDefns),
-	globals__lookup_option(statistics, bool(Statistics)),
-	maybe_report_stats(Statistics),
-	{ module_info_preds(Module, Preds) },
-	{ module_info_predids(Module, PredIds) },
-	find_undef_pred_types(PredIds, Preds, TypeDefns).
-
-	% Find any undefined types used in `:- pred' declarations.
-
-:- pred find_undef_pred_types(list(pred_id), pred_table, type_table,
-				io__state, io__state).
-:- mode find_undef_pred_types(in, in, in, di, uo).
-
-find_undef_pred_types([], _Preds, _TypeDefns) --> [].
-find_undef_pred_types([PredId | PredIds], Preds, TypeDefns) -->
-	{ map__lookup(Preds, PredId, PredDefn) },
-	{ pred_info_arg_types(PredDefn, _VarSet, ArgTypes) },
-	{ pred_info_context(PredDefn, Context) },
-	find_undef_type_list(ArgTypes, pred(PredId) - Context, TypeDefns),
-	find_undef_pred_types(PredIds, Preds, TypeDefns).
-
-	% Find any undefined types used in the bodies of other type
-	% declarations.
-
-:- pred find_undef_type_bodies(list(type_id), type_table, io__state, io__state).
-:- mode find_undef_type_bodies(in, in, di, uo).
-
-find_undef_type_bodies([], _) --> [].
-find_undef_type_bodies([TypeId | TypeIds], TypeDefns) -->
-	{ map__lookup(TypeDefns, TypeId, HLDS_TypeDefn) },
-		% XXX abstract hlds__type_defn/5
-	{ HLDS_TypeDefn = hlds__type_defn(_, _, TypeBody, _, Context) },
-	find_undef_type_body(TypeBody, type(TypeId) - Context, TypeDefns),
-	find_undef_type_bodies(TypeIds, TypeDefns).
-
-	% Find any undefined types used in the given type definition.
-
-:- pred find_undef_type_body(hlds__type_body, error_context, type_table,
-				io__state, io__state).
-:- mode find_undef_type_body(in, in, in, di, uo).
-
-find_undef_type_body(eqv_type(Type), ErrorContext, TypeDefns) -->
-	find_undef_type(Type, ErrorContext, TypeDefns).
-find_undef_type_body(uu_type(Types), ErrorContext, TypeDefns) -->
-	find_undef_type_list(Types, ErrorContext, TypeDefns).
-find_undef_type_body(du_type(Constructors), ErrorContext, TypeDefns) -->
-	find_undef_type_du_body(Constructors, ErrorContext, TypeDefns).
-find_undef_type_body(abstract_type, _ErrorContext, _TypeDefns) --> [].
-
-	% Find any undefined types in a list of types.
-
-:- pred find_undef_type_list(list(type), error_context, type_table,
-				io__state, io__state).
-:- mode find_undef_type_list(in, in, in, di, uo).
-
-find_undef_type_list([], _ErrorContext, _TypeDefns) --> [].
-find_undef_type_list([Type|Types], ErrorContext, TypeDefns) -->
-	find_undef_type(Type, ErrorContext, TypeDefns),
-	find_undef_type_list(Types, ErrorContext, TypeDefns).
-
-	% Find any undefined types in a list of contructors
-	% (the constructors for a discrimiated union type).
-
-:- pred find_undef_type_du_body(list(constructor), error_context, type_table,
-				io__state, io__state).
-:- mode find_undef_type_du_body(in, in, in, di, uo).
-
-find_undef_type_du_body([], _ErrorContext, _TypeDefns) --> [].
-find_undef_type_du_body([Constructor | Constructors], ErrorContext,
-		TypeDefns) -->
-	{ Constructor = _Functor - ArgTypes },
-	find_undef_type_list(ArgTypes, ErrorContext, TypeDefns),
-	find_undef_type_du_body(Constructors, ErrorContext, TypeDefns).
-
-	% Find any undefined types used in type.
-	% The type itself may be undefined, and also
-	% any type arguments may also be undefined.
-	% (eg. the type `undef1(undef2, undef3)' should generate 3 errors.)
-
-:- pred find_undef_type(type, error_context, type_table,
-				io__state, io__state).
-:- mode find_undef_type(in, in, in, di, uo).
-
-find_undef_type(term__variable(_), _ErrorContext, _TypeDefns) --> [].
-find_undef_type(term__functor(F, As, _), ErrorContext, TypeDefns) -->
-	% Efficiency is very important here - this is the inner loop
-	% of checking for undefined types.
-	% The tests are ordered so as to maximize effiency.
-	% Could efficiency be improved further?
-	{ list__length(As, Arity) },
-	{ make_type_id(F, Arity, TypeId) },
-	(
-		{ is_builtin_atomic_type(TypeId) }
-	->
-		[]
-	;
-		{ map__contains(TypeDefns, TypeId) }
-	->
-		[]
-	;
-		{ is_builtin_pred_type(TypeId) }
-	->
-		[]
-	;
-		report_undef_type(TypeId, ErrorContext)
-	),
-	find_undef_type_list(As, ErrorContext, TypeDefns).
-
-%-----------------------------------------------------------------------------%
-
-:- type error_context == pair(error_context_2, term__context).
-:- type error_context_2 ---> type(type_id) ; pred(pred_id).
-
-	% Output an error message about an undefined type
-	% in the specified context.
-
-:- pred report_undef_type(type_id, error_context, io__state, io__state).
-:- mode report_undef_type(in, in, di, uo).
-
-report_undef_type(TypeId, ErrorContext - Context) -->
-	prog_out__write_context(Context),
-	io__write_string("In definition of "),
-	write_error_context(ErrorContext),
-	io__write_string(":\n"),
-	prog_out__write_context(Context),
-	io__write_string("  error: undefined type "),
-	hlds_out__write_type_id(TypeId),
-	io__write_string(".\n").
-
-	% Output a description of the context where an undefined type was
-	% used.
-
-:- pred write_error_context(error_context_2, io__state, io__state).
-:- mode write_error_context(in, di, uo).
-
-write_error_context(pred(PredId)) -->
-	io__write_string("predicate "),
-	hlds_out__write_pred_id(PredId).
-write_error_context(type(TypeId)) -->
-	io__write_string("type "),
-	hlds_out__write_type_id(TypeId).
-
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
 
@@ -1696,38 +1543,6 @@ make_pred_cons_info(PredId, PredTable, FuncArity, L0, L) :-
 	;
 		L = L0
 	).
-
-	% is_builtin_atomic_type(TypeId)
-	%	is true iff 'TypeId' is the type_id of a builtin atomic type
-
-:- pred is_builtin_atomic_type(type_id).
-:- mode is_builtin_atomic_type(in) is semidet.
-
-is_builtin_atomic_type(QualifiedName - 0) :-
-	unqualify_name(QualifiedName, Name),
-	is_builtin_atomic_type_2(Name).
-
-:- pred is_builtin_atomic_type_2(string).
-:- mode is_builtin_atomic_type_2(in) is semidet.
-
-:- is_builtin_atomic_type_2([]) when ever.
-:- is_builtin_atomic_type_2([X|_]) when X.
-
-is_builtin_atomic_type_2("int").
-is_builtin_atomic_type_2("float").
-is_builtin_atomic_type_2("string").
-is_builtin_atomic_type_2("character").
-
-	% is_builtin_pred_type(TypeId)
-	%	is true iff 'TypeId' is the type_id of a builtin higher-order
-	%	predicate type.
-
-:- pred is_builtin_pred_type(type_id).
-:- mode is_builtin_pred_type(in) is semidet.
-
-is_builtin_pred_type(QualifiedName - _Arity) :-
-	unqualify_name(QualifiedName, Name),
-	Name = "pred".
 
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
