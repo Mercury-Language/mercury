b2a45fc2b4655a1564b41afcd06a7df8c17c8177
Thomas Conway <conway@cs.mu.oz.au>
1994-06-20 22:37:15 +0000

    fix.
    library.nl:
    	fix.

--- library/library.m
+++ library/library.m
@@ -9,10 +9,10 @@
 :- interface.
 
 :- import_module array, bag, bimap, bintree, bintree_set, char.
-:- import_module dirgraphs, float, graph, group, int, io.
+:- import_module float, graph, group, int, io.
 :- import_module list, map, pqueue, queue, random, require.
 :- import_module set, stack, std_util, string, term, term_io.
-:- import_module utils, varset.
+:- import_module varset.
 
 %---------------------------------------------------------------------------%
 %---------------------------------------------------------------------------%
