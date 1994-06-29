7fcfe88c257fb23be5b48fd585674d0f36bb6615
Thomas Conway <conway@cs.mu.oz.au>
1994-06-29 15:31:00 +0000

    *** empty log message ***

--- runtime/getopt.h
+++ runtime/getopt.h
@@ -1,7 +1,7 @@
 #define	GETOPTHUH	'?'
 #define	GETOPTDONE	(-1)
 
-extern int	getopt(int, char **, const char *);
+extern int	getopt(int, char *const*, const char *);
 
 extern char	*optarg;
 extern int	opterr;
