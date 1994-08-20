32a3fa046f290cba5baf935ddb37b483b66a13b5
Fergus Henderson <fjh@cs.mu.oz.au>
1994-08-20 15:21:20 +0000

    Pass an appropriate --c-include-directory option to
    mc.sh:
    	Pass an appropriate --c-include-directory option to
    	mercury_compile.

--- scripts/mc.sh
+++ scripts/mc.sh
@@ -2,6 +2,7 @@
 
 INTDIR=${MERCURY_INT_DIR:-@LIBDIR@/ints}
 DEPDIR=${MERCURY_DEP_DIR:-@LIBDIR@/deps}
+C_INCL=${MERCURY_C_INCL_DIR:-@LIBDIR@/inc}
 MC=${MERCURY_COMPILER:-@LIBDIR@/nuprolog/@FULLARCH@/mercury_compile}
 
-exec $MC -I "$INTDIR" -I "$DEPDIR" "$@"
+exec $MC -I "$INTDIR" -I "$DEPDIR" --c-include-directory "$C_INCL" "$@"
