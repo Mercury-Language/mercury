ef46405b26878b985f8914c5611ce9d285d15d87
Fergus Henderson <fjh@cs.mu.oz.au>
1995-01-20 14:13:18 +0000

    Generate code for predicates even if they have been inlined,
    code_gen.pp:
    	Generate code for predicates even if they have been inlined,
    	since we may have taken their address.

--- compiler/code_gen.pp
+++ compiler/code_gen.pp
@@ -174,51 +174,38 @@ generate_proc_list_code(ModuleInfo, PredId, PredInfo, [ProcId | ProcIds],
 	{ pred_info_procedures(PredInfo, ProcInfos) },
 		% locate the proc_info structure for this mode of the predicate
 	{ map__lookup(ProcInfos, ProcId, ProcInfo) },
-	globals__io_lookup_bool_option(inlining, Inlining),
+		% find out if the proc is deterministic/etc
+	{ proc_info_interface_determinism(ProcInfo, Category) },
+		% now generate the code for this.
+	generate_category_code(ModuleInfo, PredId, ProcId,
+		ProcInfo, Category, Instr, SUsed),
+		% turn the code tree into a list
+	{ tree__flatten(Instr, InstrList) },
+		% now the code is a list of
+		% code-fragments(==list(instr)),
+		% so we need to do a level of
+		% unwinding to get a flat list.
+	{ list__condense(InstrList, Instructions0) },
 	(
-			% if the proc is inlined and not exported
-		{ Inlining = yes },
-		{ \+ pred_info_is_exported(PredInfo) },
-		{ proc_info_goal(ProcInfo, Goal) },
-		{ code_aux__contains_only_builtins(Goal) },
-		{ code_aux__goal_is_flat(Goal) }
+		{ SUsed = yes(SlotNum) }
 	->
-		generate_proc_list_code(ModuleInfo, PredId, PredInfo, ProcIds,
-			Procedures)
+		{ code_gen__add_saved_succip(Instructions0,
+					SlotNum, Instructions) }
 	;
-			% find out if the proc is deterministic/etc
-		{ proc_info_interface_determinism(ProcInfo, Category) },
-			% now generate the code for this.
-		generate_category_code(ModuleInfo, PredId, ProcId,
-			ProcInfo, Category, Instr, SUsed),
-			% turn the code tree into a list
-		{ tree__flatten(Instr, InstrList) },
-			% now the code is a list of
-			% code-fragments(==list(instr)),
-			% so we need to do a level of
-			% unwinding to get a flat list.
-		{ list__condense(InstrList, Instructions0) },
-		(
-			{ SUsed = yes(SlotNum) }
-		->
-			{ code_gen__add_saved_succip(Instructions0,
-						SlotNum, Instructions) }
-		;
-			{ Instructions = Instructions0 }
-		),
-			% get the name and arity of this predicate
-		{ predicate_name(ModuleInfo, PredId, Name) },
-		{ predicate_arity(ModuleInfo, PredId, Arity) },
-			% construct a c_procedure structure
-			% will all the information
-		{ Procedure = c_procedure(Name, Arity, ProcId, Instructions) },
-			% and do the same thing for all
-			% the rest of the procedures
-			% for this predicate.
-		generate_proc_list_code(ModuleInfo, PredId, PredInfo, ProcIds,
-			Procedures0),
-		{ Procedures = [Procedure | Procedures0] }
-	).
+		{ Instructions = Instructions0 }
+	),
+		% get the name and arity of this predicate
+	{ predicate_name(ModuleInfo, PredId, Name) },
+	{ predicate_arity(ModuleInfo, PredId, Arity) },
+		% construct a c_procedure structure
+		% will all the information
+	{ Procedure = c_procedure(Name, Arity, ProcId, Instructions) },
+		% and do the same thing for all
+		% the rest of the procedures
+		% for this predicate.
+	generate_proc_list_code(ModuleInfo, PredId, PredInfo, ProcIds,
+		Procedures0),
+	{ Procedures = [Procedure | Procedures0] }.
 
 %
 % Generate code for the predicate (PredId,Mode).
