dd522cfc0a7176c7f32e37bcde8928487c78f5bd
Fergus Henderson <fjh@cs.mu.oz.au>
1995-02-26 14:13:32 +0000

    Rename the NU-Prolog version of the compiler as `mcn' rather than `mc'. We now use the name `mc' for the Mercury version of the compiler.

--- scripts/mcn.sh
+++ scripts/mcn.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# MC - Mercury Compiler.
+# MCN - Mercury Compiler (compiled with NU-Prolog).
 #
-# Use `mc -h' for help.
+# Use `mcn -h' for help.
 #
 # Environment variables: MERCURY_INT_DIR, MERCURY_DEP_DIR, MERCURY_C_INCL_DIR,
 # MERCURY_COMPILER.
@@ -10,6 +10,6 @@
 INTDIR=${MERCURY_INT_DIR:-@LIBDIR@/ints}
 DEPDIR=${MERCURY_DEP_DIR:-@LIBDIR@/deps}
 C_INCL=${MERCURY_C_INCL_DIR:-@LIBDIR@/inc}
-MC=${MERCURY_COMPILER:-@LIBDIR@/bin/@FULLARCH@/mercury_compile}
+MC=${MERCURY_COMPILER:-@LIBDIR@/nuprolog/@FULLARCH@/mercury_compile}
 
-exec $MC -- -I "$INTDIR" -I "$DEPDIR" --c-include-directory "$C_INCL" "$@"
+exec $MC -I "$INTDIR" -I "$DEPDIR" --c-include-directory "$C_INCL" "$@"
