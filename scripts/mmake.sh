872f1442ccd1f9f508fe552f2d7c493c0844cb4f
Fergus Henderson <fjh@cs.mu.oz.au>
1995-03-15 11:01:42 +0000

    A bunch of changes.  Add usage message.  Add -s/--save option
    scripts/mmake.sh:
    	A bunch of changes.  Add usage message.  Add -s/--save option
    	to save the generated Makefile.  Various changes so that it
    	actually works.

--- scripts/mmake.sh
+++ scripts/mmake.sh
@@ -4,11 +4,72 @@
 #
 # mmake - Mercury Make.
 #
+#	Type mmake -h for help.
+#
 #-----------------------------------------------------------------------------#
 
-MAKE=${MAKE:-make}
-MERCURY_MMAKE_VARS=${MERCURY_MAKEFILE:-@LIBDIR@/mmake/Mmake.vars}
-MERCURY_MMAKE_RULES=${MERCURY_MAKEFILE2:-@LIBDIR@/mmake/Mmake.rules}
+MAKE=${MMAKE_MAKE:-make}
+MMAKE_VARS=${MMAKE_VARS:-@LIBDIR@/mmake/Mmake.vars}
+MMAKE_RULES=${MMAKE_RULES:-@LIBDIR@/mmake/Mmake.rules}
+MERCURY_INT_DIR=${MERCURY_INT_DIR:-@LIBDIR@/ints}
+
+VPATH=${MMAKE_VPATH}:${MERCURY_INT_DIR}
+MMAKE=$0
+verbose=false
+save_makefile=false
+
+while [ $# -gt 0 ]; do
+	case $1 in
+		-h|--help)
+#-----------------------------------------------------------------------------#
+			cat << 'EOF'
+Usage: mmake [<mmake options>] [-- <make options>] <target>...
+Options:
+	-s, --save-makefile:
+		Save the generated makefile to `Mmake.makefile'.
+	-v, --verbose:
+		Print verbose progress messages.
+	-h, --help:
+		Print this usage message.
+Targets:
+	<module>.depend:
+		Make the file `<module>.dep'.  This step is required
+		in preparation for the targets below.
+	<module>:
+		Compile and link a Mercury program with main module
+		`<module>.nl' to produce an executable.
+	<module>.nu:
+		Compile and link a Mercury program with NU-Prolog
+		rather than with the Mercury compiler.
+	clean:
+		Remove intermediate files.
+	realclean:
+		Remove all automatically-generated files: intermediate files,
+		dependency files, and executables.
+EOF
+#-----------------------------------------------------------------------------#
+			exit
+			;;
+		-s|--save-makefile)
+			save_makefile=true
+			MMAKE="$MMAKE $1"
+			shift
+			;;
+		-v|--verbose)
+			verbose=true
+			MMAKE="$MMAKE $1"
+			shift
+			;;
+		--)	
+			MMAKE="$MMAKE $1"
+			shift
+			break
+			;;
+		*)
+			break
+			;;
+	esac
+done
 
 if [ -f Mmake ]; then
 	mmake="Mmake"
@@ -25,9 +86,23 @@ if [ "`echo *.d`" = "*.d" ]; then
 else
 	ds="*.d"
 fi
-VPATH=${MERCURY_INT_DIR:-@LIBDIR@/ints}:$VPATH
+
+if $save_makefile; then
+	tmp=Mmake.makefile
+else
+	tmp=/tmp/mmake.$$
+	trap 'status=$?; rm -f $tmp; exit $status' 0 1 2 3 13 15
+fi
+
+if $verbose; then
+	echo MMAKE=$MMAKE
+	echo export MMAKE
+	echo VPATH=$VPATH
+	echo export VPATH
+	echo cat ${MMAKE_VARS} $deps $ds $mmake ${MMAKE_RULES} ">" $tmp
+	echo ${MAKE} -f $tmp "$@"
+fi
+export MMAKE
 export VPATH
-tmp=/tmp/mmake.$$
-trap 'rm -f $tmp' 0 1 2 3 13 15
-cat ${MERCURY_MMAKE_VARS} $mmake ${MERCURY_MMAKE_RULES} $deps $ds > $tmp
-${MAKE} -f $tmp "$@"
+cat ${MMAKE_VARS} $deps $ds $mmake ${MMAKE_RULES} > $tmp
+${MAKE} -f $tmp -r "$@"
