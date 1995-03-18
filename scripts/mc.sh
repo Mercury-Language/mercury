3e59b94d053759d7b9b8487735d6f5004975cd7e
Fergus Henderson <fjh@cs.mu.oz.au>
1995-03-18 09:52:21 +0000

    Remove an old temporary hack.
    mc.sh:
    	Remove an old temporary hack.

--- scripts/mc.sh
+++ scripts/mc.sh
@@ -12,9 +12,4 @@ DEPDIR=${MERCURY_DEP_DIR:-@LIBDIR@/deps}
 C_INCL=${MERCURY_C_INCL_DIR:-@LIBDIR@/inc}
 MC=${MERCURY_COMPILER:-"@LIBDIR@/bin/@FULLARCH@/mercury_compile --"}
 
-# this is a temporary hack so I can get some idea of who is actually
-# using Mercury
-{ date; echo "$@"; echo; } >> /tmp/.mc_log.$USER
-chmod go+r /tmp/.mc_log.$USER
-
 exec $MC -I "$INTDIR" -I "$DEPDIR" --c-include-directory "$C_INCL" "$@"
