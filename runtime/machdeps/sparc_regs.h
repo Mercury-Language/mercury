10f730f1ad6ea4768ccc99926a5365f71041165b
Fergus Henderson <fjh@cs.mu.oz.au>
1995-03-30 21:58:28 +0000

    Add copyright notices.
    runtime/machdeps/*:
    	Add copyright notices.

--- runtime/machdeps/sparc_regs.h
+++ runtime/machdeps/sparc_regs.h
@@ -1,4 +1,9 @@
 /*
+** Copyright (C) 1995 University of Melbourne.
+** This file may only be copied under the terms of the GNU Library General
+** Public License - see the file COPYING.LIB in the Mercury distribution.
+*/
+/*
 ** Machine registers mr0 - mr36 for the SPARC architecture.
 **
 ** The first NUM_REAL_REGS of these are real machine registers.
