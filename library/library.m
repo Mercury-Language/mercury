5ac7a479c185e75118eed8b77ca47c6a3bd4a304
Andrew Bromage <bromage@cs.mu.oz.au>
1995-06-13 05:00:41 +0000

    Added relation to library.m

--- library/library.m
+++ library/library.m
@@ -19,7 +19,7 @@
 
 :- import_module array, bag, bimap, bintree, bintree_set, char, dir.
 :- import_module float, graph, group, int, io.
-:- import_module list, map, pqueue, queue, random, require.
+:- import_module list, map, pqueue, queue, random, relation, require.
 :- import_module set, stack, std_util, string, term, term_io.
 :- import_module tree234, varset, store.
 
