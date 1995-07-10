c183ab075435793860b875870201837756840871
Fergus Henderson <fjh@cs.mu.oz.au>
1995-07-10 15:21:25 +0000

    Remove obsolete comment.
    samples/committed_choice.m:
    	Remove obsolete comment.

--- samples/committed_choice.m
+++ samples/committed_choice.m
@@ -8,12 +8,6 @@
 % Which one it prints out is unspecified.
 % The implementation can pick either.
 
-% Note that the behaviour of the current Mercury implementation is to print
-% out _both_, which is incorrect.  We needed to have some way of getting all
-% solutions to a predicate, before we had implemented any all-solutions
-% predicates.  We'll fix this bug as soon as we have fully implemented
-% solutions/2.
-
 :- module committed_choice.
 :- interface.
 :- import_module io.
