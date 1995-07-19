d724a54b65b772ee459d2f9f8adbcde5f3947159
Dylan Shuttleworth <dylan@cs.mu.oz.au>
1995-07-19 06:41:46 +0000

    Added a simple exponent function to float.  Efficiency is ordinary.

--- library/float.m
+++ library/float.m
@@ -18,6 +18,7 @@
 
 :- module float.
 :- interface.
+:- import_module int, require.
 
 :- pred builtin_float_plus(float, float, float).
 :- mode builtin_float_plus(in, in, out) is det.
@@ -43,9 +44,33 @@
 :- pred builtin_float_le(float, float).
 :- mode builtin_float_le(in, in) is semidet.
 
+:- pred float__pow( float, int, float).
+:- mode float__pow( in, in, out) is det.
+%	float__pow( Base, Exponent, Answer)
+%		A limited way to calculate powers.  The exponent must be an 
+%		integer greater or equal to 0.
+
 %---------------------------------------------------------------------------%
 
 :- implementation.
+
+float__pow( X, Exp, Ans) :-
+		Exp < 0
+	->
+		error("float__pow taken with exponent < 0\n")
+	;
+		Exp = 0
+	->
+		Ans = 1.0
+	;
+		Exp  = 1 
+	->
+		Ans =  X
+	;
+		New_e is Exp - 1,
+		float__pow(X, New_e, A2),
+		builtin_float_times(X, A2, Ans)
+	.
 
 /* They're all builtins, which the compiler expands inline. */
 
