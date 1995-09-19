8f8b0a3e69e1d7143afd241b8c7ab8cd53f7c020
Fergus Henderson <fjh@cs.mu.oz.au>
1995-09-19 13:20:23 +0000

    Add test of string__to_int (0.4 had a bug in string__to_int).
    tests/general/string_test.m:
    	Add test of string__to_int (0.4 had a bug in string__to_int).

--- tests/general/string_test.m
+++ tests/general/string_test.m
@@ -7,7 +7,7 @@
 :- mode main(di, uo) is det.
 
 :- implementation.
-:- import_module string, list.
+:- import_module string, list, require.
 
 main -->
 	test("foo", "bar").
@@ -25,12 +25,24 @@ test(X, Y) -->
 	{ string__uncapitalize_first(CapX, UnCapX) },
 	write_message("uncapitalize_first CapX: ", UnCapX),
 	{ string__int_to_string(1234, Num) },
-	write_message("string_to_int 1234: ", Num),
+	write_message("int_to_string 1234: ", Num),
 	{ string__int_to_base_string(1234, 8, Num8) },
 	write_message("octal 1234: ", Num8),
 	{ string__int_to_base_string(1234, 16, Num16) },
 	write_message("hexadecimal 1234: ", Num16),
 	{ string__duplicate_char('f', 5, FiveFs) },
+	( { string__to_int("5678", Num5678) } ->
+		io__write_string("string_to_int 5678: "),
+		io__write_int(Num5678),
+		io__write_string("\n")
+	;
+		{ error("string__to_int(""5678"", _) failed") }
+	),
+	{ string__to_int("asdf", _) ->
+		error("string__to_int(""asdf"", _) succeeded")
+	;
+		true
+	},
 	write_message("Five f's: ", FiveFs),
 	{ string__pad_right(FiveFs, '.', 10, FsAndDots) },
 	write_message("Five f's and five dots: ", FsAndDots),
