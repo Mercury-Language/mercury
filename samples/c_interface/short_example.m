78896d77cab0843c581278e82beac2f5c9af4ace
Fergus Henderson <fjh@cs.mu.oz.au>
1996-06-11 04:37:09 +0000

    Add another example of the use of the C interface.
    Estimated hours taken: 0.25
    
    samples/c_interface/short_example.m:
    	Add another example of the use of the C interface.

---
+++ samples/c_interface/short_example.m
@@ -0,0 +1,20 @@
+% This is a simple example of using the C interface to call the C function
+% (or macro) puts().
+
+:- module short_example.
+:- interface.
+:- import_module io.
+
+:- pred main(io__state::di, io__state::uo) is det.
+
+:- implementation.
+
+:- pred puts(string::in, io__state::di, io__state::uo) is det.
+
+:- pragma c_header_code("#include <stdio.h>").
+
+:- pragma c_code(non_recursive, puts(S::in, Old_IO::di, New_IO::uo),
+	"puts(S); New_IO = Old_IO; "
+).
+
+main --> puts("Hello, world\n").
