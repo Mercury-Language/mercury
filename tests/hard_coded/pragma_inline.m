85c4b1b49c72e75e1c1b552d934797cbec0192d1
Simon Taylor <stayl@cs.mu.oz.au>
1996-07-24 00:16:03 +0000

    Fixed a bug in the C interface.
    Estimated hours taken: 2.5
    
    Fixed a bug in the C interface.
    The names of C variables were indexed by the corresponding Mercury variable.
    The problem arose when the same Mercury variable was passed in to a
    C code in two separate arguments, as in string__append(X, X, Y).
    If the procedure was inlined, excess assignments removed the assignment
    introduced in make_hlds to put the clause into super-homogeneous form,
    so a declaration for one C variable did not appear, and a declaration for
    the other was repeated.
    
    tests/hard_coded/pragma_inline.m
    	Add a regression test.

--- tests/hard_coded/pragma_inline.m
+++ tests/hard_coded/pragma_inline.m
@@ -4,14 +4,17 @@
 
 :- interface.
 
-:- import_module io.
+:- import_module io, string.
 
 :- pred main(io__state::di, io__state::uo) is det.
 
 :- implementation.
 
 main -->
-	c_write_string("Hello, world\n").
+	{ L = "l"},
+	{ append_strings(L, L, LL) },
+	{ string__append_list(["He", LL, "o, world\n"], String) },
+	c_write_string(String).
 
 :- pred c_write_string(string::in, io__state::di, io__state::uo) is det.
 
@@ -21,4 +24,17 @@ main -->
 ").
 
 :- pragma(inline, c_write_string/3).
+
+:- pred append_strings(string::in, string::in, string::out) is det.
+:- pragma inline(append_strings/3).
+:- pragma c_code(non_recursive, append_strings(S1::in, S2::in, S3::out), "{
+        size_t len_1, len_2;
+	Word tmp;
+	len_1 = strlen(S1);
+	len_2 = strlen(S2);
+	incr_hp_atomic(tmp, (len_1 + len_2 + sizeof(Word)) / sizeof(Word));
+	S3 = (char *) tmp;
+	strcpy(S3, S1);
+	strcpy(S3 + len_1, S2);
+}").
 
