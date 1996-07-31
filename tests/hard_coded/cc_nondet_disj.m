3ca517587947721fc30e79a19c6e342d1958dd0f
Fergus Henderson <fjh@cs.mu.oz.au>
1996-07-31 20:50:50 +0000

    Add new test `cc_nondet_disj' (we currently fail that one).
    Estimated hours taken: 0.5
    
    tests/hard_coded:
    	Add new test `cc_nondet_disj' (we currently fail that one).
    	Fix string_loop.exp, which was wrong, and then add string_loop
    	to the list of tests that we pass.

---
+++ tests/hard_coded/cc_nondet_disj.m
@@ -0,0 +1,13 @@
+:- module foo.
+:- interface.
+:- import_module io.
+
+:- pred main(io__state::di, io__state::uo) is cc_multi.
+
+:- implementation.
+
+main --> io__read_line(Res),
+	( { Res = ok(['y']) }, io__write_string("Yes\n")
+	; { Res = ok(['n']) }, io__write_string("No\n")
+	; io__write_string("Huh?\n")
+	).
