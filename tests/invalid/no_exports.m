75a3ce9c4d07d118323070a3600b24ad67f9647f
Thomas Conway <conway@cs.mu.oz.au>
1996-12-04 05:55:37 +0000

    mention, but don't add no_exports.m
    Estimated hours taken: 0.1
    
    tests/invalid/Mmake:
    	mention, but don't add no_exports.m
    
    tests/invalid/no_exports.m:
    	a module with a missing :- interface. We used to warn
    	when a module didn't export anything this seems to have
    	vanished.

---
+++ tests/invalid/no_exports.m
@@ -0,0 +1,12 @@
+:- module no_exports.
+
+% :- interface.
+
+:- import_module io.
+
+:- pred main(io__state::di, io__state::uo) is det.
+
+:- implementation.
+
+main -->
+	io__write_string("hello\n").
