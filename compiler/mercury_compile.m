0d341c161affd2e2a711e9a96a61efa494f90dc2
Simon Taylor <stayl@cs.mu.oz.au>
1996-12-18 03:28:26 +0000

    Fixed a bug in yesterday's change.
    Estimated hours taken: 0.1
    
    compiler/mercury_compile.m
    	Fixed a bug in yesterday's change.

--- compiler/mercury_compile.m
+++ compiler/mercury_compile.m
@@ -207,6 +207,8 @@ mercury_compile(Module, FactDeps) -->
 		Verbose, Stats, HLDS21),
 	    globals__io_lookup_bool_option(typecheck_only, TypeCheckOnly),
 	    globals__io_lookup_bool_option(errorcheck_only, ErrorCheckOnly),
+	    globals__io_lookup_bool_option(make_optimization_interface,
+	    	MakeOptInt),
 	    ( { TypeCheckOnly = yes } ->
 		[]
 	    ; { ErrorCheckOnly = yes } ->
@@ -214,6 +216,8 @@ mercury_compile(Module, FactDeps) -->
 		% the appropriate warnings
 		globals__io_set_option(optimize_unused_args, bool(no)),
 		mercury_compile__maybe_unused_args(HLDS21, Verbose, Stats, _)
+	    ; { MakeOptInt = yes } ->
+	    	[]
 	    ;
 		mercury_compile__maybe_output_prof_call_graph(HLDS21,
 			Verbose, Stats, HLDS25),
