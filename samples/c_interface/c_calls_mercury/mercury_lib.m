fedf4f472dcc44bf56a525821b9780f17d7b4b3d
Fergus Henderson <fjh@cs.mu.oz.au>
1996-12-20 17:50:20 +0000

    Update now that exporting semidet procedures works.
    Estimated hours taken: 0.5
    
    samples/c_interface/c_calls_mercury:
    	Update now that exporting semidet procedures works.
    	Add some examples of exporting functions.

--- samples/c_interface/c_calls_mercury/mercury_lib.m
+++ samples/c_interface/c_calls_mercury/mercury_lib.m
@@ -7,27 +7,43 @@
 :- mode foo(in) is semidet.
 :- mode foo(out) is multi.
 
+% a Mercury function with multiple modes
+:- func bar(int) = int.
+:- mode bar(in) = out is det.
+:- mode bar(out) = in is det.
+:- mode bar(in) = in is semidet.
+
+% a semidet (i.e. partial) Mercury function
+:- func baz(int) = int.
+:- mode baz(in) = out is semidet.
+
 %-----------------------------------------------------------------------------%
 :- implementation.
-:- import_module std_util.	% for solutions/2
+:- import_module std_util, int.
 
 % well, this is just a silly example...
 foo(42).
 foo(53).
 foo(197).
 
+bar(X) = X + 1.
+
+baz(1) = 9.
+baz(2) = 16.
+baz(3) = 27.
+
 %-----------------------------------------------------------------------------%
 
 % The following code provides provides access to the Mercury predicate foo
 % from C code.
 
-	% Currently only det modes can be exported directly.
-	% To export a semidet mode, we need to create a det predicate
-	% which return an extra success/failure indicator.
-	% We give it the name foo_test() in C.
-:- pred foo_test(int::in, int::out) is det.
-:- pragma export(foo_test(in, out), "foo_test").
-foo_test(X, R) :- (foo(X) -> R = 1 ; R = 0).
+:- pragma export(foo(in), "foo_test").
+
+:- pragma export(bar(in) = out, "bar").
+:- pragma export(bar(in) = in,  "bar_test").
+:- pragma export(bar(out) = in, "bar_inverse").
+
+:- pragma export(baz(in) = out, "baz").
 
 	% The nondet mode of `foo' cannot be exported directly with
 	% the current Mercury/C interface.  To get all solutions,
