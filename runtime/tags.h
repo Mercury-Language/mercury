d7aea3ddf75e4578418a3d78c7ca967a5e21b302
Fergus Henderson <fjh@cs.mu.oz.au>
1997-01-16 14:07:57 +0000

    Fix some gcc warnings about casting away const.
    Estimated hours taken: 1.5
    
    Fix some gcc warnings about casting away const.
    
    runtime/tags.h:
    	Add a const_field() macro, which is like the field() macro,
    	except that it casts the argument to `const Word *' rather than
    	to `Word *'.
    	Also a variety of minor cleanups.
    
    compiler/llds_out.m:
    	When outputing field() expressions, use the const_field()
    	macro rather than the field() macro if the expression is used
    	in an rval context.  (This avoids warnings about casting
    	away const when taking the field of a constant term.)

--- runtime/tags.h
+++ runtime/tags.h
@@ -34,14 +34,6 @@
 #define mkbody(i)	(i)
 #define unmkbody(w)	(w)
 #define	body(w, t)	((w) & (~(Word)0 >> TAGBITS))
-/*
-** the result of mkword() is cast to (const Word *), not to (Word)
-** because mkword() may be used in initializers for static constants
-** and casts from pointers to integral types are not valid
-** constant-expressions in ANSI C.
-*/
-#define	mkword(t, p)	((const Word *)((const char *)(p) + (Word)(t)))
-#define	field(t, p, i)	((Word *) body((p), (t)))[i]
 
 #else
 
@@ -51,6 +43,9 @@
 #define mkbody(i)	((i) << TAGBITS)
 #define unmkbody(w)	((Word) (w) >> TAGBITS)
 #define	body(w, t)	((w) - (t))
+
+#endif
+
 /*
 ** the result of mkword() is cast to (const Word *), not to (Word)
 ** because mkword() may be used in initializers for static constants
@@ -59,43 +54,44 @@
 */
 /* XXX can't implement the above comment yet due to bootstrapping problems! */
 #define	mkword(t, p)	((Word)((const char *)(p) + (t)))
-#define	field(t, p, i)	((Word *) body((p), (t)))[i]
 
-#endif
+#define	field(t, p, i)		((Word *) body((p), (t)))[i]
+#define	const_field(t, p, i)	((const Word *) body((p), (t)))[i]
 
 /*
-** the following macros are used by the handwritten .mod code in io.mod etc.
-** to access lists
+** the following list_* macros are used by handwritten C code
+** that needs to access Mercury lists.
 */
-
-#if TAGBITS > 0
-
-#define list_is_empty(list)	(tag(list) == mktag(0))
-#define list_head(list)		field(TAG_CONS, (list), 0)
-#define list_tail(list)		field(TAG_CONS, (list), 1)
-#define list_empty()		mkword(mktag(0), mkbody(0))
-#define list_cons(head,tail)	mkword(mktag(1), create2((head),(tail)))
-
-#else
-
-#define list_is_empty(list)	(field(mktag(0), (list), 0) == 0)
-#define list_head(list)		field(mktag(0), (list), 1)
-#define list_tail(list)		field(mktag(0), (list), 2)
-#define list_empty()		mkword(mktag(0), create1(0))
-#define list_cons(head,tail)	mkword(mktag(0), create3(1, (head), (tail)))
-
-#endif
-
-/* the rest of this file is for archaic code only */
 
 #define	bTAG_NIL	0
 #define	bTAG_CONS	1
-#define	bTAG_VAR	3
+#define	bTAG_VAR	3 	/* for Prolog-style variables */
+				/* ... but currently not used */
 
 #define	TAG_NIL		mktag(bTAG_NIL)
 #define	TAG_CONS	mktag(bTAG_CONS)
 #define	TAG_VAR		mktag(bTAG_VAR)
 
+#if TAGBITS > 0
+
+#define list_is_empty(list)	(tag(list) == TAG_NIL)
+#define list_head(list)		field(TAG_CONS, (list), 0)
+#define list_tail(list)		field(TAG_CONS, (list), 1)
+#define list_empty()		mkword(TAG_NIL, mkbody(0))
+#define list_cons(head,tail)	mkword(TAG_CONS, create2((head),(tail)))
+
+#else
+
+#define list_is_empty(list)	(field(mktag(0), (list), 0) == bTAG_NIL)
+#define list_head(list)		field(mktag(0), (list), 1)
+#define list_tail(list)		field(mktag(0), (list), 2)
+#define list_empty()		mkword(mktag(0), create1(bTAG_NIL))
+#define list_cons(head,tail)	\
+		mkword(mktag(0), create3(bTAG_CONS, (head), (tail)))
+
+#endif
+
+/* for Prolog-style variables... currently not used */
 #define	deref(pt)	do {						\
 				while (tag(pt) == TAG_VAR)		\
 					(pt) = * (Word *)		\
