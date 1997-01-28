2ef308fbd170dd8330bc958837208c1f2eb474dd
Bert Thompson <aet@cs.mu.oz.au>
1997-01-28 02:01:31 +0000

    Added strdup(). Changed util_fatal_error() to fatal().

--- bytecode/util.c
+++ bytecode/util.c
@@ -1,5 +1,5 @@
 /*
- *	$Id: util.c,v 1.1 1997-01-24 07:12:07 aet Exp $
+ *	$Id: util.c,v 1.2 1997-01-28 02:01:31 aet Exp $
  *
  *	Copyright: The University of Melbourne, 1996
  */
@@ -7,6 +7,7 @@
 /* Imports */
 #include	<stdlib.h>
 #include	<stdio.h>
+#include	<string.h>
 
 #include	<util.h>
 
@@ -23,10 +24,26 @@ util_error(char* message)
 }
 
 void
-util_fatal_error(char* message)
+fatal(char* message)
 {
 	util_error(message);
 	/* XXX: Should dump core here so we can do a post-mortem */
 	exit(EXIT_FAILURE);
 }
 
+char*
+strdup(char* str)
+{
+	int	size;
+	char	*str2, *c_p;
+
+	size = strlen(str) + 1;
+	str2 = malloc(size);
+	for (c_p=str2; *str != '\0'; str++, c_p++)
+	{
+		*c_p = *str;
+	}
+	*c_p = '\0';
+
+	return str2;
+}
