d988e8c82e0458273f4f696e911012d3aa8ab129
Fergus Henderson <fjh@cs.mu.oz.au>
1997-01-31 19:04:47 +0000

    Add an example of Mercury calling C++ code.
    Estimated hours taken: 0.25
    
    samples/c_interface/mercury_calls_cplusplus/*:
    	Add an example of Mercury calling C++ code.

---
+++ samples/c_interface/mercury_calls_cplusplus/cpp_main.h
@@ -0,0 +1,14 @@
+/*
+** Function prototype for the cpp_main() function defined in cpp_main.c.
+** Note: this is a C++ function, but it needs to be declared as
+** extern "C" so that it can be called from Mercury.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void cpp_main(void);
+
+#ifdef __cplusplus
+}
+#endif
