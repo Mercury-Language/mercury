619f65a72e52e4d90b60171f0c7ea8f148b7a6f1
Bert Thompson <aet@cs.mu.oz.au>
1997-02-01 09:15:02 +0000

    Commented disassemble().

--- bytecode/disasm.h
+++ bytecode/disasm.h
@@ -1,5 +1,5 @@
 /*
- *	$Id: disasm.h,v 1.1 1997-01-28 02:04:52 aet Exp $
+ *	$Id: disasm.h,v 1.2 1997-02-01 09:15:02 aet Exp $
  *
  *	Copyright: The University of Melbourne, 1996
  */
@@ -7,6 +7,14 @@
 #if	! defined(DISASM_H)
 #define	DISASM_H
 
+
+/*
+ *	Disassemble a Mercury bytecode file.
+ *
+ *	fp points to the beginning of a Mercury bytecode file. 
+ *	The disassembler output is directed to stdout.
+ *	XXX: Maybe we should be able to specify the output file pointer?
+ */
 void
 disassemble(FILE* fp);
 
