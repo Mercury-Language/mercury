ed9d6206b52e0af25169f22cebcadac80f6d1098
Fergus Henderson <fjh@cs.mu.oz.au>
1997-02-09 09:19:26 +0000

    Fix a spelling error in Tom's changes to restore_hp():
    Estimated hours taken: 0.5
    
    runtime/heap.h:
    	Fix a spelling error in Tom's changes to restore_hp():
    	it's min_heap_reclamation_point,
    	not  min_heap_reclaimation_point.

--- runtime/heap.h
+++ runtime/heap.h
@@ -98,13 +98,13 @@
 /*
 ** When restoring hp, we must make sure that we don't truncate the heap
 ** further than it is safe to. We can only truncate it as far as
-** min_heap_reclaimation_point. See the comments in context.h next to
-** the set_min_heap_reclaimation_point() macro.
+** min_heap_reclamation_point. See the comments in context.h next to
+** the set_min_heap_reclamation_point() macro.
 */
 #define	restore_hp(src)	(						\
 				LVALUE_CAST(Word,hp) =			\
-				  ( min_heap_reclaimation_point < (src) ? \
-				  (src) : min_heap_reclaimation_point ), \
+				  ( min_heap_reclamation_point < (src) ? \
+				  (src) : min_heap_reclamation_point ), \
 				(void)0					\
 			)
 
