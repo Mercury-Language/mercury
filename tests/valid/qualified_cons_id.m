053ccbffd477080ace285b773b0be8a836c061b5
Simon Taylor <stayl@cs.mu.oz.au>
1997-02-17 01:41:48 +0000

    Fixed comments.
    Estimated hours taken: 0.0001
    
    tests/valid/qualified_cons_id.m
    	Fixed comments.

--- tests/valid/qualified_cons_id.m
+++ tests/valid/qualified_cons_id.m
@@ -11,9 +11,6 @@
 :- inst yes
 	--->	std_util:yes(ground).
 
-	% list__apply(Cs, Bs) takes a list of closures with one
-	% output argument Cs, and calls the closures, returning
-	% the resulting bindings in Bs.
 :- pred test(maybe(T), T).
 :- mode test(in(bound(std_util:yes(ground))), out) is det.
 :- mode test(in(yes), out) is det.
