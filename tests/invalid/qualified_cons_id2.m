fff65b85fa41738e9021f68ce49dc208d574ead5
Simon Taylor <stayl@cs.mu.oz.au>
1997-02-17 01:40:28 +0000

    Fixed comments.
    Estimated hours taken: 0.0001
    
    tests/qualified_cons_id2.m
    	Fixed comments.

--- tests/invalid/qualified_cons_id2.m
+++ tests/invalid/qualified_cons_id2.m
@@ -1,4 +1,4 @@
-% Test handling of valid module qualified cons_ids.
+% Test handling of invalid module qualified cons_ids.
 :- module qualified_cons_id2.
 :- interface.
 
@@ -11,9 +11,6 @@
 :- inst yes
 	--->	qualified_cons_id2:yes(ground).
 
-	% list__apply(Cs, Bs) takes a list of closures with one
-	% output argument Cs, and calls the closures, returning
-	% the resulting bindings in Bs.
 :- pred test(maybe(T), T).
 :- mode test(in(bound(qualified_cons_id2:yes(ground))), out) is det.
 :- mode test(in(yes), out) is det.
