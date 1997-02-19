e69e67c473d032c17c45732d7010827465ccc66c
Fergus Henderson <fjh@cs.mu.oz.au>
1997-02-19 02:50:36 +0000

    Some improvements to const-correctness in the string handling code.
    Estimated hours taken: 1
    
    Some improvements to const-correctness in the string handling code.
    
    runtime/mercury_string.h:
    	Change make_aligned_string() so that the first (target)
    	argument is of type `ConstString' (const char *)
    	rather than of type `String' (char *), since it should
    	be const, because it can point to a string literal.
    	Also improve the documentation.
    
    library/std_util.m:
    	Change the code so that it matches the new definition
    	of make_aligned_string.
    	Also make the `functor' field of expand_info a `ConstString'
    	rather than a `String', since it can point to a string literal.
    
    library/io.m:
    	Change a call to make_aligned_string so that it matches the new
    	definition.

--- runtime/mercury_string.h
+++ runtime/mercury_string.h
@@ -14,35 +14,77 @@
 #include "heap.h"	/* for incr_hp_atomic */
 
 
-typedef char Char;	/* we may eventually move to using wchar_t */
+/*
+** Mercury characters are given type `Char', which is a typedef for `char'.
+** Mercury strings are stored as pointers to '\0'-terminated arrays of Char.
+**
+** We may eventually move to using wchar_t for Mercury characters and strings,
+** so it is important to use these typedefs.
+*/
+typedef char Char;
 
 typedef Char *String;
 typedef const Char *ConstString;
 
-#define string_const(string, len) ((Word)string)
-#define string_equal(s1,s2) (strcmp((char*)(s1),(char*)(s2))==0)
-
-
-/* 
-** If the string is aligned, set ptr equal to it, otherwise allocate one
-** on the heap.
+/*
+** string_const("...", len):
+**	Given a C string literal and its length, returns a Mercury string.
 */
-#define make_aligned_string(ptr, string) \
-	( tag((Word)string) != 0 ? 					\
-		(incr_hp_atomic(LVALUE_CAST(Word, (ptr)), 		\
-		    (strlen(string) + sizeof(Word)) / sizeof(Word)),	\
-		strcpy((ptr), (string)))				\
-	: 								\
-		((ptr) = (string))					\
-	)
+#define string_const(string, len) ((Word)string)
 
 /*
+** bool string_equal(ConstString s1, ConstString s2):
+**	Return true iff the two Mercury strings s1 and s2 are equal.
+*/
+#define string_equal(s1,s2) (strcmp((char*)(s1),(char*)(s2))==0)
+
+/* 
+** void make_aligned_string(ConstString & ptr, const char * string):
+**	Given a C string `string', set `ptr' to be a Mercury string
+**	with the same contents.  (`ptr' must be an lvalue.)
+**	If the resulting Mercury string is to be used by Mercury code,
+**	then the string pointed to by `string' should have been either
+**	statically allocated or allocated on the Mercury heap.
+**	
+** BEWARE: this may modify `hp', so it must only be called from
+** places where `hp' is valid.  If calling it from inside a C function,
+** rather than inside Mercury code, you may need to call
+** save/restore_transient_regs().
+**
+** Algorithm: if the string is aligned, just set ptr equal to it.
+** Otherwise, allocate space on the heap and copy the C string to
+** the Mercury string.
+*/
+#define make_aligned_string(ptr, string) 				\
+	do { 								\
+	    Word make_aligned_string_tmp;				\
+	    char * make_aligned_string_ptr;				\
+									\
+	    if (tag((Word) (string)) != 0) { 				\
+	    	incr_hp_atomic(make_aligned_string_tmp, 		\
+	    	    (strlen(string) + sizeof(Word)) / sizeof(Word));	\
+	    	make_aligned_string_ptr =				\
+		    (char *) make_aligned_string_tmp;			\
+	    	strcpy(make_aligned_string_ptr, (string));		\
+	    	(ptr) = make_aligned_string_ptr;			\
+	    } else { 							\
+	    	(ptr) = (string);					\
+	    }								\
+	} while(0)
+
+/*
+** do_hash_string(int & hash, Word string):
+**	Given a Mercury string `string', set `hash' to the hash value
+**	for that string.  (`hash' must be an lvalue.)
+**
+** This is an implementation detail used to implement hash_string().
+** It should not be used directly.  Use hash_string() instead.
+**
 ** Note that hash_string is also defined in library/string.m.
 ** The definition here and the definition in string.m
 ** must be kept equivalent.
 */
-
-#define do_hash_string(hash,s)				\
+#define do_hash_string(hash, s)				\
 	{						\
 	   int len = 0;					\
 	   hash = 0;					\
@@ -54,19 +96,23 @@ typedef const Char *ConstString;
 	   hash ^= len;					\
 	}
 
-extern	int	hash_string(Word);
+/*
+** hash_string(s):
+**	Given a Mercury string `s', return a hash value for that string.
+*/
+int	hash_string(Word);
 
 #ifdef __GNUC__
 #define hash_string(s)					\
 	({ int hash;					\
-	   do_hash_string(hash,s);			\
+	   do_hash_string(hash, s);			\
 	   hash;					\
 	})
 #endif
 
 /* 
-** if we're not using gcc, the actual definition of hash_string is in aux.c
-** it uses the macro HASH_STRING_FUNC_BODY below
+** If we're not using gcc, the actual definition of hash_string is in misc.c;
+** it uses the macro HASH_STRING_FUNC_BODY below.
 */
 
 #define HASH_STRING_FUNC_BODY				\
