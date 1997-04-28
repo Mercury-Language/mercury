3a8d2f4ca941c2da9348e09f4fec4a786cfa959d
Simon Taylor <stayl@cs.mu.oz.au>
1997-04-28 00:05:26 +0000

    Test cases for the --warn-simple-code warnings.
    Estimated hours taken: 0.2
    
    tests/warnings/simple_code.{m,exp}
    	Test cases for the --warn-simple-code warnings.

---
+++ tests/warnings/simple_code.m
@@ -0,0 +1,57 @@
+:- module simple_code.
+:- interface.
+:- import_module io.
+:- pred p(int::in, int::out) is erroneous.
+:- implementation.
+:- import_module require.
+p --> 
+	(
+		[]
+	;
+		{ error("foo") }
+	),
+	( { true } ->
+		{ Z = 2 }	
+	;
+		{ Z = 3 }
+	),
+	( { X = 3, X = 2, Z = 2 } ->
+		[]
+	;
+		[]
+	),
+	( { \+ true } ->
+		[]
+	;
+		[]
+	),
+	( { \+ det_pred } ->
+		[]
+	;	
+		[]
+	),
+	( { \+ fail_pred } ->
+		[]
+	;
+		[]
+	),
+	{ \+ fail },
+	{ obsolete },
+	( { error("blah") } ->
+		[]
+	;
+		[]
+	).
+
+:- pred det_pred is det.
+
+det_pred.
+
+:- pred fail_pred is failure.
+
+fail_pred :- fail.
+
+:- pred obsolete is det.
+:- pragma obsolete(obsolete/0).
+
+obsolete.
