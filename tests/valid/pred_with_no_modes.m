b44a9fa04263a296c22c4aeaeca01de13f9b2546
Fergus Henderson <fjh@cs.mu.oz.au>
1997-06-02 06:45:35 +0000

    Add a regression test for the bug in polymorphism.m that I just fixed.
    Estimated hours taken: 0.25
    
    tests/valid/Mmake:
    tests/valid/pred_with_no_modes.m:
    	Add a regression test for the bug in polymorphism.m that I just fixed.

---
+++ tests/valid/pred_with_no_modes.m
@@ -0,0 +1,12 @@
+% This is a regression test; version 0.6 or thereabouts got
+% an internal error in arg_modes.m, due to a bug in polymorphism.m
+% for predicates with no modes.
+
+:- module pred_with_no_modes.
+
+:- implementation.
+
+:- pred foo(((func int) = T2), T2).
+foo(X, Y) :-
+        Y = apply(X,1).
+
