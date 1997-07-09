24d19648c2e214399496ec0907fd8a6f07af851f
Fergus Henderson <fjh@cs.mu.oz.au>
1997-07-09 05:45:35 +0000

    Fix a bug.  For the example code in the bug report, simplify.m simplified the code by merging two switches, which happened to lead to one branch containing `error(...), fail'.  The problem was that determinism analysis inferred a determinism of `failure' for that branch, rather than `erroneous'.  This caused the code generator to abort, because the switch was `det' (= model_det) but the case was `failure' (= model_semi). The basic problem is that simplify.m assumed that simplifying goals in this way should never make their determinism worse (less precise), though it might make it better.  But due to the imprecision of determinism analysis, this assumption was not correct.  The fix is to change determinism analysis so that the assumption is correct.
    compiler/det_analysis.m:
    	Add a new predicate exported predicate det_conjunction_detism/3.
    	This is similar to calling det_conjunction_canfail/3
    	and det_conjunction_maxsoln/3 and combining the results,
    	except that if the first goal cannot ever succeed, the determinism
    	of the second goal is ignored.  Also, make det_conjunction_canfail/3
    	and det_conjunction_maxsoln/3 local rather than exported, to ensure
    	that callers from other modules use the new predicate.
    
    compiler/simplify.m:
    	Delete conjuncts which are known to be unreachable based on the
    	determinism information, as well as based on mode information.
    	(The previous code already deleted conjuncts which were
    	unreachable due to mode information, but the determinism
    	information may be more precise than the mode information.)
    
    compiler/simplify.m:
    compiler/follow_code.m:
    	Use det_conjunction_detism/3 rather than det_conjunction_canfail/3
    	and det_conjunction_maxsoln/3.
    
    tests/hard_coded/Mmake:
    tests/hard_coded/det_in_semidet_cntxt.m:
    tests/hard_coded/det_in_semidet_cntxt.exp:
    	Regression test.

---
+++ tests/hard_coded/det_in_semidet_cntxt.m
@@ -0,0 +1,97 @@
+% Regression test.
+% The Mercury compiler dated Wed Jul 9th 1997
+% got an internal error for this test case.
+
+:- module det_in_semidet_cntxt.
+
+:- interface.
+
+:- import_module io.
+
+:- pred main(io__state, io__state).
+:- mode main(di, uo) is det.
+
+%------------------------------------------------------------------------------%
+%------------------------------------------------------------------------------%
+
+
+:- implementation.
+
+:- import_module list, require, std_util, string.
+
+:- type	name	== 	string.
+:- type sympton	== 	string.
+:- type vaccine	== 	string.
+
+:- type disease	--->	d(name, list(sympton), list(vaccine)).
+
+main -->
+	read_diseases(_Diseases).
+
+
+%------------------------------------------------------------------------------%
+
+	%
+	% read_diseases(D)
+	%
+	% D is the list of diseases.
+	%
+:- pred read_diseases(list(disease), io__state, io__state).
+:- mode read_diseases(out, di, uo) is det.
+
+read_diseases(D) -->
+	read_disease(MaybeDisease),
+	(
+		{ MaybeDisease = yes(Disease) },
+		read_diseases(Diseases),
+		{ D = [Disease | Diseases] }
+	;
+		{ MaybeDisease = no },
+		{ D = [] }
+	).
+
+
+:- pred read_disease(maybe(disease), io__state, io__state).
+:- mode read_disease(out, di, uo) is det.
+
+read_disease(MaybeDisease) -->
+	read_name(MaybeName),
+	(
+		{ MaybeName = yes(Name) },
+		read_symptons(Symptons),
+		read_vaccines(Vaccines),
+		{ MaybeDisease = yes(d(Name, Symptons, Vaccines)) }
+	;
+		{ MaybeName = no },
+		{ MaybeDisease = no }
+	).
+		
+
+:- pred read_name(maybe(string)::out, io__state::di, io__state::uo) is det.
+
+read_name(MaybeName) --> 
+	io__read_word(Result),
+	(
+		{ Result = ok(Name0) },
+		{ string__from_char_list(Name0, Name) },
+		{ MaybeName = yes(Name) }
+	;
+		{ Result = eof },
+		{ MaybeName = no }
+	;
+		{ Result = error(Err) },
+		{ io__error_message(Err, ErrStr) },
+		{ error(ErrStr) }
+	).
+
+
+:- pred read_symptons(list(string)::out, io__state::di, io__state::uo) is det.
+read_symptons([]) --> [].
+
+:- pred read_vaccines(list(string)::out, io__state::di, io__state::uo) is det.
+read_vaccines([]) --> [].
+
+%------------------------------------------------------------------------------%
+%------------------------------------------------------------------------------%
+
+
