0265b894d54a20fe3e89c2422b73a575f25a0bba
Fergus Henderson <fjh@cs.mu.oz.au>
1997-09-11 10:21:41 +0000

    New file, used by the Mmakefile to build `libtrailed_update.a'.
    Estimated hours taken: 0.25
    
    extras/trailed_update/trailed_update.
    	New file, used by the Mmakefile to build `libtrailed_update.a'.

---
+++ extras/trailed_update/trailed_update.m
@@ -0,0 +1,6 @@
+% Currently the trailed update library only contains support
+% for arrays, but we hope to eventually add support for
+% trailed destructive update of other types.
+
+:- module trailed_update.
+:- import_module tr_array.
