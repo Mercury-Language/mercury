d97c71cb51686226ed27504932aac38e2eaeadd9
Fergus Henderson <fjh@cs.mu.oz.au>
1997-11-23 15:44:47 +0000

    Temporarily resurrected, for bootstrapping reasons.
    Estimated hours taken: 0.5
    
    runtime/imp.h:
    	Temporarily resurrected, for bootstrapping reasons.
    	(Can't remove this until the change to make llds_out.m
    	reference mercury_imp.h instead of imp.h has been installed.)

---
+++ runtime/imp.h
@@ -0,0 +1,7 @@
+/*
+** Copyright (C) 1993-1997 The University of Melbourne.
+** This file may only be copied under the terms of the GNU Library General
+** Public License - see the file COPYING.LIB in the Mercury distribution.
+*/
+
+#include "mercury_imp.h"
