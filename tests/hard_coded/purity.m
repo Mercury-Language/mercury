35fafd251a47485e8b16cd44493c1a98530b7a2a
Peter Schachte <pets@cs.mu.oz.au>
1997-12-09 04:09:33 +0000

    Make Mercury cope with impure code
    The purpose of this diff is to allow Mercury programs to contain
    impure Mercury code without the compiler changing its behavior
    inappropriately, while still allowing the compiler to aggressively
    optimize pure code.  To do this, we require impure predicates to be so
    declared, and calls to impure predicates to be flagged as such.  We
    also allow predicates implemented in terms of impure predicates to be
    promised to be pure; lacking such a promise, any predicate that calls
    an impure predicate is assumed to be impure.
    
    At the moment, we don't allow impure functions (only predicates),
    though some of the work necessary to support them has been done.
    
    Note that to make the operators work properly, the precedence of the
    `pred' and `func' operators has been changed from 1199 to 800.
    
    Estimated hours taken: 150
    
    compiler/purity.m:
    	New compiler pass for purity checking.
    compiler/hlds_goal.m:
    	Add `impure' and `semipure' to the goal_feature enum.
    compiler/hlds_out.m:
    compiler/typecheck.m:
    compiler/special_pred.m:
    	Fixed code that prints predicate name to write something more
    	helpful for special (compiler-generated) predicates.  Added
    	code to print new markers.  Added purity argument to
    	mercury_output_pred_type.  New public predicate
    	special_pred_description/2 provides an english description for
    	each compiler-generated predicate.
    compiler/hlds_pred.m:
    	Add `impure' and `semipure' to marker enum.  Added new
    	public predicates to get predicate purity and whether or not
    	it's promised to be pure.
    compiler/prog_data.m:
    compiler/mercury_to_mercury.m:
    compiler/prog_io.m:
    compiler/prog_io_goal.m:
    compiler/prog_io_pragma.m:
    compiler/prog_io_dcg.m:
    compiler/prog_util.m:
    compiler/equiv_type.m:
    compiler/intermod.m:
    compiler/mercury_to_c.m:
    compiler/module_qual.m:
    	Add purity argument to pred and func items.  Add new `impure'
    	and `semipure' operators.  Add promise_pure pragma.  Add
    	purity/2 wrapper to goal_expr type.
    compiler/make_hlds.m:
    compiler/mercury_to_goedel.m:
    	Added purity argument to module_add_{pred,func},
    	clauses_info_add_pragma_c_code, and to pred and func items.
    	Handle promise_pure pragma.  Handle purity/2 wrapper used to
    	handle user-written impurity annotations on goals.
    compiler/mercury_compile.m:
    	Add purity checking pass between type and mode checking.
    compiler/mode_errors.m:
    	Distinguish mode errors caused by impure goals preventing
    	goals being delayed.
    compiler/modes.m:
    	Don't delay impure goals, and ensure before scheduling an
    	impure goal that no goals are delayed.  Actually, we go ahead
    	and try to schedule goals even if impurity causes a problem,
    	and then if it still doesn't mode check, then we report an
    	ordinary mode error.  Only if the clause would be mode correct
    	except for an impure goal do we report it as an impurity problem.
    compiler/simplify.m:
    	Don't optimize away non-pure duplicate calls.  We could do
    	better and still optimize duplicate semipure goals without an
    	intervening impure goal, but it's probably not worth the
    	trouble.  Also don't eliminate impure goals on a failing branch.
    compiler/notes/compiler_design.html:
    	Documented purity checking pass.
    doc/reference_manual.texi:
    	Document purity system.
    doc/transition_guide.texi:
    library/nc_builtin.nl:
    library/ops.m:
    library/sp_builtin.nl:
    	New operators and new precdence for `pred' and `func'
    	operators.
    tests/hard_coded/purity.m
    tests/hard_coded/purity.exp
    tests/hard_coded/Mmakefile:
    tests/invalid/purity.m
    tests/invalid/purity_nonsense.m
    tests/invalid/purity.err_exp
    tests/invalid/purity_nonsense.err_exp
    tests/invalid/Mmakefile:
    	Test cases for purity.

---
+++ tests/hard_coded/purity.m
@@ -0,0 +1,105 @@
+% Various checks that impurity declarations are treated properly.
+
+:- module purity.
+:- interface.
+:- import_module io.
+:- impure pred main(io__state::di, io__state::uo) is det.
+
+:- implementation.
+:- import_module int, list, string.
+
+main -->
+	impure test1,
+	impure test2,
+	impure test3,
+	impure test1_inline,
+	impure test2_inline,
+	impure test3_inline.
+
+
+:- impure pred test1(io__state::di, io__state::uo) is det.
+:- impure pred test2(io__state::di, io__state::uo) is det.
+:- impure pred test3(io__state::di, io__state::uo) is det.
+
+:- impure pred test1_inline(io__state::di, io__state::uo) is det.
+:- impure pred test2_inline(io__state::di, io__state::uo) is det.
+:- impure pred test3_inline(io__state::di, io__state::uo) is det.
+
+
+:- impure pred set_x(int::in) is det.
+:- pragma c_code(set_x(X::in), will_not_call_mercury, "x=X;" ).
+
+:- impure pred incr_x is det.
+:- pragma c_code(incr_x, will_not_call_mercury, "++x;" ).
+
+:- semipure pred get_x(int::out) is det.
+:- pragma c_code(get_x(X::out), will_not_call_mercury, "X=x;").
+
+
+:- impure pred set_x_inline(int::in) is det.
+:- pragma c_code(set_x_inline(X::in), will_not_call_mercury, "x=X;" ).
+:- pragma inline(set_x_inline/1).
+
+:- impure pred incr_x_inline is det.
+:- pragma c_code(incr_x_inline, will_not_call_mercury, "++x;" ).
+:- pragma inline(incr_x_inline/0).
+
+:- semipure pred get_x_inline(int::out) is det.
+:- pragma c_code(get_x_inline(X::out), will_not_call_mercury, "X=x;").
+:- pragma inline(get_x_inline/1).
+
+
+:- pragma c_header_code("int x = 0;").
+
+
+% tempt compiler to optimize away duplicate semipure goals.
+test1 -->
+	{ semipure get_x(X) },
+	io__format("%d\n", [i(X)]),
+	{ impure set_x(X+1) },
+	{ semipure get_x(Y) },
+	io__format("%d\n", [i(Y)]).
+
+
+% tempt compiler to optimize away duplicate impure goals, or to compile away
+% det goals with no outputs.
+test2 -->
+	{ impure incr_x },
+	{ impure incr_x },
+	{ semipure get_x(Y) },
+	io__format("%d\n", [i(Y)]).
+
+% tempt compiler to optimize away impure goal in branch that cannot succeed.
+test3 -->
+	(   { impure incr_x },
+	    { fail }
+	;   { semipure get_x(Y) },
+	    io__format("%d\n", [i(Y)])
+	).
+
+%  Now do it all again with inlining requested
+
+test1_inline -->
+	{ semipure get_x_inline(X) },
+	io__format("%d\n", [i(X)]),
+	{ impure set_x_inline(X+1) },
+	{ semipure get_x_inline(Y) },
+	io__format("%d\n", [i(Y)]).
+
+
+% tempt compiler to optimize away duplicate impure goals, or to compile away
+% det goals with no outputs.
+test2_inline -->
+	{ impure incr_x_inline },
+	{ impure incr_x_inline },
+	{ semipure get_x_inline(Y) },
+	io__format("%d\n", [i(Y)]).
+
+% tempt compiler to optimize away impure goal in branch that cannot succeed.
+test3_inline -->
+	(   { impure incr_x_inline },
+	    { fail }
+	;   { semipure get_x_inline(Y) },
+	    io__format("%d\n", [i(Y)])
+	).
+
