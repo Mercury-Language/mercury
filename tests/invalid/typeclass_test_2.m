469fddf17c95a1845505473a317442f934731bd6
Fergus Henderson <fjh@cs.mu.oz.au>
1998-05-18 08:54:49 +0000

    Use more newlines in the test case source so that the test
    Estimated hours taken: 0.1
    
    tests/invalid/typeclass_test_2.m:
    tests/invalid/typeclass_test_2.err_exp:
    	Use more newlines in the test case source so that the test
    	that the error message is on the correct line number is more
    	stringent.

--- tests/invalid/typeclass_test_2.m
+++ tests/invalid/typeclass_test_2.m
@@ -5,14 +5,19 @@
 
 :- implementation.
 
-main --> io__write_int(type_num(42)).
+main -->
+	io__write_int(
+		type_num(42)		% type error here
+					% (due to the syntax error below)
+	),
+	nl.
 
 :- typeclass numbered_type(T) where [
 	func type_num(T) = int
 ].
 
 :- instance numbered_type(int) where [
-	type_num/0 is foo_type_num
+	type_num/0 is foo_type_num	% syntax error here
 ].
 
 foo_type_num(_) = 42.
