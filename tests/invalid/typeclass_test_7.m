d07a04b41b7f1e5cc300126caeeea4cab6e82cc7
Fergus Henderson <fjh@cs.mu.oz.au>
1998-06-22 08:45:21 +0000

    Improve error reporting by checking type class constraints for
    Estimated hours taken: 1
    
    compiler/typecheck.m:
    	Improve error reporting by checking type class constraints for
    	satisfiability as we go and thus reporting unsatisfiable constraints
    	as soon as possible, rather than only at the end of the clause.
    	Previously we already did that for the case of ground constraints,
    	but they are not the only unsatsfiable constraints: constraints
    	on head type params (type variables which cannot be bound) are
    	also unsatisfiable if they can't be eliminated straight away
    	by context reduction.
    
    tests/invalid/Mmakefile:
    tests/invalid/typeclass_test_7.m:
    tests/invalid/typeclass_test_7.err_exp:
    	Regression test for the above change.

---
+++ tests/invalid/typeclass_test_7.m
@@ -0,0 +1,24 @@
+:- module typeclass_test_7.
+:- interface.
+:- import_module int.
+:- pred p(T::in, int::out) is det.
+
+:- implementation.
+
+p(X, Y) :-
+	F = 42,
+	N = type_num(X),	% error should be reported on *this* line
+	N2 is N * 2,
+	Y = N2 + F.
+
+:- typeclass numbered_type(T) where [
+	func type_num(T) = int
+].
+
+:- instance numbered_type(int) where [
+	func(type_num/1) is foo_type_num
+].
+
+:- func foo_type_num(T) = int.
+foo_type_num(_) = 42.
+
