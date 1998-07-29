614554281484d527f83b7bfeeb36daad24d39afe
David Jeffery <dgj@cs.mu.oz.au>
1998-07-29 06:28:17 +0000

    Add a test case for a function as a typeclass method.
    Estimated hours taken: 0.1
    
    
    tests/valid/typeclasses/func_method.m:
            Add a test case for a function as a typeclass method. The non-locals
            were not being set correctly, which caused a software error in this
            case.
    tests/valid/typeclasses/Mmakefile:
            Turn this test on.

---
+++ tests/valid/func_method.m
@@ -0,0 +1,22 @@
+%-----------------------------------------------------------------------------%
+:- module func_method.
+:- interface.
+
+:- type pair(A, B) ---> pair(A, B).
+
+:- typeclass c(A) where [
+	func op(pair(A, B)) = pair(A, B),
+	mode op(in) = out is det
+].
+
+:- instance c(int) where [
+	func(op/1) is op_int
+].
+
+:- implementation.
+
+:- func op_int(pair(int, T)) = pair(int, T).
+:- mode op_int(in) = out is det.
+op_int(X) = X.
+
+%-----------------------------------------------------------------------------%
