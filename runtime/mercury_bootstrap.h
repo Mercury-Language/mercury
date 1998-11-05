54d3b79e3cdfd3f63e0d2c9b2fa50304c7f34e04
Zoltan Somogyi <zs@csse.unimelb.edu.au>
1998-11-05 06:27:41 +0000

    Add file accidentally missing from my previous commit.
    Estimated hours taken: 0.1
    
    runtime/mercury_bootstrap.h:
    	Add file accidentally missing from my previous commit.

---
+++ runtime/mercury_bootstrap.h
@@ -0,0 +1,18 @@
+/*
+** Copyright (C) 1998 The University of Melbourne.
+** This file may only be copied under the terms of the GNU Library General
+** Public License - see the file COPYING.LIB in the Mercury distribution.
+*/
+
+/*
+** mercury_bootstrap.h
+**
+** Temporary definitions only needed for bootstrapping.
+*/
+
+#ifndef	MERCURY_BOOTSTRAP_H
+#define	MERCURY_BOOTSTRAP_H
+
+#define	NONDET_FIXED_SIZE	MR_NONDET_FIXED_SIZE
+
+#endif	/* MERCURY_BOOTSTRAP_H */
