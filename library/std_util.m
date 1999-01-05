3ca511d8ebc1a05b2d695b8a9d0ec29a696b2f3c
Fergus Henderson <fjh@cs.mu.oz.au>
1999-01-05 17:05:06 +0000

    Add parentheses to use of `some' in type declaration for univ_value/1,
    Estimated hours taken: 0.1
    
    library/std_util.m:
    	Add parentheses to use of `some' in type declaration for univ_value/1,
    	to avoid syntax error for SICStus Prolog.

--- library/std_util.m
+++ library/std_util.m
@@ -1,5 +1,5 @@
 %-----------------------------------------------------------------------------%
-% Copyright (C) 1994-1998 The University of Melbourne.
+% Copyright (C) 1994-1999 The University of Melbourne.
 % This file may only be copied under the terms of the GNU Library General
 % Public License - see the file COPYING.LIB in the Mercury distribution.
 %-----------------------------------------------------------------------------%
@@ -84,7 +84,7 @@
 	%
 	% Warning: support for existential types is still experimental.
 	%
-:- some [T] func univ_value(univ) = T.
+:- some([T], func univ_value(univ) = T).
 
 %-----------------------------------------------------------------------------%
 
