474a223d6d9afac150f68d1cc0e2ce4f3179e359
Fergus Henderson <fjh@cs.mu.oz.au>
1999-02-08 20:52:38 +0000

    Fix typo in comment: s/fals/fails/
    Estimated hours taken: 0.01
    
    compiler/prog_io.m:
    	Fix typo in comment: s/fals/fails/

--- compiler/prog_io.m
+++ compiler/prog_io.m
@@ -1,5 +1,5 @@
 %-----------------------------------------------------------------------------%
-% Copyright (C) 1993-1998 The University of Melbourne.
+% Copyright (C) 1993-1999 The University of Melbourne.
 % This file may only be copied under the terms of the GNU General
 % Public License - see the file COPYING in the Mercury distribution.
 %-----------------------------------------------------------------------------%
@@ -142,7 +142,7 @@
 
 	% sym_name_and_args takes a term and returns a sym_name and a list of
 	% argument terms.
-	% It fals if the input is not valid syntax for a QualifiedTerm.
+	% It fails if the input is not valid syntax for a QualifiedTerm.
 :- pred sym_name_and_args(term(T), sym_name, list(term(T))).
 :- mode sym_name_and_args(in, out, out) is semidet.
 
