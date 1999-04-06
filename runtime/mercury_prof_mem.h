e72ff458d065aeb9d346d3ef7d71c6e64f34b745
Fergus Henderson <fjh@cs.mu.oz.au>
1999-04-06 14:53:03 +0000

    Change MR_prof_malloc() to ensure that the memory that it
    Estimated hours taken: 0.25
    
    runtime/mercury_prof_mem.h:
    runtime/mercury_prof_mem.c:
    	Change MR_prof_malloc() to ensure that the memory that it
    	allocates will be Word-aligned.  This change is needed
    	as a result of my previous change to mercury_heap_profile.c
    	which made it allocate strings using MR_prof_malloc().

--- runtime/mercury_prof_mem.h
+++ runtime/mercury_prof_mem.h
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 1996-1997 The University of Melbourne.
+** Copyright (C) 1996-1997, 1999 The University of Melbourne.
 ** This file may only be copied under the terms of the GNU Library General
 ** Public License - see the file COPYING.LIB in the Mercury distribution.
 */
@@ -17,11 +17,17 @@
 #include <stddef.h>	/* for size_t */
 
 /*
-** prof_malloc() allocates memory in large chunks using newmem(),
+** MR_prof_malloc() allocates memory in large chunks using newmem(),
 ** and then doles out portions of these chunks one at a time.
 ** We use prof_malloc() to reduce the chance of calling newmem()
 ** from a profiling interrupt that interrupted another call to newmem().
 ** Doing that is bad news, because newmem() is not re-entrant.
+**
+** Note that the current implementation of MR_prof_malloc only guarantees
+** that the memory will be Word-aligned; if you want to allocate types
+** that contain data types (e.g. `double') which might require stricter
+** alignment than that, then you will need to change the implementation of
+** MR_prof_malloc().
 */
 extern void *MR_prof_malloc(size_t);
 
