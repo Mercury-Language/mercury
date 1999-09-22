ab7d0f553928c1adf1bdc63f4daac91d043e130f
Zoltan Somogyi <zs@csse.unimelb.edu.au>
1999-09-22 05:37:48 +0000

    A new test case for the new backend. Mark should add the input
    Estimated hours taken: 1
    
    tests/debugger/declarative/negation.m:
    	A new test case for the new backend. Mark should add the input
    	and expected output files when the backend is complete.

---
+++ tests/debugger/declarative/negation.m
@@ -0,0 +1,42 @@
+:- module negation.
+
+:- interface.
+
+:- import_module io.
+
+:- pred main(io__state::di, io__state::uo) is det.
+
+:- implementation.
+
+:- import_module int.
+
+main(S0, S) :-
+	p(1, X),
+	io__write_int(X, S0, S).
+
+:- pred p(int, int).
+:- mode p(in, out) is det.
+
+p(A, B) :-
+	(
+		not (
+			r(A, C),
+			not q(C)
+		)
+	->
+		B = 42
+	;
+		B = A
+	).
+
+:- pred q(int).
+:- mode q(in) is semidet.
+
+q(N) :-
+	N > 10.
+
+:- pred r(int, int).
+:- mode r(in, out) is multi.
+
+r(K, K + 10).
+r(K, K + 20).
