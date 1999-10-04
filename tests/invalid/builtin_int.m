e998ecbeb61daa0bc59d969a12fed17bf21e2c57
Simon Taylor <stayl@cs.mu.oz.au>
1999-10-04 04:04:51 +0000

    Don't report that builtin.m or private_builtin.m have not
    Estimated hours taken: 0.25
    
    compiler/module_qual.m:
    	Don't report that builtin.m or private_builtin.m have not
    	been imported in error messages for undefined symbols.
    
    tests/invalid/Mmakefile:
    tests/invalid/builtin_int.m:
    tests/invalid/builtin_int.err_exp:
    	Test case.

---
+++ tests/invalid/builtin_int.m
@@ -0,0 +1,12 @@
+% Check that compiler/module_qual.m does not add a spurious
+% "(the module `builtin' has not been imported)" to the
+% end of the undefined type error message.
+:- module builtin_int.
+
+:- interface.
+
+:- pred foo(builtin__ints::in) is semidet.
+
+:- implementation.
+
+foo(1).
