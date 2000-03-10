74fab84325f1e04c336749806ca90e58cdde114a
Zoltan Somogyi <zs@csse.unimelb.edu.au>
2000-03-10 07:39:56 +0000

    New module to managing integer counters.
    Estimated hours taken: 0.5
    
    library/counter.m:
    	New module to managing integer counters.
    
    library/library.m:
    	Include the new module in the library.

--- library/library.m
+++ library/library.m
@@ -1,5 +1,5 @@
 %---------------------------------------------------------------------------%
-% Copyright (C) 1993-1999 The University of Melbourne.
+% Copyright (C) 1993-2000 The University of Melbourne.
 % This file may only be copied under the terms of the GNU Library General
 % Public License - see the file COPYING.LIB in the Mercury distribution.
 %---------------------------------------------------------------------------%
@@ -25,7 +25,7 @@
 
 :- import_module array, assoc_list, bag, benchmarking.
 :- import_module bimap, bintree, bintree_set, bool.
-:- import_module bt_array, char, dir, eqvclass, float.
+:- import_module bt_array, char, counter, dir, eqvclass, float.
 :- import_module math, getopt, graph, group, int.
 :- import_module io, list, map, multi_map, pqueue, queue, random, relation.
 :- import_module require, set, set_bbbtree, set_ordlist, set_unordlist, stack.
