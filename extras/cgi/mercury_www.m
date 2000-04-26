56d03c6f1fc8a225578ad858d5b3e4aebbf0b59f
Tyson Dowd <trd@cs.mu.oz.au>
2000-04-26 05:24:37 +0000

    Add a library building makefile.
    Estimated hours taken: 1
    
    extras/cgi/Mmakefile:
    	Add a library building makefile.
    
    extras/cgi/html.m:
    	Import list, as it is used in the interface.
    
    extras/cgi/mercury_www.m:
    	The mercury_www library module.

---
+++ extras/cgi/mercury_www.m
@@ -0,0 +1,14 @@
+%-----------------------------------------------------------------------------%
+% Copyright (C) 1998 The University of Melbourne.
+% This file may only be copied under the terms of the GNU Library General
+% Public License - see the file COPYING.LIB in the Mercury distribution.
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+
+% mercury_www -- a Mercury World Wide Web library.
+%
+
+:- module mercury_www.
+
+:- import_module cgi, html.
+
