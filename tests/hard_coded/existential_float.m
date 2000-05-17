89e72b60ab493fb35409f61e325677f407d2531b
Fergus Henderson <fjh@cs.mu.oz.au>
2000-05-17 22:05:01 +0000

    Fix some bugs in my previous change to handle boxing of existentally
    Estimated hours taken: 3
    
    compiler/ml_code_gen.m:
    	Fix some bugs in my previous change to handle boxing of existentally
    	typed output arguments.
    
    tests/hard_coded/Mmakefile:
    tests/hard_coded/existential_float.m:
    tests/hard_coded/existential_float.exp:
    	Add some additional test cases testing model_non and model_semi
    	procedures.

--- tests/hard_coded/existential_float.m
+++ tests/hard_coded/existential_float.m
@@ -8,15 +8,20 @@
 :- interface.
 :- import_module std_util.
 
+:- type foo ---> left ; right.
+
 :- some [T] func call_univ_value(univ) = T.
 
 :- some [T] func my_exist_c = T.
 :- some [T] func my_exist_f = T.
 :- some [T] func my_exist_fn = T.
 
+:- some [T] pred my_exist_p_multi(T::out) is multi.
+:- some [T] pred my_exist_p_semi(foo::in, T::out) is semidet.
+
 :- import_module io.
 
-:- pred main(io__state::di, state::uo) is det.
+:- pred main(io__state::di, state::uo) is cc_multi.
 
 :- implementation.
 :- import_module int.
@@ -33,7 +38,29 @@ main -->
 	write(my_exist_fn), nl,
 	write(call_my_exist_c), nl,
 	write(call_my_exist_f), nl,
-	write(call_my_exist_fn), nl.
+	write(call_my_exist_fn), nl,
+	( { call_my_exist_p_semi(left, X1) } ->
+		write(X1), nl
+	;
+		print("no."), nl
+	),
+	( { call_my_exist_p_semi(right, X2) } ->
+		write(X2), nl
+	;
+		print("no."), nl
+	),
+	( { my_exist_p_semi(left, X3) } ->
+		write(X3), nl
+	;
+		print("no."), nl
+	),
+	( { my_exist_p_semi(right, X4) } ->
+		write(X4), nl
+	;
+		print("no."), nl
+	),
+	{ unsorted_solutions(my_univ_p_multi, List) },
+	write(List), nl.
 
 my_exist_c = 'c'.
 
@@ -41,11 +68,23 @@ my_exist_f = 42.0.
 
 my_exist_fn = (func(X) = 2 * X).
 
+my_exist_p_multi(1.0).
+my_exist_p_multi(2.0).
+
+my_exist_p_semi(left, 33.3).
+
 call_my_exist_c = my_exist_c.
 
 call_my_exist_f = my_exist_f.
 
 call_my_exist_fn = my_exist_fn.
+
+call_my_exist_p_multi(X) :- my_exist_p_multi(X).
+
+call_my_exist_p_semi(A, B) :- my_exist_p_semi(A, B).
+
+:- mode my_univ_p_multi(out) is multi.
+my_univ_p_multi(univ(X)) :- call_my_exist_p_multi(X).
 
 :- pred foo(univ::in, io__state::di, state::uo) is det.
 foo(X) -->
