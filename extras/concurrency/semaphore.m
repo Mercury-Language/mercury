adf8aeccc170776f99e55cc22cd5ae89b332a97a
Fergus Henderson <fjh@cs.mu.oz.au>
2000-09-18 09:06:53 +0000

    Fix some bugs that resulted in compiler warnings:
    Estimated hours taken: 0.5
    
    extras/concurrency/semaphore.m:
    	Fix some bugs that resulted in compiler warnings:
    	- ensure that ME_finalize_semaphore() is declared before it is used
    	- the first argument to incr_hp() must have type `MR_Word'
    	Also:
    	- improve (clarify) the error message you get when compiling in
    	  grade hlc.gc
    	- change the layout in one spot to match our coding guidelines

--- extras/concurrency/semaphore.m
+++ extras/concurrency/semaphore.m
@@ -55,7 +55,7 @@
 
 :- pragma c_header_code("
 #if defined(MR_HIGHLEVEL_CODE) && !defined(MR_THREAD_SAFE)
-  #error Semaphores only work in the hlc.par.* grades.
+  #error Semaphores only work in the hlc.par.* or non-hlc grades.
 #endif
 
 	#include <stdio.h>
@@ -75,11 +75,19 @@
 	} ME_Semaphore;
 ").
 
+:- pragma c_header_code("
+#ifdef CONSERVATIVE_GC
+  void ME_finalize_semaphore(GC_PTR obj, GC_PTR cd);
+#endif
+").
+
 :- pragma c_code(semaphore__new(Semaphore::out, IO0::di, IO::uo),
 		[will_not_call_mercury, thread_safe], "{
+	MR_Word sem_mem;
 	ME_Semaphore	*sem;
 
-	incr_hp(sem, round_up(sizeof(ME_Semaphore), sizeof(MR_Word)));
+	incr_hp(sem_mem, round_up(sizeof(ME_Semaphore), sizeof(MR_Word)));
+	sem = (ME_Semaphore *) sem_mem;
 	sem->count = 0;
 #ifndef MR_HIGHLEVEL_CODE
 	sem->suspended = NULL;
@@ -104,7 +112,8 @@
 
 :- pragma c_code("
 #ifdef CONSERVATIVE_GC
-  void ME_finalize_semaphore(GC_PTR obj, GC_PTR cd)
+  void
+  ME_finalize_semaphore(GC_PTR obj, GC_PTR cd)
   {
 	ME_Semaphore    *sem;
 
