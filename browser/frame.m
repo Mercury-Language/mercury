8cdb83f094da94971b444f74a4a7b64d952626d4
Simon Taylor <stayl@cs.mu.oz.au>
2000-11-13 22:45:47 +0000

    string__length now has multiple modes, so its address can't be taken.
    Estimated hours taken: 0.1
    
    browser/frame.m:
    	string__length now has multiple modes, so its address can't be taken.

--- browser/frame.m
+++ browser/frame.m
@@ -97,7 +97,7 @@ frame__hglue(LeftFrame, RightFrame, GluedFrame) :-
 :- pred frame_right_pad(frame, frame).
 :- mode frame_right_pad(in, out) is det.
 frame_right_pad(Frame, PaddedFrame) :-
-	list__map(string__length, Frame, Lengths),
+	Lengths = list__map((func(Str) = string__length(Str)), Frame),
 	list__foldl(int__max, Lengths, 0, MaxLen),
 	list__map(subtract(MaxLen), Lengths, Paddings),
 	add_right_padding(Frame, Paddings, PaddedFrame).
@@ -134,7 +134,7 @@ frame_lower_pad(Frame, PadLines, PaddedFrame) :-
 
 	% Horizontal size (width) of a frame
 frame__hsize(Frame, HSize) :-
-	list__map(string__length, Frame, Lengths),
+	Lengths = list__map(func(Str) = string__length(Str), Frame),
 	list__foldl(int__max, Lengths, 0, MaxLen),
 	HSize = MaxLen.
 
