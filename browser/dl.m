881d0526efda9cde39517a177471b72756724ea3
Fergus Henderson <fjh@cs.mu.oz.au>
2001-01-11 05:41:53 +0000

    Apply some patches that had been previously applied to
    Estimated hours taken: 0.1
    
    browser/dl.m:
    	Apply some patches that had been previously applied to
    	extras/dynamic_linking/dl.m but not to the copy here.

--- browser/dl.m
+++ browser/dl.m
@@ -1,5 +1,5 @@
 %-----------------------------------------------------------------------------%
-% Copyright (C) 1998-2000 The University of Melbourne.
+% Copyright (C) 1998-2001 The University of Melbourne.
 % This file may only be copied under the terms of the GNU Library General
 % Public License - see the file COPYING.LIB in the Mercury distribution.
 %-----------------------------------------------------------------------------%
@@ -40,11 +40,16 @@
 % it; see dl_test.m for an example of this.
 %
 % The type `T' below must be a higher-order type whose arity and
-% argument types match that of the specified procedure.
+% argument types exactly match that of the specified procedure.
 % The implementation may check this at runtime, but is not required
 % to do so.  (The current implementation checks that the type is a
 % higher-order type with the appropriate arity, but it does not
 % check the argument types.)
+%
+% WARNING: for the `--high-level-code' back-end (the `hl*' grades),
+% calling dl__mercury_sym for procedures with argument types `float'
+% or `char' is not supported.
+
 :- pred dl__mercury_sym(handle::in, mercury_proc::in, dl__result(T)::out,
 	io__state::di, io__state::uo) is det.
 
@@ -53,7 +58,7 @@
 	io__state::di, io__state::uo) is det.
 
 :- implementation.
-:- import_module std_util, require, string, list.
+:- import_module std_util, require, string, list, int.
 
 :- pragma c_header_code("
 	#include <stdio.h>
@@ -114,16 +119,35 @@ open(FileName, Mode, Scope, Result) -->
 			int
 		).
 
-:- type closure
-	--->	closure(
+	% closures for the LLDS backend
+:- type ll_closure
+	--->	ll_closure(
 			closure_layout,
-			c_pointer,
-			int
+			c_pointer,	% the address of the procedure to call
+			int		% the number of curried arguments;
+					% always zero, for closures created
+					% by dl.m
 		).
+
+	% closures for the --high-level-code (MLDS) backend
+:- type hl_closure
+	--->	hl_closure(
+			closure_layout,
+			c_pointer,	% the wrapper function;
+					% this gets passed the closure
+					% as an argument
+			int,		% the number of curried arguments;
+					% always one, for closures created
+					% by dl.m
+			c_pointer	% the real function, which gets
+					% called by the wrapper function
+		).
+
 
 mercury_sym(Handle, MercuryProc0, Result) -->
 	{ check_proc_spec_matches_result_type(Result, _,
-		MercuryProc0, MercuryProc) },
+		MercuryProc0, MercuryProc1) },
+	{ check_type_is_supported(Result, _, MercuryProc1, MercuryProc) },
 	{ MangledName = proc_name_mangle(MercuryProc) },
 	sym(Handle, MangledName, Result0),
 	{
@@ -134,14 +158,98 @@ mercury_sym(Handle, MercuryProc0, Result) -->
 		%
 		% convert the procedure address to a closure
 		%
-		NumCurriedInputArgs = 0,
-		ClosureLayout = closure_layout(0, "unknown", "unknown",
-			"unknown", -1, -1, -1),
-		Closure = closure(ClosureLayout, Address, NumCurriedInputArgs),
-		private_builtin__unsafe_type_cast(Closure, Value),
+		( high_level_code ->
+			NumCurriedInputArgs = 1,
+			ClosureLayout = closure_layout(0, "unknown", "unknown",
+				"unknown", -1, -1, -1),
+			HL_Closure = hl_closure(ClosureLayout,
+				dl__generic_closure_wrapper,
+				NumCurriedInputArgs, Address),
+			private_builtin__unsafe_type_cast(HL_Closure, Value)
+		;
+			NumCurriedInputArgs = 0,
+			ClosureLayout = closure_layout(0, "unknown", "unknown",
+				"unknown", -1, -1, -1),
+			LL_Closure = ll_closure(ClosureLayout, Address,
+				NumCurriedInputArgs),
+			private_builtin__unsafe_type_cast(LL_Closure, Value)
+		),
 		Result = ok(Value)
 	}.
+
+:- pragma c_header_code("
+extern MR_Box MR_CALL ML_DL_generic_closure_wrapper(void *closure,
+	MR_Box arg1, MR_Box arg2, MR_Box arg3, MR_Box arg4, MR_Box arg5,
+	MR_Box arg6, MR_Box arg7, MR_Box arg8, MR_Box arg9, MR_Box arg10,
+	MR_Box arg11, MR_Box arg12, MR_Box arg13, MR_Box arg14, MR_Box arg15,
+	MR_Box arg16, MR_Box arg17, MR_Box arg18, MR_Box arg19, MR_Box arg20);
+").
+
+:- pragma c_code("
+
+/*
+** For the --high-level-code grades, the closure will be passed
+** as an argument to the wrapper procedure.  The wrapper procedure
+** then extracts any needed curried arguments from the closure,
+** and calls the real procedure.  Normally the wrapper procedure
+** knows which real procedure it will call, but for dl.m we use
+** a generic wrapper procedure, and treat the real procedure
+** as a curried argument of the generic wrapper.  That is always
+** the only curried argument, so all the wrapper needs to do
+** is to extract the procedure address from the closure, and
+** then call it, passing the same arguments that it was passed,
+** except for the closure itself.
+**
+** XXX Using a single generic wrapper procedure is a nasty hack. 
+** We play fast and loose with the C type system here.  In reality
+** this will get called with different return type, different
+** argument types, and with fewer than 20 arguments.  Likewise, the
+** procedure that it calls may actually have different arity, return type
+** and argument types than we pass.  So we really ought to have lots of
+** different wrapper procedures, for each different return type, number
+** of arguments, and even for each different set of argument types.
+** Doing it right might require run-time code generation!
+** But with traditional C calling conventions, using a single wrapper
+** like this will work anyway, at least for arguments whose type is the
+** same size as MR_Box.  It fails for arguments of type `char' or `float'.
+**
+** XXX This will also fail for calling conventions where the callee pops the
+** arguments.  To handle that right, we'd need different wrappers for
+** each different number of arguments.  (Doing that would also be slightly
+** more efficient, so it may worth doing...)
+**
+** There are also a couple of libraries called `ffcall' and `libffi'
+** which we might be able use to do this in a more portable manner.
+*/
+MR_Box MR_CALL
+ML_DL_generic_closure_wrapper(void *closure,
+	MR_Box arg1, MR_Box arg2, MR_Box arg3, MR_Box arg4, MR_Box arg5,
+	MR_Box arg6, MR_Box arg7, MR_Box arg8, MR_Box arg9, MR_Box arg10,
+	MR_Box arg11, MR_Box arg12, MR_Box arg13, MR_Box arg14, MR_Box arg15,
+	MR_Box arg16, MR_Box arg17, MR_Box arg18, MR_Box arg19, MR_Box arg20)
+{
+	typedef MR_Box MR_CALL FuncType(
+		MR_Box a1, MR_Box a2, MR_Box a3, MR_Box a4, MR_Box a5,
+		MR_Box a6, MR_Box a7, MR_Box a8, MR_Box a9, MR_Box a10,
+		MR_Box a11, MR_Box a12, MR_Box a13, MR_Box a14, MR_Box a15,
+		MR_Box a16, MR_Box a17, MR_Box a18, MR_Box a19, MR_Box a20);
+	FuncType *proc = (FuncType *)
+		MR_field(MR_mktag(0), closure, (MR_Integer) 3);
+	return (*proc)(arg1, arg2, arg3, arg4, arg5,
+		arg6, arg7, arg8, arg9, arg10,
+		arg11, arg12, arg13, arg14, arg15,
+		arg16, arg17, arg18, arg19, arg20);
+}
+
+").
 	 
+:- func dl__generic_closure_wrapper = c_pointer.
+:- pragma c_code(dl__generic_closure_wrapper = (WrapperFuncAddr::out),
+	[thread_safe, will_not_call_mercury],
+"
+	WrapperFuncAddr = (MR_Word) &ML_DL_generic_closure_wrapper;
+").
+
 %
 % Check that the result type matches the information
 % in the procedure specification.
@@ -152,6 +260,11 @@ check_proc_spec_matches_result_type(_Result, Value, Proc0, Proc) :-
 	Proc0 = mercury_proc(IsPredOrFunc, _Module, _Name, ProcArity, _Mode),
 	type_ctor_name_and_arity(type_ctor(type_of(Value)),
 		TypeModule, TypeName, TypeArity),
+	( TypeName = "func" ->
+		TypeProcArity = TypeArity - 1
+	;
+		TypeProcArity = TypeArity
+	),
 	(
 		( TypeModule \= "builtin"
 		; TypeName \= "pred", TypeName \= "func"
@@ -176,16 +289,67 @@ check_proc_spec_matches_result_type(_Result, Value, Proc0, Proc) :-
 			Msg),
 		error(Msg)
 	;
-		ProcArity \= TypeArity
+		ProcArity \= TypeProcArity
 	->
 		string__int_to_string(ProcArity, ProcArityString),
-		string__int_to_string(TypeArity, TypeArityString),
+		string__int_to_string(TypeProcArity, TypeArityString),
 		string__append_list([
 			"dl__mercury_sym: arity mismatch: ",
 			"argument has ", ProcArityString, " argument(s), ",
 			"result type has ", TypeArityString, " arguments(s)"],
 			Msg),
 		error(Msg)
+	;
+		Proc = Proc0
+	).
+
+%
+% Check that the given higher-order type is supported.
+%
+% For the MLDS back-end, we normally need wrapper functions
+% for closures; the wrapper functions convert from type MR_Box
+% to the appropriate argument type, and then call the function
+% with the unboxed argument types.  Generating those on-the-fly
+% here would be tricky!  Instead, we only try to handle the cases
+% where we can use a single generic wrapper, i.e. arguments with
+% types other than `char' or `float'.  All other argument types
+% are word-sized, and will hopefully be passed in the same way
+% by the C compiler.
+%
+% This procedure checks, for the MLDS back-end, that you're
+% not using it on a procedure with argument types `char' or
+% `float', and that the procedure doesn't have more arguments
+% than the generic wrapper can handle.
+%
+% XXX this doesn't catch the case of no_tag types that
+% end up being equivalent to `float' or `char'.
+%
+:- pred check_type_is_supported(dl__result(T)::unused, T::unused,
+		mercury_proc::in, mercury_proc::out) is det.
+check_type_is_supported(_Result, Value, Proc0, Proc) :-
+	(
+		high_level_code,
+		list__member(ArgType, type_args(type_of(Value))),
+		% The following line might be more efficient,
+		% but is not yet supported by the MLDS back-end
+		% ArgType = type_of(_ `with_type` float))
+		ArgTypeCtor = type_ctor(ArgType),
+		( type_ctor_name(ArgTypeCtor) = "float"
+		; type_ctor_name(ArgTypeCtor) = "char"
+		),
+		type_ctor_module_name(ArgTypeCtor) = "builtin"
+	->
+		error("sorry, not implemented: dl__mercury_sym for procedure with argument type `float' or `char'")
+	;
+		high_level_code,
+		% The generic wrapper only works for procedures with up to
+		% 20 arguments.
+		% For nondet procedures, two of the arguments get used up
+		% for the continuation function and the environment pointer,
+		% so we can only support 18 other arguments.
+		type_ctor_arity(type_ctor(type_of(Value))) > 18
+	->
+		error("sorry, not implemented: dl__mercury_sym for procedure with more than 18 arguments")
 	;
 		Proc = Proc0
 	).
@@ -244,3 +408,16 @@ close(handle(Handle), Result) -->
 	dlclose((void *)Handle)
 #endif
 ").
+
+%-----------------------------------------------------------------------------%
+
+:- pred high_level_code is semidet.
+:- pragma c_code(high_level_code, [will_not_call_mercury, thread_safe], "
+#ifdef MR_HIGHLEVEL_CODE
+	SUCCESS_INDICATOR = TRUE;
+#else
+	SUCCESS_INDICATOR = FALSE;
+#endif
+").
+
+%-----------------------------------------------------------------------------%
