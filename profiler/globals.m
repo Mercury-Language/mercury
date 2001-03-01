29850ce29cc61f265cb0e8075992eda3093f7dc9
Fergus Henderson <fjh@cs.mu.oz.au>
2001-03-01 16:55:19 +0000

    Fix a bug that showed up with the new compiler:
    Estimated hours taken: 0.1
    
    profiler/globals.m:
    	Fix a bug that showed up with the new compiler:
    	import `io' in the interface section.

--- profiler/globals.m
+++ profiler/globals.m
@@ -1,5 +1,5 @@
 %-----------------------------------------------------------------------------%
-% Copyright (C) 1995, 1997-1998 The University of Melbourne.
+% Copyright (C) 1995, 1997-1998, 2001 The University of Melbourne.
 % This file may only be copied under the terms of the GNU General
 % Public License - see the file COPYING in the Mercury distribution.
 %-----------------------------------------------------------------------------%
@@ -16,7 +16,7 @@
 %-----------------------------------------------------------------------------%
 
 :- interface.
-:- import_module bool, list, options, getopt.
+:- import_module bool, list, options, getopt, io.
 
 :- type globals.
 
@@ -95,7 +95,7 @@
 %-----------------------------------------------------------------------------%
 
 :- implementation.
-:- import_module map, std_util, io, require.
+:- import_module map, std_util, require.
 
 %-----------------------------------------------------------------------------%
 
