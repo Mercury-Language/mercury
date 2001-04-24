a86be6fdad40701ff6a54443a5439e6adecd7207
Zoltan Somogyi <zs@csse.unimelb.edu.au>
2001-04-24 06:10:17 +0000

    Add wrap_blocks.m, which I forgot to cvs add in my --use-local-vars change.
    Estimated hours taken: 0.1
    Branches: main
    
    Add wrap_blocks.m, which I forgot to cvs add in my --use-local-vars change.

---
+++ compiler/wrap_blocks.m
@@ -0,0 +1,79 @@
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2001 The University of Melbourne.
+% This file may only be copied under the terms of the GNU General
+% Public License - see the file COPYING in the Mercury distribution.
+%-----------------------------------------------------------------------------%
+%
+% File: wrap_blocks.m
+%
+% Author: zs.
+%
+% The optimizations in value_number.m and use_local_vars.m insert into
+% instruction sequences references to temporary variables whose values
+% need be preserved only within an extended basic block. The wrap_blocks pass
+% looks for references to temporaries and introduces block instructions
+% whenever it sees them. These block instructions go from the first reference
+% to a temporary to the end of its extended basic block.
+
+%-----------------------------------------------------------------------------%
+
+:- module wrap_blocks.
+
+:- interface.
+
+:- import_module llds.
+:- import_module list.
+
+:- pred wrap_blocks(list(instruction)::in, list(instruction)::out)
+	is det.
+
+:- implementation.
+
+:- import_module opt_util.
+:- import_module bool, int, std_util, require.
+
+wrap_blocks(Instrs0, Instrs) :-
+	wrap_instrs(Instrs0, 0, 0, [], Instrs).
+
+	% R is the number of the highest numbered tempr variable seen so far;
+	% R = 0 means we haven't seen any temp variables. Similarly, F is the
+	% highest numbered tempf variable seen so far. RevSofar is a
+	% reversed list of instructions starting with the first instruction
+	% in this block that accesses a temp variable. Invariant: RevSofar
+	% is always empty if R = 0 and F = 0.
+
+:- pred wrap_instrs(list(instruction)::in, int::in, int::in,
+	list(instruction)::in, list(instruction)::out) is det.
+
+wrap_instrs([], R, F, RevSofar, []) :-
+	( RevSofar = [_|_] ->
+		error("procedure ends with fallthrough")
+	; ( R > 0 ; F > 0 ) ->
+		error("procedure ends without closing block")
+	;
+		true
+	).
+wrap_instrs([Instr0 | Instrs0], R0, F0, RevSofar, Instrs) :-
+	Instr0 = Uinstr0 - _Comment0,
+	opt_util__count_temps_instr(Uinstr0, R0, R1, F0, F1),
+	( ( R1 > 0 ; F1 > 0) ->
+		( opt_util__can_instr_fall_through(Uinstr0, no) ->
+			list__reverse([Instr0 | RevSofar], BlockInstrs),
+			wrap_instrs(Instrs0, 0, 0, [], Instrs1),
+			Instrs = [block(R1, F1, BlockInstrs) - "" | Instrs1]
+		; Uinstr0 = label(_) ->
+			list__reverse(RevSofar, BlockInstrs),
+			wrap_instrs(Instrs0, 0, 0, [], Instrs1),
+			Instrs = [block(R1, F1, BlockInstrs) - "", Instr0
+				| Instrs1]
+		;
+			wrap_instrs(Instrs0, R1, F1,
+				[Instr0 | RevSofar], Instrs)
+		)
+	;
+		wrap_instrs(Instrs0, 0, 0, [], Instrs1),
+		Instrs = [Instr0 | Instrs1]
+	).
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
