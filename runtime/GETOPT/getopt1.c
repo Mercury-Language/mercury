9364b31dcdcb75a35d48a51d320797256f25eb89
Peter Ross <petdr@cs.mu.oz.au>
2001-06-05 13:25:55 +0000

    Define __GNU_LIBRARY__ by including stdio.h before getopt.h.
    Estimated hours taken: 0.2
    Branches: main
    
    runtime/GETOPT/getopt1.c:
        Define __GNU_LIBRARY__ by including stdio.h before getopt.h.
    
    util/Mmakefile:
        Remove harding coding of __GNU_LIBRARY__ define as we should only
        define this when we are using the GNU version of libc.

--- runtime/GETOPT/getopt1.c
+++ runtime/GETOPT/getopt1.c
@@ -21,6 +21,8 @@
 #include <config.h>
 #endif
 
+#include <stdio.h>
+
 #include "getopt.h"
 
 #if !defined (__STDC__) || !__STDC__
@@ -30,8 +32,6 @@
 #define const
 #endif
 #endif
-
-#include <stdio.h>
 
 /* Comment out all this code if we are using the GNU C Library, and are not
    actually compiling the library itself.  This code is part of the GNU C
