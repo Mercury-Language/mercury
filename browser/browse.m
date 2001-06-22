60405b3a2458b617536e035aab6e917b17e1664d
Zoltan Somogyi <zs@csse.unimelb.edu.au>
2001-06-22 08:28:55 +0000

    Make the prettyprinters in this file use limited_deconstruct to avoid
    Estimated hours taken: 0.2
    Branches: main
    
    browser/browse.m:
    	Make the prettyprinters in this file use limited_deconstruct to avoid
    	performance problems with large arrays.

--- browser/browse.m
+++ browser/browse.m
@@ -492,18 +492,22 @@ term_to_string(Univ, MaxSize, MaxDepth, Str) :-
 :- pred term_to_string_2(univ, int, int, int, int, int, string).
 :- mode term_to_string_2(in, in, in, out, in, in, out) is det.
 term_to_string_2(Univ, MaxSize, CurSize, NewSize, MaxDepth, CurDepth, Str) :-
-	( ( (CurSize >= MaxSize) ; (CurDepth >= MaxDepth) ) ->
-		% Str = "...",
-		term_compress(Univ, Str),
-		NewSize = CurSize
-	;
-		deconstruct(univ_value(Univ), Functor, _Arity, Args),
+	(
+		CurSize < MaxSize,
+		CurDepth < MaxDepth,
+		limited_deconstruct(univ_value(Univ), MaxSize, Functor,
+			_Arity, Args)
+	->
 		CurSize1 is CurSize + 1,
 		CurDepth1 is CurDepth + 1,
 		term_to_string_list(Args, MaxSize, CurSize1, NewSize,
 			MaxDepth, CurDepth1, ArgStrs),
 		brack_args(ArgStrs, BrackArgsStr),
 		string__append_list([Functor, BrackArgsStr], Str)
+	;
+		% Str = "...",
+		term_compress(Univ, Str),
+		NewSize = CurSize
 	).
 
 :- pred term_to_string_list(list(univ), int, int, int, int, int, list(string)).
@@ -549,7 +553,7 @@ comma_args(Args, Str) :-
 :- pred term_compress(univ, string).
 :- mode term_compress(in, out) is det.
 term_compress(Univ, Str) :-
-	deconstruct(univ_value(Univ), Functor, Arity, _Args),
+	functor(univ_value(Univ), Functor, Arity),
 	( Arity = 0 ->
 		Str = Functor
 	;
@@ -557,7 +561,6 @@ term_compress(Univ, Str) :-
 		append_list([Functor, "/", ArityS], Str)
 	).
 	
-
 %---------------------------------------------------------------------------%
 %
 % Print using the pretty printer from the standard library.
@@ -593,12 +596,12 @@ term_to_string_verbose(Univ, MaxSize, MaxDepth, X, Y, Str) :-
 :- mode term_to_string_verbose_2(in, in, in, out, in, in, out) is det.
 term_to_string_verbose_2(Univ, MaxSize, CurSize, NewSize,
 		MaxDepth, CurDepth, Frame) :-
-	( ((CurSize >= MaxSize) ; (CurDepth >= MaxDepth)) ->
-		term_compress(Univ, Line),
-		Frame = [Line],
-		NewSize = CurSize
-	;
-		deconstruct(univ_value(Univ), Functor, _Arity, Args),
+	(
+		CurSize < MaxSize,
+		CurDepth < MaxDepth,
+		limited_deconstruct(univ_value(Univ), MaxSize, Functor,
+			_Arity, Args)
+	->
 		CurSize1 is CurSize + 1,
 		CurDepth1 is CurDepth + 1,
 		ArgNum = 1,
@@ -606,6 +609,10 @@ term_to_string_verbose_2(Univ, MaxSize, CurSize, NewSize,
 			MaxSize, CurSize1, NewSize,
 			MaxDepth, CurDepth1, ArgsFrame),
 		frame__vglue([Functor], ArgsFrame, Frame)
+	;
+		term_compress(Univ, Line),
+		Frame = [Line],
+		NewSize = CurSize
 	).
 
 :- pred term_to_string_verbose_list(list(univ), int, int, int, int,
