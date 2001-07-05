0474481d4763bd974ec10e236e80ad3a658c7183
Erwan Jahier <Erwan.Jahier@imag.fr>
2001-07-05 08:05:37 +0000

    Add a post-process predicate in collect. This predicate is optional
    Estimated hours taken: 7
    branches: main.
    
    Add a post-process predicate in collect. This predicate is optional
    and automatically added if the collect user do not define it.
    
    browser/collect_lib.m:
    	Add support for dynamically link the post_process/2 predicates.
    
    extras/morphine/source/collect.in:
    extras/morphine/source/collect.op
    	Add support for automatically add a post_process predicate definition
    	as well as a accumulator_type definition.
    
    
    trace/mercury_trace_external.h:
    trace/mercury_trace_external.c:
    	Change the name of the accumulator variable type from
    	MR_collecting_variable to MR_accumulator variable. Also Add a new
    	variable type MR_collected_variable that contains the value that
    	is sent to the collect caller process (instead of
    	MR_collecting_variable).
    
    	Apply (post_process_ptr) (which points to
    	collect__post_process/2) to the accumulator variable before
    	sending the collect result.
    
    extras/morphine/non-regression-tests/solutions:
    	Replace collected_type by accumulator_type.
    
    extras/morphine/non-regression-tests/queens.exp:
    extras/morphine/non-regression-tests/queens.in:
    extras/morphine/non-regression-tests/test_ln:
    	Add a new test case entry to test line numbers and the use of
    	post_process/2 with collect.

--- browser/collect_lib.m
+++ browser/collect_lib.m
@@ -15,13 +15,11 @@
 %
 %	To use it, users just need to define 4 things in a file, using the
 %	Mercury syntax:
-%		1) a `collected_type' which is the type of the collecting 
-%		   variable that will contain the result of the monitoring
-%		   activity.
+%		1) `accumulator_type' which is the type of the accumulator.
 %		2) The predicate initialize/1 which initializes the 
 %		   collecting variable. initialize/1 should respect the
 %		   following declarations:
-%			:- pred initialize(collected_type).
+%			:- pred initialize(accumulator_type).
 %			:- mode initialize(out) is det.
 %		3) The predicate filter/4 which updates the collecting  
 %		   variable at each execution event. filter/4 also outputs  
@@ -31,17 +29,27 @@
 %		   variable is always set to `continue', the collecting will 
 %		   process until the last event is reached. filter/4 should 
 %		   respect the following declarations:
-%			:- pred filter(event, collected_type, collected_type,
+%			:- pred filter(event, accumulator_type, accumulator_type,
 %				stop_or_continue).
 %			:- mode filter(in, di, uo, out) is det.
-%		4) And eventually the mode definition of the second and the 
+%               4) Optionally, a `post_process/2' function that lets
+%   	           one post-process the final value of the
+%   	           accumulator. `post_process/2' should respect the
+%   	           following declarations:
+%  	                :- pred post_process(accumulator_type, collected_type).
+%	                :- mode post_process(in, out) is det.
+% 	           If `collected_type' is different from `accumulator_type', 
+% 	           the `accumulator_type' should also be defined; otherwise
+% 	           by default, `accumulator_type' is automatically defined as
+% 	           being the same type as `collected_type'.
+%		5) And optionally the mode definition of the second and the 
 %		   third arguments of filter/4: `acc_in' and `acc_out'. Those
 %		   mode have `di' and `uo' respectively as default values.
 %
 % 	Then, this file is used to generate the Mercury module `collect.m',
 % 	which is compiled and dynamically linked with the current execution.
 % 	When a `collect' request is made from the external debugger, a variable
-% 	of type collected_type is first initialized (with initialize/1) and
+% 	of type accumulator_type is first initialized (with initialize/1) and
 % 	then updated (with filter/4) for all the events of the remaining
 % 	execution. When the end of the execution is reached, the last value of
 % 	the collecting variable is send to the debugger.
@@ -58,7 +66,7 @@
 
 dummy_pred_to_avoid_warning_about_nothing_exported.
 
-:- pragma export(link_collect(in, out, out, out, out, out, out, di, uo), 
+:- pragma export(link_collect(in, out, out, out, out, out, out, out, di, uo), 
 	"ML_CL_link_collect").
 
 :- pragma export(unlink_collect(in, di, uo), "ML_CL_unlink_collect").
@@ -70,10 +78,10 @@ dummy_pred_to_avoid_warning_about_nothing_exported.
 % at each event.
 
 % dynamically link the collect module;
-:- pred link_collect(string, c_pointer, c_pointer, c_pointer, c_pointer, 
+:- pred link_collect(string, c_pointer, c_pointer, c_pointer, c_pointer, c_pointer, 
 	dl__result(handle), char, io__state, io__state).
-:- mode link_collect(in, out, out, out, out, out, out, di, uo) is det.
-link_collect(ObjectFile, Filter, Initialize, SendResult, GetCollectType, 
+:- mode link_collect(in, out, out, out, out, out, out, out, di, uo) is det.
+link_collect(ObjectFile, Filter, Initialize, PostProcess, SendResult, GetCollectType, 
 	MaybeHandle, Result) -->
 	%
 	% Link in the object code for the module `collect' from ObjectFile.
@@ -84,6 +92,7 @@ link_collect(ObjectFile, Filter, Initialize, SendResult, GetCollectType,
 		print("dlopen failed: "), print(Msg), nl,
 		{ set_to_null_pointer(Initialize) },
 		{ set_to_null_pointer(Filter) },
+		{ set_to_null_pointer(PostProcess) },
 		{ set_to_null_pointer(SendResult) },
 		{ set_to_null_pointer(GetCollectType) },
 		{ Result = 'n' }
@@ -95,22 +104,26 @@ link_collect(ObjectFile, Filter, Initialize, SendResult, GetCollectType,
  		%
 		dl__sym(Handle, "ML_COLLECT_initialize", MaybeInitialize),
 		dl__sym(Handle, "ML_COLLECT_filter", MaybeFilter),
+		dl__sym(Handle, "ML_COLLECT_post_process", MaybePostProcess),
 		dl__sym(Handle, "ML_COLLECT_send_collect_result", MaybeSendResult),
-		dl__sym(Handle, "ML_COLLECT_collecting_variable_type", MaybeType),
+		dl__sym(Handle, "ML_COLLECT_collected_variable_type", MaybeType),
 		(
 			{ MaybeInitialize = ok(Initialize0) },
 			{ MaybeFilter = ok(Filter0) },
+			{ MaybePostProcess = ok(PostProcess0) },
 			{ MaybeSendResult = ok(SendResult0) },
 			{ MaybeType = ok(Type0) }
 		->
 			{ Result = 'y' },
 			{ Initialize = Initialize0 },
 			{ Filter = Filter0 },
+			{ PostProcess = PostProcess0 },
 			{ GetCollectType = Type0 },
 			{ SendResult = SendResult0 }
 		;
 			{ set_to_null_pointer(Initialize) },
 			{ set_to_null_pointer(Filter) },
+			{ set_to_null_pointer(PostProcess) },
 			{ set_to_null_pointer(SendResult) },
 			{ set_to_null_pointer(GetCollectType) },
 			{ Result = 'n' }
--- trace/mercury_trace_external.c
+++ trace/mercury_trace_external.c
@@ -125,7 +125,19 @@ static	MR_external_debugger_mode_type
 ** a collect request.
 */
 
-static  MR_Word	MR_collecting_variable;
+static  MR_Word	MR_accumulator_variable;
+
+/*
+** Global variable that is sent to collect caller.
+*/
+
+static  MR_Word	MR_collected_variable;
+
+/*
+** Function pointer used to post-process the result of the collect activity
+*/
+
+static void	(*post_process_ptr)(MR_Word, MR_Word *);
 
 /*
 ** Function pointer used to sent the collecting variable to the external 
@@ -755,7 +767,7 @@ MR_trace_event_external(MR_Trace_Cmd_Info *cmd, MR_Event_Info *event_info)
 			case MR_REQUEST_LINK_COLLECT:
 			  {
 			        MR_Char	result;
-				MR_Word	MR_collecting_variable_type;
+				MR_Word	MR_accumulator_variable_type;
 
 				if (MR_debug_socket) {
 					fprintf(stderr, "\nMercury runtime: "
@@ -768,6 +780,7 @@ MR_trace_event_external(MR_Trace_Cmd_Info *cmd, MR_Event_Info *event_info)
 			       		    MR_object_file_name,
 					    (MR_Word *) &cmd->MR_filter_ptr,
 					    (MR_Word *) &initialize_ptr,
+					    (MR_Word *) &post_process_ptr,
 					    (MR_Word *) &send_collect_result_ptr,
 					    (MR_Word *) &get_collect_var_type_ptr,
 					    &collect_lib_maybe_handle,
@@ -779,12 +792,12 @@ MR_trace_event_external(MR_Trace_Cmd_Info *cmd, MR_Event_Info *event_info)
 						"link_collect_succeeded");
 					MR_TRACE_CALL_MERCURY(
 					    (*get_collect_var_type_ptr)(
-						&MR_collecting_variable_type));
-					MR_collecting_variable = 
+						&MR_accumulator_variable_type));
+					MR_accumulator_variable = 
 					    MR_make_permanent(
-						MR_collecting_variable,
+						MR_accumulator_variable,
 						(MR_TypeInfo) 
-						MR_collecting_variable_type);
+						MR_accumulator_variable_type);
 				} else {
 					MR_send_message_to_socket(
 						"link_collect_failed");
@@ -802,7 +815,7 @@ MR_trace_event_external(MR_Trace_Cmd_Info *cmd, MR_Event_Info *event_info)
 						"collect_linked");
 					external_debugger_mode = MR_collecting;
 					MR_TRACE_CALL_MERCURY(
-					  (*initialize_ptr)(&MR_collecting_variable));
+					  (*initialize_ptr)(&MR_accumulator_variable));
 
 					/*
 					** In order to perform the collect from
@@ -1504,8 +1517,8 @@ MR_COLLECT_filter(MR_FilterFuncPtr filter_ptr, MR_Unsigned seqno,
 		layout->MR_sll_entry->MR_sle_detism,
 		(MR_String) path,
 		lineno,
-		MR_collecting_variable,
-		&MR_collecting_variable,
+		MR_accumulator_variable,
+		&MR_accumulator_variable,
 		&result));
 	*stop_collecting = (result == 'y');
 }
@@ -1548,8 +1561,12 @@ static void
 MR_send_collect_result(void)
 {
 	MR_TRACE_CALL_MERCURY(
+		(*post_process_ptr)(
+			MR_accumulator_variable, 
+			&MR_collected_variable);
+
 		(*send_collect_result_ptr)(
-			MR_collecting_variable, 
+			MR_collected_variable, 
 			(MR_Word) &MR_debugger_socket_out));
 #if defined(HAVE_DLFCN_H) && defined(HAVE_DLCLOSE)
 	MR_TRACE_CALL_MERCURY(
