36050375bf6b710a07e399d86959f9dad709b27d
Fergus Henderson <fjh@cs.mu.oz.au>
2001-08-06 06:20:29 +0000

    Fix a bug in the handling of pragma c_code / foreign_proc: in
    Estimated hours taken: 2
    Branches: main
    
    compiler/polymorphism.m:
    	Fix a bug in the handling of pragma c_code / foreign_proc: in
    	the case where the procedure had both type class constraints
    	and unconstrained type variables, it was getting the arguments
    	in the wrong order.

--- compiler/polymorphism.m
+++ compiler/polymorphism.m
@@ -1634,11 +1634,22 @@ polymorphism__process_c_code(PredInfo, NumExtraVars, Impl, OrigArgTypes0,
 	require(unify(NEVs, NumExtraVars), 
 		"list length mismatch in polymorphism processing pragma_c"),
 
-	polymorphism__c_code_add_typeinfos(
-			PredTypeVars, PredTypeVarSet, ExistQVars, Impl,
-			ArgInfo0, ArgInfo1),
+%	The argument order is as follows:
+%	first the UnivTypeInfos (for universally quantified type variables)
+% 	then the ExistTypeInfos (for existentially quantified type variables)
+%	then the UnivTypeClassInfos (for universally quantified constraints)
+%	then the ExistTypeClassInfos (for existentially quantified constraints)
+%	and finally the original arguments of the predicate.
+%
+%	But since we're building ArgInfo by starting with the original
+%	arguments and prepending things as we go, we need to do it in
+%	reverse order.
+
 	polymorphism__c_code_add_typeclass_infos(
 			UnivCs, ExistCs, PredTypeVarSet, Impl,
+			ArgInfo0, ArgInfo1),
+	polymorphism__c_code_add_typeinfos(
+			PredTypeVars, PredTypeVarSet, ExistQVars, Impl,
 			ArgInfo1, ArgInfo),
 
 	%
