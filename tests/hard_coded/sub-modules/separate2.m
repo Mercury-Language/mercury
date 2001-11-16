976da6e014f2b17584e6b072caf096ea62a84038
Peter Ross <petdr@cs.mu.oz.au>
2001-11-16 14:49:18 +0000

    Update the test so that it also tests a separate sub-module whose
    Estimated hours taken: 0.5
    Branches: main
    
    tests/hard_coded/sub-modules/include_parent.m:
    tests/hard_coded/sub-modules/separate2.m:
    tests/hard_coded/sub-modules/use_submodule.m:
    tests/hard_coded/sub-modules/use_submodule.exp:
        Update the test so that it also tests a separate sub-module whose
        filename isn't fully qualified.

---
+++ tests/hard_coded/sub-modules/separate2.m
@@ -0,0 +1,25 @@
+% Used by use_submodule.m
+
+:- module include_parent__separate2.
+
+:- interface.
+
+% The parent module includes io.
+
+:- pred hello(io__state::di, io__state::uo) is det.
+
+  :- module include_parent__separate2__nested.
+  :- interface.
+  :- pred hello(io__state::di, io__state::uo) is det.
+  :- end_module include_parent__separate2__nested.
+
+:- implementation.
+
+hello -->
+	io__write_string("include_parent__separate2: hello\n").
+
+  :- module include_parent__separate2__nested.
+  :- implementation.
+  hello -->
+	io__write_string("include_parent__separate2__nested: hello\n").
+  :- end_module include_parent__separate2__nested.
