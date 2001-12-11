eaa867440762b72b88c85f73a2ad2d5442696962
Fergus Henderson <fjh@cs.mu.oz.au>
2001-12-11 09:01:25 +0000

    Fix some problems with mdb and readline 4.2 that arose due to the use
    Estimated hours taken: 6
    Branches: main
    
    Fix some problems with mdb and readline 4.2 that arose due to the use
    of readline when input is redirected from an input file.  Readline 4.2
    issues a run-time warning, complaining that it can't get the terminal
    characteristics.
    
    One unfortunate consequence of this change is that none of the test
    cases in our test suite actually test the use of readline anymore.
    However, none of the line-editing features were being tested anyway,
    and it's not really possible to test the use of readline properly
    with our current test framework.
    
    configure.in:
    runtime/mercury_conf.h.in:
    runtime/RESERVED_MACRO_NAMES:
    	Check for the existence of isatty().
    
    trace/mercury_trace_readline.c:
    	Only use readline() if the input is a terminal.
    
    trace/mercury_trace_internal.c:
    	Delete the special handling of the `echo' command when readline
    	is enabled, because whether or not readline is enabled now has
    	no effect on echoing when the input is not a terminal.
    	Double-echoing when the input *is* a terminal and the "echo on"
    	command was used is probably appropriate anyway.
    
    tests/debugger/Mmakefile:
    tests/debugger/interactive.inp:
    tests/debugger/interactive.exp:
    	Reenable the `interactive' test case, since the spurious
    	readline-related failures should not occur anymore.
    	This required updating the input file and expected output
    	file to reflect various earlier changes:
    	- readline is used for query input,
    	  so queries have to be all on one line
    	- queries are now echoed properly when `echo on' is set
    	- detailed stack traces include goal paths
    	- we fixed a bug with the display of caller line numbers
    	- we fixed a bug with the display of switch goal paths

--- trace/mercury_trace_internal.c
+++ trace/mercury_trace_internal.c
@@ -91,14 +91,10 @@ static	int			MR_scroll_limit = 24;
 static	int			MR_scroll_next = 0;
 
 /*
-** We echo each command just as it is executed iff this variable is TRUE,
-** unless we're using GNU readline.  If we're using readline, then readline
-** echos things anyway, so in that case we ignore this variable.
+** We echo each command just as it is executed iff this variable is TRUE.
 */
 
-#ifdef MR_NO_USE_READLINE
 static	bool			MR_echo_commands = FALSE;
-#endif
 
 /*
 ** The details of the source server, if any.
@@ -1815,31 +1811,21 @@ MR_trace_handle_cmd(char **words, int word_count, MR_Trace_Cmd_Info *cmd,
 	} else if (streq(words[0], "echo")) {
 		if (word_count == 2) {
 			if (streq(words[1], "off")) {
-#ifdef MR_NO_USE_READLINE
 				MR_echo_commands = FALSE;
 				if (MR_trace_internal_interacting) {
 					fprintf(MR_mdb_out,
 						"Command echo disabled.\n");
 				}
-#else
-				/* with readline, echoing is always enabled */
-				fprintf(MR_mdb_err, "Sorry, cannot disable "
-					"echoing when using GNU readline.\n");
-				
-#endif
 			} else if (streq(words[1], "on")) {
-#ifdef MR_NO_USE_READLINE
 				if (!MR_echo_commands) {
 					/*
 					** echo the `echo on' command
-					** This is needed for testing, so that
-					** we get the same output both with
-					** and without readline.
+					** This is needed for historical reasons
+					** (compatibly with out existing test suite).
 					*/
 					fprintf(MR_mdb_out, "echo on\n");
 					MR_echo_commands = TRUE;
 				}
-#endif
 				if (MR_trace_internal_interacting) {
 					fprintf(MR_mdb_out,
 						"Command echo enabled.\n");
@@ -1849,17 +1835,11 @@ MR_trace_handle_cmd(char **words, int word_count, MR_Trace_Cmd_Info *cmd,
 			}
 		} else if (word_count == 1) {
 			fprintf(MR_mdb_out, "Command echo is ");
-#ifdef MR_NO_USE_READLINE
 			if (MR_echo_commands) {
 				fprintf(MR_mdb_out, "on.\n");
 			} else {
 				fprintf(MR_mdb_out, "off.\n");
 			}
-#else
-			/* with readline, echoing is always enabled */
-			fprintf(MR_mdb_out, "on.\n");
-#endif
-			
 		} else {
 			MR_trace_usage("parameter", "echo");
 		}
@@ -3427,13 +3407,10 @@ MR_trace_getline(const char *prompt, FILE *mdb_in, FILE *mdb_out)
 
 	line = MR_trace_readline(prompt, mdb_in, mdb_out);
 
-	/* if we're using readline, then readline does the echoing */
-#ifdef MR_NO_USE_READLINE
 	if (MR_echo_commands && line != NULL) {
 		fputs(line, mdb_out);
 		putc('\n', mdb_out);
 	}
-#endif
 
 	return line;
 }
