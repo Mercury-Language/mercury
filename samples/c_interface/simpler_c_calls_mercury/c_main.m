66bf99c9d6738104c695d7a7336df8fb34c4fdd6
Simon Taylor <stayl@cs.mu.oz.au>
2001-12-21 06:37:46 +0000

    Simplify the simpler_c_calls_mercury and
    Estimated hours taken: 0.25
    Branches: main
    
    Simplify the simpler_c_calls_mercury and
    simpler_cplusplus_calls_mercury samples to
    avoid explicit construction of the `ml' command line.
    
    samples/c_interface/simpler_c_calls_mercury/c_main.c:
    	Renamed as main.c.
    
    samples/c_interface/simpler_cplusplus_calls_mercury/cpp_main.cc:
    	Renamed as main.cc.
    
    samples/c_interface/simpler_c_calls_mercury/c_main.m:
    samples/c_interface/simpler_cplusplus_calls_mercury/cpp_main.m:
    	Empty main modules, used to set the name executable
    	produced by mmake.
    
    samples/c_interface/simpler_cplusplus_calls_mercury/Mmakefile:
    samples/c_interface/simpler_c_calls_mercury/Mmakefile:
    	Handle file renamings.
    
    	Remove explicit rules to build executables.
    
    	Remove unnecessary header file dependencies.

---
+++ samples/c_interface/simpler_c_calls_mercury/c_main.m
@@ -0,0 +1,9 @@
+%-----------------------------------------------------------------------------%
+% This source file is hereby placed in the public domain.  -stayl (the author).
+%
+% This empty module is used to give the executable produced
+% by mmake the correct name.
+%-----------------------------------------------------------------------------%
+:- module c_main.
+
+:- import_module mercury_lib.
