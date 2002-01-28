88205f583e82246648c6f2e20b73f8fa4b9957b8
Zoltan Somogyi <zs@csse.unimelb.edu.au>
2002-01-28 09:21:33 +0000

    Update the TypeCtorInfo structure to reflect the recent bootstrapping
    Estimated hours taken: 0.1
    Branches: main
    
    java/TypeCtorInfo_Struct.java:
    	Update the TypeCtorInfo structure to reflect the recent bootstrapping
    	changes.

--- java/TypeCtorInfo_Struct.java
+++ java/TypeCtorInfo_Struct.java
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2001 The University of Melbourne.
+// Copyright (C) 2001-2002 The University of Melbourne.
 // This file may only be copied under the terms of the GNU Library General
 // Public License - see the file COPYING.LIB in the Mercury distribution.
 //
@@ -9,17 +9,14 @@ package mercury.runtime;
 public class TypeCtorInfo_Struct {
     
 	public int                              arity;
-	public mercury.runtime.Unify            unify_pred;
-	public mercury.runtime.Unify            new_unify_pred;
-	public mercury.runtime.Compare          compare_pred;
+	public int                              type_ctor_version;
 	public mercury.runtime.TypeCtorRep      type_ctor_rep;
-	public mercury.runtime.ProcAddr         unused1;    // spare 
-	public mercury.runtime.ProcAddr         unused2;    // spare 
+	public int                              type_ctor_num_ptags; // if DU
+	public mercury.runtime.Unify            unify_pred;
+	public mercury.runtime.Compare          compare_pred;
 	public java.lang.String                 type_ctor_module_name;
 	public java.lang.String                 type_ctor_name;
-	public int                              type_ctor_version;
 	public mercury.runtime.TypeFunctors     type_functors;
 	public mercury.runtime.TypeLayout       type_layout;
 	public int 			        type_ctor_num_functors;
-	public int                              type_ctor_num_ptags; // if DU 
 }
