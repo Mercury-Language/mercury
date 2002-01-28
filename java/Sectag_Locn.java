dd3bedbbdabf4cd4b78ea1391464afd99e3b3ba4
Fergus Henderson <fjh@cs.mu.oz.au>
2002-01-28 07:52:54 +0000

    With this change, the files in the `java' directory now all compile
    Branches: main
    Estimated hours taken: 0.5
    
    With this change, the files in the `java' directory now all compile
    fine with `javac' (after `mkdir mercury; ln -s .. mercury/runtime')
    except for the reference to `mercury.Builtin.builtin_comparison_result_0'
    in Compare.java.
    
    java/Sectag_Locn.java:
    	Fix typos.

--- java/Sectag_Locn.java
+++ java/Sectag_Locn.java
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2001 The University of Melbourne.
+// Copyright (C) 2001-2002 The University of Melbourne.
 // This file may only be copied under the terms of the GNU Library General
 // Public License - see the file COPYING.LIB in the Mercury distribution.
 //
@@ -14,8 +14,8 @@ public class Sectag_Locn {
 
 	public int value;
 
-	public Sectag_locn(int arg) {
-		this.value = arg
+	public Sectag_Locn(int arg) {
+		this.value = arg;
 	}
 }
 
