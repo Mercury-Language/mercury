69cd17b8662097654306177b966a09b31490a421
Fergus Henderson <fjh@cs.mu.oz.au>
2002-08-22 07:58:34 +0000

    Delete an bogus call to io__told.
    Estimated hours taken: 0.25
    Branches: main
    
    profiler/mercury_profile.m:
    	Delete an bogus call to io__told.
    	This was resulting an exception since stayl's 2002/07/02 change
    	to library/io.m which trapped attempts to close stdout.

--- profiler/mercury_profile.m
+++ profiler/mercury_profile.m
@@ -1,5 +1,5 @@
 %-----------------------------------------------------------------------------%
-% Copyright (C) 1995-1997, 1999 The University of Melbourne.
+% Copyright (C) 1995-1997, 1999, 2002 The University of Melbourne.
 % This file may only be copied under the terms of the GNU General
 % Public License - see the file COPYING in the Mercury distribution.
 %-----------------------------------------------------------------------------%
@@ -186,8 +186,7 @@ main_2(no, Args) -->
 
 		io__set_output_stream(StdOut, _),
 		output__main(OutputProf, IndexMap),
-		io__write_char('\n'),
-		io__told
+		io__write_char('\n')
         ).
 
 
