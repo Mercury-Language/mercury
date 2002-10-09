29d5ccc679c17b9b086a57c3924a4acfb376b8f0
Mark Brown <dougl@cs.mu.oz.au>
2002-10-09 08:50:02 +0000

    A new test case which we don't pass, plus an XXX comment
    Estimated hours taken: 0.5
    Branches: main
    
    tests/debugger/declarative/Mmakefile:
    tests/debugger/declarative/ho3.exp:
    tests/debugger/declarative/ho3.inp:
    tests/debugger/declarative/ho3.m:
    	A new test case which we don't pass, plus an XXX comment
    	detailing the problem.

---
+++ tests/debugger/declarative/ho3.m
@@ -0,0 +1,47 @@
+:- module ho3.
+:- interface.
+:- import_module io.
+:- pred main(io__state::di, io__state::uo) is det.
+:- implementation.
+:- import_module list.
+
+main -->
+	{
+		p(1)
+	->
+		S1 = ""
+	;
+		S1 = "\\+ "
+	},
+	{
+		p(2)
+	->
+		S2 = ""
+	;
+		S2 = "\\+ "
+	},
+	io__write_strings([S1, "p(1).\n", S2, "p(2).\n"]).
+
+:- pred p(int).
+:- mode p(in) is semidet.
+
+p(N) :-
+	q(N, Q),
+	Q(0).
+
+:- pred q(int, pred(int)).
+:- mode q(in, out(pred(in) is semidet)) is multi.
+
+q(N, x(N)).
+q(N, y(N)).
+
+:- pred x(int, int).
+:- mode x(in, in) is semidet.
+
+x(A, A).
+
+:- pred y(int, int).
+:- mode y(in, in) is semidet.
+
+y(_, -1).
+
