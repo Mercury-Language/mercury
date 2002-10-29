e155315b3ad874c64381773e99a65b17f6b51d57
Fergus Henderson <fjh@cs.mu.oz.au>
2002-10-29 02:42:56 +0000

    Delete this long-obsolete file, which hasn't been used since 1998.
    Estimated hours taken: 0.1
    Branches: main
    
    library/mercury_builtin.m:
    	Delete this long-obsolete file, which hasn't been used since 1998.

--- library/mercury_builtin.m
+++
@@ -1,8 +0,0 @@
-%---------------------------------------------------------------------------%
-% Copyright (C) 1994-1998 The University of Melbourne.
-% This file may only be copied under the terms of the GNU Library General
-% Public License - see the file COPYING.LIB in the Mercury distribution.
-%---------------------------------------------------------------------------%
-
-% IMPORTANT NOTE!  This file is no longer used.
-% Its contents have been moved to builtin.m and private_builtin.m.
