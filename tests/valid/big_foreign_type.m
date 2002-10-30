c6de5002b40ce73a96498b91ae233e536debc0b5
Fergus Henderson <fjh@cs.mu.oz.au>
2002-10-30 03:30:24 +0000

    Add a new test case to test foreign types whose size is different
    Estimated hours taken: 0.75
    
    tests/valid/Mmakefile:
    tests/valid/big_foreign_type.m:
    	Add a new test case to test foreign types whose size is different
    	than "MR_Word".

---
+++ tests/valid/big_foreign_type.m
@@ -0,0 +1,53 @@
+% Test the use of foreign types,
+% e.g. those that don't fit exactly into a single word.
+
+:- module big_foreign_type.
+:- interface.
+
+:- type foo.
+:- pragma foreign_type(c, foo, "struct Foo").
+:- type foo2.
+:- pragma foreign_type(c, foo2, "char").
+:- type foo3.
+:- pragma foreign_type(c, foo3, "double").
+:- type foo4.
+:- pragma foreign_type(c, foo4, "enum e").
+
+:- func bar(foo) = foo.
+:- func bar2(foo2) = foo2.
+:- func bar3(foo3) = foo3.
+:- func bar4(foo4) = foo4.
+
+:- func baz(foo) = foo.
+:- func baz2(foo2) = foo2.
+:- func baz3(foo3) = foo3.
+:- func baz4(foo4) = foo4.
+
+:- implementation.
+
+:- pragma c_header_code("
+	struct Foo {
+		int x, y, z;
+	};
+
+	enum e { e0, e1, e2, e42 = 42 };
+").
+
+:- pragma foreign_proc(c, bar(X::in) = (Y::out),
+	[will_not_call_mercury, promise_pure], "Y = X;").
+:- pragma foreign_proc(c, bar2(X::in) = (Y::out),
+	[will_not_call_mercury, promise_pure], "Y = X;").
+:- pragma foreign_proc(c, bar3(X::in) = (Y::out),
+	[will_not_call_mercury, promise_pure], "Y = 2.0 * X;").
+:- pragma foreign_proc(c, bar4(X::in) = (Y::out),
+	[will_not_call_mercury, promise_pure], "Y = X;").
+
+baz(X) = X.
+baz2(X) = X.
+baz3(X) = X.
+baz4(X) = X.
+
+:- pragma export(baz(in) = out, "baz").
+:- pragma export(baz2(in) = out, "baz2").
+:- pragma export(baz3(in) = out, "baz3").
+:- pragma export(baz4(in) = out, "baz4").
