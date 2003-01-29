20ec77ecb180fc6e1be83f0fd1076a2620b000bb
Fergus Henderson <fjh@cs.mu.oz.au>
2003-01-29 12:32:49 +0000

    Allow `-NaN' as valid output for the NaN tests.
    Estimated hours taken: 0.5
    Branches: main
    
    tests/general/string_format_special_floats.m:
    tests/general/string_format_special_floats.exp:
    	Allow `-NaN' as valid output for the NaN tests.
    	This is not really valid output IMHO, but that's
    	what the Solaris 2.7 C library printf() outputs,
    	and it's not useful to report this as a failure in
    	the Mercury test suite.
    
    	Also, split up the tests for +Inf and -Inf,
    	so that the test case checks that infinities
    	are formatted with the correct sign.

--- tests/general/string_format_special_floats.m
+++ tests/general/string_format_special_floats.m
@@ -20,8 +20,11 @@
 
 main -->
 	{ Inf = (max+max) },
-	io__write_string("Infinity:\n"),
-	list__foldl(test_floats(is_infinity, [Inf, -Inf]), ["%e", "%f", "%g"]),
+	io__write_string("Plus Infinity:\n"),
+	list__foldl(test_floats(is_plus_infinity, [Inf]), ["%e", "%f", "%g"]),
+	io__write_string("Minus Infinity:\n"),
+	list__foldl(test_floats(is_minus_infinity, [-Inf]),
+		["%e", "%f", "%g"]),
 	io__write_string("Not a number:\n"),
 	list__foldl(test_floats(is_nan, [0.0 * Inf]), ["%e", "%f", "%g"]).
 
@@ -44,13 +47,19 @@ test_float(FormatString, IsValid, Float) -->
 		io__write_string("failure '" ++ FloatString ++ "'\n")
 	).
 
-:- pred is_infinity(string::in) is semidet.
+:- pred is_plus_infinity(string::in) is semidet.
 
-is_infinity(String) :-
+is_plus_infinity(String) :-
 	LowerCaseString = string__to_lower(String),
 	( LowerCaseString = "infinity"
 	; LowerCaseString = "inf"
-	; LowerCaseString = "-infinity"
+	).
+
+:- pred is_minus_infinity(string::in) is semidet.
+
+is_minus_infinity(String) :-
+	LowerCaseString = string__to_lower(String),
+	( LowerCaseString = "-infinity"
 	; LowerCaseString = "-inf"
 	).
 
@@ -58,7 +67,15 @@ is_infinity(String) :-
 
 is_nan(String) :-
 	LowerCaseString = string__to_lower(String),
-	LowerCaseString = "nan".
+	( LowerCaseString = "nan"
+	% XXX Actually, it makes no sense to put a minus sign on a NaN,
+	%     since NaNs aren't signed.  However, the printf() function in
+	%     some C libraries (in particular, the one for Solaris 2.7)
+	%     do that.  Arguably that's a bug, but we can't do much about
+	%     bugs in the Solaris C library, so we don't want to report a
+	%     test case failure for that.  Hence we allow -NaN here.
+	; LowerCaseString = "-nan"
+	).
 
 %------------------------------------------------------------------------------%
 %------------------------------------------------------------------------------%
