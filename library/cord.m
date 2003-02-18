5ce05f67e028ea4f7ae14bd148bd2f6b94644be8
Ralph Becket <rafe@csse.unimelb.edu.au>
2003-02-18 06:27:32 +0000

    Added a new library module, cord, for a collection type supporting O(1)
    Estimated hours taken: 4
    Branches: main
    
    Added a new library module, cord, for a collection type supporting O(1)
    insertion and concatenation.
    
    NEWS:
    	Report the new addition.
    
    library/cord.m:
    	Added.
    
    tests/hard_coded/Mmakefile:
    tests/hard_coded/test_cord.m:
    tests/hard_coded/test_cord.exp:
    	Added a test case for the new cord module.

---
+++ library/cord.m
@@ -0,0 +1,236 @@
+%---------------------------------------------------------------------------%
+% cord.m
+% vim: ft=mercury ts=4 sw=4 et wm=0 tw=0
+%---------------------------------------------------------------------------%
+% Copyright (C) 2002-2003 The University of Melbourne.
+% This file may only be copied under the terms of the GNU Library General
+% Public License - see the file COPYING.LIB in the Mercury distribution.
+%---------------------------------------------------------------------------%
+% Ralph Becket <rafe@cs.mu.oz.au>
+% Mon Feb  3 12:27:53 EST 2003
+%
+% A cord is a sequence sequence type supporting O(1) consing and
+% concatenation.  A cord is essentially a tree structure with data stored
+% in the leaf nodes.  Joining two cords together to construct a new cord
+% is therefore an O(1) operation.
+%
+% This data type is intended for situations where efficient, linearised
+% collection of data is required.
+%
+% While this data type presents a list-like interface, calls to list/1 and
+% head_tail/3 in particular are O(n) in the size of the cord.
+%
+%---------------------------------------------------------------------------%
+
+:- module cord.
+
+:- interface.
+
+:- import_module list, int.
+
+
+
+    % Cords that contain the same members in the same order will not
+    % necessarily have the same representation and will, therefore,
+    % not necessarily either unify or compare as equal.
+    %
+    % The exception to this rule is that the empty cord does have a
+    % unique representation.
+    %
+:- type cord(T).
+
+
+
+    % The list of data in a cord:
+    %
+    %   list(empty        ) = []
+    %   list(from_list(Xs)) = Xs
+    %   list(cons(X, C)   ) = [X | list(C)]
+    %   list(TA ++ TB     ) = list(TA) ++ list(TB)
+    %
+:- func list(cord(T)) = list(T).
+
+    % The unique representation for the empty cord:
+    %
+    %   list(empty) = []
+    %
+:- func empty = cord(T).
+
+    % list(singleton(X)) = [X]
+    %
+:- func singleton(T) = cord(T).
+
+    % list(from_list(Xs)) = Xs
+    % An O(1) operation.
+    %
+:- func from_list(list(T)) = cord(T).
+
+    % list(cons(X, C)) = [X | list(C)]
+    % An O(1) operation.
+    %
+:- func cons(T, cord(T)) = cord(T).
+
+    % list(snoc(C, X)) = list(C) ++ [X]
+    % An O(1) operation.
+    %
+:- func snoc(cord(T), T) = cord(T).
+
+    % list(CA ++ CB) = list(CA) ++ list(CB)
+    % An O(1) operation.
+    %
+:- func cord(T) ++ cord(T) = cord(T).
+
+    %     head_tail(C0, X, C)  =>  list(C0) = [X | list(C)]
+    % not head_tail(C0, _, _)  =>  C0 = empty
+    % An O(n) operation, although traversing an entire cord with
+    % head_tail/3 gives O(1) amortized cost for each call.
+    %
+:- pred head_tail(cord(T), T,   cord(T)).
+:- mode head_tail(in,      out, out    ) is semidet.
+
+    % length(C) = list.length(list(C))
+    % An O(n) operation.
+    %
+:- func length(cord(T)) = int.
+
+    % member(X, C) <=> list.member(X, list(C)).
+    %
+:- pred member(T,   cord(T)).
+:- mode member(out, in     ) is nondet.
+
+    % list(map(F, C)) = list.map(F, list(C))
+    %
+:- func map(func(T) = U, cord(T)) = cord(U).
+
+    % foldl(F, C, A) = list.foldl(F, list(C), A).
+    %
+:- func foldl(func(T, U) = U, cord(T), U) = U.
+
+    % foldr(F, C, A) = list.foldr(F, list(C), A).
+    %
+:- func foldr(func(T, U) = U, cord(T), U) = U.
+
+    % equal(CA, CB)  <=>  list(CA) = list(CB).
+    % An O(n) operation where n = length(CA) + length(CB).
+    %
+    % (Note: the current implementation works exactly this way.)
+    %
+:- pred equal(cord(T), cord(T)).
+:- mode equal(in,      in     ) is semidet.
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+
+:- implementation.
+
+
+
+    % We impose the following invariants to ensure we have a unique
+    % representation for the empty cord (this makes the implementation
+    % simpler.)
+    %
+    %   all [C] not C = leaves([])
+    %   all [C] not C = branch(nil, _)
+    %   all [C] not C = branch(_, nil)
+    %
+:- type cord(T)
+    --->    nil
+    ;       leaf(T)
+    ;       leaves(list(T))
+    ;       branch(cord(T), cord(T)).
+
+%-----------------------------------------------------------------------------%
+
+empty = nil.
+
+%-----------------------------------------------------------------------------%
+
+singleton(X) = leaf(X).
+
+%-----------------------------------------------------------------------------%
+
+from_list(Xs) = ( if Xs = [] then nil else leaves(Xs) ).
+
+%-----------------------------------------------------------------------------%
+
+list(C) = list_2(C, []).
+
+
+:- func list_2(cord(T), list(T)) = list(T).
+
+list_2(nil,            Xs) = Xs.
+list_2(leaf(Y),        Xs) = [Y | Xs].
+list_2(leaves(Ys),     Xs) = Ys ++ Xs.
+list_2(branch(CA, CB), Xs) = list_2(CA, list_2(CB, Xs)).
+
+%-----------------------------------------------------------------------------%
+
+cons(X, C) = ( if C = nil then leaf(X) else branch(leaf(X), C) ).
+
+%-----------------------------------------------------------------------------%
+
+snoc(C, X) = ( if C = nil then leaf(X) else branch(C, leaf(X)) ).
+
+%-----------------------------------------------------------------------------%
+
+CA ++ CB = (      if CA = nil then CB
+             else if CB = nil then CA
+             else             branch(CA, CB)
+           ).
+
+%-----------------------------------------------------------------------------%
+
+head_tail(leaf(X),          X, nil ).
+
+head_tail(leaves([X | Xs]), X, C   ) :-
+    C = ( if Xs = [] then nil else leaves(Xs) ).
+
+head_tail(branch(CA0, CB),  X, C   ) :-
+    head_tail(CA0, X, CA),
+    C = CA ++ CB.
+
+%-----------------------------------------------------------------------------%
+
+length(C) = foldl(func(_, N) = N + 1, C, 0).
+
+%-----------------------------------------------------------------------------%
+
+member(X, leaf(X)).
+
+member(X, leaves(Xs)) :-
+    member(X, Xs).
+
+member(X, branch(CA, _)) :-
+    member(X, CA).
+
+member(X, branch(_, CB)) :-
+    member(X, CB).
+
+%-----------------------------------------------------------------------------%
+
+map(_, nil           ) = nil.
+map(F, leaf(X)       ) = leaf(F(X)).
+map(F, leaves(Xs)    ) = leaves(map(F, Xs)).
+map(F, branch(CA, CB)) = branch(map(F, CA), map(F, CB)).
+
+%-----------------------------------------------------------------------------%
+
+foldl(_, nil,            A) = A.
+foldl(F, leaf(X),        A) = F(X, A).
+foldl(F, leaves(Xs),     A) = foldl(F, Xs, A).
+foldl(F, branch(CA, CB), A) = foldl(F, CB, foldl(F, CA, A)).
+
+%-----------------------------------------------------------------------------%
+
+foldr(_, nil,            A) = A.
+foldr(F, leaf(X),        A) = F(X, A).
+foldr(F, leaves(Xs),     A) = foldr(F, Xs, A).
+foldr(F, branch(CA, CB), A) = foldr(F, CA, foldr(F, CB, A)).
+
+%-----------------------------------------------------------------------------%
+
+equal(CA, CB) :-
+    list(CA) = list(CB).
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
---
+++ tests/hard_coded/test_cord.m
@@ -0,0 +1,178 @@
+%-----------------------------------------------------------------------------%
+% test_cord.m
+% Ralph Becket <rafe@cs.mu.oz.au>
+% Tue Feb 11 16:05:29 EST 2003
+% vim: ft=mercury ts=4 sw=4 et wm=0 tw=0
+%
+%-----------------------------------------------------------------------------%
+
+:- module test_cord.
+
+:- interface.
+
+:- import_module io.
+
+
+
+:- pred main(io::di, io::uo) is det.
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+
+:- implementation.
+
+:- import_module cord, list, string, std_util.
+
+%-----------------------------------------------------------------------------%
+
+main -->
+
+  { Z    = empty,
+    A    = cons(a, Z),
+    B    = from_list([b]),
+    AB   = A ++ B,
+    BA   = B ++ A,
+    ABA1 = A ++ BA,
+    ABA2 = AB ++ A,
+    BAB1 = B ++ AB,
+    BAB2 = BA ++ B,
+    ABBA = AB ++ BA,
+    BAAB = BA ++ AB
+  },
+
+    io.format("\ncords:\n", []),
+    io.format("Z    = ", []), io.print(Z   ), io.nl,
+    io.format("A    = ", []), io.print(A   ), io.nl,
+    io.format("B    = ", []), io.print(B   ), io.nl,
+    io.format("AB   = ", []), io.print(AB  ), io.nl,
+    io.format("BA   = ", []), io.print(BA  ), io.nl,
+    io.format("ABA1 = ", []), io.print(ABA1), io.nl,
+    io.format("ABA2 = ", []), io.print(ABA2), io.nl,
+    io.format("BAB1 = ", []), io.print(BAB1), io.nl,
+    io.format("BAB2 = ", []), io.print(BAB2), io.nl,
+    io.format("ABBA = ", []), io.print(ABBA), io.nl,
+    io.format("BAAB = ", []), io.print(BAAB), io.nl,
+
+  { ConstructionResult =
+        (      if list(Z) \= []  then "list(Z) \\= []"
+          else if list(A) \= [a] then "list(A) \\= [a]"
+          else if list(B) \= [b] then "list(B) \\= [b]"
+          else                        "ok"
+        )
+  },
+
+    io.format("\nconstruction: %s\n", [s(ConstructionResult)]),
+
+  { ConcatenationResult =
+        (      if list(AB)   \= [a, b]       then "list(AB)   \\= [a, b]"
+          else if list(BA)   \= [b, a]       then "list(BA)   \\= [b, a]"
+          else if list(ABA1) \= [a, b, a]    then "list(ABA1) \\= [a, b, a]"
+          else if list(ABA2) \= [a, b, a]    then "list(ABA2) \\= [a, b, a]"
+          else if list(BAB1) \= [b, a, b]    then "list(BAB1) \\= [b, a, b]"
+          else if list(BAB2) \= [b, a, b]    then "list(BAB2) \\= [b, a, b]"
+          else if list(ABBA) \= [a, b, b, a] then "list(ABBA) \\= [a, b, b, a]"
+          else if list(BAAB) \= [b, a, a, b] then "list(BAAB) \\= [b, a, a, b]"
+          else                                    "ok"
+        )
+  },
+
+    io.format("\nconcatenation: %s\n", [s(ConcatenationResult)]),
+
+  { EqualsResult =
+        (      if not equal(ABA1, ABA2) then "not equal(ABA1, ABA2)"
+          else if not equal(BAB1, BAB2) then "not equal(BAB1, BAB2)"
+          else                               "ok"
+        )
+  },
+
+    io.format("\nequals: %s\n", [s(EqualsResult)]),
+
+  { IdentityResult =
+        (      if AB   ++ empty \= AB   then "AB   ++ empty \\= AB  "
+          else if BA   ++ empty \= BA   then "BA   ++ empty \\= BA  "
+          else if ABA1 ++ empty \= ABA1 then "ABA1 ++ empty \\= ABA1"
+          else if ABA2 ++ empty \= ABA2 then "ABA2 ++ empty \\= ABA2"
+          else if BAB1 ++ empty \= BAB1 then "BAB1 ++ empty \\= BAB1"
+          else if BAB2 ++ empty \= BAB2 then "BAB2 ++ empty \\= BAB2"
+          else if ABBA ++ empty \= ABBA then "ABBA ++ empty \\= ABBA"
+          else if BAAB ++ empty \= BAAB then "BAAB ++ empty \\= BAAB"
+
+          else if empty ++ AB   \= AB   then "empty ++ AB   \\= AB  "
+          else if empty ++ BA   \= BA   then "empty ++ BA   \\= BA  "
+          else if empty ++ ABA1 \= ABA1 then "empty ++ ABA1 \\= ABA1"
+          else if empty ++ ABA2 \= ABA2 then "empty ++ ABA2 \\= ABA2"
+          else if empty ++ BAB1 \= BAB1 then "empty ++ BAB1 \\= BAB1"
+          else if empty ++ BAB2 \= BAB2 then "empty ++ BAB2 \\= BAB2"
+          else if empty ++ ABBA \= ABBA then "empty ++ ABBA \\= ABBA"
+          else if empty ++ BAAB \= BAAB then "empty ++ BAAB \\= BAAB"
+
+          else    "ok"
+        )
+  },
+
+    io.format("\nidentity: %s\n", [s(IdentityResult)]),
+
+  { LengthResult =
+        (      if length(Z)    \= 0 then "list(Z)    \\= 0"
+          else if length(A)    \= 1 then "list(A)    \\= 1"
+          else if length(B)    \= 1 then "list(B)    \\= 1"
+          else if length(AB)   \= 2 then "list(AB)   \\= 2"
+          else if length(BA)   \= 2 then "list(BA)   \\= 2"
+          else if length(ABA1) \= 3 then "list(ABA1) \\= 3"
+          else if length(ABA2) \= 3 then "list(ABA2) \\= 3"
+          else if length(BAB1) \= 3 then "list(BAB1) \\= 3"
+          else if length(BAB2) \= 3 then "list(BAB2) \\= 3"
+          else if length(ABBA) \= 4 then "list(ABBA) \\= 4"
+          else if length(BAAB) \= 4 then "list(BAAB) \\= 4"
+          else                           "ok"
+        )
+  },
+
+    io.format("\nlength: %s\n", [s(LengthResult)]),
+
+  { MemberResult =
+        ( if   solutions((pred(X::out) is nondet :- member(X, ABBA))) \= [a, b]
+          then "members(ABBA) \\= [a, b]"
+          else "ok"
+        )
+  },
+
+    io.format("\nmembership: %s\n", [s(MemberResult)]),
+
+  { MapResult =
+        ( if   list(map(func(X) = ( if X = a then p else q ), AB)) \= [p, q]
+          then "map(|a -> p, b -> q|, AB) \\= [p, q]"
+          else "ok"
+        )
+  },
+
+    io.format("\nmap: %s\n", [s(MapResult)]),
+
+  { FoldlResult =
+        ( if   foldl(func(X, Xs) = [X | Xs], AB, []) \= [b, a]
+          then "foldl(cons, AB, []) \\= [b, a]"
+          else
+          if   foldl(func(X, Xs) = [X | Xs], from_list([a, b]), []) \= [b, a]
+          then "foldl(cons, from_list([a, b]), []) \\= [b, a]"
+          else "ok"
+        )
+  },
+
+    io.format("\nfoldl: %s\n", [s(FoldlResult)]),
+
+  { FoldrResult =
+        ( if   foldr(func(X, Xs) = [X | Xs], AB, []) \= [a, b]
+          then "foldr(cons, AB, []) \\= [a, b]"
+          else
+          if   foldr(func(X, Xs) = [X | Xs], from_list([a, b]), []) \= [a, b]
+          then "foldr(cons, from_list([a, b]), []) \\= [a, b]"
+          else "ok"
+        )
+  },
+
+    io.format("\nfoldr: %s\n", [s(FoldrResult)]),
+
+    [].
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
