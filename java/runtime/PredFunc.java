22834078babd26302fe50132636eef418a851d55
Fergus Henderson <fjh@cs.mu.oz.au>
2004-02-09 09:17:27 +0000

    New file.  Needed as a result of Zoltan's recent
    Estimated hours taken: 0.25
    Branches: main
    
    java/runtime/PredFunc.java:
    	New file.  Needed as a result of Zoltan's recent
    	typeclass_info-related changes.

---
+++ java/runtime/PredFunc.java
@@ -0,0 +1,19 @@
+//
+// Copyright (C) 2004 The University of Melbourne.
+// This file may only be copied under the terms of the GNU Library General
+// Public License - see the file COPYING.LIB in the Mercury distribution.
+//
+
+package mercury.runtime;
+
+public class PredFunc {
+	
+	public static final int MR_PRED = 0;
+	public static final int MR_FUNC = 1;
+
+	public int value;
+
+	public PredFunc(int arg) {
+		this.value = arg;
+	}
+}
