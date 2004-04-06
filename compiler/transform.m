7f2426759067568e1f66b22e4d9fb84d4399e8a8
Zoltan Somogyi <zs@csse.unimelb.edu.au>
2004-04-06 22:49:49 +0000

    Bring this module up to date with our current style guidelines.
    Estimated hours taken: 0.1
    Branches: main
    
    compiler/transform.m:
    	Bring this module up to date with our current style guidelines.

--- compiler/transform.m
+++ compiler/transform.m
@@ -1,5 +1,5 @@
 %-----------------------------------------------------------------------------%
-% Copyright (C) 1995-1998, 2003 The University of Melbourne.
+% Copyright (C) 1995-1998, 2003-2004 The University of Melbourne.
 % This file may only be copied under the terms of the GNU General
 % Public License - see the file COPYING in the Mercury distribution.
 %-----------------------------------------------------------------------------%
@@ -36,13 +36,8 @@
 
 :- import_module list.
 
-%:- pred unfold__in_proc(pred_id, proc_id, hlds_goal_expr,
-%			mode_info, mode_info).
-%:- mode unfold__in_proc(in, in, out, in, out) is det.
-
-:- pred transform__reschedule_conj(list(hlds_goal), list(hlds_goal), 
-			mode_info, mode_info).
-:- mode transform__reschedule_conj(in, out, in, out) is det.
+:- pred transform__reschedule_conj(list(hlds_goal)::in, list(hlds_goal)::out, 
+	mode_info::in, mode_info::out) is det.
 
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
@@ -51,38 +46,32 @@
 
 :- import_module check_hlds__delay_info.
 :- import_module check_hlds__mode_util.
-:- import_module ll_backend__code_aux.
+:- import_module hlds__instmap.
 :- import_module parse_tree__prog_data.
 
 :- import_module map, set, std_util.
 :- import_module term, require.
 :- import_module varset.
-:- import_module hlds__instmap.
 
 %-----------------------------------------------------------------------------%
 
-% unfold__in_proc(
+transform__reschedule_conj([], [], !ModeInfo).
+transform__reschedule_conj([Goal0 | Goals0], Goals, !ModeInfo) :-
+	mode_info_get_instmap(!.ModeInfo, InstMap0),
+	mode_info_get_delay_info(!.ModeInfo, DelayInfo0),
 
-%-----------------------------------------------------------------------------%
-
-transform__reschedule_conj([], []) --> [].
-transform__reschedule_conj([Goal0 | Goals0], Goals) -->
-	=(ModeInfo0),
-	{ mode_info_get_instmap(ModeInfo0, InstMap0) },
-	{ mode_info_get_delay_info(ModeInfo0, DelayInfo0) },
-
-	{ delay_info__wakeup_goals(DelayInfo0, WokenGoals, DelayInfo1) },
-	mode_info_set_delay_info(DelayInfo1),
-	( { WokenGoals \= [] } ->
-	    { list__append(WokenGoals, [Goal0 | Goals0], Goals1) },
-	    transform__reschedule_conj(Goals1, Goals)
+	delay_info__wakeup_goals(DelayInfo0, WokenGoals, DelayInfo1),
+	mode_info_set_delay_info(DelayInfo1, !ModeInfo),
+	( WokenGoals \= [] ->
+		list__append(WokenGoals, [Goal0 | Goals0], Goals1),
+		transform__reschedule_conj(Goals1, Goals, !ModeInfo)
 	;
-	    { Goal0 = _Goal0Goal - Goal0Info },
-	    { goal_info_get_instmap_delta(Goal0Info, InstMapDelta) },
-	    { instmap__apply_instmap_delta(InstMap0, InstMapDelta, InstMap1) },
-	    mode_info_set_instmap(InstMap1),
-	    transform__reschedule_conj(Goals0, Goals1),
-	    { Goals = [Goal0 | Goals1] }
+		Goal0 = _Goal0Goal - Goal0Info,
+		goal_info_get_instmap_delta(Goal0Info, InstMapDelta),
+		instmap__apply_instmap_delta(InstMap0, InstMapDelta, InstMap1),
+		mode_info_set_instmap(InstMap1, !ModeInfo),
+		transform__reschedule_conj(Goals0, Goals1, !ModeInfo),
+		Goals = [Goal0 | Goals1]
 	).
 
 %-----------------------------------------------------------------------------%
