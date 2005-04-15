9967578ae9a2b1bb308465fa22cc1ba7665820e5
Julien Fischer <juliensf@csse.unimelb.edu.au>
2005-04-15 06:25:13 +0000

    Handle compare_representation in the code that reads in deep profiles.
    Estimated hours taken: 0.2
    Branches: main, release
    
    Handle compare_representation in the code that reads in deep profiles.
    Currently, this causes an assertion failure in the deep profiler.
    
    deep_profiler/read_profile.m:
    	Handle compare_representation like the other special predicates.

--- deep_profiler/read_profile.m
+++ deep_profiler/read_profile.m
@@ -399,6 +399,8 @@ refined_proc_id_to_string(uci_pred(TypeName, TypeModule, _DefModule,
         PredName = "Compare"
     ; RawPredName = "__Index__" ->
         PredName = "Index"
+    ; RawPredName = "__CompareRep__" ->
+        PredName = "CompareRep"
     ;
         string__append("unknown special predicate name ", RawPredName, Msg),
         error(Msg)
