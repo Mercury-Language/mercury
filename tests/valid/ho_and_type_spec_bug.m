8a47080b04a175e9e3e264e154a60e14411bef5b
Julien Fischer <juliensf@csse.unimelb.edu.au>
2005-05-02 08:19:08 +0000

    Add a test case for a bug that causes an assertion failure in the
    Estimated hours taken: 6
    Branches: main, release
    
    Add a test case for a bug that causes an assertion failure in the
    compiler.  It seems to be caused by the interaction of higher-order and
    type specialization; a unification involving the construction of a
    type_ctor_info (for the type being specialized) in a predicate created
    by higher-order specialization seems to have an incorrect entry in the
    instmap - this results in simplify incorrectly optimizing away the
    unification, even though its output is need later.  (The unification is
    the one involving V_20 in the predicate foldl_ho1/5).
    
    This is the same bug that causes the module compiler/mode_robdd.tfeirn
    to have to be compiled at a reduced optimization level.  We don't
    currently have a fix for this (I'm still working on that), however a
    workaround is to disable higher-order specialization for any affected
    modules.  (In what is probably another bug, you don't seem to be able to
    disable type-specialization, if a predicate has a type_spec pragma on
    it).
    
    tests/valid/Mmakefile:
    tests/valid/Mercury.options:
    tests/valid/ho_and_type_spec_bug.m:
    	A test case that exposes a bug in some of the optimization
    	passes.

---
+++ tests/valid/ho_and_type_spec_bug.m
@@ -0,0 +1,64 @@
+% Compiling the following program at -O3 causes the following
+% assertion failure in the compiler:
+%
+%	 Uncaught Mercury exception:
+%	 Software Error: map__lookup: key not found
+% 		Key Type: term.var(parse_tree.prog_data.prog_var_type)
+% 		Key Value: var(20)
+% 		Value Type: ll_backend.var_locn.var_state
+%
+:- module ho_and_type_spec_bug.
+
+:- interface.
+
+:- type sparse_bitset(T) ---> [] ; [ int | sparse_bitset(T) ].
+
+:- type robdd(T) ---> robdd(int).
+
+:- type unit ---> unit.
+
+:- pragma type_spec(alpha/4, T = unit).
+:- pred alpha(sparse_bitset(int)::in, robdd(T)::in,
+	robdd(T)::in, robdd(T)::out) is det.
+
+:- implementation.
+
+:- typeclass foo(T) where [
+	func from_int(int) = T
+].	
+
+:- instance foo(int) where [
+	from_int(X) = X
+].
+
+alpha(T, E, R0, R) :-
+	Closure = beta(E),
+	foldl(Closure, T, R0, R).
+
+:- pred beta(robdd(T)::in, int::in, robdd(T)::in, robdd(T)::out) is det.
+
+beta(_E, V, _R0, R) :- R = gamma(V).
+
+:- func gamma(int) = robdd(T).
+:- pragma no_inline(gamma/1).
+:- pragma foreign_proc("C",
+	gamma(V::in) = (F::out),
+	[will_not_call_mercury, promise_pure],
+"
+	/* V F */
+").
+
+:- pred foldl(pred(T, U, U), sparse_bitset(T), U, U) <= foo(T).
+:- mode foldl(pred(in, in, out) is det, in, in, out) is det.
+
+foldl(_, [], Acc, Acc).
+foldl(P, [ OffSet | T ], Acc0, Acc) :-
+	fold_bits(P, OffSet, Acc0, Acc1),
+	foldl(P, T, Acc1, Acc).
+
+:- pred fold_bits(pred(T, U, U), int, U, U) <= foo(T).
+:- mode fold_bits(pred(in, in, out) is det, in, in, out) is det.
+
+fold_bits(P, Offset, Acc0, Acc) :-
+ 	Elem = from_int(Offset),
+	P(Elem, Acc0, Acc).
