cd2674bfce9c73c04c80d84c37317c7c15496605
Ralph Becket <rafe@csse.unimelb.edu.au>
2005-08-04 04:55:43 +0000

    Fix a missing case in the previous fix for the following:
    Estimated hours taken: 0.1
    Branches: main, release
    
    library/string.m:
    	Fix a missing case in the previous fix for the following:
    	Bugfix: previously string.value_to_revstrings was not calling
    	univ_value on arguments of functors that are handled specially.  This
    	caused spurious "univ_cons" functors to appear in the string
    	representations of some terms.

--- library/string.m
+++ library/string.m
@@ -4395,7 +4395,8 @@ ordinary_term_to_revstrings(NonCanon, OpsTable, Priority, X, !Rs) :-
 		Args = [BracedTerm]
 	->
 		add_revstring("{ ", !Rs),
-		value_to_revstrings(NonCanon, OpsTable, BracedTerm, !Rs),
+		value_to_revstrings(NonCanon, OpsTable,
+			univ_value(BracedTerm), !Rs),
 		add_revstring(" }", !Rs)
 	;
 		Functor = "{}",
