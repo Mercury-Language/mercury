99a9f4a363c6fb9b7595488aa523ec01096c83af
Zoltan Somogyi <zs@csse.unimelb.edu.au>
2005-09-05 06:21:51 +0000

    Clarify the documentation a bit, and convert to four-space indentation.
    Estimated hours taken: 0.1
    Branches: main
    
    library/enum.m:
    	Clarify the documentation a bit, and convert to four-space indentation.

--- library/enum.m
+++ library/enum.m
@@ -1,19 +1,17 @@
 %-----------------------------------------------------------------------------%
-% Copyright (C) 2000 The University of Melbourne.
+% vim: ft=mercury ts=4 sw=4 et wm=0 tw=0
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2000, 2005 The University of Melbourne.
 % This file may only be copied under the terms of the GNU Library General
 % Public License - see the file COPYING.LIB in the Mercury distribution.
 %-----------------------------------------------------------------------------%
+%
 % File: enum.m.
 % Author: stayl.
-% Stability: very low.
+% Stability: medium
 %
-% This module provides the typeclass `enum', which describes
-% types which can be converted to and from integers without loss
-% of information.
-%
-% The interface of this module is likely to change.
-% At the moment it is probably best to only use the `enum'
-% type class for types to be stored in `sparse_bitset's.
+% This module provides the typeclass `enum', which describes types
+% which can be converted to and from integers without loss of information.
 %
 %-----------------------------------------------------------------------------%
 
@@ -22,7 +20,8 @@
 :- interface.
 
 	% For all instances the following must hold:
-	%	all [X, Int] (X = from_int(to_int(X)))
+	%	all [X] (X = from_int(to_int(X)))
+	%	all [Int] (some [Y] Int = to_int(Y) => from_int(Int) = Y)
 :- typeclass enum(T) where [
 	func to_int(T) = int,
 	func from_int(int) = T is semidet
