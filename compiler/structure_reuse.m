db5d688c435b7abae731e1faa83bac3c8a96d51a
Nancy Mazur <Nancy.Mazur@cs.kuleuven.ac.be>
2006-05-10 15:20:57 +0000

    Provide the direct reuse analysis part of the structure reuse analysis (which
    Estimated hours taken: 25
    Branches: main
    
    Provide the direct reuse analysis part of the structure reuse analysis (which
    itself is part of the CTGC system).
    
    compiler/ctgc.datastruct.m:
    compiler/ctgc.util.m:
    	Additional predicates.
    
    compiler/ctgc.m:
    	Add structure reuse module.
    
    compiler/handle_options.m:
    compiler/options.m:
    	Add new options "structure_reuse_analysis" and related ones.
    
    compiler/handle_options.m:
    compiler/hlds_out.m:
    	Add dump option "R" to dump structure reuse related information
    	in the hlds_dump files.
    
    compiler/hlds_goal.m:
    	Types to record structure reuse information at the level of each
    	goal.
    	Additional "case_get_goal" function to extract the goal from an case.
    
    compiler/mercury_compile.m:
    	Add structure reuse analysis as a new compiler stage.
    
    compiler/structure_reuse.analysis.m:
    	The top level analysis predicates.
    
    compiler/structure_reuse.m:
    compiler/structure_reuse.direct.m:
    compiler/structure_reuse.direct.choose_reuse.m:
    compiler/structure_reuse.direct.detect_garbage.m:
    	Direct reuse analysis is split into 2 steps: determining when and how
    	data structures become garbage, and then choosing how these dead
    	data structures might best be reused.
    
    compiler/structure_reuse.domain.m:
    	The abstract domain for keeping track of reuse conditions, the main
    	domain in the structure reuse analysis.
    
    compiler/structure_reuse.lbu.m:
    compiler/structure_reuse.lfu.m:
    	To determine whether data structures become dead or not, one needs to
    	know which variables in a goal are needed with respect to forward
    	execution (lfu = local forward use), and backward execution, i.e.
    	backtracking (lbu = local backward use). These two modules provide
    	the necessary functionality to pre-annotate the goals with lfu and
    	lbu information.
    
    compiler/structure_sharing.analysis.m:
    compiler/structure_sharing.domain.m:
    	Remove the structure sharing table from the interface of the analysis
    	predicate in structure_sharing.analysis.m;
    	Move predicates to structure_sharing.domain.m so that they become
    	more easily accessible for the structure_reuse modules.
    
    compiler/prog_data.m:
    	New types "dead_var", "live_var" and alike.

---
+++ compiler/structure_reuse.m
@@ -0,0 +1,32 @@
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2006 The University of Melbourne.
+% This file may only be copied under the terms of the GNU General
+% Public License - see the file COPYING in the Mercury distribution.
+%-----------------------------------------------------------------------------%
+%
+% File: structure_reuse.m
+% Main author: nancy
+%
+% Package grouping all the modules related to the structure reuse analysis 
+% part of compile-time garbage collection.
+%
+%-----------------------------------------------------------------------------%
+
+:- module transform_hlds.ctgc.structure_reuse.
+
+:- interface.
+
+:- include_module analysis. 
+	:- include_module lfu.
+	:- include_module lbu.
+	:- include_module direct.
+	%:- include_module indirect.
+
+% :- include_module util.
+:- include_module domain.
+
+:- import_module hlds.
+:- import_module transform_hlds.ctgc.structure_sharing.
+:- import_module check_hlds.
+
+:- end_module transform_hlds.ctgc.structure_reuse.
