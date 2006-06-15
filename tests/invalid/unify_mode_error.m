94da3f1eb288704d2bc3d05027b928e6aceae6ff
Julien Fischer <juliensf@csse.unimelb.edu.au>
2006-06-15 04:48:36 +0000

    Back out my bogus change to this file.
    tests/invalid/unify_mode_error.m:
    	Back out my bogus change to this file.
    
    tests/invalid/unify_mode_error.err_exp:
    	Update the expected output to conform to recent changes to the
    	behaviour of the `Please recompile with `-E' prompt'.

--- tests/invalid/unify_mode_error.m
+++ tests/invalid/unify_mode_error.m
@@ -1,3 +1,15 @@
-unify_mode_error.m:001: Warning: module should start with a `:- module' declaration.
-unify_mode_error.m:001: Warning: interface for module `unify_mode_error' does
-unify_mode_error.m:001:   not export anything.
+% The compiler of 15/9/1999 reported the wrong predicate
+% name in the mode error for this test case because polymorphism.m
+% substituted private_builtin__builtin_unify_int/2 for builtin__unify/2.
+% That optimization is now only done by higher_order.m after all errors
+% have been reported.
+:- module unify_mode_error.
+
+:- interface.
+
+:- pred foo(int::in, int::out) is det.
+
+:- implementation.
+
+foo(X, Y) :-
+	builtin__unify(X, Y).
