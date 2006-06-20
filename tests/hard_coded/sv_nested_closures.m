a652b9bc4cfaf4d1bbf72ea3c228230699ea61eb
Julien Fischer <juliensf@csse.unimelb.edu.au>
2006-06-20 08:48:51 +0000

    Fix a bug with state variables and closures nested more than one level deep
    Estimated hours taken: 3
    Branches: main, release
    
    Fix a bug with state variables and closures nested more than one level deep
    reported by valery@freesurf.fr.  The problem was that the map that kept track
    of which state variables came from the scope enclosing a lambda expression
    (and are hence read-only inside the lambda expressiong), was being incorrectly
    overwritten when a nested lambda expression was encountered.  The fix is to
    merge the set of state variables from the body of the lambda with those from
    the enclosing scope before processing the nested lambda.  (Actually in order
    to ensure that we get things correct if the same variable is used in multiple
    scopes we overlay the maps rather than merge them.)
    
    compiler/state_var.m:
    	When preparing to process a lambda expression don't throw away any
    	state variable mappings from enclosing scopes.
    
    tests/hard_coded/Mmakefile:
    tests/hard_coded/sv_nested_closures.{m,exp}:
    	Test case for the above.

---
+++ tests/hard_coded/sv_nested_closures.m
@@ -0,0 +1,42 @@
+% vim: ft=mercury ts=4 sw=4 et
+:- module sv_nested_closures.
+:- interface.
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module int.
+:- import_module list.
+:- import_module string.
+
+main(!IO) :-
+    some [!X] (
+        !:X = 10,
+        Foo = (pred(I::in, !.IO::di, !:IO::uo) is det :-
+            Bar = (pred(J::in, !.IO::di, !:IO::uo) is det :-
+                Result = J + I + !.X,
+                io.write_int(Result, !IO),
+                io.nl(!IO)
+            ),
+            Bar(4, !IO)
+        ),
+        Foo(3, !IO)
+    ),
+    baz(-200, X, !IO),
+    io.write_int(X, !IO),
+    io.nl(!IO).
+
+:- pred baz(int::in, int::out, io::di, io::uo) is det.
+
+baz(!Y, !IO) :-
+    io.format("(baz) !.Y = %d\n", [i(!.Y)], !IO),
+    BazFoo = (pred(!.Y::in, !:Y::out, !.IO::di, !:IO::uo) is det :-
+        io.format("(BazFoo) !.Y = %d\n", [i(!.Y)], !IO),
+        BazBar = (pred(!.Z::in, !:Z::out, !.IO::di, !:IO::uo) is det :-
+            io.format("(BazBar) !.Y = %d\n", [i(!.Y)], !IO)
+        ),
+        BazBar(400, _, !IO)
+    ),
+    BazFoo(700, _, !IO).
