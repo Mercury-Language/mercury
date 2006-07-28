d62dac66141eb412528e2986c59f43b3be89d8d0
Julien Fischer <juliensf@csse.unimelb.edu.au>
2006-07-28 05:14:28 +0000

    Fix compilation of the posix binding by conforming to
    Estimated hours taken: 0
    Branches: main, release
    
    extras/posix/posix.exec.m:
    	Fix compilation of the posix binding by conforming to
    	recent changes in the stdlib.

--- extras/posix/posix.exec.m
+++ extras/posix/posix.exec.m
@@ -1,5 +1,5 @@
 %------------------------------------------------------------------------------%
-% Copyright (C) 2001 The University of Melbourne.
+% Copyright (C) 2001, 2006 The University of Melbourne.
 % This file may only be copied under the terms of the GNU Library General
 % Public License - see the file COPYING.LIB in the Mercury distribution.
 %------------------------------------------------------------------------------%
@@ -25,11 +25,10 @@
 
 :- implementation.
 
-:- import_module array, std_util.
+:- import_module array.
+:- import_module pair.
 
-:- pragma c_header_code("
-	#include <unistd.h>
-").
+:- pragma foreign_decl("C", "#include <unistd.h>").
 
 %------------------------------------------------------------------------------%
 
