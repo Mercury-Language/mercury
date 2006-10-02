bd94a2a70bc0bb21db5c42772b1f843655d15452
Julien Fischer <juliensf@csse.unimelb.edu.au>
2006-10-02 07:10:04 +0000

    Fix a bug with intermodule optimization and termination analysis.
    Estimated hours taken: 3
    Branches: main, release
    
    Fix a bug with intermodule optimization and termination analysis.  The problem
    was that not all of the termination_info pragmas for all of the opt_exported
    procedures were being written to the .opt file.  In some cases where the
    procedure was defined by a foreign clause, this lead to the termination
    foreign code attributes conflicting with the analyser's default assumptions
    about procedures without termination_info pragmas.
    
    This diff fixes the bug in one direction: making sure that we always adjust
    the import status of items written to .opt files.  This is sufficient to get
    the termination analyser to write out the missing termination_info pragmas.
    (Previously we used to only do this if intermodule unused argument analysis
    was enabled for some reason.)
    
    I'm working on a separate fix in the other direction, i.e. making the
    termination analyser a bit more robust with respect to missing information -
    I'll post that as a separate diff.  (In any case this change is useful in it's
    own right - it should improve accuracy).
    
    compiler/intermod.m:
     	Always adjust the import status of items written to .opt files.

--- compiler/intermod.m
+++ compiler/intermod.m
@@ -5,10 +5,10 @@
 % This file may only be copied under the terms of the GNU General
 % Public License - see the file COPYING in the Mercury distribution.
 %-----------------------------------------------------------------------------%
-
+% 
 % File: intermod.m.
 % Main author: stayl.
-
+% 
 % This module writes out the interface for inter-module optimization.
 % The .opt file includes:
 %   - The clauses for exported preds that can be inlined.
@@ -32,7 +32,7 @@
 % determinism declarations do not have clauses exported, since this would
 % require running mode analysis and determinism analysis before writing the
 % .opt file, significantly increasing compile time for a very small gain.
-
+% 
 %-----------------------------------------------------------------------------%
 
 :- module transform_hlds.intermod.
@@ -164,7 +164,6 @@ write_optfile(!ModuleInfo, !IO) :-
         globals.io_lookup_bool_option(deforestation, Deforestation, !IO),
         globals.io_lookup_int_option(higher_order_size_limit,
             HigherOrderSizeLimit, !IO),
-        globals.io_lookup_bool_option(intermod_unused_args, UnusedArgs, !IO),
         some [!IntermodInfo] (
             init_intermod_info(!.ModuleInfo, !:IntermodInfo),
             gather_preds(PredIds, yes, Threshold, HigherOrderSizeLimit,
@@ -175,12 +174,7 @@ write_optfile(!ModuleInfo, !IO) :-
             intermod_info_get_module_info(!.IntermodInfo, !:ModuleInfo),
             io.set_output_stream(OutputStream, _, !IO),
             io.close_output(FileStream, !IO),
-            (
-                UnusedArgs = yes,
-                do_adjust_pred_import_status(!.IntermodInfo, !ModuleInfo)
-            ;
-                UnusedArgs = no
-            )
+            do_adjust_pred_import_status(!.IntermodInfo, !ModuleInfo)
         )
     ),
     % restore the option setting that we overrode above
