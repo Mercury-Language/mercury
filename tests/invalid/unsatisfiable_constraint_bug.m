1c0b5fd8a8fa5ad04995f0c9ee6c050b05f19c3b
Mark Brown <mark@csse.unimelb.edu.au>
2006-11-21 11:47:49 +0000

    Fix a bug reported by Peter Ross.
    Estimated hours taken: 6
    Branches: main, release
    
    Fix a bug reported by Peter Ross.
    
    compiler/typeclasses.m:
    	When recovering after an unsatisfiable constraint, don't merely
    	delete the existing unproven constraints.  Instead construct new
    	hlds_constraints from scratch.  This ensures that the invariants
    	on it are maintained.
    
    tests/invalid/Mmakefile:
    tests/invalid/unsatisfiable_constraint_bug.err_exp:
    tests/invalid/unsatisfiable_constraint_bug.m:
    	Regression test.

---
+++ tests/invalid/unsatisfiable_constraint_bug.m
@@ -0,0 +1,33 @@
+% The code caused the compiler the abort, rather than report an error
+% for the unsatisfiable constraints.
+
+% Software Error: map.lookup: key not found
+% Key Type: term.var(parse_tree.prog_data.prog_var_type)
+% Key Value: var(6)
+% Value Type: parse_tree.prog_data.mer_type
+
+:- module unsatisfiable_constraint_bug.
+:- interface.
+:- import_module io.
+:- import_module stream.
+
+:- pred test_stream(S::in, B::di, B::uo, io::di, io::uo) is det
+        <= (reader(S, int, B, E), writer(S, int, B)).
+            % XXX add these two lines and the program compiles
+            % reader(S, float, B, E), writer(S, float, B)).
+
+:- implementation.
+
+test_stream(S, !Buffer, !IO) :-
+	stream.put(S, 10, !Buffer),
+	stream.put(S, 3.14, !Buffer),
+	stream.get(S, ResultA : stream.result(int, E), !Buffer),
+	stream.get(S, ResultB : stream.result(float, E), !Buffer),
+	stream.get(S, ResultC : stream.result(float, E), !Buffer),
+	io.write(ResultA, !IO),
+	io.nl(!IO),
+	io.write(ResultB, !IO),
+	io.nl(!IO),
+	io.write(ResultC, !IO),
+	io.nl(!IO).
+
