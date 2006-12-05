0e8eaabfad8248c6b378e2eaa3a7521ad2fdc315
Peter Wang <novalazy@gmail.com>
2006-12-05 03:45:08 +0000

    Add dup2() system call.
    Estimated hours taken: 0.1
    Branches: main
    
    extras/posix/posix.dup.m:
    	Add dup2() system call.

--- extras/posix/posix.dup.m
+++ extras/posix/posix.dup.m
@@ -1,5 +1,5 @@
 %------------------------------------------------------------------------------%
-% Copyright (C) 2001 The University of Melbourne.
+% Copyright (C) 2001, 2006 The University of Melbourne.
 % This file may only be copied under the terms of the GNU Library General
 % Public License - see the file COPYING.LIB in the Mercury distribution.
 %------------------------------------------------------------------------------%
@@ -14,6 +14,9 @@
 
 :- pred dup(fd, posix__result(fd), io__state, io__state).
 :- mode dup(in, out, di, uo) is det.
+
+:- pred dup2(fd, fd, posix__result(fd), io__state, io__state).
+:- mode dup2(in, in, out, di, uo) is det.
 
 %------------------------------------------------------------------------------%
 
@@ -42,6 +45,26 @@ dup(Fd, Result) -->
 :- pragma c_code(dup0(OldFd::in, NewFd::out, IO0::di, IO::uo),
 		[will_not_call_mercury, thread_safe], "{
 	NewFd = dup(OldFd);
+	IO = IO0;
+}").
+
+%------------------------------------------------------------------------------%
+
+dup2(OldFd, NewFd, Result) -->
+	dup2_2(OldFd, NewFd, fd(Ret)),
+	( if { Ret < 0 } then
+		errno(Err),
+		{ Result = error(Err) }
+	else
+		{ Result = ok(fd(Ret)) }
+	).
+
+:- pred dup2_2(fd, fd, fd, io__state, io__state).
+:- mode dup2_2(in, in, out, di, uo) is det.
+
+:- pragma c_code(dup2_2(OldFd::in, NewFd::in, Ret::out, IO0::di, IO::uo),
+		[will_not_call_mercury, thread_safe], "{
+	Ret = dup2(OldFd, NewFd);
 	IO = IO0;
 }").
 
