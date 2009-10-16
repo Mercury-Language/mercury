681f0dfda1a2270acca104c9ec3b598fbf9fa444
Zoltan Somogyi <zs@csse.unimelb.edu.au>
2009-10-16 07:27:45 +0000

    The test case for mantis bug 113.
    Estimated hours taken: 0.2
    Branches: main
    
    tests/invalid/bug113.{m,err_exp}:
    	The test case for mantis bug 113.
    
    tests/invalid/Mmakefile:
    	Enable the new test case.

---
+++ tests/invalid/bug113.m
@@ -0,0 +1,48 @@
+%-----------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et tw=0 wm=0 ft=mercury
+%-----------------------------------------------------------------------------%
+%
+% This program (derived from library/bitmap.m) causes the following abort
+% in the rotd-2009-10-11.
+%
+% Software Error: prog_type.m: Unexpected: type_to_ctor_and_args_det:
+%   type_to_ctor_and_args failed
+%
+% To reproduce: mmc bug113.m
+%
+%-----------------------------------------------------------------------------%
+:- module bug113.
+:- interface.
+
+:- import_module bool.
+:- import_module list.
+
+%-----------------------------------------------------------------------------%
+
+:- type bitmap ---> bitmap.
+:- type foo == bool.
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+
+:- implementation.
+
+:- import_module exception.
+:- import_module int.
+:- import_module string.
+
+%-----------------------------------------------------------------------------%
+
+new(N) = new(N, no).
+
+new(N, B) = BM :-
+    ( if N < 0 then
+        throw_bitmap_error("bitmap.new: negative size") = _ : int
+      else
+        X    = initializer(B),
+        BM0  = initialize_bitmap(allocate_bitmap(N), N, X),
+        BM   = clear_filler_bits(BM0)
+    ).
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
