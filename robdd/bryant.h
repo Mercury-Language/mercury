33085af0e4143505dcd70ccc14132f9603f271e6
Paul Bone <pbone@csse.unimelb.edu.au>
2010-02-11 04:41:03 +0000

    Fix a copyright message.
    Correct a copyright message that was incorrectly modified by the commit hooks.
    
    robdd/bryant.h:
        As above.

--- robdd/bryant.h
+++ robdd/bryant.h
@@ -1,5 +1,6 @@
 /*
-** Copyright (C) 1995, 2001-2005 Peter Schachte and, 2010 The University of Melbourne.
+** Copyright (C) 1995, 2001-2005 Peter Schachte 
+** Copyright (C) 1995, 2001-2005, 2010 The University of Melbourne.
 ** This file may only be copied under the terms of the GNU Library General
 ** Public License - see the file COPYING.LIB in the Mercury distribution.
 */
