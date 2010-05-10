6c549acd64f6aed942ddcfafa43b158e7cf8d744
Peter Wang <novalazy@gmail.com>
2010-05-10 06:31:01 +0000

    On startup, check for the SSDB_TTY environment variable.
    Branches: main, 10.04
    
    ssdb/ssdb.m:
            On startup, check for the SSDB_TTY environment variable.  If set, all
            debugger I/O is performed through the device named by the environment
            variable.  This is equivalent to the `mdb --tty <file-name>' option.
    
            Trap SIGINT and stop at the next possible point (C and Java grades).
    
            Add `print VAR' and `print N' commands.
    
            Add `vars' command.
    
    library/rtti_implementation.m:
            Make `deconstruct' not escape special characters in the returned
            functor name argument for characters and strings, to match the
            behaviour on C backends.

--- library/rtti_implementation.m
+++ library/rtti_implementation.m
@@ -1,7 +1,7 @@
 %-----------------------------------------------------------------------------%
 % vim: ft=mercury ts=4 sw=4 et wm=0 tw=0
 %-----------------------------------------------------------------------------%
-% Copyright (C) 2001-2007, 2009 The University of Melbourne.
+% Copyright (C) 2001-2007, 2009-2010 The University of Melbourne.
 % This file may only be copied under the terms of the GNU Library General
 % Public License - see the file COPYING.LIB in the Mercury distribution.
 %-----------------------------------------------------------------------------%
@@ -111,7 +111,6 @@
 :- import_module maybe.
 :- import_module require.
 :- import_module string.
-:- import_module term_io.
 :- import_module type_desc.
 
 %-----------------------------------------------------------------------------%
@@ -1879,7 +1878,7 @@ deconstruct_2(Term, TypeInfo, TypeCtorInfo, TypeCtorRep, NonCanon,
     ;
         TypeCtorRep = tcr_char,
         det_dynamic_cast(Term, Char),
-        Functor = term_io.quoted_char(Char),
+        Functor = string.from_char_list(['\'', Char, '\'']),
         Arity = 0,
         Arguments = []
     ;
@@ -1891,7 +1890,7 @@ deconstruct_2(Term, TypeInfo, TypeCtorInfo, TypeCtorRep, NonCanon,
     ;
         TypeCtorRep = tcr_string,
         det_dynamic_cast(Term, String),
-        Functor = term_io.quoted_string(String),
+        Functor = string.append_list(["\"", String, "\""]),
         Arity = 0,
         Arguments = []
     ;
