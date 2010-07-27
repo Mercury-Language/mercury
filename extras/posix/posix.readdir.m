f53181dff155118ab2c3902441915c29d1fb7a3a
Julien Fischer <juliensf@csse.unimelb.edu.au>
2010-07-27 20:03:41 +0000

    Replace a c_header_code pragma with a foreign_decl pragma
    Branches: main
    
    extras/posix/psoix.readdir.m:
    	Replace a c_header_code pragma with a foreign_decl pragma
    	for C.

--- extras/posix/posix.readdir.m
+++ extras/posix/posix.readdir.m
@@ -1,5 +1,5 @@
 %------------------------------------------------------------------------------%
-% Copyright (C) 2001, 2007 The University of Melbourne.
+% Copyright (C) 2001, 2007, 2010 The University of Melbourne.
 % This file may only be copied under the terms of the GNU Library General
 % Public License - see the file COPYING.LIB in the Mercury distribution.
 %------------------------------------------------------------------------------%
@@ -21,7 +21,7 @@
 
 :- implementation.
 
-:- pragma c_header_code("
+:- pragma foreign_decl("C", "
 #include <sys/types.h>
 #include <dirent.h>
 ").
