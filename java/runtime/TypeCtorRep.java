a0e81a4384f1fe65502a24c60164fb06331aa0fc
Julien Fischer <juliensf@csse.unimelb.edu.au>
2010-09-22 12:46:17 +0000

    Merge trunk_snapshot_20100916190937 upto trunk_snapshot_20100922220921 onto the 10.04 branch.

--- java/runtime/TypeCtorRep.java
+++ java/runtime/TypeCtorRep.java
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2001-2005 The University of Melbourne.
+// Copyright (C) 2001-2005, 2010 The University of Melbourne.
 // This file may only be copied under the terms of the GNU Library General
 // Public License - see the file COPYING.LIB in the Mercury distribution.
 //
@@ -9,6 +9,13 @@ package jmercury.runtime;
 public class TypeCtorRep implements java.io.Serializable {
 	
 	// Constants are in private_builtin.m, named MR_TYPECTOR_REP_*.
+	//
+	// XXX We have to duplicate these here for use by TypeInfo_Struct.java
+	// In fact, all the values should be moved here, and the compiler
+	// should generate references to these constants instead of those in
+	// private_builtin.
+	public static final int MR_TYPECTOR_REP_EQUIV		= 6;
+	public static final int MR_TYPECTOR_REP_EQUIV_GROUND	= 29;
 
 	// Instance variable for TypeCtorRep objects.
 	
