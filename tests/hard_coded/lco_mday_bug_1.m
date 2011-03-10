bc932e1fd1f71224ec695e8046f86378cc552e1c
Zoltan Somogyi <zs@csse.unimelb.edu.au>
2011-03-10 05:45:35 +0000

    Fix another problem with lco reported by Michael Day.
    Estimated hours taken: 2
    Branches: main
    
    Fix another problem with lco reported by Michael Day. The problem was that
    (a) the hlc backend always passes floats as unboxed entities, but stored in
    structures in boxed form, and (b) the lco transformation can change float
    arguments from being passed in structures to being passed as arguments.
    The problem is that the argument will be a pointer to an UNBOXED float,
    even though the structure field whose address supplies the value of the
    pointer is supposed to contain a BOXED float.
    
    compiler/lco.m:
    	Look for this situation, and if it arises, don't apply lco.
    
    tests/hard_coded/lco_mday_bug_{1,2}.{m,exp}:
    	Two versions of the test case that exhibit this bug. The first version
    	works with floats, and shows the bug; the second replaces the floats
    	with strings, and it works even without this fix. This proves that the
    	problem was one described above.
    
    tests/hard_coded/Mmakefiles:
    tests/hard_coded/Mercury.options:
    	Enable the new test cases, and compile them with lco.

---
+++ tests/hard_coded/lco_mday_bug_1.m
@@ -0,0 +1,74 @@
+% vim: ts=4 sw=4 et ft=mercury
+
+:- module lco_mday_bug_1.
+
+:- interface.
+
+:- import_module io.
+
+:- pred main(io, io).
+:- mode main(di, uo) is det.
+
+:- implementation.
+
+:- import_module int, float.
+
+:- type number
+    --->    num_int(int)
+    ;       num_float(float).
+
+:- type standard_func
+    --->    date_func
+    ;       date_parse.
+
+main(!IO) :-
+    p(X),
+    io.write(num_float(X), !IO),
+    io.nl(!IO),
+    call_standard_func(date_parse, Res),
+    (
+        Res = num_int(I),
+        io.write_string("int ", !IO),
+        io.write_int(I, !IO),
+        io.nl(!IO)
+    ;
+        Res = num_float(F),
+        io.write_string("float ", !IO),
+        io.write_float(F, !IO),
+        io.nl(!IO)
+    ),
+    io.write(Res, !IO),
+    io.nl(!IO).
+
+%--------------------------------------------------------------------%
+
+:- pragma no_inline(p/1).
+:- pred p(float::out) is det.
+
+p(43.0).
+
+%--------------------------------------------------------------------%
+
+:- pred call_standard_func(standard_func, number).
+:- mode call_standard_func(in, out) is det.
+
+call_standard_func(date_func, Res) :-
+    Res = num_int(0).
+call_standard_func(date_parse, Res) :-
+    to_integer(num_int(0), T),
+    Res = num_float(T).
+
+%--------------------------------------------------------------------%
+
+:- pred to_integer(number, float).
+:- mode to_integer(in, out) is det.
+
+to_integer(N0, N) :-
+    (
+        N0 = num_int(_),
+        N = 41.0
+    ;
+        N0 = num_float(_),
+        call_standard_func(date_func, _Res2),
+        N = 42.0
+    ).
---
+++ tests/hard_coded/lco_mday_bug_2.m
@@ -0,0 +1,51 @@
+:- module lco_mday_bug_2.
+
+:- interface.
+
+:- import_module io.
+
+:- pred main(io, io).
+:- mode main(di, uo) is det.
+
+:- implementation.
+
+:- import_module int.
+
+:- type number
+    --->    num_int(int)
+    ;	    num_float(string).
+
+:- type standard_func
+    --->    date_func
+    ;	    date_parse.
+
+main(!IO) :-
+    call_standard_func(date_parse, Res),
+    io.write(Res, !IO),
+    io.nl(!IO).
+
+%--------------------------------------------------------------------%
+
+:- pred call_standard_func(standard_func, number).
+:- mode call_standard_func(in, out) is det.
+
+call_standard_func(date_func, Res) :-
+    Res = num_int(0).
+call_standard_func(date_parse, Res) :-
+    to_integer(num_int(0), T),
+    Res = num_float(T).
+
+%--------------------------------------------------------------------%
+
+:- pred to_integer(number, string).
+:- mode to_integer(in, out) is det.
+
+to_integer(N0, N) :-
+    (
+	N0 = num_int(_),
+	N = "41"
+    ;
+	N0 = num_float(_),
+	call_standard_func(date_func, _Res2),
+	N = "42"
+    ).
