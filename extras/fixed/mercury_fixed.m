b3b155be92cd1be53aa325ec750fc80e54aa87ba
Julien Fischer <juliensf@csse.unimelb.edu.au>
2011-08-02 07:55:09 +0000

    Fix top-level invocations of mmake in the extras distribution.
    Branches: main, 11.07
    
    Fix top-level invocations of mmake in the extras distribution.  They were
    breaking because the lex subdirectory didn't have an Mmakefile.  (It uses
    mmc --make and a normal Makefile instead.)
    
    Make more of the extras distribution build from the top-level.
    
    extras/lex/Mmakefile:
    	Add an Mmakefile that contains the targets required by the top-level
    	extras distribution Mmakefile.  Each of the targets just forwards
    	the work to the actual Makefile.
    
    extras/Mmakefile:
    	Update the list of things that won't compile ``out-of-the-box''.
    	(XXX we should use autoconf to configure these.)
    
    	Build the base64 encoding library, the fixed point arithmetic library
    	and the error utility by default.
    
    extras/README:
    	Update the description of the lazy_evaluation subdirectory.
    
    extras/base64/Makefile:
    extras/base64/Mmakefile:
    extras/base64/mercury_base64.m:
    extars/base64/Mercury.options:
    	Build and install base64 as a library.  We use mmc --make, controlled
    	from a normal Makefile to do this and then put a forwarding Mmakefile
    	in place using so that compilation from the top-level of the extras
    	distribution works.  (One reason for doing this is that mmc --make
    	provides grade filtering capabilities which are needed here since
    	this library will only work in C grades.)
    
    extras/base64/base64.m:
    	Avoid a compilation error: sizeof cannot be used on things with
    	an incomplete type.
    
    extras/fixed/Makefile:
    extras/fixed/Mmakefile:
    extras/fixed/Mercury.options:
    extras/fixed/mercury_fixed.m:
    	Build and install fixed as a library.  As with base64, use mmc --make
    	and add a forwarding Mmakefile.
    
    extras/fixed/fixed.m:
    	Style and formatting fixes.
    
    extras/lex/Makefile:
    	Add a realclean target
    
    extras/lex/lex.lexeme.m:
    	Replace a call to a deprecated procedure.

---
+++ extras/fixed/mercury_fixed.m
@@ -0,0 +1,10 @@
+%-----------------------------------------------------------------------------%
+% vim: ft=mercury ts=4 sw=4 et
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2011 The University of Melbourne.
+% This file may only be copied under the terms of the GNU Library General
+% Public License - see the file COPYING.LIB in the Mercury distribution.
+%-----------------------------------------------------------------------------%
+
+:- module mercury_fixed.
+:- import_module fixed.
