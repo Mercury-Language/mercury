1f9321240ef548a775269c21b8aa1ac011352c34
Zoltan Somogyi <zs@csse.unimelb.edu.au>
2011-11-28 05:57:36 +0000

    Fix Mantis bug 237.
    Estimated hours taken: 0.3
    Branches: main, release
    
    Fix Mantis bug 237.
    
    compiler/polymorphism.m:
    	If the processing of a promise_solution scope generates new variables
    	(e.g. PolyConst variables), do not reuse them outside the scope,
    	since that leads to Mantis bug 237.
    
    tests/hard_coded/promise_eqv_solns_typeclasses.exp.{m,exp}:
    	New test case for this bug.
    
    tests/hard_coded/Mmakefile:
    	Enable the new test case.

---
+++ tests/hard_coded/promise_eqv_solns_typeclasses.m
@@ -0,0 +1,41 @@
+% vim: ts=4 sw=4 et ft=mercury
+
+:- module promise_eqv_solns_typeclasses.
+
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module list.
+
+:- typeclass foo(X) where [].
+:- instance foo(int) where [].
+
+main(!IO) :-
+    ( foo(2, N) ->
+        io.write_int(N, !IO),
+        io.nl(!IO)
+    ;
+        io.write_string("no solution\n", !IO)
+    ).
+
+:- pred foo(X::in, X::out) is semidet <= foo(X).
+
+foo(X, Y) :-
+    % The calls to reverse() are necessary so things aren't
+    % optimized away...
+    promise_equivalent_solutions [Bar] (
+        list.reverse([X], Bar)
+    ),
+    (
+        Bar = [],
+        false
+    ;
+        Bar = [Z | L],
+        list.reverse(L, _),
+        Y = Z
+    ).
