8225fdbe7649b387ecec0776ea8054f74dca6722
Julien Fischer <juliensf@csse.unimelb.edu.au>
2011-11-30 15:38:57 +0000

    Fix a problem that prevents Java 1.7 from compiling the Java code generated by
    Branches: main, 11.07
    
    Fix a problem that prevents Java 1.7 from compiling the Java code generated by
    the Mercury compiler.  Previously, the TypeInfo_Struct class in the Java
    version of Mercury's runtime defined the following constructors:
    
        public TypeInfo_Struct(TypeInfoStruct ti, int arity, Object... as)
        public TypeInfo_Struct(TypeInfoStruct ti, Object... as)
    
    Changes to the way most specific varargs method selection works in Java 1.7
    means the above is now rejected by the Java compiler.  (It should apparently
    have never been allowed in the first place.)
    
    The fix is to delete the first constructor above from the runtime and not
    generate code that provides the arity.  (The arity argument was only ever used
    as a sanity check on the number of the following arguments.)
    
    java/runtime/TypeInfo_Struct.java
    	Delete one of the ambiguous constructors from this class.
    
    compiler/rtti_to_mlds.m:
    	Don't pass an arity argument to the constructor for TypeInfo_Struct
    	objects.

--- compiler/rtti_to_mlds.m
+++ compiler/rtti_to_mlds.m
@@ -455,13 +455,27 @@ gen_type_info_defn(ModuleInfo, RttiTypeInfo, Name, RttiId, !GlobalData) :-
                 !GlobalData),
             RttiTypeCtor = var_arity_id_to_rtti_type_ctor(VarArityId),
             module_info_get_name(ModuleInfo, ModuleName),
-            Initializer = init_struct(mlds_rtti_type(item_type(RttiId)), [
-                gen_init_rtti_name(ModuleName, RttiTypeCtor,
-                    type_ctor_type_ctor_info),
-                gen_init_int(list.length(ArgTypes)),
-                gen_init_cast_rtti_datas_array(mlds_type_info_type,
-                    ModuleName, ArgRttiDatas)
-            ]),
+            module_info_get_globals(ModuleInfo, Globals),
+            globals.get_target(Globals, TargetLang),
+
+            InitRttiName = gen_init_rtti_name(ModuleName, RttiTypeCtor,
+                type_ctor_type_ctor_info),
+            InitCastRttiDatasArray = gen_init_cast_rtti_datas_array(
+                mlds_type_info_type, ModuleName, ArgRttiDatas),
+            ( TargetLang = target_java ->
+                % For Java we need to omit the arity argument as the
+                % TypeInfo_Struct class doesn't have a constructor that
+                % supports it -- see java/runtime/TypeInfo_Struct.java for
+                % details.
+                InitializerArgs = [InitRttiName, InitCastRttiDatasArray]
+            ; 
+                InitializerArgs = [
+                    InitRttiName,
+                    gen_init_int(list.length(ArgTypes)),
+                    InitCastRttiDatasArray]
+            ),
+            Initializer = init_struct(mlds_rtti_type(item_type(RttiId)),
+                InitializerArgs),
             rtti_entity_name_and_init_to_defn(Name, RttiId, Initializer,
                 !GlobalData),
 
