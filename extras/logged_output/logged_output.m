d898c1deecd64761a85f7a36f523e23da318cd44
Julien Fischer <juliensf@csse.unimelb.edu.au>
2012-01-04 11:49:07 +0000

    Fix extras/logged_output: it was suffering from some bit rot and wouldn't have
    Branches: main, 11.07
    
    Fix extras/logged_output: it was suffering from some bit rot and wouldn't have
    worked correctly in any case since it makes use of C variable length argument
    lists in an undefined manner.
    
    extras/logged_output/logged_output.m:
    	Replace a call to an out-of-date form of the MR_incr_hp macro.
    
    	In the definition of the the function ME_logged_output_vprintf,
    	make a copy of the varags state variable before the first call
    	to vprintf.  This fix is _NOT_ portable, the right thing to do
    	would be to use C99's va_copy macro, but if there currently
    	appear to be some problems with the Mercury runtime headers
    	when GCC is used in C99 (or GNU99) mode.
    
    	Syntax and formatting updates.
    
    extras/logged_output/main.m:
    	Syntax and formatting updates.

--- extras/logged_output/logged_output.m
+++ extras/logged_output/logged_output.m
@@ -1,17 +1,19 @@
-%------------------------------------------------------------------------------%
-% Copyright (C) 2000, 2005 The University of Melbourne.
+%-----------------------------------------------------------------------------%
+% vim: ft=mercury ts=4 sw=4 et
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2000, 2005, 2012 The University of Melbourne.
 % This file may only be copied under the terms of the GNU Library General
 % Public License - see the file COPYING.LIB in the Mercury distribution.
-%------------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
 %
-% module: 	logged_output.m
-% main author:	Peter Ross (petdr@miscrit.be)
+% module:   logged_output.m
+% main author:  Peter Ross (petdr@miscrit.be)
 %
 % This provides an implementation of a stream which writes to stdout and
 % logs to a file at the same time.
 %
-%------------------------------------------------------------------------------%
-%------------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
 
 :- module logged_output.
 
@@ -19,51 +21,51 @@
 
 :- import_module io.
 
-:- pred logged_output__init(string::in, io__result(io__output_stream)::out,
-		io__state::di, io__state::uo) is det.
+:- pred logged_output.init(string::in, io.result(io.text_output_stream)::out,
+    io::di, io::uo) is det.
 
-%------------------------------------------------------------------------------%
-%------------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
 
 :- implementation.
 
-logged_output__init(FileName, Result) -->
-	create_stream(FileName, Stream),
-	{ Result = ok(Stream) }.
-	
-%------------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
 
-:- pred create_stream(string::in, io__output_stream::out, io::di, io::uo)
-	is det.
+logged_output.init(FileName, Result, !IO) :-
+    create_stream(FileName, Stream, !IO),
+    Result = ok(Stream).
+    
+%-----------------------------------------------------------------------------%
+
+:- pred create_stream(string::in, io.output_stream::out, io::di, io::uo)
+    is det.
 
 :- pragma foreign_proc("C",
-	create_stream(FileName::in, IOStream::out, IO0::di, IO::uo), 
-	[will_not_call_mercury, promise_pure],
+    create_stream(FileName::in, IOStream::out, _IO0::di, _IO::uo), 
+    [will_not_call_mercury, promise_pure],
 "
-	MercuryFile	*stream;
-	FILE		*file;
+    MercuryFile *stream;
+    FILE        *file;
 
-	file = fopen(FileName, ""w"");
+    file = fopen(FileName, ""w"");
 
-	MR_incr_hp((MR_Word) stream, ((sizeof(MercuryFile) / sizeof(MR_Word)) + 1));
+    MR_incr_hp_type(stream, MercuryFile);
 
-	stream->stream_type	= MR_FILE_STREAM;
-	stream->stream_info.file= file;
-	stream->line_number	= 1;
+    stream->stream_type = MR_FILE_STREAM;
+    stream->stream_info.file = file;
+    stream->line_number = 1;
 
-	stream->close		= ME_logged_output_close;
-	stream->read		= ME_logged_output_read;
-	stream->write		= ME_logged_output_write;
+    stream->close       = ME_logged_output_close;
+    stream->read        = ME_logged_output_read;
+    stream->write       = ME_logged_output_write;
 
-	stream->flush		= ME_logged_output_flush;
-	stream->ungetc		= ME_logged_output_ungetch;
-	stream->getc		= ME_logged_output_getch;
-	stream->vprintf		= ME_logged_output_vfprintf;
-	stream->putc		= ME_logged_output_putch;
+    stream->flush       = ME_logged_output_flush;
+    stream->ungetc      = ME_logged_output_ungetch;
+    stream->getc        = ME_logged_output_getch;
+    stream->vprintf     = ME_logged_output_vfprintf;
+    stream->putc        = ME_logged_output_putch;
 
-	IOStream = (MercuryFilePtr) stream;
-
-	IO = IO0;
+    IOStream = MR_wrap_output_stream(stream);
 ").
 
 
@@ -74,14 +76,15 @@ logged_output__init(FileName, Result) -->
   #error ""you need to use version of the mercury compiler configured with --enable-new-mercuryfile-struct""
 #endif
 
-#include ""stdio.h""
+#include <stdio.h>
+#include <stdarg.h>
 
 int ME_logged_output_putch(MR_StreamInfo *info, int);
 int ME_logged_output_close(MR_StreamInfo *info);
 int ME_logged_output_vfprintf(MR_StreamInfo *info,
-		const char *format, va_list ap);
+        const char *format, va_list ap);
 int ME_logged_output_write(MR_StreamInfo *info,
-		const void *buffer, size_t size);
+        const void *buffer, size_t size);
 
 int ME_logged_output_getch(MR_StreamInfo *info);
 int ME_logged_output_ungetch(MR_StreamInfo *info,int);
@@ -93,32 +96,41 @@ int ME_logged_output_read(MR_StreamInfo *info, void *buffer, size_t size);
 int
 ME_logged_output_putch(MR_StreamInfo *info, int ch)
 {
-	putc(ch, stdout);
-	return putc(ch, info->file);
+    putc(ch, stdout);
+    return putc(ch, info->file);
 }
 
 int
 ME_logged_output_close(MR_StreamInfo *info)
 {
-	return fclose(info->file);
+    return fclose(info->file);
 }
   
 int
 ME_logged_output_vfprintf(MR_StreamInfo *info, const char *format, va_list ap)
 {
-	int rc;							  
-	vfprintf(stdout, format, ap);
-	rc = vfprintf(info->file, format, ap);
-	return rc;
+    int rc;
+    va_list log_ap;
+    /*
+    ** XXX we are not allowed to resue ap after a call to vfprintf
+    ** (as this code originally did) -- doing so results in undefined
+    ** behaviour.  Making a copy of ap (here log_ap) in the following manner
+    ** is NOT portable.  (C99 provides the macro va_copy, which would allow
+    ** us to do this portably.)
+    */
+    *log_ap = *ap;
+    vfprintf(stdout, format, ap);
+    rc = vfprintf(info->file, format, log_ap);
+    return rc;
 }
 
 int
 ME_logged_output_write(MR_StreamInfo *info, const void *buffer, size_t size)
 {
-	int rc;							       
-	fwrite(buffer, sizeof(unsigned char), size, stdout);
-	rc = fwrite(buffer, sizeof(unsigned char), size, info->file);
-	return (rc < size ? -1 : rc);
+    int rc;                                
+    fwrite(buffer, sizeof(unsigned char), size, stdout);
+    rc = fwrite(buffer, sizeof(unsigned char), size, info->file);
+    return (rc < size ? -1 : rc);
 }
   
 /*
@@ -128,28 +140,29 @@ ME_logged_output_write(MR_StreamInfo *info, const void *buffer, size_t size)
 int
 ME_logged_output_getch(MR_StreamInfo *info) 
 {
-	MR_fatal_error(""ME_logged_output_getch"");
+    MR_fatal_error(""ME_logged_output_getch"");
 }
-	  
+      
 int
 ME_logged_output_ungetch(MR_StreamInfo *info, int ch)
 {
-	MR_fatal_error(""ME_logged_output_ungetch"");
+    MR_fatal_error(""ME_logged_output_ungetch"");
 }
 
 int
 ME_logged_output_flush(MR_StreamInfo *info)
 {
-	fflush(stdout);
-	return fflush(info->file);
+    fflush(stdout);
+    return fflush(info->file);
 }
   
 int
 ME_logged_output_read(MR_StreamInfo *info, void *buffer, size_t size)
 { 
-	MR_fatal_error(""ME_logged_output_read"");
+    MR_fatal_error(""ME_logged_output_read"");
 }
 ").
 
 %------------------------------------------------------------------------------%
+:- end_module logged_output.
 %------------------------------------------------------------------------------%
