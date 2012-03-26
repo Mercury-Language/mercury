b3786ea0ac3c561bd2783d0442230c951b4d9b48
Zoltan Somogyi <zs@csse.unimelb.edu.au>
2012-03-26 04:25:37 +0000

    Fix white space.
    Estimated hours taken: 0.1
    Branches: main
    
    library/integer.m:
    	Fix white space.

--- library/integer.m
+++ library/integer.m
@@ -1,7 +1,7 @@
 %-----------------------------------------------------------------------------%
 % vim: ft=mercury ts=4 sw=4 et wm=0 tw=0
 %-----------------------------------------------------------------------------%
-% Copyright (C) 1997-2000, 2003-2007, 2011 The University of Melbourne.
+% Copyright (C) 1997-2000, 2003-2007, 2011-2012 The University of Melbourne.
 % This file may only be copied under the terms of the GNU Library General
 % Public License - see the file COPYING.LIB in the Mercury distribution.
 %-----------------------------------------------------------------------------%
@@ -871,7 +871,7 @@ head(I) = (I = i(_, [Hd|_T]) ->  Hd ; func_error("integer.head: []") ).
 :- func head_tail(integer) = digit.
 
 head_tail(I) =
-    (I = i(_, [_ | [HT | _]]) ->
+    ( I = i(_, [_ | [HT | _]]) ->
         HT
     ;
         func_error("integer.head_tail: []")
