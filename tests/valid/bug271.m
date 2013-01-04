fefe868097437671bdd633adc74c96fdd6a458d9
Paul Bone <paul@bone.id.au>
2013-01-04 11:34:01 +1100

    Add a test case for bug 271.
    The bug results in a compiler crash when the compiler constructs a
    typeclass info variable that it has already constructed on another branch
    and there is also an existentially quantified variable present.
    
    tests/valid/bug271.m:
    tests/valid/Mmakefile:
    tests/valid/Mercury.options:
        As above.

---
+++ tests/valid/bug271.m
@@ -0,0 +1,61 @@
+
+:- module bug271.
+
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module int.
+:- import_module list.
+:- import_module string.
+
+:- type a_or_b
+    --->    a
+    ;       b.
+
+main(!IO) :-
+    get_a_or_b(AB),
+    (
+        AB = a,
+        obj_to_num(obj, X),
+        S = to_string(X)
+    ;
+        AB = b,
+        obj_to_num(obj, X),
+        S = to_string(X)
+    ),
+    format("%s\n", [s(S)], !IO).
+
+:- pred get_a_or_b(a_or_b::out) is det.
+
+:- typeclass literal(X) where [
+    func literal_type(X) = literal_type
+].
+
+:- typeclass num(X) where [
+    func to_string(X) = string
+].
+
+:- some [Y] (pred obj_to_num(object(X)::in, Y::out) is det => num(Y)) <= literal(X).
+
+% We need a clause so that Y gets a type assignment.
+obj_to_num(_, 15).
+
+:- type object(L) --->
+    object(L).
+:- type literal ---> literal.
+:- type literal_type ---> literal_type.
+:- func obj = object(literal).
+
+:- instance literal(literal) where [
+    (literal_type(literal) = literal_type)
+].
+
+:- instance num(int) where [
+    (to_string(X) = string(X))
+].
+
