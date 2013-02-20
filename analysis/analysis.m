9f83932ac14e6c89cd85559f77e285dc6073fe09
Julien Fischer <jfischer@opturion.com>
2013-02-20 17:23:01 +1100

    Delete the old `analysis' directory.
    analysis/README.txt:
        Shift the contents of this file in to compiler/notes/analysis.txt.
    
    analysis/:
        Delete this directory and the remainder of its contents.
        We only kept it around in order to workaround limitations of CVS
        and since we no longer use that we don't need to any more.

--- analysis/analysis.m
+++
@@ -1,9 +0,0 @@
-%-----------------------------------------------------------------------------%
-% vim: ft=mercury ts=4 sw=4 et
-%-----------------------------------------------------------------------------%
-% Copyright (C) 2003-2004, 2006-2008 The University of Melbourne.
-% This file may only be copied under the terms of the GNU Library General
-% Public License - see the file COPYING.LIB in the Mercury distribution.
-%-----------------------------------------------------------------------------%
-%
-% The former contents of this file are now in compiler/analysis.m.
