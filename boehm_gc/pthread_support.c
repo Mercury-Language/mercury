e0e6e09c858c83eddaff33a09796903f4146451f
Paul Bone <paul@bone.id.au>
2014-09-24 11:53:41 +1000

    boehm_gc: Fix declration of setup_mark_lock()
    These patches have been applied to upstream boehm_gc by Ivan Maidanski.
    We're applying them here as we're tracking their fixes for the TSX lock
    elision bug.
    
    boehm_gc/pthread_support.c:
        Rename GC_setup_mark_lock() to setup_mark_lock() and make it static.
        Add static declration of setup_mark_lock();
        Call setup_mark_lock() while setting up parallel marking.
    
    boehm_gc/include/private/pthread_support.h:
        Remove declaration of GC_setup_mark_lock();
    
    boehm_gc/misc.c:
        Remove call to GC_setup_mark_lock();

--- boehm_gc/pthread_support.c
+++ boehm_gc/pthread_support.c
@@ -935,6 +935,10 @@ STATIC void GC_fork_child_proc(void)
                                         ptr_t *startp, ptr_t *endp);
 #endif
 
+#ifdef PARALLEL_MARK
+  static void setup_mark_lock(void);
+#endif
+
 /* We hold the allocation lock. */
 GC_INNER void GC_thr_init(void)
 {
@@ -1056,6 +1060,7 @@ GC_INNER void GC_thr_init(void)
       GC_parallel = TRUE;
       /* Disable true incremental collection, but generational is OK.   */
       GC_time_limit = GC_TIME_UNLIMITED;
+      setup_mark_lock();
     }
     /* If we are using a parallel marker, actually start helper threads. */
     if (GC_parallel) {
@@ -1879,7 +1884,7 @@ static pthread_cond_t builder_cv = PTHREAD_COND_INITIALIZER;
   }
 #endif /* GLIBC_2_19_TSX_BUG */
 
-GC_INNER void GC_setup_mark_lock(void)
+static void setup_mark_lock(void)
 {
 # ifdef GLIBC_2_19_TSX_BUG
     pthread_mutexattr_t mattr;
