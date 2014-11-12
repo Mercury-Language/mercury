f16a3c778b1ad4ca6d9c29dcb18a2ac77f6a4298
Paul Bone <paul@bone.id.au>
2014-11-12 15:55:11 +1100

    Remove old boehm_gc/ directory.
    boehm_gc/:
        Removed.

--- boehm_gc/build_atomic_ops.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-P=`pwd`/libatomic_ops-install
-cd libatomic_ops
-# Mercury-specific: allow additional arguments.
-# Mercury-specific: explicitly set --libdir to avoid some 64-bit Linux
-# systems installing in $P/lib64.
-./configure --prefix=$P --libdir=$P/lib "$@"
-$MAKE CC="$CC" AR="$AR" RANLIB="$RANLIB" install
