997976407261cf2f54920f7f75a887d16a7123e8
Peter Wang <novalazy@gmail.com>
2015-03-17 16:05:24 +1100

    Build string switch tries in the target string encoding.
    The compiler should work in code units of the TARGET string encoding
    when building tries for string switches.  Using its own string encoding
    would be incorrect if it differs from the target encoding.  Currently
    that would only occur if the compiler is built in a java/csharp grade
    (uses UTF-16 internally) and invoked to target high-level C (uses UTF-8).
    
    Another motivation for this change is to remove a place where the
    compiler behaviour depends on the setting of `--cross-compiling'.
    As of now, the `--cross-compiling' option has no effect.
    
    compiler/backend_libs.m:
    compiler/string_encoding.m:
    	Add new module with helper predicates.
    
    compiler/ml_string_switch.m:
    	Convert strings to/from code units in the target string
    	encoding.
    
    compiler/ml_switch_gen.m:
    	Remove restriction on compiling string switches using tries when
    	`--cross-compiling' is enabled.
    
    compiler/notes/compiler_design.html:
    	Document the new module.

---
+++ compiler/string_encoding.m
@@ -0,0 +1,71 @@
+%----------------------------------------------------------------------------%
+% vim: ft=mercury ts=4 sw=4 et
+%----------------------------------------------------------------------------%
+% Copyright (C) 2015 The Mercury team.
+% This file may only be copied under the terms of the GNU General
+% Public License - see the file COPYING in the Mercury distribution.
+%----------------------------------------------------------------------------%
+
+:- module backend_libs.string_encoding.
+:- interface.
+
+:- import_module libs.
+:- import_module libs.globals.
+
+:- import_module list.
+
+:- type string_encoding
+    --->    utf8
+    ;       utf16.
+
+:- func target_string_encoding(compilation_target) = string_encoding.
+
+:- pred to_code_unit_list(string_encoding::in, string::in, list(int)::out)
+    is det.
+
+:- pred from_code_unit_list(string_encoding::in, list(int)::in, string::out)
+    is semidet.
+
+%----------------------------------------------------------------------------%
+%----------------------------------------------------------------------------%
+
+:- implementation.
+
+:- import_module string.
+
+target_string_encoding(Target) = Encoding :-
+    (
+        ( Target = target_c
+        ; Target = target_erlang
+        ),
+        Encoding = utf8
+    ;
+        ( Target = target_il
+        ; Target = target_java
+        ; Target = target_csharp
+        ),
+        Encoding = utf16
+    ).
+
+to_code_unit_list(Encoding, String, CodeUnits) :-
+    (
+        Encoding = utf8,
+        string.to_utf8_code_unit_list(String, CodeUnits)
+    ;
+        Encoding = utf16,
+        string.to_utf16_code_unit_list(String, CodeUnits)
+    ).
+
+from_code_unit_list(Encoding, CodeUnits, String) :-
+    require_complete_switch [Encoding]
+    (
+        Encoding = utf8,
+        string.from_utf8_code_unit_list(CodeUnits, String)
+    ;
+        Encoding = utf16,
+        string.from_utf16_code_unit_list(CodeUnits, String)
+    ).
+
+%----------------------------------------------------------------------------%
+:- end_module backend_libs.string_encoding.
+%----------------------------------------------------------------------------%
