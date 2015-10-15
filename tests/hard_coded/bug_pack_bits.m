df85dc94d500a50bf9ada7ff2d2dabcfc9ef07e4
Peter Wang <novalazy@gmail.com>
2015-10-15 14:17:27 +1100

    Delete platform specifics in test case.
    tests/hard_coded/bug_pack_bits.m:
    	Delete platform specifics which are irrelevant to this test
    	case.
    
    	Delete unnecessary comments.

--- tests/hard_coded/bug_pack_bits.m
+++ tests/hard_coded/bug_pack_bits.m
@@ -42,55 +42,24 @@ getaddrinfo([addrinfo(fam_inet, sock_stream, 89),
              addrinfo(fam_inet6, sock_stream, 80),
              addrinfo(fam_inet6, sock_dgram, 80)]).
 
-    % Protocol number.
-    %
-    % See getprotobyname/4.
-    %
 :- type protocol_num == int.
 
-    % The socket family. This type is incomplete, support for socket
-    % families such as IPX or appletalk will probably never be added.
-    % However Unix domain sockets may be added in the future.
-    %
 :- type family
     --->    fam_inet
     ;       fam_inet6.
 
-%---------------------------------------------------------------------------%
-
-    % The socket type. Informally (for fam_inet and fam_inet6) these
-    % correspond to TCP and UDP respectively. More precicely these specify
-    % the socket's behavour, the protocol is optionally specified
-    % seperately.
-    %
 :- type socktype
     --->    sock_stream
     ;       sock_dgram.
 
-:- pragma foreign_decl("C",
-"
-#ifdef MR_WIN32
-    #include ""mercury_windows.h""
-    #include <winsock2.h>
-    #include <ws2tcpip.h>
-#else
-    #include <netinet/in.h>
-    #include <arpa/inet.h>
-#endif
-").
-
-%---------------------------------------------------------------------------%
-
-    % This list of address families is from socket(2) on linux.
-    %
 :- pragma foreign_enum("C", family/0, [
-    fam_inet       - "AF_INET",
-    fam_inet6      - "AF_INET6"
+    fam_inet       - "1",
+    fam_inet6      - "2"
 ]).
 
 :- pragma foreign_enum("C", socktype/0, [
-    sock_stream    - "SOCK_STREAM",
-    sock_dgram     - "SOCK_DGRAM"
+    sock_stream    - "3",
+    sock_dgram     - "4"
 ]).
 
 %---------------------------------------------------------------------------%
