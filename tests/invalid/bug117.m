84bda9a5faced6c35da68fabb875588bf437c6ff
Julien Fischer <jfischer@opturion.com>
2015-10-31 19:05:49 +1100

    Add a regression test for bug #117.
    tests/invalid/Mmakefile:
    tests/invalid/bug117.{m,err_exp}:
        As above.

---
+++ tests/invalid/bug117.m
@@ -0,0 +1,22 @@
+% Regression test for bug #117.
+
+:- module bug117.
+:- interface.
+:- import_module io.
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+:- import_module list.
+
+main(!IO):-
+    extract(hold([])) = List,
+    List = [Head | _],
+    io.write_int(Head, !IO),
+    io.nl(!IO).
+
+:- type hold(T) ---> hold(T).
+
+:- func extract(hold(list(int))) = list(int).
+:- mode extract(in) = out(non_empty_list) is det.
+
+extract(hold(X)) = X.
