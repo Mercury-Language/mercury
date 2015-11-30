188526335432be14b639da0316dfe58476f38a8d
Julien Fischer <jfischer@opturion.com>
2015-11-30 16:50:06 +1100

    Add a test case for bug #115.
    tests/invalid/bug115.{m,err_exp}:
    tests/invalid/Mmakefile:
        As above.  Note that the actual problem this tests for has been
        fixed since we moved to use Unicode.

---
+++ tests/invalid/bug115.m
@@ -0,0 +1,16 @@
+% The unquoted name below caused a segmentation fault in rotd-2009-11-05.
+%
+:- module bug115.
+:- interface.
+
+:- pred unquoted_dèja(int::in) is semidet.
+
+:- pred 'quoted_dèja'(int::in) is semidet.
+
+% comment dèja
+
+:- implementation.
+
+unquoted_dèja(5).
+
+'quoted_dèja'(5).
