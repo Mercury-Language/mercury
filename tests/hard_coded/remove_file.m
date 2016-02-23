5cbbc2fd3418349268a5a1f6bf67cb2f08ea69c5
Julien Fischer <jfischer@opturion.com>
2016-02-23 14:45:19 +1100

    Fix the failure of hard_coded/remove_file in the Java grade.
    tests/hard_coded/remove_file.exp3:
        Add an alternative expected output for this one.  This current
        output is the best we can do while we still support Java 1.5
        and 1.6.
    
    tests/hard_coded/remove_file.m:
        Update syntax.

--- tests/hard_coded/remove_file.m
+++ tests/hard_coded/remove_file.m
@@ -10,54 +10,50 @@
 :- interface.
 :- use_module io.
 
-:- pred main(io__state::di, io__state::uo) is det.
+:- pred main(io.state::di, io.state::uo) is det.
 
 :- implementation.
 
-main -->
-    io__make_temp(Name),
-    %%%%%%% io__print("Temp file name = "), io__print(Name), io__nl,
-    io__tell(Name, TellResult),
+main(!IO) :-
+    io.make_temp(Name, !IO),
+    %%%%%%% io.print("Temp file name = ", !IO), io.print_line(Name, !IO),
+    io.tell(Name, TellResult, !IO),
     (
-        { TellResult = io__ok },
-        io__print("Just testing"), io__nl,
-        io__told,
-        io__remove_file(Name, RemoveResult),
+        TellResult = io.ok,
+        io.print_line("Just testing", !IO),
+        io.told(!IO),
+        io.remove_file(Name, RemoveResult, !IO),
         (
-            { RemoveResult = io__ok },
-            io__see(Name, SeeResult),
-            ( { SeeResult = io__ok } ->
-                io__print("Remove didn't remove file\n"),
-                io__set_exit_status(1)
-            ;
-                io__print("Test passed\n")
+            RemoveResult = io.ok,
+            io.see(Name, SeeResult, !IO),
+            ( if SeeResult = io.ok then
+                io.print("Remove didn't remove file\n", !IO),
+                io.set_exit_status(1, !IO)
+            else
+                io.print("Test passed\n", !IO)
             )
         ;
-            { RemoveResult = io__error(RemoveError) },
-            io__print("Remove failed: "),
-            { io__error_message(RemoveError, RemoveErrorMsg) },
-            io__print(RemoveErrorMsg),
-            io__nl,
-            io__set_exit_status(1)
+            RemoveResult = io.error(RemoveError),
+            io.print("Remove failed: ", !IO),
+            io.error_message(RemoveError, RemoveErrorMsg),
+            io.print_line(RemoveErrorMsg, !IO),
+            io.set_exit_status(1, !IO)
         ),
-        io__remove_file(Name, RemoveAgainResult),
+        io.remove_file(Name, RemoveAgainResult, !IO),
         (
-            { RemoveAgainResult = io__ok },
-            io__print("Second remove didn't report failure\n"),
-            io__set_exit_status(1)
+            RemoveAgainResult = io.ok,
+            io.print("Second remove didn't report failure\n", !IO),
+            io.set_exit_status(1, !IO)
         ;
-            { RemoveAgainResult = io__error(RemoveAgainError) },
-            io__print("Second remove failed, as expected: "),
-            { io__error_message(RemoveAgainError, RemoveAgainErrorMsg) },
-            io__print(RemoveAgainErrorMsg),
-            io__nl
+            RemoveAgainResult = io.error(RemoveAgainError),
+            io.print("Second remove failed, as expected: ", !IO),
+            io.error_message(RemoveAgainError, RemoveAgainErrorMsg),
+            io.print_line(RemoveAgainErrorMsg, !IO)
         )
     ;
-        { TellResult = io__error(TellError) },
-        io__print("Tell failed: "),
-        { io__error_message(TellError, TellErrorMsg) },
-        io__print(TellErrorMsg),
-        io__nl,
-        io__set_exit_status(1)
+        TellResult = io.error(TellError),
+        io.print("Tell failed: ", !IO),
+        io.error_message(TellError, TellErrorMsg),
+        io.print_line(TellErrorMsg, !IO),
+        io.set_exit_status(1, !IO)
     ).
-
