5fe4e36b71ed5c46c4e58416a3d58e835b9cdc5a
Julien Fischer <jfischer@opturion.com>
2016-04-26 15:38:28 +1000

    Enable invalid_purity/pragma_c_and_mercury.
    tests/invalid_purity/pragma_c_and_mercury.m:
        Fix the XXX regarding foreign code definitions that was preventing this
        from working.
    
        Fix the placement of the mode declarations.
    
    tests/invalid_purity/pragma_c_and_mercury.err_exp:
        Update line numbers in expected error.
    
        Also, the compiler no longer omits the spurious message about extra
        information being available if `-E' is given.
    
    tests/invalid_purity/Mmakefile:
        Enable the above test.

--- tests/invalid_purity/pragma_c_and_mercury.m
+++ tests/invalid_purity/pragma_c_and_mercury.m
@@ -2,10 +2,8 @@
 % vim: ts=4 sw=4 et ft=mercury
 %---------------------------------------------------------------------------%
 %
-% A test that we report an error for procedures defined with both
-% c_code and Mercury clauses, but without `pragma promise_pure'.
-
-% At the time of writing, we fail this test in IL and Java grades.
+% A test that we report an error for procedures defined with both foreign and
+% Mercury clauses, but without `pragma promise_pure'.
 
 :- module pragma_c_and_mercury.
 
@@ -13,14 +11,34 @@
 
 :- pred c_int_unify(int, int).
 
-:- implementation.
-
 :- mode c_int_unify(in, out) is det.
 
 :- mode c_int_unify(out, in) is det.
 
-:- pragma(c_code, c_int_unify(Int0::in, Int::out), "Int = Int0;").
+:- implementation.
 
-:- pragma(c_code, c_int_unify(Int::out, Int0::in), "Int = Int0;").
+:- pragma foreign_proc("C",
+    c_int_unify(Int0::in, Int::out), [promise_pure], "Int = Int0;").
+
+:- pragma foreign_proc("C",
+    c_int_unify(Int::out, Int0::in), [promise_pure], "Int = Int0;").
+
+:- pragma foreign_proc("Java",
+    c_int_unify(Int0::in, Int::out), [promise_pure], "Int = Int0;").
+
+:- pragma foreign_proc("Java",
+    c_int_unify(Int::out, Int0::in), [promise_pure], "Int = Int0;").
+
+:- pragma foreign_proc("C#",
+    c_int_unify(Int0::in, Int::out), [promise_pure], "Int = Int0;").
+
+:- pragma foreign_proc("C#",
+    c_int_unify(Int::out, Int0::in), [promise_pure], "Int = Int0;").
+
+:- pragma foreign_proc("Erlang",
+    c_int_unify(Int0::in, Int::out), [promise_pure], "Int = Int0").
+
+:- pragma foreign_proc("Erlang",
+    c_int_unify(Int::out, Int0::in), [promise_pure], "Int = Int0").
 
 c_int_unify(X, X).
