6f2f17359eba8b8626738a1b938ecca141eeb2c3
Julien Fischer <jfischer@opturion.com>
2016-05-26 13:02:27 +1000

    Improve test coverage for foreign_{decl,code} pragmas.
    tests/invalid/bad_foreign_code.{m,err_exp}:
    tests/invalid/bad_foreign_decl.{m,err_exp}:
        Test for the error messages caused by syntax errors.
    
    tests/invalid/Mmakefile:
         Include the new tests.

---
+++ tests/invalid/bad_foreign_code.m
@@ -0,0 +1,45 @@
+%---------------------------------------------------------------------------%
+% vim: ft=mercury ts=4 sw=4 et wm=0 tw=0
+%---------------------------------------------------------------------------%
+%
+% Test for error messages produced by syntax errors in 'foreign_code' pragmas.
+%
+
+:- module bad_foreign_code.
+:- interface.
+
+:- type foo ---> foo.
+
+:- implementation.
+
+    % Too few arguments.
+    %
+:- pragma foreign_code("C").
+
+    % Too many arguments.
+    %
+:- pragma foreign_code("C", "", "").
+
+    % Invalid foreign language.
+    %
+:- pragma foreign_code("InvalidLanguage", "").
+
+    % Code argument is not a string or include_file.
+    %
+:- pragma foreign_code("C", 2222).
+
+    % Argument of include_file is not a string.
+    %
+:- pragma foreign_code("C", include_file(3333)).
+
+    % Argument of include_file is the empty string.
+    %
+:- pragma foreign_code("C", include_file("")).
+
+    % Check that the correct contexts are associated with errors
+    % for each argument.
+    %
+:- pragma foreign_code(
+    "InvalidLanguage",
+    6666
+).
---
+++ tests/invalid/bad_foreign_decl.m
@@ -0,0 +1,54 @@
+%---------------------------------------------------------------------------%
+% vim: ft=mercury ts=4 sw=4 et wm=0 tw=0
+%---------------------------------------------------------------------------%
+%
+% Test for error messages produced by syntax errors in 'foreign_decl' pragmas.
+%
+
+:- module bad_foreign_decl.
+:- interface.
+
+:- type foo ---> foo.
+
+:- implementation.
+
+    % Too few arguments.
+    %
+:- pragma foreign_decl("C").
+
+    % Too many arguments.
+    %
+:- pragma foreign_decl("C", local, "", "").
+
+    % Invalid foreign language.
+    %
+:- pragma foreign_decl("InvalidLanguage", "").
+
+    % DeclCode argument is not a string or include_file.
+    %
+:- pragma foreign_decl("C", 1234).
+
+    % 'local' is misspelled.
+    %
+:- pragma foreign_decl("C", lcoal, "").
+
+    % 'local' is an integer.
+    %
+:- pragma foreign_decl("C", 4444, "").
+
+    % Argument of include file is not a string.
+    %
+:- pragma foreign_decl("C", include_file(5555)).
+
+    % Argument of include_file is an empty string.
+    % XXX The compiler currently accepts this.
+:- pragma foreign_decl("C", include_file("")).
+
+    % Check that the correct contexts are associated with errors
+    % for each argument.  (XXX they're currently not!)
+    %
+:- pragma foreign_decl(
+    "InvalidLanguage",
+    lcoal,
+    7777
+).
