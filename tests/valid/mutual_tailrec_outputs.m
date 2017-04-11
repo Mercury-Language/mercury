b3828ab66864fa626665e4554f737ca55a2fd177
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2017-04-12 08:37:08 +1000

    Fix programming style.

--- tests/valid/mutual_tailrec_outputs.m
+++ tests/valid/mutual_tailrec_outputs.m
@@ -1,3 +1,11 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+%
+% This test case triggered a problem in the code in mark_tail_calls.m
+% that checks for mutual recursion (part of the implementation of
+% --inline-linear-tail-rec-sccs): the compiler crashed, because
+% the callee and the caller had different number of parameters.
 
 :- module mutual_tailrec_outputs.
 
@@ -5,44 +13,35 @@
 
 :- import_module list.
 
-:- type a ---> a.
+:- type a
+    --->    a.
 
-% This case triggered a problem with mark_tail_calls when it checked for
-% mutual recursion, which was introduced with --inline-linear-tail-rec-sccs.
-% The compiler crashed when the number of parameters in the callee and
-% caller didn't match.
-
-:- pred my_condense(list(list(list(a))), list(a)).
-:- mode my_condense(in, out) is det.
+:- pred my_condense(list(list(list(a)))::in, list(a)::out) is det.
 
 :- implementation.
 
 my_condense([], []).
-my_condense([GList|Gs0], Gs) :-
+my_condense([GList | Gs0], Gs) :-
     ( if GList = [GList0], Gs0 = [] then
-        % fast path, avoid rebuilding list
+        % Fast path, avoid rebuilding list.
         Gs = GList0
     else
         my_condense0(GList, Gs0, Gs)
     ).
 
-:- pred my_condense0(list(list(a)), list(list(list(a))), list(a)).
-:- mode my_condense0(in, in, out) is det.
+:- pred my_condense0(list(list(a))::in, list(list(list(a)))::in, list(a)::out)
+    is det.
 
 my_condense0([], Gs1, Gs) :-
     my_condense(Gs1, Gs).
-
-my_condense0([G0|Gs0], Gs1, Gs) :-
+my_condense0([G0 | Gs0], Gs1, Gs) :-
     my_condense1(G0, Gs0, Gs1, Gs).
 
-:- pred my_condense1(list(a), list(list(a)), list(list(list(a))), list(a)).
-:- mode my_condense1(in, in, in, out) is det.
+:- pred my_condense1(list(a)::in, list(list(a))::in, list(list(list(a)))::in,
+    list(a)::out) is det.
 
 my_condense1([], Gs1, Gs2, Gs) :-
     my_condense0(Gs1, Gs2, Gs).
-
-my_condense1([G|Gs0], Gs1, Gs2, Gs) :-
+my_condense1([G | Gs0], Gs1, Gs2, Gs) :-
     my_condense1(Gs0, Gs1, Gs2, Gs3),
-    Gs = [G|Gs3].
-
-
+    Gs = [G | Gs3].
