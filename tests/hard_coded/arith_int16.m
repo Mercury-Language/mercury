509e2156bc50fa46ca33544796b91f474971a963
Julien Fischer <juliensf@gmail.com>
2017-08-21 22:02:22 +1000

    Add tests of basic arithmetic for 16-bit integers.
    tests/hard_coded/arith_int16.{m,exp}:
    tests/hard_coded/arith_uint16.{m,exp}:
    tests/hard_coded/Mmakefile:
         As above.

---
+++ tests/hard_coded/arith_int16.m
@@ -0,0 +1,117 @@
+%---------------------------------------------------------------------------%
+% vim: ft=mercury ts=4 sw=4 et wm=0 tw=0
+%---------------------------------------------------------------------------%
+
+% Test arithmetic operations for signed 16-bit integers.
+
+:- module arith_int16.
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is cc_multi.
+
+%---------------------------------------------------------------------------%
+%---------------------------------------------------------------------------%
+
+:- implementation.
+
+:- import_module int16.
+
+:- import_module exception.
+:- import_module list.
+:- import_module string.
+
+%---------------------------------------------------------------------------%
+
+main(!IO) :-
+    run_unop_test(int16.(+), "+", !IO),
+    io.nl(!IO),
+    run_unop_test(int16.(-), "-", !IO),
+    io.nl(!IO),
+    %run_unop_test(int16.abs, "abs", !IO),
+    %io.nl(!IO),
+    run_binop_test((func(X, Y) = X + Y), "+", !IO),
+    io.nl(!IO),
+    run_binop_test((func(X, Y) = X - Y), "-", !IO),
+    io.nl(!IO),
+    run_binop_test((func(X, Y) = X * Y), "*", !IO),
+    io.nl(!IO),
+    run_binop_test((func(X, Y) = X / Y), "/", !IO),
+    io.nl(!IO),
+    run_binop_test((func(X, Y) = X rem Y), "rem", !IO).
+
+%---------------------------------------------------------------------------%
+
+:- pred run_unop_test((func(int16) = int16)::in, string::in,
+    io::di, io::uo) is cc_multi.
+
+run_unop_test(UnOpFunc, Desc, !IO) :-
+    io.format("*** Test unary operation '%s' ***\n\n", [s(Desc)], !IO),
+    As = numbers,
+    list.foldl(run_unop_test_2(UnOpFunc, Desc), As, !IO).
+
+:- pred run_unop_test_2((func(int16) = int16)::in, string::in, int16::in,
+    io::di, io::uo) is cc_multi.
+
+run_unop_test_2(UnOpFunc, Desc, A, !IO) :-
+    ( try []
+        Result0 = UnOpFunc(A)
+    then
+        ResultStr = int16_to_string(Result0)
+    catch_any _ ->
+        ResultStr = "<<exception>>"
+    ),
+    io.format("%s %s = %s\n",
+        [s(Desc), s(int16_to_string(A)), s(ResultStr)], !IO).
+
+%---------------------------------------------------------------------------%
+
+:- pred run_binop_test((func(int16, int16) = int16)::in, string::in,
+    io::di, io::uo) is cc_multi.
+
+run_binop_test(BinOpFunc, Desc, !IO) :-
+    io.format("*** Test binary operation '%s' ***\n\n", [s(Desc)], !IO),
+    As = numbers,
+    Bs = numbers,
+    list.foldl(run_binop_test_2(BinOpFunc, Desc, Bs), As, !IO).
+
+:- pred run_binop_test_2((func(int16, int16) = int16)::in, string::in,
+    list(int16)::in, int16::in, io::di, io::uo) is cc_multi.
+
+run_binop_test_2(BinOpFunc, Desc, Bs, A, !IO) :-
+    list.foldl(run_binop_test_3(BinOpFunc, Desc, A), Bs, !IO).
+
+:- pred run_binop_test_3((func(int16, int16) = int16)::in, string::in,
+    int16::in, int16::in, io::di, io::uo) is cc_multi.
+
+run_binop_test_3(BinOpFunc, Desc, A, B, !IO) :-
+    ( try []
+        Result0 = BinOpFunc(A, B)
+    then
+        ResultStr = int16_to_string(Result0)
+    catch_any _ ->
+        ResultStr = "<<exception>>"
+    ),
+    io.format("%s %s %s = %s\n",
+        [s(int16_to_string(A)), s(Desc), s(int16_to_string(B)), s(ResultStr)], !IO).
+
+:- func numbers = list(int16).
+
+numbers = [
+    -32_768_i16,
+    -128_i16,
+    0_i16,
+    1_i16,
+    2_i16,
+    8_i16,
+    10_i16,
+    16_i16,
+    127_i16,
+    255_i16,
+    32_767_i16
+].
+
+%---------------------------------------------------------------------------%
+:- end_module arith_int16.
+%---------------------------------------------------------------------------%
---
+++ tests/hard_coded/arith_uint16.m
@@ -0,0 +1,82 @@
+%---------------------------------------------------------------------------%
+% vim: ft=mercury ts=4 sw=4 et wm=0 tw=0
+%---------------------------------------------------------------------------%
+
+% Test arithmetic operations for unsigned 16-bit integers.
+
+:- module arith_uint16.
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is cc_multi.
+
+%---------------------------------------------------------------------------%
+%---------------------------------------------------------------------------%
+
+:- implementation.
+
+:- import_module uint16.
+
+:- import_module exception.
+:- import_module list.
+:- import_module string.
+
+%---------------------------------------------------------------------------%
+
+main(!IO) :-
+    run_binop_test((func(X, Y) = X + Y), "+", !IO),
+    io.nl(!IO),
+    run_binop_test((func(X, Y) = X - Y), "-", !IO),
+    io.nl(!IO),
+    run_binop_test((func(X, Y) = X * Y), "*", !IO),
+    io.nl(!IO),
+    run_binop_test((func(X, Y) = X / Y), "/", !IO),
+    io.nl(!IO),
+    run_binop_test((func(X, Y) = X rem Y), "rem", !IO).
+
+:- pred run_binop_test((func(uint16, uint16) = uint16)::in, string::in,
+    io::di, io::uo) is cc_multi.
+
+run_binop_test(BinOpFunc, Desc, !IO) :-
+    io.format("*** Test binary operation '%s' ***\n\n", [s(Desc)], !IO),
+    As = numbers,
+    Bs = numbers,
+    list.foldl(run_binop_test_2(BinOpFunc, Desc, Bs), As, !IO).
+
+:- pred run_binop_test_2((func(uint16, uint16) = uint16)::in, string::in,
+    list(uint16)::in, uint16::in, io::di, io::uo) is cc_multi.
+
+run_binop_test_2(BinOpFunc, Desc, Bs, A, !IO) :-
+    list.foldl(run_binop_test_3(BinOpFunc, Desc, A), Bs, !IO).
+
+:- pred run_binop_test_3((func(uint16, uint16) = uint16)::in, string::in,
+    uint16::in, uint16::in, io::di, io::uo) is cc_multi.
+
+run_binop_test_3(BinOpFunc, Desc, A, B, !IO) :-
+    ( try []
+        Result0 = BinOpFunc(A, B)
+    then
+        ResultStr = uint16_to_string(Result0)
+    catch_any _ ->
+        ResultStr = "<<exception>>"
+    ),
+    io.format("%s %s %s = %s\n",
+        [s(uint16_to_string(A)), s(Desc), s(uint16_to_string(B)), s(ResultStr)], !IO).
+
+:- func numbers = list(uint16).
+
+numbers = [
+    0_u16,
+    1_u16,
+    2_u16,
+    8_u16,
+    10_u16,
+    16_u16,
+    255_u16,
+    65_535_u16
+].
+
+%---------------------------------------------------------------------------%
+:- end_module arith_uint16.
+%---------------------------------------------------------------------------%
