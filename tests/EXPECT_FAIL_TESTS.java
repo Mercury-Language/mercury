d7e3991c68c39f8cca1342bdfeeadb841a88491d
Julien Fischer <juliensf@gmail.com>
2017-10-20 00:58:21 -0400

    Expect some test case failures in the Java grade.
    tests/EXPECT_FAIL_TESTS.java:
         Expect these tests to fail -- most are due to the RTTI
         implementation used by the java grade being incomplete.

---
+++ tests/EXPECT_FAIL_TESTS.java
@@ -0,0 +1,11 @@
+hard_coded/ee_dummy
+hard_coded/eqv_type_bug
+hard_coded/functor_ho_inst_excp
+hard_coded/functor_ho_inst_excp_2
+hard_coded/init_excp
+hard_coded/mutable_excp
+hard_coded/null_char
+hard_coded/pretty_printing
+hard_coded/tuple_test
+hard_coded/write_binary
+hard_coded/write_xml
