fa0cf1fd25e6c18370c7db3e4520b50073915357
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2018-01-29 23:57:27 +1100

    Add the not-yet-passing test case for Mantis 441.

---
+++ tests/hard_coded/bug441.m
@@ -0,0 +1,20 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+
+:- module bug441.
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module bug441_sub.
+
+main(!IO) :- do_fmain(hello, !IO).
+
+:- func hello : fmain `with_inst` fmain.
+
+hello(FIO) = printLine("Hello World!", FIO).
