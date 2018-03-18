1eb261808a73f8217cee97da284b729b462a222c
Julien Fischer <juliensf@gmail.com>
2018-03-18 14:27:41 +1100

    Delete the old type_ctor_rep definitions for the Java grade.
    library/private_builtin.m:
         Delete the copy of the type_ctor_rep constants defined here.
         The replacements were added to the runtime several months ago.

--- library/private_builtin.m
+++ library/private_builtin.m
@@ -1838,69 +1838,6 @@ const MR_FA_TypeInfo_Struct1 ML_type_info_for_list_of_pseudo_type_info = {
         // XXX stub only
     }
 
-    // TypeCtorRep constants
-    //
-    // NOTE: the actual definitions of these constants are now in
-    // java/runtime/TypeCtorRep.java.  These copies are for backwards
-    // compatibility with existing code -- they will eventually be removed.
-    public static final int MR_TYPECTOR_REP_ENUM                    = 0;
-    public static final int MR_TYPECTOR_REP_ENUM_USEREQ             = 1;
-    public static final int MR_TYPECTOR_REP_DU                      = 2;
-    public static final int MR_TYPECTOR_REP_DU_USEREQ               = 3;
-    public static final int MR_TYPECTOR_REP_NOTAG                   = 4;
-    public static final int MR_TYPECTOR_REP_NOTAG_USEREQ            = 5;
-    public static final int MR_TYPECTOR_REP_EQUIV                   = 6;
-    public static final int MR_TYPECTOR_REP_FUNC                    = 7;
-    public static final int MR_TYPECTOR_REP_INT                     = 8;
-    public static final int MR_TYPECTOR_REP_UINT                    = 9;
-    public static final int MR_TYPECTOR_REP_CHAR                    = 10;
-    public static final int MR_TYPECTOR_REP_FLOAT                   = 11;
-    public static final int MR_TYPECTOR_REP_STRING                  = 12;
-    public static final int MR_TYPECTOR_REP_PRED                    = 13;
-    public static final int MR_TYPECTOR_REP_SUBGOAL                 = 14;
-    public static final int MR_TYPECTOR_REP_VOID                    = 15;
-    public static final int MR_TYPECTOR_REP_C_POINTER               = 16;
-    public static final int MR_TYPECTOR_REP_TYPEINFO                = 17;
-    public static final int MR_TYPECTOR_REP_TYPECLASSINFO           = 18;
-    public static final int MR_TYPECTOR_REP_ARRAY                   = 19;
-    public static final int MR_TYPECTOR_REP_SUCCIP                  = 20;
-    public static final int MR_TYPECTOR_REP_HP                      = 21;
-    public static final int MR_TYPECTOR_REP_CURFR                   = 22;
-    public static final int MR_TYPECTOR_REP_MAXFR                   = 23;
-    public static final int MR_TYPECTOR_REP_REDOFR                  = 24;
-    public static final int MR_TYPECTOR_REP_REDOIP                  = 25;
-    public static final int MR_TYPECTOR_REP_TRAIL_PTR               = 26;
-    public static final int MR_TYPECTOR_REP_TICKET                  = 27;
-    public static final int MR_TYPECTOR_REP_NOTAG_GROUND            = 28;
-    public static final int MR_TYPECTOR_REP_NOTAG_GROUND_USEREQ     = 29;
-    public static final int MR_TYPECTOR_REP_EQUIV_GROUND            = 30;
-    public static final int MR_TYPECTOR_REP_TUPLE                   = 31;
-    public static final int MR_TYPECTOR_REP_RESERVED_ADDR           = 32;
-    public static final int MR_TYPECTOR_REP_RESERVED_ADDR_USEREQ    = 33;
-    public static final int MR_TYPECTOR_REP_TYPECTORINFO            = 34;
-    public static final int MR_TYPECTOR_REP_BASETYPECLASSINFO       = 35;
-    public static final int MR_TYPECTOR_REP_TYPEDESC                = 36;
-    public static final int MR_TYPECTOR_REP_TYPECTORDESC            = 37;
-    public static final int MR_TYPECTOR_REP_FOREIGN                 = 38;
-    public static final int MR_TYPECTOR_REP_REFERENCE               = 39;
-    public static final int MR_TYPECTOR_REP_STABLE_C_POINTER        = 40;
-    public static final int MR_TYPECTOR_REP_STABLE_FOREIGN          = 41;
-    public static final int MR_TYPECTOR_REP_PSEUDOTYPEDESC          = 42;
-    public static final int MR_TYPECTOR_REP_DUMMY                   = 43;
-    public static final int MR_TYPECTOR_REP_BITMAP                  = 44;
-    public static final int MR_TYPECTOR_REP_FOREIGN_ENUM            = 45;
-    public static final int MR_TYPECTOR_REP_FOREIGN_ENUM_USEREQ     = 46;
-    public static final int MR_TYPECTOR_REP_INT8                    = 47;
-    public static final int MR_TYPECTOR_REP_UINT8                   = 48;
-    public static final int MR_TYPECTOR_REP_INT16                   = 49;
-    public static final int MR_TYPECTOR_REP_UINT16                  = 50;
-    public static final int MR_TYPECTOR_REP_INT32                   = 51;
-    public static final int MR_TYPECTOR_REP_UINT32                  = 52;
-    public static final int MR_TYPECTOR_REP_INT64                   = 53;
-    public static final int MR_TYPECTOR_REP_UINT64                  = 54;
-    public static final int MR_TYPECTOR_REP_UNKNOWN                 = 55;
-    public static final int MR_TYPECTOR_REP_MAX                     = 56;
-
     public static final int MR_SECTAG_NONE              = 0;
     public static final int MR_SECTAG_NONE_DIRECT_ARG   = 1;
     public static final int MR_SECTAG_LOCAL             = 2;
