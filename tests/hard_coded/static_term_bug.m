Mercury-Language/mercury

commit f6e2290031d4318de693a5d377e602f02e09ed1e

Harmonize more of {,ml_}unify_gen_{de,}construct.m.
compiler/goal_form.m:
    Add some predicates for checking that terms with no_tag and direct_arg_tag
    cons_ids have exactly one argument, and return that argument.

    Add some predicates for checking that from_ground_term_construct
    scope goals, and the conjuncts inside them, have the expected shape,
    and return information about what is inside that shape.

compiler/hlds_data.m:
    Put the various cons_tags into a logical order.

compiler/unify_gen_construct.m:
compiler/unify_gen_deconstruct.m:
compiler/ml_unify_gen_construct.m:
compiler/ml_unify_gen_deconstruct.m:
    Use calls to the new predicates in goal_form.m to replace large
    chunks of boring code where possible.

    Factor out some other chunks of code as well.

    Put the switches on cons_tags into the same order as the new order
    of the cons_tags in hlds_data.m where possible.

    Use names based on LHSVar/RHSVar, rather than Var/ArgVar, in many more
    places.

compiler/goal_form.m
@@ -22,6 +22,7 @@
 :- import_module hlds.hlds_module.
 :- import_module hlds.hlds_pred.
 :- import_module parse_tree.
+:- import_module parse_tree.prog_data.
 :- import_module parse_tree.set_of_var.
 
 :- import_module bool.
@@ -195,12 +196,51 @@
 :- func goal_expr_has_subgoals(hlds_goal_expr) = has_subgoals.
 
 %-----------------------------------------------------------------------------%
+
+    % Insist on both given lists having exactly one element,
+    % and return those elements. The lists are intended to contain
+    % the arguments and unify modes respectively of a function symbol
+    % whose representation is no_tag or direct_arg_tag, which means
+    % that its arity must be exactly one.
+    %
+:- pred get_notag_or_direct_arg_arg_mode(list(Arg)::in, list(UM)::in,
+    Arg::out, UM::out) is det.
+
+    % A version of the above without the modes.
+    %
+:- pred get_notag_or_direct_arg_arg(list(Arg)::in, Arg::out) is det.
+
+%-----------------------------------------------------------------------------%
+
+:- type is_termvar_needed
+    --->    termvar_is_not_needed
+    ;       termvar_is_needed.
+
+    % Given the TermVar and the subgoal of a from_ground_term_construct scope,
+    % return an indication of whether the termvar is needed outside the scope,
+    % the conjuncts inside the scope, and the conjunction's goal_info.
+    % The conjuncts should all be processable (without an abort) by
+    % get_from_ground_term_construct_conjunct_info.
+    %
+:- pred get_from_ground_term_construct_info(prog_var::in, hlds_goal::in,
+    is_termvar_needed::out, list(hlds_goal)::out, hlds_goal_info::out) is det.
+
+    % get_from_ground_term_construct_conjunct_info(Goal,
+    %   LHSVar, ConsId, RHSVars, GoalInfo):
+    %
+    % A conjunct inside a from_ground_term_construct scope *must* be a
+    % construction unification; return its details.
+    % 
+:- pred get_from_ground_term_construct_conjunct_info(hlds_goal::in,
+    prog_var::out, cons_id::out, list(prog_var)::out, hlds_goal_info::out)
+    is det.
+
+%-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
 
 :- implementation.
 
 :- import_module hlds.code_model.
-:- import_module parse_tree.prog_data.
 :- import_module parse_tree.prog_data_foreign.
 :- import_module parse_tree.prog_data_pragma.
 :- import_module transform_hlds.
@@ -1213,6 +1253,71 @@ goal_expr_has_subgoals(GoalExpr) = HasSubGoals :-
         ; ShortHand = bi_implication(_, _)
         ),
         HasSubGoals = has_subgoals
+    ).
+
+%-----------------------------------------------------------------------------%
+
+get_notag_or_direct_arg_arg_mode(RHSVars, ArgModes, RHSVar, ArgMode) :-
+    ( if
+        RHSVars = [RHSVarPrime],
+        ArgModes = [ArgModePrime]
+    then
+        RHSVar = RHSVarPrime,
+        ArgMode = ArgModePrime
+    else
+        unexpected($pred, "arity != 1)")
+    ).
+
+get_notag_or_direct_arg_arg(RHSVars, RHSVar) :-
+    ( if RHSVars = [RHSVarPrime] then
+        RHSVar = RHSVarPrime
+    else
+        unexpected($pred, "arity != 1)")
+    ).
+
+%-----------------------------------------------------------------------------%
+
+get_from_ground_term_construct_info(TermVar, Goal,
+         TermVarIsNeeded, Conjuncts, GoalInfo) :-
+    Goal = hlds_goal(GoalExpr, GoalInfo),
+    NonLocals = goal_info_get_nonlocals(GoalInfo),
+    set_of_var.to_sorted_list(NonLocals, NonLocalList),
+    (
+        (
+            NonLocalList = [],
+            TermVarIsNeeded = termvar_is_not_needed
+        ;
+            NonLocalList = [NonLocal],
+            ( if NonLocal = TermVar then
+                TermVarIsNeeded = termvar_is_needed
+            else
+                unexpected($pred, "unexpected nonlocal")
+            )
+        ),
+        ( if GoalExpr = conj(plain_conj, ConjunctsPrime) then
+            Conjuncts = ConjunctsPrime
+        else
+            unexpected($pred, "unexpected nonlocal")
+        )
+    ;
+        NonLocalList = [_, _ | _],
+        unexpected($pred, "unexpected nonlocals")
+    ).
+
+get_from_ground_term_construct_conjunct_info(Goal, LHSVar, ConsId, RHSVars,
+        GoalInfo) :-
+    Goal = hlds_goal(GoalExpr, GoalInfo),
+    ( if
+        GoalExpr = unify(_, _, _, Unify, _),
+        Unify = construct(LHSVarPrime, ConsIdPrime, RHSVarsPrime,
+            _, _, _, SubInfo),
+        SubInfo = no_construct_sub_info
+    then
+        LHSVar = LHSVarPrime,
+        ConsId = ConsIdPrime,
+        RHSVars = RHSVarsPrime
+    else
+        unexpected($pred, "unexpected goal as fgt_construct conjunct")
     ).
 
 %-----------------------------------------------------------------------------%

compiler/hlds_data.m
@@ -55,88 +55,33 @@
     % tagged) pointer to memory on the heap.
     %
 :- type cons_tag
-    --->    string_tag(string)
-            % Strings are represented using the MR_string_const() macro;
-            % in the current implementation, Mercury strings are represented
-            % just as C null-terminated strings.
+    % The kinds of constants that may appear in user code.
+    --->    int_tag(int_tag)
+            % This means the constant is represented as a word containing
+            % the specified integer value. This is used for enumerations and
+            % character constants, as well as for integer constants of every
+            % possible size and signedness.
 
     ;       float_tag(float)
             % Floats are represented using the MR_float_to_word(),
             % MR_word_to_float(), and MR_float_const() macros. The default
             % implementation of these is to use boxed double-precision floats.
 
-    ;       int_tag(int_tag)
-            % This means the constant is represented as a word containing
-            % the specified integer value. This is used for enumerations and
-            % character constants, as well as for integer constants of every
-            % possible size and signedness.
+    ;       string_tag(string)
+            % Strings are represented using the MR_string_const() macro;
+            % in the current implementation, Mercury strings are represented
+            % just as C null-terminated strings.
 
     ;       foreign_tag(foreign_language, string)
-            % This means the constant is represented by the string which is
+            % This means the constant is represented by the string, which is
             % embedded directly in the target language. This is used for
-            % foreign enumerations, i.e. those enumeration types that are the
-            % subject of a foreign_enum pragma.
+            % foreign enumerations, i.e. those enumeration types that are
+            % the subject of a foreign_enum pragma.
 
-    ;       closure_tag(pred_id, proc_id, lambda_eval_method)
-            % Higher-order pred closures tags. These are represented as
-            % a pointer to an argument vector. For closures with
-            % lambda_eval_method `normal', the first two words of the argument
-            % vector hold the number of args and the address of the procedure
-            % respectively. The remaining words hold the arguments.
-
-    ;       type_ctor_info_tag(module_name, string, arity)
-            % This is how we refer to type_ctor_info structures represented
-            % as global data. The args are the name of the module the type
-            % is defined in, and the name of the type, and its arity.
-
-    ;       base_typeclass_info_tag(module_name, class_id, string)
-            % This is how we refer to base_typeclass_info structures
-            % represented as global data. The first argument is the name
-            % of the module containing the instance declaration, the second
-            % is the class name and arity, while the third is the string which
-            % uniquely identifies the instance declaration (it is made from
-            % the type of the arguments to the instance decl).
-
-    ;       type_info_const_tag(int)
-    ;       typeclass_info_const_tag(int)
-
-    ;       ground_term_const_tag(int, cons_tag)
-
-    ;       tabling_info_tag(pred_id, proc_id)
-            % This is how we refer to the global structures containing
-            % tabling pointer variables and related data. The word just
-            % contains the address of the global struct.
-
-    ;       deep_profiling_proc_layout_tag(pred_id, proc_id)
-            % This is for constants representing procedure descriptions for
-            % deep profiling.
-
-    ;       table_io_entry_tag(pred_id, proc_id)
-            % This is for constants representing the structure that allows us
-            % to decode the contents of the answer block containing the
-            % headvars of I/O primitives.
-
-    ;       single_functor_tag
-            % This is for types with a single functor (and possibly also some
-            % constants represented using reserved addresses -- see below).
-            % For these types, we don't need any tags. We just store a pointer
-            % to the argument vector.
-
-    ;       unshared_tag(ptag)
-            % This is for constants or functors which can be distinguished
-            % with just a primary tag. An "unshared" tag is one which fits
-            % on the bottom of a pointer (i.e. two bits for 32-bit
-            % architectures, or three bits for 64-bit architectures), and is
-            % used for just one functor. For constants we store a tagged zero,
-            % for functors we store a tagged pointer to the argument vector.
-
-    ;       direct_arg_tag(ptag)
-            % This is for functors which can be distinguished with just a
-            % primary tag. The primary tag says which of the type's functors
-            % (which must have arity 1) this word represents. However, the
-            % body of the word is not a pointer to a cell holding the argument;
-            % it IS the value of that argument, which must be an untagged
-            % pointer to a cell.
+    ;       dummy_tag
+            % This is for constants that are the only function symbol in their
+            % type. Such function symbols contain no information, and thus
+            % do not need to be represented at all.
 
     ;       shared_local_tag_no_args(ptag, local_sectag, lsectag_mask)
             % This is for constants in types that also have non-constants.
@@ -156,15 +101,56 @@
             % symbol, it is possible for this not to be the case. In that case,
             % the local secondary tag will occupy zero bits.
 
-    ;       shared_local_tag_with_args(ptag, local_sectag)
-            % As the name implies this cons_id is a variant of
-            % shared_local_tag_no_args that is intended for function symbols
-            % that *do* have arguments, arguments that fit into a single word
-            % *after* the primary and the local secondary tag.
-            % If a primary tag value has any such cons_ids allocated for it,
-            % then the bits in a word after the primary tag may include
-            % these arguments, so accessing the secondary tag requires masking
-            % off all the non-sectag bits.
+    % The kinds of constants that cannot appear in user code,
+    % being generated only inside the compiler.
+
+    ;       ground_term_const_tag(int, cons_tag)
+
+    ;       type_info_const_tag(int)
+    ;       typeclass_info_const_tag(int)
+
+    ;       type_ctor_info_tag(module_name, string, arity)
+            % This is how we refer to type_ctor_info structures represented
+            % as global data. The args are the name of the module the type
+            % is defined in, and the name of the type, and its arity.
+
+    ;       base_typeclass_info_tag(module_name, class_id, string)
+            % This is how we refer to base_typeclass_info structures
+            % represented as global data. The first argument is the name
+            % of the module containing the instance declaration, the second
+            % is the class name and arity, while the third is the string which
+            % uniquely identifies the instance declaration (it is made from
+            % the type of the arguments to the instance decl).
+
+    ;       deep_profiling_proc_layout_tag(pred_id, proc_id)
+            % This is for constants representing procedure descriptions for
+            % deep profiling.
+
+    ;       tabling_info_tag(pred_id, proc_id)
+            % This is how we refer to the global structures containing
+            % tabling pointer variables and related data. The word just
+            % contains the address of the global struct.
+
+    ;       table_io_entry_tag(pred_id, proc_id)
+            % This is for constants representing the structure that allows us
+            % to decode the contents of the answer block containing the
+            % headvars of I/O primitives.
+
+    % The kinds of non-constants that may appear in user code.
+
+    ;       single_functor_tag
+            % This is for types with a single functor (and possibly also some
+            % constants represented using reserved addresses -- see below).
+            % For these types, we don't need any tags. We just store a pointer
+            % to the argument vector.
+
+    ;       unshared_tag(ptag)
+            % This is for constants or functors which can be distinguished
+            % with just a primary tag. An "unshared" tag is one which fits
+            % on the bottom of a pointer (i.e. two bits for 32-bit
+            % architectures, or three bits for 64-bit architectures), and is
+            % used for just one functor. For constants we store a tagged zero,
+            % for functors we store a tagged pointer to the argument vector.
 
     ;       shared_remote_tag(ptag, remote_sectag)
             % This is for functors or constants which cannot be distinguished
@@ -177,15 +163,38 @@
             % the memory cell containing the arguments, and distinguishes
             % the several functors that all share the same primary tag value.
 
-    ;       dummy_tag
-            % This is for constants that are the only function symbol in their
-            % type. Such function symbols contain no information, and thus
-            % do not need to be represented at all.
+    ;       shared_local_tag_with_args(ptag, local_sectag)
+            % As the name implies this cons_id is a variant of
+            % shared_local_tag_no_args that is intended for function symbols
+            % that *do* have arguments, arguments that fit into a single word
+            % *after* the primary and the local secondary tag.
+            % If a primary tag value has any such cons_ids allocated for it,
+            % then the bits in a word after the primary tag may include
+            % these arguments, so accessing the secondary tag requires masking
+            % off all the non-sectag bits.
 
-    ;       no_tag.
+    ;       no_tag
             % This is for types with a single functor of arity one. In this
             % case, we don't need to store the functor, and instead we store
             % the argument directly.
+
+    ;       direct_arg_tag(ptag)
+            % This is for functors which can be distinguished with just a
+            % primary tag. The primary tag says which of the type's functors
+            % (which must have arity 1) this word represents. However, the
+            % body of the word is not a pointer to a cell holding the argument;
+            % it IS the value of that argument, which must be an untagged
+            % pointer to a cell.
+
+    % The kinds of non-constants that cannot appear in user code,
+    % being generated only inside the compiler.
+
+    ;       closure_tag(pred_id, proc_id, lambda_eval_method).
+            % Higher-order pred closures tags. These are represented as
+            % a pointer to an argument vector. For closures with
+            % lambda_eval_method `normal', the first two words of the argument
+            % vector hold the number of args and the address of the procedure
+            % respectively. The remaining words hold the arguments.
 
 :- inst memory_cell_tag for cons_tag/0
     --->    single_functor_tag

compiler/ml_unify_gen_deconstruct.m
@@ -73,6 +73,7 @@
 :- import_module backend_libs.builtin_ops.
 :- import_module check_hlds.
 :- import_module check_hlds.type_util.
+:- import_module hlds.goal_form.
 :- import_module hlds.hlds_module.
 :- import_module hlds.hlds_pred.
 :- import_module libs.
@@ -103,13 +104,13 @@ ml_generate_deconstruction_unification(LHSVar, ConsId, RHSVars, ArgModes,
     (
         CanFail = can_fail,
         ExpectedCodeModel = model_semi,
-        ml_gen_semi_deconstruct(LHSVar, ConsId, RHSVars, ArgModes, Context,
-            Defns, UnifyStmts, !Info)
+        ml_generate_semi_deconstruction(LHSVar, ConsId, RHSVars, ArgModes,
+            Context, Defns, UnifyStmts, !Info)
     ;
         CanFail = cannot_fail,
         ExpectedCodeModel = model_det,
-        ml_gen_det_deconstruct(LHSVar, ConsId, RHSVars, ArgModes, Context,
-            Defns, UnifyStmts, !Info)
+        ml_generate_det_deconstruction(LHSVar, ConsId, RHSVars, ArgModes,
+            Context, Defns, UnifyStmts, !Info)
     ),
     (
         % Note that we can deallocate a cell even if the unification fails;
@@ -149,17 +150,17 @@ ml_generate_deconstruction_unification(LHSVar, ConsId, RHSVars, ArgModes,
     %           ...
     %       }
     %
-:- pred ml_gen_semi_deconstruct(prog_var::in, cons_id::in, list(prog_var)::in,
-    list(unify_mode)::in, prog_context::in,
+:- pred ml_generate_semi_deconstruction(prog_var::in, cons_id::in,
+    list(prog_var)::in, list(unify_mode)::in, prog_context::in,
     list(mlds_local_var_defn)::out, list(mlds_stmt)::out,
     ml_gen_info::in, ml_gen_info::out) is det.
 
-ml_gen_semi_deconstruct(LHSVar, ConsId, RHSVars, ArgModes, Context,
+ml_generate_semi_deconstruction(LHSVar, ConsId, RHSVars, ArgModes, Context,
         Defns, Stmts, !Info) :-
     ml_generate_test_var_has_cons_id(LHSVar, ConsId, TestRval, !Info),
     ml_gen_set_success(TestRval, Context, SetTestResultStmt, !Info),
     ml_gen_test_success(SucceededRval, !Info),
-    ml_gen_det_deconstruct(LHSVar, ConsId, RHSVars, ArgModes, Context,
+    ml_generate_det_deconstruction(LHSVar, ConsId, RHSVars, ArgModes, Context,
         Defns, DetDeconstructStmts, !Info),
     (
         DetDeconstructStmts = [],
@@ -190,19 +191,19 @@ ml_gen_semi_deconstruct(LHSVar, ConsId, RHSVars, ArgModes, Context,
     %       A2 = arg(X, f, 2);
     %       ...
     %
-:- pred ml_gen_det_deconstruct(prog_var::in, cons_id::in, list(prog_var)::in,
-    list(unify_mode)::in, prog_context::in,
+:- pred ml_generate_det_deconstruction(prog_var::in, cons_id::in,
+    list(prog_var)::in, list(unify_mode)::in, prog_context::in,
     list(mlds_local_var_defn)::out, list(mlds_stmt)::out,
     ml_gen_info::in, ml_gen_info::out) is det.
 
-ml_gen_det_deconstruct(LHSVar, ConsId, RHSVars, ArgModes, Context,
+ml_generate_det_deconstruction(LHSVar, ConsId, RHSVars, ArgModes, Context,
         Defns, Stmts, !Info) :-
     ml_cons_id_to_tag(!.Info, ConsId, ConsTag),
     (
-        ( ConsTag = string_tag(_String)
-        ; ConsTag = int_tag(_IntTag)
+        ( ConsTag = int_tag(_)
+        ; ConsTag = float_tag(_)
+        ; ConsTag = string_tag(_)
         ; ConsTag = foreign_tag(_, _)
-        ; ConsTag = float_tag(_Float)
         ; ConsTag = dummy_tag
         ; ConsTag = shared_local_tag_no_args(_, _, _)
         ),
@@ -211,45 +212,33 @@ ml_gen_det_deconstruct(LHSVar, ConsId, RHSVars, ArgModes, Context,
         Defns = [],
         Stmts = []
     ;
-        ( ConsTag = closure_tag(_, _, _)
+        ( ConsTag = ground_term_const_tag(_, _)
         ; ConsTag = type_ctor_info_tag(_, _, _)
         ; ConsTag = base_typeclass_info_tag(_, _, _)
         ; ConsTag = type_info_const_tag(_)
         ; ConsTag = typeclass_info_const_tag(_)
-        ; ConsTag = ground_term_const_tag(_, _)
-        ; ConsTag = tabling_info_tag(_, _)
         ; ConsTag = deep_profiling_proc_layout_tag(_, _)
+        ; ConsTag = tabling_info_tag(_, _)
         ; ConsTag = table_io_entry_tag(_, _)
+        ; ConsTag = closure_tag(_, _, _)
         ),
         unexpected($pred, "unexpected tag")
     ;
         ConsTag = no_tag,
-        ( if
-            RHSVars = [RHSVar],
-            ArgModes = [ArgMode]
-        then
-            ml_gen_dynamic_deconstruct_no_tag(!.Info, LHSVar, RHSVar, ArgMode,
-                Context, Stmts),
-            Defns = []
-        else
-            unexpected($pred, "no_tag: arity != 1")
-        )
+        get_notag_or_direct_arg_arg_mode(RHSVars, ArgModes, RHSVar, ArgMode),
+        ml_gen_dynamic_deconstruct_no_tag(!.Info, LHSVar, RHSVar, ArgMode,
+            Context, Stmts),
+        Defns = []
     ;
         ConsTag = direct_arg_tag(Ptag),
-        ( if
-            RHSVars = [RHSVar],
-            ArgModes = [ArgMode]
-        then
-            ml_gen_dynamic_deconstruct_direct_arg(!.Info, Ptag, LHSVar, RHSVar,
-                ArgMode, Context, Stmts),
-            Defns = []
-        else
-            unexpected($pred, "direct_arg_tag: arity != 1")
-        )
+        get_notag_or_direct_arg_arg_mode(RHSVars, ArgModes, RHSVar, ArgMode),
+        ml_gen_dynamic_deconstruct_direct_arg(!.Info, Ptag, LHSVar, RHSVar,
+            ArgMode, Context, Stmts),
+        Defns = []
     ;
         ( ConsTag = single_functor_tag
-        ; ConsTag = unshared_tag(_Ptag)
-        ; ConsTag = shared_remote_tag(_Ptag, _)
+        ; ConsTag = unshared_tag(_)
+        ; ConsTag = shared_remote_tag(_, _)
         ),
         ml_variable_type(!.Info, LHSVar, LHSVarType),
         ml_gen_var(!.Info, LHSVar, LHSVarLval),

compiler/unify_gen_deconstruct.m
@@ -84,6 +84,7 @@
 :- import_module backend_libs.builtin_ops.
 :- import_module check_hlds.
 :- import_module check_hlds.type_util.
+:- import_module hlds.goal_form.
 :- import_module hlds.hlds_code_util.
 :- import_module hlds.hlds_pred.
 :- import_module hlds.vartypes.
@@ -171,8 +172,8 @@ generate_semi_deconstruction(LHSVar, ConsId, RHSVars, Modes, Code,
 %---------------------------------------------------------------------------%
 
     % Generate a deterministic deconstruction. In a deterministic
-    % deconstruction, we know the value of the ptag, so we don't need
-    % to generate a test.
+    % deconstruction, we know the value of the cons_id, so we don't need
+    % to generate a test for it.
     %
     % Deconstructions are generated semi-eagerly. Any test sub-unifications
     % are generated eagerly (they _must_ be), but assignment unifications
@@ -211,50 +212,37 @@ generate_det_deconstruction(LHSVar, ConsId, RHSVars, ArgModes, Code,
         unexpected($pred, "unexpected tag")
     ;
         ConsTag = no_tag,
-        ( if
-            RHSVars = [RHSVar],
-            ArgModes = [ArgMode]
-        then
-            VarType = variable_type(CI, LHSVar),
-            IsDummy = is_type_a_dummy(ModuleInfo, VarType),
-            (
-                IsDummy = is_dummy_type,
-                % We must handle this case specially. If we didn't, the
-                % generated code would copy the reference to the Var's
-                % current location, which may be stackvar(N) or framevar(N)
-                % for negative N, to be the location of Arg, and since Arg
-                % may not be a dummy type, it would actually use that location.
-                % This can happen in the unify/compare routines for e.g.
-                % io.state.
-                ( if variable_is_forward_live(!.CLD, RHSVar) then
-                    assign_const_to_var(RHSVar, const(llconst_int(0)),
-                        CI, !CLD)
-                else
-                    true
-                ),
-                Code = empty
-            ;
-                IsDummy = is_not_dummy_type,
-                ArgType = variable_type(CI, RHSVar),
-                FieldAndArgVar =
-                    field_and_arg_var(uv_var(LHSVar), RHSVar, ArgType),
-                generate_deconstruct_unify_arg(FieldAndArgVar, ArgMode, Code,
-                    CI, !CLD)
-            )
-        else
-            unexpected($pred, "no_tag: arity != 1")
+        get_notag_or_direct_arg_arg_mode(RHSVars, ArgModes, RHSVar, ArgMode),
+        VarType = variable_type(CI, LHSVar),
+        IsDummy = is_type_a_dummy(ModuleInfo, VarType),
+        (
+            IsDummy = is_dummy_type,
+            % We must handle this case specially. If we didn't, the
+            % generated code would copy the reference to the Var's
+            % current location, which may be stackvar(N) or framevar(N)
+            % for negative N, to be the location of Arg, and since Arg
+            % may not be a dummy type, it would actually use that location.
+            % This can happen in the unify/compare routines for e.g.
+            % io.state.
+            ( if variable_is_forward_live(!.CLD, RHSVar) then
+                assign_const_to_var(RHSVar, const(llconst_int(0)), CI, !CLD)
+            else
+                true
+            ),
+            Code = empty
+        ;
+            IsDummy = is_not_dummy_type,
+            ArgType = variable_type(CI, RHSVar),
+            FieldAndArgVar =
+                field_and_arg_var(uv_var(LHSVar), RHSVar, ArgType),
+            generate_deconstruct_unify_arg(FieldAndArgVar, ArgMode, Code,
+                CI, !CLD)
         )
     ;
         ConsTag = direct_arg_tag(Ptag),
-        ( if
-            RHSVars = [RHSVar],
-            ArgModes = [ArgMode]
-        then
-            generate_direct_arg_deconstruct(LHSVar, RHSVar, Ptag, ArgMode,
-                Code, CI, !CLD)
-        else
-            unexpected($pred, "direct_arg_tag: arity != 1")
-        )
+        get_notag_or_direct_arg_arg_mode(RHSVars, ArgModes, RHSVar, ArgMode),
+        generate_direct_arg_deconstruct(LHSVar, RHSVar, Ptag, ArgMode,
+            Code, CI, !CLD)
     ;
         (
             ConsTag = single_functor_tag,

commit 45ba4d04884e9c08843012347e5419db0d4a0463

Add regression test.
Add regression test for a bug introduced in
commit f6e2290031d4318de693a5d377e602f02e09ed1e
and fixed in
commit 53e73a9e952a9e0c6e11020d5c92a429ae0fd01a

tests/hard_coded/Mmakefile:
tests/hard_coded/static_term_bug.m:
tests/hard_coded/static_term_bug.exp:
    Add new test.

tests/hard_coded/Mmakefile
@@ -318,6 +318,7 @@ ORDINARY_PROGS = \
 	special_char \
 	stable_sort \
 	static_no_tag \
+	static_term_bug \
 	stdlib_init \
 	stream_format \
 	stream_ignore_ws \

tests/hard_coded/static_term_bug.exp
@@ -1,0 +1,1 @@
+data(data_info(top, 1234))

tests/hard_coded/static_term_bug.m
@@ -1,0 +1,40 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+
+:- module static_term_bug.
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+%---------------------------------------------------------------------------%
+
+:- implementation.
+
+:- type data
+    --->    none
+    ;       data(data_info).
+
+:- type data_info
+    --->    data_info(side, int).
+
+:- type side
+    --->    top
+    ;       bottom.
+
+main(!IO) :-
+    ( if get_data("top", Data) then
+        io.write(Data, !IO),
+        io.nl(!IO)
+    else
+        io.write_string("failed\n", !IO)
+    ).
+
+:- pred get_data(string::in, data::out) is semidet.
+:- pragma no_inline(get_data/2).
+
+get_data(Side, Data) :-
+    Side = "top",
+    Data = data(data_info(top, 1234)).