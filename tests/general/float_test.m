3848d58571b23494d75196acc0c805928dd6ede3
Julien Fischer <juliensf@gmail.com>
2018-09-04 06:15:49 +0000

    Update syntax in a test case.
    tests/general/float_test.m:
        As above; the actual test has not changed (yet).

--- tests/general/float_test.m
+++ tests/general/float_test.m
@@ -2,15 +2,20 @@
 % vim: ts=4 sw=4 et ft=mercury
 %---------------------------------------------------------------------------%
 %
-% A very basic check of floating point arithmetic and string__to_float.
+% A very basic check of floating point arithmetic and string.to_float.
 % Now tests maths library stuff too.
+%
+%---------------------------------------------------------------------------%
 
 :- module float_test.
 :- interface.
+
 :- import_module io.
 
-:- pred main(io__state, io__state).
-:- mode main(di, uo) is det.
+:- pred main(io::di, io::uo) is det.
+
+%---------------------------------------------------------------------------%
+%---------------------------------------------------------------------------%
 
 :- implementation.
 
@@ -19,92 +24,112 @@
 :- import_module math.
 :- import_module string.
 
-main -->
-    ( { string__to_float("1234.5678", F) } ->
-        write_message("1234.5678: ", F)
-    ;
-        io__write_string("can't parse 1234.5678")
+%---------------------------------------------------------------------------%
+
+main(!IO) :-
+    ( if string.to_float("1234.5678", F) then
+        write_message("1234.5678: ", F, !IO)
+    else
+        io.write_string("can't parse 1234.5678", !IO)
     ),
-    test(3.0, 4.0),
-    test(41.0, -3.0),
-    test_constants,
-    test_math_constants,
-    test_rounding(2.7),
-    test_rounding(-3.6),
-    test_power(2.2),
-    test_trig(0.5),
-    test_inv_trig(0.6).
+    test_arithmetic(3.0, 4.0, !IO),
+    test_arithmetic(41.0, -3.0, !IO),
+    test_constants(!IO),
+    test_math_constants(!IO),
+    test_rounding(2.7, !IO),
+    test_rounding(-3.6, !IO),
+    test_power(2.2, !IO),
+    test_trig(0.5, !IO),
+    test_inv_trig(0.6, !IO).
 
-:- pred test(float, float, io__state, io__state).
-:- mode test(in, in, di, uo) is det.
+:- pred write_message(string::in, float::in, io::di, io::uo) is det.
 
-test(X, Y) -->
-    {
-        Plus = X + Y,
-        Times = X * Y,
-        Minus = X - Y,
-        Divide = X / Y,
-        Pow = math__pow(X, Y)
-    },
-    write_message("X: ", X),
-    write_message("Y: ", Y),
-    write_message("X + Y: ", Plus),
-    write_message("X * Y: ", Times),
-    write_message("X - Y: ", Minus),
-    write_message("X / Y: ", Divide),
-    write_message("X ^ Y: ", Pow).
+write_message(String, Float, !IO) :-
+    io.format("%s%6.3g\n", [s(String), f(Float)], !IO).
 
-:- pred write_message(string, float, io__state, io__state).
-:- mode write_message(in, in, di, uo) is det.
+%---------------------------------------------------------------------------%
 
-write_message(String, Float) -->
-    { string__format("%s%6.3g\n", [s(String), f(Float)], Message) },
-    io__write_string(Message).
+:- pred test_arithmetic(float::in, float::in, io::di, io::uo) is det.
 
-:- pred test_constants(io__state :: di, io__state :: uo) is det.
-test_constants -->
-    write_message("Float max: ", float__max),
-    write_message("Float min: ", float__min),
-    write_message("Float epsilon: ", float__epsilon).
+test_arithmetic(X, Y, !IO) :-
+    Plus = X + Y,
+    Times = X * Y,
+    Minus = X - Y,
+    Divide = X / Y,
+    Pow = math.pow(X, Y),
+    write_message("X: ", X, !IO),
+    write_message("Y: ", Y, !IO),
+    write_message("X + Y: ", Plus, !IO),
+    write_message("X * Y: ", Times, !IO),
+    write_message("X - Y: ", Minus, !IO),
+    write_message("X / Y: ", Divide, !IO),
+    write_message("X ^ Y: ", Pow, !IO).
 
-:- pred test_math_constants(io__state :: di, io__state :: uo) is det.
-test_math_constants -->
-    write_message("Pi: ", math__pi),
-    write_message("e: ", math__e).
+%---------------------------------------------------------------------------%
 
-:- pred test_rounding(float :: in, io__state :: di, io__state :: uo) is det.
-test_rounding(X) -->
-    write_message("X: ", X),
-    write_message("ceil(X): ", math__ceiling(X)),
-    write_message("floor(X): ", math__floor(X)),
-    write_message("round(X): ", math__round(X)),
-    write_message("truncate(X): ", math__truncate(X)).
+:- pred test_constants(io::di, io::uo) is det.
 
-:- pred test_power(float :: in, io__state :: di, io__state :: uo) is det.
-test_power(X) -->
-    write_message("X: ", X),
-    write_message("sqrt(X): ", math__sqrt(X)),
-    write_message("ln(X): ", math__ln(X)),
-    write_message("log2(X): ", math__log2(X)),
-    write_message("log10(X): ", math__log10(X)),
-    write_message("log(2.1, X): ", math__log(2.1, X)),
-    write_message("exp(X): ", math__exp(X)).
+test_constants(!IO) :-
+    write_message("Float max: ", float.max, !IO),
+    write_message("Float min: ", float.min, !IO),
+    write_message("Float epsilon: ", float.epsilon, !IO).
 
-:- pred test_trig(float :: in, io__state :: di, io__state :: uo) is det.
-test_trig(X) -->
-    write_message("X: ", X),
-    write_message("sin(X): ", math__sin(X)),
-    write_message("cos(X): ", math__cos(X)),
-    write_message("tan(X): ", math__tan(X)),
-    write_message("sinh(X): ", math__sinh(X)),
-    write_message("cosh(X): ", math__cosh(X)),
-    write_message("tanh(X): ", math__tanh(X)),
+%---------------------------------------------------------------------------%
+
+:- pred test_math_constants(io::di, io::uo) is det.
+
+test_math_constants(!IO) :-
+    write_message("Pi: ", math.pi, !IO),
+    write_message("e: ", math.e, !IO).
+
+%---------------------------------------------------------------------------%
+
+:- pred test_rounding(float::in, io::di, io::uo) is det.
+
+test_rounding(X, !IO) :-
+    write_message("X: ", X, !IO),
+    write_message("ceil(X): ", math.ceiling(X), !IO),
+    write_message("floor(X): ", math.floor(X), !IO),
+    write_message("round(X): ", math.round(X), !IO),
+    write_message("truncate(X): ", math.truncate(X), !IO).
+
+%---------------------------------------------------------------------------%
+
+:- pred test_power(float::in, io::di, io::uo) is det.
+
+test_power(X, !IO) :-
+    write_message("X: ", X, !IO),
+    write_message("sqrt(X): ", math.sqrt(X), !IO),
+    write_message("ln(X): ", math.ln(X), !IO),
+    write_message("log2(X): ", math.log2(X), !IO),
+    write_message("log10(X): ", math.log10(X), !IO),
+    write_message("log(2.1, X): ", math.log(2.1, X), !IO),
+    write_message("exp(X): ", math.exp(X), !IO).
+
+%---------------------------------------------------------------------------%
+
+:- pred test_trig(float::in, io::di, io::uo) is det.
+
+test_trig(X, !IO) :-
+    write_message("X: ", X, !IO),
+    write_message("sin(X): ", math.sin(X), !IO),
+    write_message("cos(X): ", math.cos(X), !IO),
+    write_message("tan(X): ", math.tan(X), !IO),
+    write_message("sinh(X): ", math.sinh(X), !IO),
+    write_message("cosh(X): ", math.cosh(X), !IO),
+    write_message("tanh(X): ", math.tanh(X), !IO),
     write_message("atan2(sin(X), cos(X)): ",
-        math__atan2(math__sin(X), math__cos(X))).
+        math.atan2(math.sin(X), math.cos(X)), !IO).
 
-:- pred test_inv_trig(float :: in, io__state :: di, io__state :: uo) is det.
-test_inv_trig(X) -->
-    write_message("X: ", X),
-    write_message("asin(X): ", math__asin(X)),
-    write_message("acos(X): ", math__acos(X)),
-    write_message("atan(X): ", math__atan(X)).
+%---------------------------------------------------------------------------%
+
+:- pred test_inv_trig(float::in, io::di, io::uo) is det.
+
+test_inv_trig(X, !IO) :-
+    write_message("X: ", X, !IO),
+    write_message("asin(X): ", math.asin(X), !IO),
+    write_message("acos(X): ", math.acos(X), !IO),
+    write_message("atan(X): ", math.atan(X), !IO).
+
+%---------------------------------------------------------------------------%
+%---------------------------------------------------------------------------%
