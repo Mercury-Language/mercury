d793b7f3be29a354059aefbd25a556e1ab6dcc4a
Peter Wang <novalazy@gmail.com>
2019-06-01 18:53:32 +1000

    Make mercury_event_scanner.h a static file.
    trace/Mmakefile:
    trace/mercury_event_scanner.h:
    trace/.gitignore:
        Add mercury_event_scanner.h as a static file, no need to generate it.

---
+++ trace/mercury_event_scanner.h
@@ -0,0 +1,11 @@
+// vim: ts=4 sw=4 expandtab ft=c
+
+// Copyright (C) 2019 The Mercury team.
+// This file is distributed under the terms specified in COPYING.LIB.
+
+#ifndef MERCURY_EVENT_SCANNER_H
+#define MERCURY_EVENT_SCANNER_H
+
+extern int mercury_event_lex(void);
+
+#endif	// MERCURY_EVENT_SCANNER_H
