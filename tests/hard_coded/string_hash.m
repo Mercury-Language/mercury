f64050ba34fc9f1ce721cff27f1d29afe74792f2
Adrian Wong <adrianwjw@gmail.com>
2019-07-27 15:53:54 +1000

    Add tests for string.hash{4,5,6}.
    tests/hard_coded/string_hash.m:
        As above.

--- tests/hard_coded/string_hash.m
+++ tests/hard_coded/string_hash.m
@@ -1,8 +1,12 @@
 % vim: ts=4 sw=4 et ft=mercury
 
 % Test that string.hash and MR_hash_string return the same value.
-% Do the same for string.hash2 and MR_hash_string2, and for
-% string.hash3 and MR_hash_string3.
+% Do the same for:
+% * string.hash2 and MR_hash_string2,
+% * string.hash3 and MR_hash_string3,
+% * string.hash4 and MR_hash_string4,
+% * string.hash5 and MR_hash_string5,
+% * string.hash6 and MR_hash_string6.
 
 :- module string_hash.
 
@@ -46,48 +50,27 @@ test(Length, !Succeeded, !RS, !IO) :-
 
         LibHash1 = string.hash(String),
         RuntimeHash1 = runtime_string_hash(String),
-        ( LibHash1 = RuntimeHash1 ->
-            true
-        ;
-            !:Succeeded = no,
-            io.write_string("failed hash1: runtime ", !IO),
-            io.write_int(RuntimeHash1, !IO),
-            io.write_string(", library ", !IO),
-            io.write_int(LibHash1, !IO),
-            io.write_string(": """, !IO),
-            io.write_string(String, !IO),
-            io.write_string("""\n", !IO)
-        ),
+        test_hash("hash1", LibHash1, RuntimeHash1, String, !Succeeded, !IO),
 
         LibHash2 = string.hash2(String),
         RuntimeHash2 = runtime_string_hash2(String),
-        ( LibHash2 = RuntimeHash2 ->
-            true
-        ;
-            !:Succeeded = no,
-            io.write_string("failed hash2: runtime ", !IO),
-            io.write_int(RuntimeHash2, !IO),
-            io.write_string(", library ", !IO),
-            io.write_int(LibHash2, !IO),
-            io.write_string(": """, !IO),
-            io.write_string(String, !IO),
-            io.write_string("""\n", !IO)
-        ),
+        test_hash("hash2", LibHash2, RuntimeHash2, String, !Succeeded, !IO),
 
         LibHash3 = string.hash3(String),
         RuntimeHash3 = runtime_string_hash3(String),
-        ( LibHash3 = RuntimeHash3 ->
-            true
-        ;
-            !:Succeeded = no,
-            io.write_string("failed hash3: runtime ", !IO),
-            io.write_int(RuntimeHash3, !IO),
-            io.write_string(", library ", !IO),
-            io.write_int(LibHash3, !IO),
-            io.write_string(": """, !IO),
-            io.write_string(String, !IO),
-            io.write_string("""\n", !IO)
-        ),
+        test_hash("hash3", LibHash3, RuntimeHash3, String, !Succeeded, !IO),
+
+        LibHash4 = string.hash4(String),
+        RuntimeHash4 = runtime_string_hash4(String),
+        test_hash("hash4", LibHash4, RuntimeHash4, String, !Succeeded, !IO),
+
+        LibHash5 = string.hash5(String),
+        RuntimeHash5 = runtime_string_hash5(String),
+        test_hash("hash5", LibHash5, RuntimeHash5, String, !Succeeded, !IO),
+
+        LibHash6 = string.hash6(String),
+        RuntimeHash6 = runtime_string_hash6(String),
+        test_hash("hash6", LibHash6, RuntimeHash6, String, !Succeeded, !IO),
 
         test(Length - 1, !Succeeded, !RS, !IO)
     ).
@@ -111,6 +94,23 @@ rand_char(Char, !RS) :-
     % U+0001..U+10ffff (avoid null character).
     Int = 1 + (Rand `mod` char.max_char_value),
     char.det_from_int(Int, Char).
+
+:- pred test_hash(string::in, int::in, int::in, string::in,
+    bool::in, bool::out, io::di, io::uo) is det.
+
+test_hash(HashName, LibHash, RuntimeHash, String, !Succeeded, !IO) :-
+    ( LibHash = RuntimeHash ->
+        true
+    ;
+        !:Succeeded = no,
+        io.write_string("failed " ++ HashName ++ ": runtime ", !IO),
+        io.write_int(RuntimeHash, !IO),
+        io.write_string(", library ", !IO),
+        io.write_int(LibHash, !IO),
+        io.write_string(": """, !IO),
+        io.write_string(String, !IO),
+        io.write_string("""\n", !IO)
+    ).
 
 :- pragma foreign_decl("C", "#include ""mercury_string.h""").
 
@@ -139,4 +139,31 @@ rand_char(Char, !RS) :-
     [promise_pure, will_not_call_mercury],
 "
     Hash = MR_hash_string3(StringArg);
+").
+
+:- func runtime_string_hash4(string) = int.
+
+:- pragma foreign_proc("C",
+    runtime_string_hash4(StringArg::in) = (Hash::out),
+    [promise_pure, will_not_call_mercury],
+"
+    Hash = MR_hash_string4(StringArg);
+").
+
+:- func runtime_string_hash5(string) = int.
+
+:- pragma foreign_proc("C",
+    runtime_string_hash5(StringArg::in) = (Hash::out),
+    [promise_pure, will_not_call_mercury],
+"
+    Hash = MR_hash_string5(StringArg);
+").
+
+:- func runtime_string_hash6(string) = int.
+
+:- pragma foreign_proc("C",
+    runtime_string_hash6(StringArg::in) = (Hash::out),
+    [promise_pure, will_not_call_mercury],
+"
+    Hash = MR_hash_string6(StringArg);
 ").
