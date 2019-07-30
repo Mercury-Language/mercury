bfcdbbd34612d6723aec821d1a992481118ff8b7
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2019-07-30 13:30:37 +0200

    Eliminate a hugely long single line in a test output.

--- tests/par_conj/pathological_right_recursion.m
+++ tests/par_conj/pathological_right_recursion.m
@@ -1,7 +1,6 @@
 %---------------------------------------------------------------------------%
 % vim: ts=4 sw=4 et ft=mercury
 %---------------------------------------------------------------------------%
-%
 
 :- module pathological_right_recursion.
 
@@ -19,32 +18,32 @@
 
 main(!IO) :-
     count_up(0, 100000, List),
-    write(List, !IO),
-    nl(!IO).
+    list.chunk(List, 10, ListChunks),
+    io.write_list(ListChunks, "", io.write_line, !IO).
 
 :- pred count_up(int::in, int::in, list(int)::out) is det.
 
 count_up(N0, Max, List) :-
-    ( N0 >= Max ->
+    ( if N0 >= Max then
         List = []
-    ;
-        % The pathological case for recursion with the parallel conjunction is
-        % right-recursion.  That is where any conjunct but the left most is
-        % recursive and the compiler cannot re-order the conjuncts because of a
-        % dependency (such as N below).
+    else
+        % The pathological case for recursion with the parallel conjunction
+        % is right-recursion. That is where any conjunct other than
+        % the leftmost is recursive, and the compiler cannot re-order
+        % the conjuncts because of a dependency (such as N below).
         %
-        % If this code where to create 1000 contexts, each with their own
-        % stacks it would consume too much memory and possibly bring the OS to
-        % it's knees.
+        % If this code were to create 1000 contexts, each with their own
+        % stacks, it would consume too much memory and possibly bring the OS
+        % to its knees.
         (
             N = N0 + 1,
-            % spend some time so that the spark representing the second
+            % Spend some time so that the spark representing the second
             % conjunct will probably be stolen.
             loop(0, 1000, N2)
         &
             (
                 count_up(N0 + 1, Max, List0),
-                List = [N , N2 | List0]
+                List = [N, N2 | List0]
             )
         )
     ).
@@ -52,8 +51,8 @@ count_up(N0, Max, List) :-
 :- pred loop(int::in, int::in, int::out) is det.
 
 loop(N, Max, Result) :-
-    ( N < Max ->
-        loop(N+1, Max, Result)
-    ;
+    ( if N < Max then
+        loop(N + 1, Max, Result)
+    else
         Result = N
     ).
