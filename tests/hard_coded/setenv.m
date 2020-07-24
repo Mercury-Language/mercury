823adf77f7532ce5356c1d4a77e047f8646b0e14
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2020-07-24 17:14:50 +1000

    Improve programming style.

--- tests/hard_coded/setenv.m
+++ tests/hard_coded/setenv.m
@@ -17,11 +17,11 @@ main(!IO) :-
     io.set_environment_var("foo", "bar", !IO),
 
     % Earlier versions of the Mercury library relied on putenv, which
-    % on many platforms requires that we don't garbage collect the
-    % string we pass it. This code tests we can handle that.
-    use_mem(1000000, !IO),
+    % on many platforms requires that we don't garbage collect the string
+    % we pass it. This code tests whether we can handle that.
+    use_mem(1_000_000, !IO),
     gc.garbage_collect(!IO),
-    use_mem(1000000, !IO),
+    use_mem(1_000_000, !IO),
 
     io.get_environment_var("foo", Res, !IO),
     (
@@ -36,9 +36,8 @@ main(!IO) :-
 
 use_mem(N, !IO) :-
     io.write_string("Use mem: ", !IO),
-    ( if length(1 `..` N) = N then
+    ( if list.length(1 `..` N) = N then
         io.write_string("ok\n", !IO)
     else
         io.write_string("hmm\n", !IO)
     ).
-
