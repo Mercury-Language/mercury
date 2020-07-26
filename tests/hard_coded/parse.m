700f71ef353cc47a4c345887151a7601147480af
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2020-07-26 19:45:07 +1000

    Fix programming style.

--- tests/hard_coded/parse.m
+++ tests/hard_coded/parse.m
@@ -5,6 +5,7 @@
 % Mercury 0.9 had a bug where a program compiled with tracing
 % could not have a module with the same name as one of the
 % browser library modules, which are not user-visible.
+%
 
 :- module parse.
 
@@ -12,9 +13,9 @@
 
 :- import_module io.
 
-:- pred main(io__state::di, io__state::uo) is det.
+:- pred main(io::di, io::uo) is det.
 
 :- implementation.
 
-main -->
-    io__write_string("ok\n").
+main(!IO) :-
+    io.write_string("ok\n", !IO).
