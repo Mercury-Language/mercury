b3b140207b04c8644df7bf4671b479223607bb00
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2021-09-30 16:45:25 +1000

    Update the style of a test.

--- tests/hard_coded/pack_args_intermod2.m
+++ tests/hard_coded/pack_args_intermod2.m
@@ -39,25 +39,25 @@
     ;       hog.        % 7
 
 ani(I) =
-    ( I = 0 -> ant
-    ; I = 10 -> bat
-    ; I = 20 -> cat
-    ; I = 30 -> dog
-    ; I = 40 -> eel
-    ; I = 50 -> fox
-    ; I = 60 -> gnu
-    ;           hog
+    ( if I = 0 then ant
+    else if I = 10 then bat
+    else if I = 20 then cat
+    else if I = 30 then dog
+    else if I = 40 then eel
+    else if I = 50 then fox
+    else if I = 60 then gnu
+    else hog
     ).
 
 write_struct(struct(A, B, I, S, C, D, E, F), !IO) :-
-    write_animal(A, !IO), write_string(", ", !IO),
-    write_animal(B, !IO), write_string(", ", !IO),
-    write_int(I, !IO),    write_string(", ", !IO),
-    write_string(S, !IO), write_string(", ", !IO),
-    write_animal(C, !IO), write_string(", ", !IO),
-    write_animal(D, !IO), write_string(", ", !IO),
-    write_animal(E, !IO), write_string(", ", !IO),
+    write_animal(A, !IO),    io.write_string(", ", !IO),
+    write_animal(B, !IO),    io.write_string(", ", !IO),
+    io.write_int(I, !IO),    io.write_string(", ", !IO),
+    io.write_string(S, !IO), io.write_string(", ", !IO),
+    write_animal(C, !IO),    io.write_string(", ", !IO),
+    write_animal(D, !IO),    io.write_string(", ", !IO),
+    write_animal(E, !IO),    io.write_string(", ", !IO),
     write_animal(F, !IO).
 
 write_animal(Animal, !IO) :-
-    write(Animal, !IO).
+    io.write(Animal, !IO).
