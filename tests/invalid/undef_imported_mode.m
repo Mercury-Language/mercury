9964b425e06ccafecfe0bc27def547e780d1ccc1
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2021-11-02 01:23:13 +1100

    Add a test case for a recent bug report.

---
+++ tests/invalid/undef_imported_mode.m
@@ -0,0 +1,21 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+
+:- module undef_imported_mode.
+
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+%---------------------------------------------------------------------------%
+
+:- implementation.
+
+:- import_module undef_imported_mode_2.
+
+main(!IO) :-
+    bad(42, N),
+    io.write_int(N, !IO).
