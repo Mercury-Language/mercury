d218e1b5735eb658ca8fe56abbdcdc55ca5aa8ec
Julien Fischer <juliensf@gmail.com>
2022-01-17 22:46:06 +1100

    Various fixes for the extras.
    extras/complex_numbers/*.m:
        Rename files containing submodules so that we can (correctly) build
        this library without first building a source file mapping.
    
    extras/complex_numbers/samples/Mmakefile:
    extras/complex_numbers/tests/Mmakefile:
        Tell the C compiler where the header files for libcomplex_numbers
        are.
    
    extras/curs/samples/Mmakefile:
        Pass the -I option to the C compiler via the CFLAGS variable, not
        the MGNUCFLAGS variable; the latter just causes the mgnuc script
        to halt option processing and construct an invalid command line
        for the C compiler.
    
    extras/curses/*.m:
        Rename files containing submodules so that we can (correctly) build
        this library without first building a source file mapping.
    
    extras/curses/mcurses.m:
        Style and formatting fixes.
    
    extras/curses/sample/Mmakefile:
         Pass the -I option to the C compiler via the CFLAGS variable.

--- extras/curses/mcurses.m
+++ extras/curses/mcurses.m
@@ -3,10 +3,8 @@
 % This file may only be copied under the terms of the GNU General
 % Public License - see the file COPYING in the Mercury Distribution.
 %----------------------------------------------------------------------------%
-
-%----------------------------------------------------------------------------%
 %
-% top level of the curses binding
+% Top-level of the curses binding
 %
 % Please note that this is still a partial binding; it does not provide
 % complete curses functionality.
@@ -17,6 +15,14 @@
 %     * Colour on a character by character basis.
 %
 %----------------------------------------------------------------------------%
+
 :- module mcurses.
 :- interface.
-:- include_module basics, user, misc.
+
+:- include_module basics.
+:- include_module misc.
+:- include_module user.
+
+%----------------------------------------------------------------------------%
+:- end_module mcurses.
+%----------------------------------------------------------------------------%
