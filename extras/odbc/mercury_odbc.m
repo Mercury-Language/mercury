133d040411f6604800e708b76ac790becaf82cc1
Julien Fischer <juliensf@gmail.com>
2022-01-17 14:12:51 +1100

    Use a different library name for the ODBC binding.
    When building the ODBC binding as a library, name it libmercury_odbc, rather
    than libodbc; unixODBC uses the former name, which is at the very least
    confusing and appears to the source of linker errors on some platforms.
    
    extras/odbc/mercury_odbc.m:
        An empty module importing odbc, that is used to force the library
        name to be libmercury_odbc instead of libodbc.
    
    extras/odbc/Mmakefile:
        Do not warn about the module we use force the library name not exporting
        anything.
    
        Delete a workaround for really old versions of GCC.

---
+++ extras/odbc/mercury_odbc.m
@@ -0,0 +1,12 @@
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2022 The Mercury team.
+% This file is distributed under the terms specified in COPYING.LIB.
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+
+% mercury_odbc -- a Mercury ODBC library.
+
+:- module mercury_odbc.
+:- interface.
+
+:- import_module odbc.
