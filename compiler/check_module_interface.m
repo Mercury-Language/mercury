41d133b8adba436a6414e9fcba700a828185c180
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2022-04-05 10:32:10 +1000

    Update the "no export" criteria and message.
    compiler/check_module_interface.m:
        Generate a "this module does not export anything" warning for modules
        that contain only single include_module declaration, since such modules
        could be deleted with no effect.
    
        Change the warning message to list every kind of declaration/definition
        that does not depend on the presence of other kinds of declarations or
        definitions.
    
    deep_profiler/Mercury.options:
        Shut up this warning for mdprof_fb, since right now, it contains
        only a single include_module, even it is designed to later contain more.
    
    tests/invalid/empty_interface.err_exp:
    tests/invalid/typeclass_missing_det_3.err_exp:
    tests/invalid_nodepend/bigtest.err_exp:
    tests/invalid_nodepend/duplicate_modes.err_exp:
    tests/invalid_nodepend/errors2.err_exp:
    tests/invalid_nodepend/no_exports.err_exp:
    tests/invalid_nodepend/prog_io_erroneous.err_exp:
    tests/invalid_nodepend/typeclass_test_11.err_exp:
    tests/invalid_nodepend/vars_in_wrong_places.err_exp:
        Expect the updated wording of the warning message.

--- compiler/check_module_interface.m
+++ compiler/check_module_interface.m
@@ -72,10 +72,11 @@ check_module_interface_for_no_exports(Globals, ParseTreeModuleSrc, !Specs) :-
             _ImpClauses, _ImpForeignExportEnums,
             _ImpDeclPragmas, _ImpImplPragmas, _ImpPromises,
             _ImpInitialises, _ImpFinalises, _ImpMutables),
-        % XXX ITEM_LIST Should we return "yes" for an item_block
-        % that contains only ONE include_module declaration?
+        map.count(IntIncls, NumIntIncls),
         ( if
-            map.is_empty(IntIncls),
+            ( NumIntIncls = 0
+            ; NumIntIncls = 1
+            ),
             type_ctor_checked_map_get_src_defns(TypeCtorCheckedMap,
                 IntTypeDefns, _, _),
             IntTypeDefns = [],
@@ -113,33 +114,58 @@ check_module_interface_for_no_exports(Globals, ParseTreeModuleSrc, !Specs) :-
             IntDeclPragmas = [],
             IntPromises = []
         then
-            generate_no_exports_warning(ModuleName, ModuleNameContext, !Specs)
+            generate_no_exports_warning(ModuleName, ModuleNameContext,
+                NumIntIncls, !Specs)
         else
             true
         )
     ).
 
+:- inst num_int_incls for int/0
+    --->    0
+    ;       1.
+
 :- pred generate_no_exports_warning(module_name::in, prog_context::in,
+    int::in(num_int_incls),
     list(error_spec)::in, list(error_spec)::out) is det.
 
-generate_no_exports_warning(ModuleName, Context, !Specs) :-
-    Msg = simple_msg(Context,
+generate_no_exports_warning(ModuleName, Context, NumIntIncls, !Specs) :-
+    MainMsg = simple_msg(Context,
         [always([invis_order_default_start(2),
             words("Warning: the interface of module"),
             qual_sym_name(ModuleName),
             words("does not export anything."), nl]),
+        % We don't list mode declarations because they don't make sense
+        % without a corresponding pred or func declaration.
+        % We don't list decl pragmas for the same reason.
+        % We don't list promises because although they don't *have to be*
+        % about predicates and functions defined in the same module,
+        % they *should be*.
         verbose_only(verbose_always,
             [words("To be useful, a module should export something."),
             words("A file should contain at least one declaration"),
             words("other than"), decl("import_module"),
             words("in its interface section(s)."),
-            words("This would normally be a"),
-            decl("pred"), suffix(","), decl("func"), suffix(","),
-            decl("type"), suffix(","), decl("inst"), words("or"),
-            decl("mode"), words("declaration."), nl])
-        ]),
-    Spec = error_spec($pred, severity_warning, phase_term_to_parse_tree,
-        [Msg]),
+            words("This would normally be a"), decl("pred"), words("or"),
+            decl("func"), words("declaration, or a"),
+            decl("type"), suffix(","), decl("inst"), suffix(","),
+            decl("mode"), suffix(","), decl("typeclass"), words("or"),
+            decl("instance"), words("definition."), nl]
+        )]),
+    (
+        NumIntIncls = 0,
+        Msgs = [MainMsg]
+    ;
+        NumIntIncls = 1,
+        InclMsg = simple_msg(Context,
+            [verbose_only(verbose_always,
+                [words("A module that includes a single submodule"),
+                words("is not useful, because it can be replaced"),
+                words("by that submodule."), nl])
+            ]),
+        Msgs = [MainMsg, InclMsg]
+    ),
+    Spec = error_spec($pred, severity_warning, phase_term_to_parse_tree, Msgs),
     !:Specs = [Spec | !.Specs].
 
 %---------------------------------------------------------------------------%
