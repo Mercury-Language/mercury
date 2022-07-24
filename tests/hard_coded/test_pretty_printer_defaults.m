3d47ded492b12a807656a53a4263e510abeec38c
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2022-07-25 06:48:34 +1000

    Improve programming style.

--- tests/hard_coded/test_pretty_printer_defaults.m
+++ tests/hard_coded/test_pretty_printer_defaults.m
@@ -1,7 +1,7 @@
 %---------------------------------------------------------------------------%
 % vim: ts=4 sw=4 et ft=mercury
 %---------------------------------------------------------------------------%
-
+%
 % test_pretty_printer_defaults.m
 % Ralph Becket <rafe@csse.unimelb.edu.au>
 % Tue Aug  7 15:29:20 EST 2007
@@ -39,19 +39,16 @@ main(!IO) :-
     VA = version_array(L),
     M = map.from_corresponding_lists(L, L) : map(int, int),
     pretty_printer.get_default_formatter_map(FMap, !IO),
-    io.print(FMap, !IO),
-    io.nl(!IO),
-    pretty_printer.write_doc(
-        docs([
-            str("list:    "), format(L), nl,
-            str("array:   "), format(A), nl,
-            str("map:     "), format(M), nl,
-            str("tuple:   "), format({1, '2', 3.0, "four"}), nl,
-            str("strings: "), format("this is a string"), nl,
-            str("ints:    "), format(42), str(" "), format(-123), nl,
-            str("floats:  "), format(3.141), str(" "), format(-10.0), nl,
-            str("chars:   "), format([a, '*', '\n']), nl,
-            str("version_array:"), format(VA), nl
-        ]),
-        !IO
-    ).
+    io.print_line(FMap, !IO),
+    Doc = docs([
+        str("list:    "), format(L), nl,
+        str("array:   "), format(A), nl,
+        str("map:     "), format(M), nl,
+        str("tuple:   "), format({1, '2', 3.0, "four"}), nl,
+        str("strings: "), format("this is a string"), nl,
+        str("ints:    "), format(42), str(" "), format(-123), nl,
+        str("floats:  "), format(3.141), str(" "), format(-10.0), nl,
+        str("chars:   "), format([a, '*', '\n']), nl,
+        str("version_array:"), format(VA), nl
+    ]),
+    pretty_printer.write_doc(Doc, !IO).
