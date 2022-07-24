8da84c4ee56d352240ba90d815b2869566e9c50e
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2022-07-25 07:13:17 +1000

    Make test_pretty_printer's output more checkable.
    tests/hard_coded/test_pretty_printer.m:
    tests/hard_coded/test_pretty_printer_helper.m:
        This test checks the operation of the library's pretty_printer module
    
        - on several docs, and
        - with several parameters.
    
        With the old organization of this test case, the output consisted of
    
        - all docs with the first set of parameter values,
        - all docs with the next set of parameter values,
        and so on.
    
        This diff reorders the output so that it now consists of
    
        - the first doc with all sets of parameter values,
        - the next doc with all sets of parameter values,
        and so on.
    
        This makes it much easier to check the output.
    
        The new approach also
    
        - gives a name to each doc,
        - prints the doc using the largest parameter values (longest lines,
          most lines allowed, etc) first, which allows all the later outputs
          for the same doc with smaller parameters values to be checked against
          it more easily.
    
        This diff also moves the code that serves as the infrastructure
        of the test case to the new module test_pretty_printer_helper.m,
        leaving test_pretty_printer.m to contain just the code that
        constructs the docs whose prettyprinting is to be tested. (Previously,
        these two classes of code were interleaved in the source code.)
    
    tests/hard_coded/test_pretty_printer.exp:
        Expect the output in the updated order.

--- tests/hard_coded/test_pretty_printer.m
+++ tests/hard_coded/test_pretty_printer.m
@@ -12,181 +12,41 @@
 
 :- import_module io.
 
-:- pred main(io::di, io::uo) is cc_multi.
+:- pred main(io::di, io::uo) is det.
 
 %---------------------------------------------------------------------------%
 
 :- implementation.
 
-:- import_module deconstruct.
+:- import_module test_pretty_printer_helper.
+
 :- import_module float.
 :- import_module int.
 :- import_module list.
 :- import_module map.
-:- import_module ops.
-:- import_module pair.
 :- import_module pretty_printer.
-:- import_module solutions.
 :- import_module string.
-:- import_module term_io.
-:- import_module type_desc.
 :- import_module univ.
 
-:- type test_case
-    --->    test_case(
-                line_width              :: int,
-                max_lines               :: int,
-                fs_limit                :: func_symbol_limit,
-                doc                     :: doc
-            ).
-
-:- type op_tree
-    --->    x
-    ;       - op_tree
-    ;       op_tree + op_tree
-    ;       op_tree - op_tree
-    ;       op_tree * op_tree
-    ;       op_tree / op_tree.
-
-:- type church
-    --->    zero
-    ;       succ(church).
-
-:- type non_canonical_bool
-    --->    non_canonical_bool(int)
-    where equality is non_canonical_bool_eq.
+%---------------------------------------------------------------------------%
 
 main(!IO) :-
-    set_default_formatter("list",    "list",       1, fmt_list,   !IO),
-    set_default_formatter("tree234", "tree234",    2, fmt_map,    !IO),
-    set_default_formatter("builtin", "character",  0, fmt_char,   !IO),
-    set_default_formatter("builtin", "float",      0, fmt_float,  !IO),
-    set_default_formatter("builtin", "int",        0, fmt_int,    !IO),
-    set_default_formatter("builtin", "string",     0, fmt_string, !IO),
-    unsorted_solutions(test_case, TestCases),
+    setup_pretty_printer(!IO),
+    test_cases(TestCases),
     list.foldl(run_test_case, TestCases, !IO).
 
-:- pred run_test_case(test_case::in, io::di, io::uo) is det.
+%---------------------------------------------------------------------------%
 
-run_test_case(TestCase, !IO) :-
-    TestCase = test_case(LineWidth, MaxLines, Limit, Doc),
-    set_default_params(pp_params(LineWidth, MaxLines, Limit), !IO),
-    io.format("\nlimit = %s, max lines = %d, line width = %d",
-        [s(string.string(Limit)), i(MaxLines), i(LineWidth)], !IO),
-    Ruler = "\n|" ++ string.duplicate_char(('-'), LineWidth - 2) ++ "|\n",
-    io.write_string(Ruler, !IO),
-    pretty_printer.write_doc(Doc, !IO),
-    io.write_string(Ruler, !IO).
+:- pred test_cases(list(test_case)::out) is det.
 
-:- func fmt_float : pretty_printer.formatter.
-
-fmt_float(Univ, _) =
-    ( if Univ = univ(Float) then
-        str(string.float_to_string(Float))
-    else
-        str("?fmt_float?")
-    ).
-
-:- func fmt_int : pretty_printer.formatter.
-
-fmt_int(Univ, _) =
-    ( if Univ = univ(Int) then
-        str(string.int_to_string(Int))
-    else
-        str("?fmt_int?")
-    ).
-
-:- func fmt_string : pretty_printer.formatter.
-
-fmt_string(Univ, _) =
-    ( if Univ = univ(String) then
-        docs([str("\""), str(String), str("\"")])
-    else
-        str("?fmt_string?")
-    ).
-
-:- func fmt_char : pretty_printer.formatter.
-
-fmt_char(Univ, _) =
-    ( if Univ = univ(Char) then
-        str(term_io.quoted_char(Char))
-    else
-        str("?fmt_char?")
-    ).
-
-:- func fmt_list(univ, list(type_desc)) = doc.
-
-fmt_list(Univ, ArgDescs) = Doc :-
-    ( if
-        ArgDescs = [ArgDesc],
-        has_type(Arg, ArgDesc),
-        same_list_type(List, Arg),
-        Value = univ_value(Univ),
-        dynamic_cast(Value, List),
-        UnivList = list.map(make_univ, List)
-    then
-        Doc = indent([str("["), format_list(UnivList, str(", ")), str("]")])
-    else
-        Doc = str("?fmt_list?")
-    ).
-
-:- pred same_list_type(list(T)::unused, T::unused) is det.
-
-same_list_type(_, _).
-
-:- type key_value(K, V)
-    --->    (K -> V).
-
-:- func fmt_map(univ, list(type_desc)) = doc.
-
-fmt_map(Univ, ArgDescs) = Doc :-
-    ( if
-        ArgDescs = [KArgDesc, VArgDesc],
-        has_type(K, KArgDesc),
-        has_type(V, VArgDesc),
-        same_map_type(Map, K, V),
-        Value = univ_value(Univ),
-        dynamic_cast(Value, Map)
-    then
-        UnivList =
-            map.foldr(func(KK, VV, KVs) = [univ(KK -> VV) | KVs], Map, []),
-        Doc = indent([
-            str("map(["), format_list(UnivList, str(", ")), str("])")
-        ])
-    else
-        Doc = str("?fmt_map?")
-    ).
-
-:- func fmt_susp_seq(int) = doc.
-
-fmt_susp_seq(N) =
-    ( if N =< 1 then
-        str("1")
-    else
-        docs([
-            group([nl, format(N), str(".")]),
-            format_susp((func) = fmt_susp_seq(N - 1))
-        ])
-    ).
-
-:- func make_univ(T) = univ.
-
-make_univ(X) = univ(X).
-
-:- pred same_map_type(map(K, V)::unused, K::unused, V::unused) is det.
-
-same_map_type(_, _, _).
-
-:- pred test_case(test_case::out) is multi.
-
-test_case(test_case(LineWidth, MaxLines, Limit, Doc)) :-
+test_cases(Docs) :-
     List = 1..100,
     ListUniv = list.map(func(X) = univ(X), List),
     MapStr = list.foldl(
-        func(X, M) = M ^ elem(X) := "0x" ++ int_to_base_string(X, 16),
+        (func(X, M) = M ^ elem(X) := "0x" ++ int_to_base_string(X, 16)),
         List, map.init : map(int, string)),
     MapFloat = list.foldl(
-        func(X, M) = M ^ elem(X) := float(X),
+        (func(X, M) = M ^ elem(X) := float(X)),
         List, map.init : map(int, float)),
     OpTree = mk_op_tree(200),
     Church = list.foldl(func(_, X) = succ(X), 1..10, zero),
@@ -206,45 +66,56 @@ test_case(test_case(LineWidth, MaxLines, Limit, Doc)) :-
         format(non_canonical_bool(42)), nl,
         format(non_canonical_bool(43)), nl
     ]),
-    ( Limit = linear(100)
-    ; Limit = linear(10)
-    ; Limit = triangular(100)
-    ; Limit = triangular(10)
-    ; Limit = linear(1)
-    ; Limit = triangular(1)
-    ),
-    ( MaxLines = 10
-    ; MaxLines = 3
-    ),
-    ( LineWidth = 78
-    ; LineWidth = 38
-    ),
-    ( Doc = format(List)
-    ; Doc = format_list(ListUniv, str(", "))
-    ; Doc = format(MapFloat)
-    ; Doc = format(MapStr)
-    ; Doc = format(OpTree)
-    ; Doc = format(Church)
-    ; Doc = fmt_susp_seq(100)
-    ; Doc = format(Tuple)
-    ; Doc = format(Square)
-    ; Doc = NonCanonTest
-    ; Doc = IndentTest
-    ).
+
+    Docs = [
+        test_case("list",            format(List)),
+        test_case("list_univ",       format_list(ListUniv, str(", "))),
+        test_case("map_float",       format(MapFloat)),
+        test_case("map_hext_str",    format(MapStr)),
+        test_case("op_tree",         format(OpTree)),
+        test_case("church",          format(Church)),
+        test_case("lazy_countdown",  lazy_countdown(100)),
+        test_case("tuple",           format(Tuple)),
+        test_case("square",          format(Square)),
+        test_case("noncanon",        NonCanonTest),
+        test_case("indent",          IndentTest)
+    ].
+
+%---------------------------------------------------------------------------%
+
+:- type church
+    --->    zero
+    ;       succ(church).
+
+%---------------------------------------------------------------------------%
+
+:- type op_tree
+    --->    x
+    ;       - op_tree
+    ;       op_tree + op_tree
+    ;       op_tree - op_tree
+    ;       op_tree * op_tree
+    ;       op_tree / op_tree.
 
 :- func mk_op_tree(int) = op_tree.
 
 mk_op_tree(N) =
-    (      if N =< 3      then x
-      else if N mod 5 = 0 then - mk_op_tree(N - 1)
-      else if N mod 5 = 1 then mk_op_tree(0 + N/2) + mk_op_tree(0 + N/3)
-      else if N mod 5 = 2 then mk_op_tree(1 + N/2) - mk_op_tree(1 + N/3)
-      else if N mod 5 = 3 then mk_op_tree(0 + N/2) * mk_op_tree(2 + N/3)
-      else                     mk_op_tree(1 + N/2) / mk_op_tree(0 + N/3)
+    (    if N =< 3      then x
+    else if N mod 5 = 0 then - mk_op_tree(N - 1)
+    else if N mod 5 = 1 then mk_op_tree(0 + N/2) + mk_op_tree(0 + N/3)
+    else if N mod 5 = 2 then mk_op_tree(1 + N/2) - mk_op_tree(1 + N/3)
+    else if N mod 5 = 3 then mk_op_tree(0 + N/2) * mk_op_tree(2 + N/3)
+    else                     mk_op_tree(1 + N/2) / mk_op_tree(0 + N/3)
     ).
 
+%---------------------------------------------------------------------------%
+
+:- type non_canonical_bool
+    --->    non_canonical_bool(int)
+    where equality is non_canonical_bool_eq.
+
 :- pred non_canonical_bool_eq(non_canonical_bool::in, non_canonical_bool::in)
-        is semidet.
+    is semidet.
 
 non_canonical_bool_eq(A, B) :-
     promise_equivalent_solutions [AX, BX] (
@@ -252,5 +123,19 @@ non_canonical_bool_eq(A, B) :-
         B = non_canonical_bool(BX)
     ),
     AX /\ 1 = BX /\ 1.
+
+%---------------------------------------------------------------------------%
+
+:- func lazy_countdown(int) = doc.
+
+lazy_countdown(N) =
+    ( if N =< 1 then
+        str("1")
+    else
+        docs([
+            group([nl, format(N), str(".")]),
+            format_susp((func) = lazy_countdown(N - 1))
+        ])
+    ).
 
 %---------------------------------------------------------------------------%
---
+++ tests/hard_coded/test_pretty_printer_helper.m
@@ -0,0 +1,200 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+%
+% Utilities for the test cases that test the pretty_printer's operation.
+%
+%---------------------------------------------------------------------------%
+
+:- module test_pretty_printer_helper.
+
+:- interface.
+
+:- import_module io.
+:- import_module pretty_printer.
+
+%---------------------------------------------------------------------------%
+
+:- pred setup_pretty_printer(io::di, io::uo) is det.
+
+%---------------------------------------------------------------------------%
+
+:- type test_case
+    --->    test_case(string, doc).
+
+:- pred run_test_case(test_case::in, io::di, io::uo) is det.
+
+%---------------------------------------------------------------------------%
+
+:- implementation.
+
+:- import_module int.
+:- import_module list.
+:- import_module map.
+:- import_module string.
+:- import_module term_io.
+:- import_module type_desc.
+:- import_module univ.
+
+%---------------------------------------------------------------------------%
+
+setup_pretty_printer(!IO) :-
+    set_default_formatter("builtin", "int",        0, fmt_int,    !IO),
+    set_default_formatter("builtin", "float",      0, fmt_float,  !IO),
+    set_default_formatter("builtin", "character",  0, fmt_char,   !IO),
+    set_default_formatter("builtin", "string",     0, fmt_string, !IO),
+    set_default_formatter("list",    "list",       1, fmt_list,   !IO),
+    set_default_formatter("tree234", "tree234",    2, fmt_map,    !IO).
+
+%---------------------%
+
+:- func fmt_int(univ, list(type_desc)) = doc.
+
+fmt_int(Univ, _) =
+    ( if Univ = univ(Int) then
+        str(string.int_to_string(Int))
+    else
+        str("?fmt_int?")
+    ).
+
+%---------------------%
+
+:- func fmt_float(univ, list(type_desc)) = doc.
+
+fmt_float(Univ, _) =
+    ( if Univ = univ(Float) then
+        str(string.float_to_string(Float))
+    else
+        str("?fmt_float?")
+    ).
+
+%---------------------%
+
+:- func fmt_char(univ, list(type_desc)) = doc.
+
+fmt_char(Univ, _) =
+    ( if Univ = univ(Char) then
+        str(term_io.quoted_char(Char))
+    else
+        str("?fmt_char?")
+    ).
+
+%---------------------%
+
+:- func fmt_string(univ, list(type_desc)) = doc.
+
+fmt_string(Univ, _) =
+    ( if Univ = univ(String) then
+        docs([str("\""), str(String), str("\"")])
+    else
+        str("?fmt_string?")
+    ).
+
+%---------------------%
+
+:- func fmt_list(univ, list(type_desc)) = doc.
+
+fmt_list(Univ, ArgDescs) = Doc :-
+    ( if
+        ArgDescs = [ArgDesc],
+        has_type(Arg, ArgDesc),
+        same_list_type(List, Arg),
+        Value = univ_value(Univ),
+        dynamic_cast(Value, List),
+        UnivList = list.map(make_univ, List)
+    then
+        Doc = indent([str("["), format_list(UnivList, str(", ")), str("]")])
+    else
+        Doc = str("?fmt_list?")
+    ).
+
+:- pred same_list_type(list(T)::unused, T::unused) is det.
+
+same_list_type(_, _).
+
+:- func make_univ(T) = univ.
+
+make_univ(X) = univ(X).
+
+%---------------------%
+
+:- func fmt_map(univ, list(type_desc)) = doc.
+
+fmt_map(Univ, ArgDescs) = Doc :-
+    ( if
+        ArgDescs = [KArgDesc, VArgDesc],
+        has_type(K, KArgDesc),
+        has_type(V, VArgDesc),
+        same_map_type(Map, K, V),
+        Value = univ_value(Univ),
+        dynamic_cast(Value, Map)
+    then
+        UnivList =
+            map.foldr(func(KK, VV, KVs) = [univ(KK -> VV) | KVs], Map, []),
+        Doc = indent([
+            str("map(["), format_list(UnivList, str(", ")), str("])")
+        ])
+    else
+        Doc = str("?fmt_map?")
+    ).
+
+:- type key_value(K, V)
+    --->    (K -> V).
+
+:- pred same_map_type(map(K, V)::unused, K::unused, V::unused) is det.
+
+same_map_type(_, _, _).
+
+%---------------------------------------------------------------------------%
+
+run_test_case(TestCase, !IO) :-
+    TestCase = test_case(Name, Doc),
+    io.nl(!IO),
+    io.write_string("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n", !IO),
+    io.format("Test case %s\n", [s(Name)], !IO),
+    io.write_string("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n", !IO),
+
+    LineWidths = [78, 38],
+    MaxLines = [10, 3],
+    Limits = [linear(100), linear(10), linear(1),
+        triangular(100), triangular(10), triangular(1)],
+    run_test_case_loop_1(Doc, LineWidths, MaxLines, Limits, !IO).
+
+:- pred run_test_case_loop_1(doc::in, list(int)::in, list(int)::in,   
+    list(func_symbol_limit)::in, io::di, io::uo) is det.
+
+run_test_case_loop_1(_Doc, [], _MaxLines, _Limits, !IO).
+run_test_case_loop_1(Doc, [LineWidth | LineWidths], MaxLines, Limits, !IO) :-
+    run_test_case_loop_2(Doc, LineWidth, MaxLines, Limits, !IO),
+    run_test_case_loop_1(Doc, LineWidths, MaxLines, Limits, !IO).
+
+:- pred run_test_case_loop_2(doc::in, int::in, list(int)::in,   
+    list(func_symbol_limit)::in, io::di, io::uo) is det.
+
+run_test_case_loop_2(_Doc, _LineWidth, [], _Limits, !IO).
+run_test_case_loop_2(Doc, LineWidth, [MaxLine | MaxLines], Limits, !IO) :-
+    run_test_case_loop_3(Doc, LineWidth, MaxLine, Limits, !IO),
+    run_test_case_loop_2(Doc, LineWidth, MaxLines, Limits, !IO).
+
+:- pred run_test_case_loop_3(doc::in, int::in, int::in,   
+    list(func_symbol_limit)::in, io::di, io::uo) is det.
+
+run_test_case_loop_3(_Doc, _LineWidth, _MaxLines, [], !IO).
+run_test_case_loop_3(Doc, LineWidth, MaxLines, [Limit | Limits], !IO) :-
+    run_test_case_with_params(Doc, LineWidth, MaxLines, Limit, !IO),
+    run_test_case_loop_3(Doc, LineWidth, MaxLines, Limits, !IO).
+
+:- pred run_test_case_with_params(doc::in, int::in, int::in,
+    func_symbol_limit::in, io::di, io::uo) is det.
+
+run_test_case_with_params(Doc, LineWidth, MaxLines, Limit, !IO) :-
+    Params = pp_params(LineWidth, MaxLines, Limit),
+    io.format("\nline width = %d, max lines = %d, limit = %s",
+        [i(LineWidth), i(MaxLines), s(string.string(Limit))], !IO),
+    Ruler = "\n|" ++ string.duplicate_char(('-'), LineWidth - 2) ++ "|\n",
+    io.write_string(Ruler, !IO),
+    set_default_params(Params, !IO),
+    pretty_printer.write_doc(Doc, !IO),
+    io.write_string(Ruler, !IO).
+
+%---------------------------------------------------------------------------%
