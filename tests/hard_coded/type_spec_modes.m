55521025bb3426618540edf781d0eaf54813aa86
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2022-07-27 11:18:57 +1000

    Test type_spec pragmas for functions.
    tests/hard_coded/type_spec_modes.{m,exp}:
        Add a test of type specialization pragmas for functions, since
        none of our other tests of type_spec pragmas does so.

--- tests/hard_coded/type_spec_modes.m
+++ tests/hard_coded/type_spec_modes.m
@@ -1,7 +1,7 @@
 %---------------------------------------------------------------------------%
 % vim: ts=4 sw=4 et ft=mercury
 %---------------------------------------------------------------------------%
-%
+
 :- module type_spec_modes.
 :- interface.
 
@@ -11,6 +11,7 @@
 
 :- implementation.
 
+:- import_module bool.
 :- import_module int.
 :- import_module list.
 
@@ -18,14 +19,37 @@
 :- mode my_unify(in, in) is semidet.
 :- mode my_unify(in, out) is det.
 :- mode my_unify(out, in) is det.
+
+:- pragma no_inline(my_unify/2).
 :- pragma type_spec(my_unify(in, in), T = list(int)).
 :- pragma type_spec(my_unify(in, in), T = int).
 :- pragma type_spec(my_unify(in, out), T = int).
 :- pragma type_spec(my_unify(out, in), T = list(int)).
 
-:- pragma no_inline(my_unify/2).
-
 my_unify(X, X).
+
+:- func is_ascending(T, list(T)) = bool.
+:- mode is_ascending(in, in) = out.
+
+:- pragma type_spec(is_ascending(in, in) = out, T = int).
+
+is_ascending(A, Bs) = IsAscending :-
+    (
+        Bs = [],
+        IsAscending = yes
+    ;
+        Bs = [B | Cs],
+        compare(CmpRes, A, B),
+        (
+            CmpRes = (<),
+            IsAscending = is_ascending(B, Cs)
+        ;
+            ( CmpRes = (=)
+            ; CmpRes = (>)
+            ),
+            IsAscending = no
+        )
+    ).
 
 main(!IO) :-
     ( if my_unify(1, 1) then
@@ -43,4 +67,16 @@ main(!IO) :-
         io.write_string("yes\n", !IO)
     else
         io.write_string("no\n", !IO)
-    ).
+    ),
+    IsAscending123 = is_ascending(1, [2, 3]),
+    io.write_line(IsAscending123, !IO),
+    IsAscending133 = is_ascending(1, [3, 3]),
+    io.write_line(IsAscending133, !IO),
+    IsAscending213 = is_ascending(2, [1, 3]),
+    io.write_line(IsAscending213, !IO),
+    IsAscendingABC = is_ascending("a", ["b", "c"]),
+    io.write_line(IsAscendingABC, !IO),
+    IsAscendingACC = is_ascending("a", ["c", "c"]),
+    io.write_line(IsAscendingACC, !IO),
+    IsAscendingBAC = is_ascending("b", ["a", "c"]),
+    io.write_line(IsAscendingBAC, !IO).
