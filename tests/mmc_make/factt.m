Mercury-Language/mercury

commit 5f50259d16c12286261d070f04d66f2ab19ced92

Write to explicitly named streams in many modules.
Right now, most parts of the compiler write to the "current output stream".
This was a pragmatic choice at the time, but has not aged well. The problem
is that the answer to the question "where is the current output stream going?"
is not obvious in *all* places in the compiler (although it is obvious in
most). When using such implicit streams, finding where the output is going
to in a given predicate requires inspecting not just the ancestors of that
predicate, but also all their older siblings (since any of them could have
changed the current stream), *including* their entire call trees. This is
usually an infeasible task. By constrast, if we explicitly pass streams
to all output operations, we need only follow the places where the variable
representing that stream is bound, which the mode system makes easy.

This diff switches large parts of the compiler over to doing output only
to explicitly passed streams, never to the implicit "current output stream".
The parts it switches over are the parts that rely to a significant degree
on the innermost change, which is to the "output" typeclass in
parse_tree_out_info.m. This is the part that has to be switched over to
explicit streams first, because (a) many modules such as mercury_to_mercury.m
rely on the output typeclass, and (b) most other modules that do output
call predicates in these modules. Starting anywhere else would be like
building a skyscraper starting at the top.

This typeclass, output(U), has two instances: output(io), and output(string),
so you could output either to the current output stream, or to a string.
To allow the specification of the destination stream in the first case,
this diff changes the typeclass to output(S, U) with a functional dependency
from U to S, with the two instances being output(io.text_output_stream, io)
and output(unit, string). (The unit arg is ignored in the second case.)

compiler/make.module_dep_file.m
@@ -53,6 +53,7 @@
 :- import_module parse_tree.mercury_to_mercury.
 :- import_module parse_tree.parse_error.
 :- import_module parse_tree.parse_sym_name.
+:- import_module parse_tree.parse_tree_out_info.
 :- import_module parse_tree.prog_data_foreign.
 :- import_module parse_tree.prog_item.
 :- import_module parse_tree.prog_out.
@@ -324,10 +325,9 @@ do_write_module_dep_file(Globals, ModuleAndImports, !IO) :-
     io.open_output(ProgDepFile, ProgDepResult, !IO),
     (
         ProgDepResult = ok(ProgDepStream),
-        io.set_output_stream(ProgDepStream, OldOutputStream, !IO),
         choose_module_dep_file_version(ModuleAndImports, Version),
-        do_write_module_dep_file_2(ModuleAndImports, Version, !IO),
-        io.set_output_stream(OldOutputStream, _, !IO),
+        do_write_module_dep_file_2(ProgDepStream, ModuleAndImports,
+            Version, !IO),
         io.close_output(ProgDepStream, !IO)
     ;
         ProgDepResult = error(Error),
@@ -349,10 +349,11 @@ choose_module_dep_file_version(ModuleAndImports, Version) :-
         Version = module_dep_file_v2
     ).
 
-:- pred do_write_module_dep_file_2(module_and_imports::in,
-    module_dep_file_version::in, io::di, io::uo) is det.
+:- pred do_write_module_dep_file_2(io.text_output_stream::in,
+    module_and_imports::in, module_dep_file_version::in,
+    io::di, io::uo) is det.
 
-do_write_module_dep_file_2(ModuleAndImports, Version, !IO) :-
+do_write_module_dep_file_2(Stream, ModuleAndImports, Version, !IO) :-
     module_and_imports_get_source_file_name(ModuleAndImports, SourceFileName),
     module_and_imports_get_source_file_module_name(ModuleAndImports,
         SourceFileModuleName),
@@ -369,28 +370,31 @@ do_write_module_dep_file_2(ModuleAndImports, Version, !IO) :-
     module_and_imports_get_c_j_cs_fims(ModuleAndImports, CJCsEFIMs),
     module_and_imports_get_foreign_include_files(ModuleAndImports,
         ForeignIncludeFiles),
-    io.write_string("module(", !IO),
+    io.write_string(Stream, "module(", !IO),
     version_number(Version, VersionNumber),
-    io.write_int(VersionNumber, !IO),
-    io.write_string(", """, !IO),
-    io.write_string(SourceFileName, !IO),
-    io.write_string(""",\n\t", !IO),
-    mercury_output_bracketed_sym_name(SourceFileModuleName, !IO),
-    io.write_string(",\n\t{", !IO),
-    io.write_list(set.to_sorted_list(Ancestors),
-        ", ", mercury_output_bracketed_sym_name, !IO),
-    io.write_string("},\n\t{", !IO),
-    io.write_list(IntDeps, ", ", mercury_output_bracketed_sym_name, !IO),
-    io.write_string("},\n\t{", !IO),
-    io.write_list(ImpDeps, ", ", mercury_output_bracketed_sym_name, !IO),
-    io.write_string("},\n\t{", !IO),
-    io.write_list(Children, ", ", mercury_output_bracketed_sym_name, !IO),
-    io.write_string("},\n\t{", !IO),
-    io.write_list(set.to_sorted_list(NestedChildren),
-        ", ", mercury_output_bracketed_sym_name, !IO),
-    io.write_string("},\n\t{", !IO),
-    io.write_list(FactDeps, ", ", io.write, !IO),
-    io.write_string("},\n\t{", !IO),
+    io.write_int(Stream, VersionNumber, !IO),
+    io.write_string(Stream, ", """, !IO),
+    io.write_string(Stream, SourceFileName, !IO),
+    io.write_string(Stream, """,\n\t", !IO),
+    mercury_output_bracketed_sym_name(SourceFileModuleName, Stream, !IO),
+    io.write_string(Stream, ",\n\t{", !IO),
+    write_out_list(mercury_output_bracketed_sym_name, ", ",
+        set.to_sorted_list(Ancestors), Stream, !IO),
+    io.write_string(Stream, "},\n\t{", !IO),
+    write_out_list(mercury_output_bracketed_sym_name, ", ",
+        IntDeps, Stream, !IO),
+    io.write_string(Stream, "},\n\t{", !IO),
+    write_out_list(mercury_output_bracketed_sym_name, ", ",
+        ImpDeps, Stream, !IO),
+    io.write_string(Stream, "},\n\t{", !IO),
+    write_out_list(mercury_output_bracketed_sym_name, ", ",
+        Children, Stream, !IO),
+    io.write_string(Stream, "},\n\t{", !IO),
+    write_out_list(mercury_output_bracketed_sym_name, ", ",
+        set.to_sorted_list(NestedChildren), Stream, !IO),
+    io.write_string(Stream, "},\n\t{", !IO),
+    io.write_string(Stream, string.join_list(", ", FactDeps), !IO),
+    io.write_string(Stream, "},\n\t{", !IO),
     (
         ContainsForeignCode = foreign_code_langs_known(ForeignLanguageSet),
         ForeignLanguages = set.to_sorted_list(ForeignLanguageSet)
@@ -404,46 +408,47 @@ do_write_module_dep_file_2(ModuleAndImports, Version, !IO) :-
         % XXX CLEANUP We should try replacing this assignment with an abort.
         ForeignLanguages = []
     ),
-    io.write_list(ForeignLanguages,
-        ", ", mercury_output_foreign_language_string, !IO),
-    io.write_string("},\n\t{", !IO),
+    write_out_list(mercury_output_foreign_language_string, ", ",
+        ForeignLanguages, Stream, !IO),
+    io.write_string(Stream, "},\n\t{", !IO),
     FIMSpecs = get_all_fim_specs(CJCsEFIMs),
-    io.write_list(set.to_sorted_list(FIMSpecs), ", ", write_fim_spec, !IO),
-    io.write_string("},\n\t", !IO),
+    write_out_list(write_fim_spec, ", ",
+        set.to_sorted_list(FIMSpecs), Stream, !IO),
+    io.write_string(Stream, "},\n\t", !IO),
     contains_foreign_export_to_string(ContainsForeignExport,
         ContainsForeignExportStr),
-    io.write_string(ContainsForeignExportStr, !IO),
-    io.write_string(",\n\t", !IO),
+    io.write_string(Stream, ContainsForeignExportStr, !IO),
+    io.write_string(Stream, ",\n\t", !IO),
     % The has_main/no_main slot is not needed anymore, so we just put no_main
     % in there always.
-    io.write_string("no_main", !IO),
+    io.write_string(Stream, "no_main", !IO),
     (
         Version = module_dep_file_v1
     ;
         Version = module_dep_file_v2,
-        io.write_string(",\n\t{", !IO),
-        io.write_list(cord.list(ForeignIncludeFiles),
-            ", ", write_foreign_include_file_info, !IO),
-        io.write_string("}", !IO)
+        io.write_string(Stream, ",\n\t{", !IO),
+        write_out_list(write_foreign_include_file_info, ", ",
+            cord.list(ForeignIncludeFiles), Stream, !IO),
+        io.write_string(Stream, "}", !IO)
     ),
-    io.write_string("\n).\n", !IO).
+    io.write_string(Stream, "\n).\n", !IO).
 
-:- pred write_fim_spec(fim_spec::in,
+:- pred write_fim_spec(fim_spec::in, io.text_output_stream::in,
     io::di, io::uo) is det.
 
-write_fim_spec(FIMSpec, !IO) :-
+write_fim_spec(FIMSpec, Stream, !IO) :-
     FIMSpec = fim_spec(Lang, ForeignImport),
-    mercury_output_foreign_language_string(Lang, !IO),
-    io.write_string(" - ", !IO),
-    mercury_output_bracketed_sym_name(ForeignImport, !IO).
+    mercury_output_foreign_language_string(Lang, Stream, !IO),
+    io.write_string(Stream, " - ", !IO),
+    mercury_output_bracketed_sym_name(ForeignImport, Stream, !IO).
 
 :- pred write_foreign_include_file_info(foreign_include_file_info::in,
-    io::di, io::uo) is det.
+    io.text_output_stream::in, io::di, io::uo) is det.
 
-write_foreign_include_file_info(ForeignInclude, !IO) :-
+write_foreign_include_file_info(ForeignInclude, Stream, !IO) :-
     ForeignInclude = foreign_include_file_info(Lang, FileName),
-    mercury_output_foreign_language_string(Lang, !IO),
-    io.write_string(" - ", !IO),
+    mercury_output_foreign_language_string(Lang, Stream, !IO),
+    io.write_string(Stream, " - ", !IO),
     term_io.quote_string(FileName, !IO).
 
 :- pred contains_foreign_export_to_string(contains_foreign_export, string).
@@ -942,9 +947,9 @@ cleanup_module_dep_file(Globals, ModuleName, !Info, !IO) :-
 maybe_write_importing_module(_, no, !IO).
 maybe_write_importing_module(ModuleName, yes(ImportingModuleName), !IO) :-
     io.write_string("** Module `", !IO),
-    write_sym_name(ModuleName, !IO),
+    write_sym_name_to_cur_stream(ModuleName, !IO),
     io.write_string("' is imported or included by module `", !IO),
-    write_sym_name(ImportingModuleName, !IO),
+    write_sym_name_to_cur_stream(ImportingModuleName, !IO),
     io.write_string("'.\n", !IO).
 
 %-----------------------------------------------------------------------------%

commit 1bc5cadf2522650fd19fbe3dd528af8b49641dc6

Fix writing fact table file names in .module_dep files.
Commit 5f50259d16c12286261d070f04d66f2ab19ced92 inadvertently wrote out
the list of fact table file names in a .module_dep file without quoting,
so the .module_dep parser would fail to read it back.

compiler/make.module_dep_file.m:
    Write fact table file names in .module_dep files as quoted strings.

tests/mmc_make/Mmakefile:
tests/mmc_make/factt.m:
tests/mmc_make/factt_examples:
    Add a test case.

compiler/make.module_dep_file.m
@@ -389,9 +389,10 @@ do_write_module_dep_file_to_stream(Stream, Globals,
     MaybeTopModule = Baggage ^ mb_maybe_top_module,
     NestedSubModules = get_nested_children_list_of_top_module(MaybeTopModule),
     get_fact_tables(ParseTreeModuleSrc, FactTableFilesSet),
-    set.to_sorted_list(FactTableFilesSet, FactTableFiles),
+    FactTableFilesStrs = list.map(term_io.quoted_string,
+        set.to_sorted_list(FactTableFilesSet)),
     globals.get_backend_foreign_languages(Globals, BackendLangsList),
-    BackendLangs = set.list_to_set(BackendLangsList), 
+    BackendLangs = set.list_to_set(BackendLangsList),
     get_foreign_code_langs(ParseTreeModuleSrc, CodeLangs),
     get_foreign_export_langs(ParseTreeModuleSrc, ExportLangs),
     set.intersect(BackendLangs, CodeLangs, BackendCodeLangs),
@@ -434,7 +435,7 @@ do_write_module_dep_file_to_stream(Stream, Globals,
         s(bracketed_sym_names_to_comma_list_string(ImpDeps)),
         s(bracketed_sym_names_to_comma_list_string(Children)),
         s(bracketed_sym_names_to_comma_list_string(NestedSubModules)),
-        s(string.join_list(", ", FactTableFiles)),
+        s(string.join_list(", ", FactTableFilesStrs)),
         s(string.join_list(", ", CodeLangStrs)),
         s(string.join_list(", ", FIMSpecStrs)),
         s(ContainsForeignExportStr),

tests/mmc_make/Mmakefile
@@ -10,7 +10,7 @@ MAYBE_J1 =
 
 MMAKE_USE_MMC_MAKE=yes
 
-PROGS =	\
+PROGS0 =	\
 	bug489 \
 	complex_test \
 	hello \
@@ -18,6 +18,15 @@ PROGS =	\
 	include_file2 \
 	linkage_test \
 	rebuild
+
+ifeq "$(filter csharp% java%,$(GRADE))" ""
+	C_ONLY_PROGS = \
+		factt
+else
+	C_ONLY_PROGS =
+endif
+
+PROGS = $(PROGS0) $(C_ONLY_PROGS)
 
 # These tests only work if the workspace was compiled with `--use-subdirs'.
 ifneq ($(origin WORKSPACE),undefined)
@@ -58,6 +67,13 @@ rebuild.runtest:
 # The compiler used to fail when invoked as `mmc --make build_object.o'.
 build_object.runtest: build_object.o
 
+# Check that no errors occur while reading back the .module_dep file on the
+# second run.
+factt.runtest:
+	$(MCM) --make factt
+	$(MCM) --make factt >factt.err2 2>&1
+	! grep -i 'Error' factt.err2
+
 .PHONY: install_libs
 install_libs: start_runtests_local
 	$(MMAKE) TESTS_FLAGS
@@ -82,7 +98,7 @@ TESTS_FLAGS: ../TESTS_FLAGS
 realclean_local: realclean_mmc_make
 
 realclean_mmc_make: TESTS_FLAGS
-	rm -rf install include_file2.err rebuild.err2
+	rm -rf install include_file2.err rebuild.err2 factt.err2
 	# ./TESTS_FLAGS is expected by the following line.
 	cd lib; $(MCM) complex_numbers.realclean linkage_test2.realclean
 	rm -f TESTS_FLAGS

tests/mmc_make/factt.m
@@ -1,0 +1,39 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+
+:- module factt.
+:- interface.
+
+:- import_module io.
+:- pred main(io::di, io::uo) is det.
+
+%---------------------------------------------------------------------------%
+
+:- implementation.
+
+:- import_module int.
+:- import_module list.
+:- import_module string.
+
+:- pred example(int::in, int::out, int::out) is semidet.
+:- pragma fact_table(example/3, "factt_examples").
+
+%---------------------------------------------------------------------------%
+
+main(!IO) :-
+    show_examples(1, !IO).
+
+:- pred show_examples(int::in, io::di, io::uo) is det.
+
+show_examples(Cur, !IO) :-
+    ( if Cur > 50 then
+        true
+    else
+        ( if example(Cur, A, B) then
+            io.format("%2d %2d %2d\n", [i(Cur), i(A), i(B)], !IO)
+        else
+            io.format("%2d  -  -\n", [i(Cur)], !IO)
+        ),
+        show_examples(Cur + 1, !IO)
+    ).

tests/mmc_make/factt_examples
@@ -1,0 +1,4 @@
+example(1,0,1).
+%to
+example(50,1,50).
+