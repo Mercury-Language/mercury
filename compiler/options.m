3dfa25d6415febd8b30fce91c5d274c1b7a90f60
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2022-12-28 05:42:08 +1100

    Fix a compiler abort in term_pass2.m.
    compiler/term_pass2.m:
        Fix a compiler abort that would occur if we replaced a call
        to array.lookup with a call to array.unsafe_lookup in array_to_doc_loop
        in pretty_printer.m.
    
    compiler/options.m:
        Provide a way to check for the presence of the fix.
    
    NEWS:
        Mention the fix.

--- compiler/options.m
+++ compiler/options.m
@@ -3210,6 +3210,8 @@ long_option("ushift-2022-12-07",
                                     compiler_sufficiently_recent).
 long_option("strtrie-2022-12-08",
                                     compiler_sufficiently_recent).
+long_option("term-pass2-2022-12-28",
+                                    compiler_sufficiently_recent).
 long_option("experiment",           experiment).
 long_option("experiment1",          experiment1).
 long_option("experiment2",          experiment2).
