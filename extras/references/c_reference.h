485d644d37f2d273185f387362446bf1698647e8
Julien Fischer <juliensf@gmail.com>
2023-02-25 12:59:54 +1100

    Update extras/references.
    extra/references/c_reference.h:
         Replace refernces to the old c_header_code and c_code
         pragmas.

--- extras/references/c_reference.h
+++ extras/references/c_reference.h
@@ -7,9 +7,9 @@
 /*
 ** This file exposes the C types used by the reference and nb_reference
 ** modules, so that one can allocate them somewhere other than the heap if
-** one so desires.  Normally this should not be necessary, and the cleaner,
+** one so desires. Normally this should not be necessary, and the cleaner,
 ** safer interfaces provided by reference.m and nb_reference.m should be
-** used instead.  However, sometimes it is useful; for example, the HAL
+** used instead. However, sometimes it is useful; for example, the HAL
 ** compiler would like to be able to allocate them at compile-time-known
 ** locations, in order to implement global variables.
 **
@@ -21,21 +21,24 @@
 ** The following example declares an ME_Reference `foo' at a
 ** compile-time-known location, and provides a zero-arity function for
 ** returning the corresponding Mercury object of type `reference/1'.  Note
-** that this reference should be initialised with the `reference:init/2'
+** that this reference should be initialised with the `reference.init/2'
 ** predicate before use: see the documentation of that predicate in
 ** reference.m for more caveats.
 **
-** :- pragma c_header_code("
-** #include ""c_reference.h""
-** extern ME_Reference foo;
+** :- pragma foreign_decl("C", "
+**     #include ""c_reference.h""
+**     extern ME_Reference foo;
 ** ").
 **
-** :- pragma c_code("
-** ME_Reference foo;
+** :- pragma foreign_code("C", "
+**     ME_Reference foo;
 ** ").
 **
-** :- pragma c_code(foo_reference = (X::out), will_not_call_mercury, "
-**         X = (MR_Word) &foo;
+** :- pragma foreign_proc(
+**     foo_reference = (X::out),
+**     [will_not_call_mercury],
+** "
+**     X = (MR_Word) &foo;
 ** ").
 */
 
@@ -45,11 +48,10 @@
 #include "mercury_trail.h"
 
 typedef struct {
-	void *value;
-	MR_ChoicepointId id;
+    void *value;
+    MR_ChoicepointId id;
 } ME_Reference;
 
 typedef MR_Word ME_NbReference;
 
 #endif	/* not C_REFERENCE_H */
-
