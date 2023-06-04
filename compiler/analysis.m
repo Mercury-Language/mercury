961577c7d0db51ec33bf7dab1c43e3e39a1bd405
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2023-06-04 10:21:58 +0200

    Fix indentation.

--- compiler/analysis.m
+++ compiler/analysis.m
@@ -50,22 +50,22 @@
 :- typeclass compiler(Compiler) where [
     func compiler_name(Compiler) = string,
 
-    % Describe the analyses which can be performed by a compiler.
-    %
+        % Describe the analyses which can be performed by a compiler.
+        %
     pred analyses(Compiler, analysis_name, analysis_type),
     mode analyses(in, in, out) is semidet,
     mode analyses(in, out, out) is multi,
 
-    % module_name_to_read_file_name(Compiler, Globals, Ext,
-    %   ModuleName, MaybeFileName, !IO)
-    %
+        % module_name_to_read_file_name(Compiler, Globals, Ext,
+        %   ModuleName, MaybeFileName, !IO)
+        %
     pred module_name_to_read_file_name(Compiler::in, globals::in,
         other_ext::in, module_name::in, maybe_error(string)::out,
         io::di, io::uo) is det,
 
-    % module_name_to_write_file_name(Compiler, Globals, Ext,
-    %   ModuleName, FileName, !IO)
-    %
+        % module_name_to_write_file_name(Compiler, Globals, Ext,
+        %   ModuleName, FileName, !IO)
+        %
     pred module_name_to_write_file_name(Compiler::in, globals::in,
         other_ext::in, module_name::in, string::out, io::di, io::uo) is det
 ].
