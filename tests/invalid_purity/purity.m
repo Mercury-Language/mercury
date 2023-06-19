d4e7cde83e0a6c9a3ba20c82cb3695387f171088
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2023-06-19 10:33:26 +0200

    Update tests/invalid_purity/purity.m.

--- tests/invalid_purity/purity.m
+++ tests/invalid_purity/purity.m
@@ -5,8 +5,7 @@
 :- module purity.
 :- interface.
 
-%----------------------------------------------------------------
-%  Needed for later tests.
+% This type is used by the tests below.
 :- type foo
     --->    a
     ;       b.
@@ -14,26 +13,26 @@
 :- implementation.
 
 :- impure pred imp is det.
+
 :- pragma foreign_proc("C",
     imp,
     [will_not_call_mercury],
 "
     ;
 ").
-
 :- pragma foreign_proc("Java",
     imp,
     [will_not_call_mercury],
 "
     ;
 ").
-
 :- pragma foreign_proc("C#",
     imp,
     [will_not_call_mercury],
 "
     ;
 ").
+imp.
 
 :- semipure pred semi is semidet.
 :- pragma foreign_proc("C",
@@ -42,26 +41,27 @@
 "
     SUCCESS_INDICATOR = 0;
 ").
-
 :- pragma foreign_proc("Java",
     semi,
     [promise_semipure, will_not_call_mercury],
 "
     SUCCESS_INDICATOR = false;
 ").
-
 :- pragma foreign_proc("C#",
     semi,
     [promise_semipure, will_not_call_mercury],
 "
     SUCCESS_INDICATOR = false;
 ").
+semi :-
+    semidet_fail.
 
-:- pred in(foo).
-:- mode in(in) is semidet.
+:- pred in(foo::in) is semidet.
+
 in(a).
 
 :- semipure pred semi(foo::in) is semidet.
+
 :- pragma foreign_proc("C",
     semi(X::in),
     [will_not_call_mercury, promise_semipure],
@@ -69,7 +69,6 @@ in(a).
     /* X */
     SUCCESS_INDICATOR = 0;
 ").
-
 :- pragma foreign_proc("Java",
     semi(X::in),
     [will_not_call_mercury, promise_semipure],
@@ -77,7 +76,6 @@ in(a).
     /* X */
     SUCCESS_INDICATOR = false;
 ").
-
 :- pragma foreign_proc("C#",
     semi(X::in),
     [will_not_call_mercury, promise_semipure],
@@ -108,6 +106,8 @@ in(a).
 "
     SUCCESS_INDICATOR = false;
 ").
+imp1(_) :-
+    semidet_fail.
 
 %----------------------------------------------------------------
 %  Warnings.
@@ -122,7 +122,8 @@ w2.
 
 :- impure pred w3 is semidet.
 
-w3 :- semipure semi.
+w3 :-
+    semipure semi.
 
 :- pred w4 is det.
 :- pragma promise_pure(w4/0).
@@ -132,35 +133,42 @@ w4.
 :- impure pred w5 is det.
 :- pragma promise_pure(w5/0).
 
-w5 :- impure imp.
+w5 :-
+    impure imp.
 
 :- semipure pred w6 is semidet.
 :- pragma promise_pure(w6/0).
 
-w6 :- semipure semi.
+w6 :-
+    semipure semi.
 
 %----------------------------------------------------------------
 %  Errors.
 
 :- pred e1 is det.
 
-e1 :- impure imp.
+e1 :-
+    impure imp.
 
 :- pred e2 is semidet.
 
-e2 :- semipure semi.
+e2 :-
+    semipure semi.
 
 :- semipure pred e3 is det.
 
-e3 :- impure imp.
+e3 :-
+    impure imp.
 
 :- impure pred e4 is det.
 
-e4 :- imp.
+e4 :-
+    imp.
 
 :- semipure pred e5 is semidet.
 
-e5 :- semi.
+e5 :-
+    semi.
 
 :- impure pred e6 is semidet.
 
@@ -175,10 +183,11 @@ e7 :-
     impure imp1(X),
     X = a.
 
-:- type e8 ---> e8(foo) where equality is imp2.
+:- type e8
+    --->    e8(foo)
+    where equality is imp2.
 
-:- impure pred imp2(e8, e8).
-:- mode imp2(in, in) is semidet.
+:- impure pred imp2(e8::in, e8::in) is semidet.
 
 :- pragma foreign_proc("C",
     imp2(_X::in, _Y::in),
@@ -186,11 +195,14 @@ e7 :-
 "
     SUCCESS_INDICATOR = 0;
 ").
+imp2(_, _) :-
+    semidet_fail.
 
-:- type e9 ---> e9(foo) where equality is semi2.
+:- type e9
+    --->    e9(foo)
+    where equality is semi2.
 
-:- semipure pred semi2(e9, e9).
-:- mode semi2(in, in) is semidet.
+:- semipure pred semi2(e9::in, e9::in) is semidet.
 
 :- pragma foreign_proc("C",
     semi2(_X::in, _Y::in),
@@ -198,6 +210,8 @@ e7 :-
 "
     SUCCESS_INDICATOR = 0;
 ").
+semi2(_, _) :-
+    semidet_fail.
 
 :- pred e10 is semidet.
 
@@ -210,9 +224,3 @@ e10 :-
 e11 :-
     Goal2 = (pred(X::in) is semidet :- semi(X)),
     call(Goal2, b).
-
-imp.
-semi :- semidet_fail.
-imp1(_) :- semidet_fail.
-imp2(_, _) :- semidet_fail.
-semi2(_, _) :- semidet_fail.
