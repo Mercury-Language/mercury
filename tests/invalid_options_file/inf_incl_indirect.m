c16f53c0a39c8a45f3cc1cff8ae62c07378eeff6
Julien Fischer <juliensf@gmail.com>
2023-07-08 16:49:20 +1000

    Fix some test failures on Windows.
    tests/invalid_options_file/inf_incl_direct.err_exp2:
    tests/invalid_options_file/inf_incl_indirect.err_exp2:
    tests/invalid_options_file/nonexistent_file.err_exp2:
         Handle the case where "\" is used as the directory separator.
    
    tests/invalid_options_file/inf_incl_direct.m:
    tests/invalid_options_file/inf_incl_indirect.m:
    tests/invalid_options_file/nonexistent_file.m:
        Document what each of the .err_exp files corresponds to.

--- tests/invalid_options_file/inf_incl_indirect.m
+++ tests/invalid_options_file/inf_incl_indirect.m
@@ -1,6 +1,8 @@
 %---------------------------------------------------------------------------%
 % vim: ts=4 sw=4 et ft=mercury
 %---------------------------------------------------------------------------%
+% The .err_exp file is for systems where the directory separator is "/".
+% The .err_exp2 file is for systems where the directory separator is "\".
 
 :- module infinite_include_indirect.
 
