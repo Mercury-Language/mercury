7e147c92ef84da303e467f8e2c5ac0ede9cafa65
Peter Wang <novalazy@gmail.com>
2023-07-12 11:29:28 +1000

    Change git submodule update command in prepare.sh.
    prepare.sh:
        Don't pass --remote to the 'git submodule update'.
        We want the boehm_gc submodule to be checked out at the commit
        recorded in the mercury repository, not automatically update to the
        latest commit on the branch listed in .gitmodules. Similarly for
        libatomic_ops within the boehm_gc submodule.
    
        Fix help message.

--- prepare.sh
+++ prepare.sh
@@ -1,15 +1,16 @@
-#!/bin/sh -e
+#!/bin/sh
 # This script should be run in a fresh git checkout in order to initialise
 # the necessary git submodules and produce the configure script.
+set -e
 
 if [ ! -e boehm_gc/.git ]; then
     echo "Setting up submodules"
     if git submodule --quiet init; then
-        git submodule update --remote --init --recursive
+        git submodule update --init --recursive
     else
-        echo "There was a problem configuring the submodules.  If the"
-        echo "repositories could not be found then edit .git/config and run"
-        echo "get submodule update"
+        echo "There was a problem configuring the submodules."
+        echo "If the repositories could not be found then edit .gitmodules"
+        echo "then run: git submodule update --recursive"
         exit 1
     fi
 fi
