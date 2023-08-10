Mercury-Language/mercury

commit 315fd9eb03b4afec6f6959fe600065e9a7295c70

Add a subdir_setting field to the globals.
compiler/globals.m:
    Define a new enum type with three values, which represents the three
    legal combination of the values of the use_subdir and use_grade_subdir
    options.

    Add a field of this type to the globals.

compiler/handle_options.m:
    Set up the value of this field in the globals. This means initializing
    it to a non-meaningful value when the globals is first created, and
    filling it in with the final meaningful value when we finish the setup
    of the global structure.

compiler/options.m:
    Add a "setting_only_" to the internal names of the use_subdir and
    use_grade_subdir options, to signal that their only valid use
    from now on is in deciding the value of the new globals field.
    (Their user-facing names remain unchanged.)

compiler/compile_target_code.m:
compiler/file_names.m:
compiler/make.program_target.m:
compiler/module_cmds.m:
compiler/write_deps_file.m:
    Rewrite code that used to make decisions based on the values of
    the use_subdir and use_grade_subdir options with code that makes
    decisions based on the value of the new field. A field retrieval
    followed by a three-way switch is simpler (and faster) than
    two option lookups and one two-way switch inside another.

    Consistently reorder the code so that the code of the simpler cases
    come first.

    Note a situation where we should be using an even simpler basis
    for the decision.

compiler/handle_options.m
@@ -724,10 +724,16 @@ convert_options_to_globals(ProgressStream, DefaultOptionTable, OptionTable0,
         FileInstallCmd = install_cmd_user(InstallCmd, InstallCmdDirOption)
     ),
 
+    % The use_cur_dir we pass here as the subdir setting is a dummy;
+    % the real value is set at the very end of this predicate,
+    % We can do this because no code between here and there uses
+    % the value of that field, and we *have* to do this because
+    % the bcode between here and there *can* update the values of
+    % the options from which the subdir setting is computed.
     globals_init(DefaultOptionTable, OptionTable0, !.OptTuple, OpMode, Target,
         WordSize, GC_Method, TermNorm, Term2Norm,
         TraceLevel, TraceSuppress, SSTraceLevel,
-        MaybeThreadSafe, C_CompilerType, CSharp_CompilerType,
+        MaybeThreadSafe, C_CompilerType, CSharp_CompilerType, use_cur_dir,
         ReuseStrategy, MaybeFeedbackInfo,
         HostEnvType, SystemEnvType, TargetEnvType, FileInstallCmd,
         LimitErrorContextsMap, !:Globals),
@@ -1157,7 +1163,25 @@ convert_options_to_globals(ProgressStream, DefaultOptionTable, OptionTable0,
     ),
     globals.set_opt_tuple(!.OptTuple, !Globals),
     postprocess_options_libgrades(!Globals, !Specs),
-    globals_init_mutables(!.Globals, !IO).
+    globals_init_mutables(!.Globals, !IO),
+
+    lookup_bool_option(OptionTable0, setting_only_use_subdirs, UseSubdirs),
+    (
+        UseSubdirs = no,
+        SubdirSetting = use_cur_dir
+    ;
+        UseSubdirs = yes,
+        lookup_bool_option(OptionTable0, setting_only_use_grade_subdirs,
+            UseGradeSubdirs),
+        (
+            UseGradeSubdirs = no,
+            SubdirSetting = use_cur_ngs_subdir
+        ;
+            UseGradeSubdirs = yes,
+            SubdirSetting = use_cur_ngs_gs_subdir
+        )
+    ),
+    globals.set_subdir_setting(SubdirSetting, !Globals).
 
 %---------------------------------------------------------------------------%
 
@@ -2391,7 +2415,7 @@ maybe_disable_smart_recompilation(ProgressStream, OpMode, !Globals, !IO) :-
     is det.
 
 handle_directory_options(OpMode, !Globals) :-
-    globals.lookup_bool_option(!.Globals, use_grade_subdirs,
+    globals.lookup_bool_option(!.Globals, setting_only_use_grade_subdirs,
         UseGradeSubdirs),
 
     % This is needed for library installation (the library grades
@@ -2407,14 +2431,14 @@ handle_directory_options(OpMode, !Globals) :-
             UseGradeSubdirs = bool.yes
         )
     then
-        globals.set_option(use_subdirs, bool(yes), !Globals)
+        globals.set_option(setting_only_use_subdirs, bool(yes), !Globals)
     else
         true
     ),
 
     % We only perform the library grade install check if we are
     % building a linked target using mmc --make or if we are building
-    % a single source file linked target.  (The library grade install
+    % a single source file linked target. (The library grade install
     % check is *not* compatible with the use of mmake.)
     ( if
         (
@@ -2623,7 +2647,8 @@ handle_directory_options(OpMode, !Globals) :-
     % module_name_to_file_name uses the plain header name, so we need to
     % add the full path to the header files in the current directory,
     % and any directories listed with --search-library-files-directory.
-    globals.lookup_bool_option(!.Globals, use_subdirs, UseSubdirs),
+    globals.lookup_bool_option(!.Globals, setting_only_use_subdirs,
+        UseSubdirs),
     ( if
         (
             UseGradeSubdirs = bool.yes,

compiler/compile_target_code.m
@@ -315,17 +315,19 @@ gather_c_compiler_flags(Globals, PIC, AllCFlags) :-
     join_string_list(C_Flags_List, "", "", " ", CFLAGS),
     gather_compiler_specific_flags(Globals, CC_Specific_CFLAGS),
 
-    globals.lookup_bool_option(Globals, use_subdirs, UseSubdirs),
+    globals.get_subdir_setting(Globals, SubdirSetting),
     (
-        UseSubdirs = yes,
+        SubdirSetting = use_cur_dir,
+        SubDirInclOpt = ""
+    ;
+        ( SubdirSetting = use_cur_ngs_subdir
+        ; SubdirSetting = use_cur_ngs_gs_subdir
+        ),
         % The source file (foo.c) will be compiled in a subdirectory
         % (either Mercury/cs, foo.dir, or Mercury/dirs/foo.dir, depending
         % on which of these two options is set) so we need to add `-I.'
         % so it can include header files in the source directory.
         SubDirInclOpt = "-I. "
-    ;
-        UseSubdirs = no,
-        SubDirInclOpt = ""
     ),
 
     gather_c_include_dir_flags(Globals, InclOpt),
@@ -936,20 +938,21 @@ compile_java_files(Globals, ProgressStream, ErrorStream,
         TargetDebugOpt = ""
     ),
 
-    globals.lookup_bool_option(Globals, use_subdirs, UseSubdirs),
-    globals.lookup_bool_option(Globals, use_grade_subdirs, UseGradeSubdirs),
+    globals.get_subdir_setting(Globals, SubdirSetting),
     globals.lookup_string_option(Globals, target_arch, TargetArch),
     (
-        UseSubdirs = yes,
+        SubdirSetting = use_cur_dir,
+        DirOpts = ""
+    ;
         (
-            UseGradeSubdirs = yes,
+            SubdirSetting = use_cur_ngs_subdir,
+            SourceDirName = "Mercury"/"javas",
+            DestDirName = "Mercury"/"classs"
+        ;
+            SubdirSetting = use_cur_ngs_gs_subdir,
             grade_directory_component(Globals, Grade),
             SourceDirName = "Mercury"/Grade/TargetArch/"Mercury"/"javas",
             DestDirName = "Mercury"/Grade/TargetArch/"Mercury"/"classs"
-        ;
-            UseGradeSubdirs = no,
-            SourceDirName = "Mercury"/"javas",
-            DestDirName = "Mercury"/"classs"
         ),
         % Javac won't create the destination directory for class files,
         % so we need to do it.
@@ -959,9 +962,6 @@ compile_java_files(Globals, ProgressStream, ErrorStream,
             "-sourcepath ", SourceDirName, " ",
             "-d ", DestDirName, " "
         ])
-    ;
-        UseSubdirs = no,
-        DirOpts = ""
     ),
 
     globals.lookup_string_option(Globals, filterjavac_command, MFilterJavac),

compiler/file_names.m
@@ -1213,12 +1213,14 @@ module_name_to_file_name_ext(Globals, From, Search, StatOnlyMkdir, Ext,
             ext_mmake_fragment_extension_dir(ExtMf, ExtStr, SubDirName)
         ),
         BaseNameNoExt = sym_name_to_string_sep(ModuleName, "."),
-        globals.lookup_bool_option(Globals, use_subdirs, UseSubdirs),
+        globals.get_subdir_setting(Globals, SubdirSetting),
         (
-            UseSubdirs = no,
+            SubdirSetting = use_cur_dir,
             DirNames = []
         ;
-            UseSubdirs = yes,
+            ( SubdirSetting = use_cur_ngs_subdir
+            ; SubdirSetting = use_cur_ngs_gs_subdir
+            ),
             DirNames = ["Mercury", SubDirName]
         )
     ;
@@ -1235,14 +1237,14 @@ module_name_to_file_name_ext(Globals, From, Search, StatOnlyMkdir, Ext,
         % directory only with --no-use-grade-subdirs; with --use-grade-subdirs,
         % they go in a grade subdir.
         BaseNameNoExt = sym_name_to_string_sep(ModuleName, "."),
-        globals.lookup_bool_option(Globals, use_grade_subdirs,
-            UseGradeSubdirs),
+        globals.get_subdir_setting(Globals, SubdirSetting),
         (
-            UseGradeSubdirs = no,
+            ( SubdirSetting = use_cur_dir
+            ; SubdirSetting = use_cur_ngs_subdir
+            ),
             DirNames = []
         ;
-            UseGradeSubdirs = yes,
-            % This implies --use-subdirs as well.
+            SubdirSetting = use_cur_ngs_gs_subdir,
             DirNames = make_grade_subdir_name(Globals, [SubDirName])
         )
     ;
@@ -1260,21 +1262,16 @@ module_name_to_file_name_ext(Globals, From, Search, StatOnlyMkdir, Ext,
             DirNames = []
         ;
             Search = do_not_search,
-            globals.lookup_bool_option(Globals, use_subdirs, UseSubdirs),
+            globals.get_subdir_setting(Globals, SubdirSetting),
             (
-                UseSubdirs = no,
+                SubdirSetting = use_cur_dir,
                 DirNames = []
             ;
-                UseSubdirs = yes,
-                globals.lookup_bool_option(Globals, use_grade_subdirs,
-                    UseGradeSubdirs),
-                (
-                    UseGradeSubdirs = no,
-                    DirNames = ["Mercury", SubDirName]
-                ;
-                    UseGradeSubdirs = yes,
-                    DirNames = make_grade_subdir_name(Globals, [SubDirName])
-                )
+                SubdirSetting = use_cur_ngs_subdir,
+                DirNames = ["Mercury", SubDirName]
+            ;
+                SubdirSetting = use_cur_ngs_gs_subdir,
+                DirNames = make_grade_subdir_name(Globals, [SubDirName])
             )
         )
     ;
@@ -1289,26 +1286,21 @@ module_name_to_file_name_ext(Globals, From, Search, StatOnlyMkdir, Ext,
             ext_analysis_extension_dir(ExtAn, ExtStr, SubDirName)
         ),
         BaseNameNoExt = sym_name_to_string_sep(ModuleName, "."),
-        globals.lookup_bool_option(Globals, use_subdirs, UseSubdirs),
+        globals.get_subdir_setting(Globals, SubdirSetting),
         (
-            UseSubdirs = no,
+            SubdirSetting = use_cur_dir,
             DirNames = []
         ;
-            UseSubdirs = yes,
-            globals.lookup_bool_option(Globals, use_grade_subdirs,
-                UseGradeSubdirs),
+            SubdirSetting = use_cur_ngs_subdir,
+            DirNames = ["Mercury", SubDirName]
+        ;
+            SubdirSetting = use_cur_ngs_gs_subdir,
             (
-                UseGradeSubdirs = no,
+                Search = do_search,
                 DirNames = ["Mercury", SubDirName]
             ;
-                UseGradeSubdirs = yes,
-                (
-                    Search = do_search,
-                    DirNames = ["Mercury", SubDirName]
-                ;
-                    Search = do_not_search,
-                    DirNames = make_grade_subdir_name(Globals, [SubDirName])
-                )
+                Search = do_not_search,
+                DirNames = make_grade_subdir_name(Globals, [SubDirName])
             )
         )
     ;
@@ -1344,21 +1336,16 @@ module_name_to_file_name_ext(Globals, From, Search, StatOnlyMkdir, Ext,
             ext_analysis_ds_extension_dir(ExtAnDs, ExtStr, SubDirName)
         ),
         BaseNameNoExt = sym_name_to_string_sep(ModuleName, "."),
-        globals.lookup_bool_option(Globals, use_subdirs, UseSubdirs),
+        globals.get_subdir_setting(Globals, SubdirSetting),
         (
-            UseSubdirs = no,
+            SubdirSetting = use_cur_dir,
             DirNames = []
         ;
-            UseSubdirs = yes,
-            globals.lookup_bool_option(Globals, use_grade_subdirs,
-                UseGradeSubdirs),
-            (
-                UseGradeSubdirs = no,
-                DirNames = ["Mercury", SubDirName]
-            ;
-                UseGradeSubdirs = yes,
-                DirNames = make_grade_subdir_name(Globals, [SubDirName])
-            )
+            SubdirSetting = use_cur_ngs_subdir,
+            DirNames = ["Mercury", SubDirName]
+        ;
+            SubdirSetting = use_cur_ngs_gs_subdir,
+            DirNames = make_grade_subdir_name(Globals, [SubDirName])
         )
     ;
         % The cur_ngs_gs_java group of extensions.
@@ -1367,21 +1354,16 @@ module_name_to_file_name_ext(Globals, From, Search, StatOnlyMkdir, Ext,
         ext_target_java_extension_dirs(ExtJava, ExtStr, SubDirNames),
         BaseParentDirs = ["jmercury"],
         mangle_sym_name_for_java(ModuleName, module_qual, "__", BaseNameNoExt),
-        globals.lookup_bool_option(Globals, use_subdirs, UseSubdirs),
+        globals.get_subdir_setting(Globals, SubdirSetting),
         (
-            UseSubdirs = no,
+            SubdirSetting = use_cur_dir,
             DirNames = BaseParentDirs
         ;
-            UseSubdirs = yes,
-            globals.lookup_bool_option(Globals, use_grade_subdirs,
-                UseGradeSubdirs),
-            (
-                UseGradeSubdirs = no,
+            SubdirSetting = use_cur_ngs_subdir,
                 DirNames = ["Mercury" |  SubDirNames]
-            ;
-                UseGradeSubdirs = yes,
-                DirNames = make_grade_subdir_name(Globals, SubDirNames)
-            )
+        ;
+            SubdirSetting = use_cur_ngs_gs_subdir,
+            DirNames = make_grade_subdir_name(Globals, SubDirNames)
         )
     ),
     CurDirFileName = BaseNameNoExt ++ ExtStr,
@@ -1518,22 +1500,18 @@ module_name_to_make_var_name(ModuleName, MakeVarName) :-
 %---------------------------------------------------------------------------%
 
 get_class_dir_name(Globals, ClassDirName) :-
-    globals.lookup_bool_option(Globals, use_grade_subdirs, UseGradeSubdirs),
-    globals.lookup_bool_option(Globals, use_subdirs, UseSubdirs),
+    globals.get_subdir_setting(Globals, SubdirSetting),
     (
-        UseGradeSubdirs = yes,
+        SubdirSetting = use_cur_dir,
+        ClassDirName = "."
+    ;
+        SubdirSetting = use_cur_ngs_subdir,
+        ClassDirName = "Mercury" / "classs"
+    ;
+        SubdirSetting = use_cur_ngs_gs_subdir,
         grade_directory_component(Globals, Grade),
         globals.lookup_string_option(Globals, target_arch, TargetArch),
         ClassDirName = "Mercury" / Grade / TargetArch / "Mercury" / "classs"
-    ;
-        UseGradeSubdirs = no,
-        (
-            UseSubdirs = yes,
-            ClassDirName = "Mercury" / "classs"
-        ;
-            UseSubdirs = no,
-            ClassDirName = "."
-        )
     ).
 
 %---------------------------------------------------------------------------%

compiler/globals.m
@@ -167,16 +167,30 @@
     ;       cc_cl_x64(maybe(int))  % MSVC targeting x86_64 (x64).
     ;       cc_unknown.
 
+:- type clang_version
+    --->    clang_version(int, int, int).
+
     % For the csharp backend, which csharp compiler are we using?
     %
 :- type csharp_compiler_type
     --->    csharp_microsoft
     ;       csharp_mono
-    ;       csharp_unknown
-    .
+    ;       csharp_unknown.
 
-:- type clang_version
-    --->    clang_version(int, int, int).
+%---------------------%
+
+    % Which subdirectories should we put new files in, and which subdirectories
+    % should we assume existing files are in?
+:- type subdir_setting
+    --->    use_cur_dir
+            % All files are always in the current directory.
+    ;       use_cur_ngs_subdir
+            % Files can be either in the current directory,
+            % or in a non-grade-specific subdirectory.
+    ;       use_cur_ngs_gs_subdir.
+            % Files can be in the current directory,
+            % in a non-grade-specific subdirectory, or
+            % in a grade-specific subdirectory.
 
 %---------------------%
 
@@ -291,8 +305,8 @@
     termination_norm::in, termination_norm::in,
     trace_level::in, trace_suppress_items::in, ssdb_trace_level::in,
     may_be_thread_safe::in, c_compiler_type::in, csharp_compiler_type::in,
-    reuse_strategy::in, maybe(feedback_info)::in, env_type::in,
-    env_type::in, env_type::in, file_install_cmd::in,
+    subdir_setting::in, reuse_strategy::in, maybe(feedback_info)::in,
+    env_type::in, env_type::in, env_type::in, file_install_cmd::in,
     limit_error_contexts_map::in, globals::out) is det.
 
 :- pred get_default_options(globals::in, option_table::out) is det.
@@ -311,6 +325,7 @@
 :- pred get_c_compiler_type(globals::in, c_compiler_type::out) is det.
 :- pred get_csharp_compiler_type(globals::in, csharp_compiler_type::out)
     is det.
+:- pred get_subdir_setting(globals::in, subdir_setting::out) is det.
 :- pred get_reuse_strategy(globals::in, reuse_strategy::out) is det.
 :- pred get_maybe_feedback_info(globals::in, maybe(feedback_info)::out) is det.
 :- pred get_host_env_type(globals::in, env_type::out) is det.
@@ -327,6 +342,8 @@
 :- pred set_options(option_table::in, globals::in, globals::out) is det.
 :- pred set_opt_tuple(opt_tuple::in, globals::in, globals::out) is det.
 :- pred set_op_mode(op_mode::in, globals::in, globals::out) is det.
+:- pred set_subdir_setting(subdir_setting::in,
+    globals::in, globals::out) is det.
 :- pred set_word_size(word_size::in, globals::in, globals::out) is det.
 :- pred set_gc_method(gc_method::in, globals::in, globals::out) is det.
 :- pred set_trace_level(trace_level::in, globals::in, globals::out) is det.
@@ -772,6 +789,7 @@ convert_line_number_range(RangeStr, line_number_range(MaybeMin, MaybeMax)) :-
                 % to allow them to be packed together.
                 g_csharp_compiler_type      :: csharp_compiler_type,
                 g_target                    :: compilation_target,
+                g_subdir_setting            :: subdir_setting,
                 g_word_size                 :: word_size,
                 g_gc_method                 :: gc_method,
                 g_termination_norm          :: termination_norm,
@@ -787,13 +805,13 @@ convert_line_number_range(RangeStr, line_number_range(MaybeMin, MaybeMax)) :-
 globals_init(DefaultOptions, Options, OptTuple, OpMode,
         Target, WordSize, GC_Method, TerminationNorm, Termination2Norm,
         TraceLevel, TraceSuppress, SSTraceLevel, MaybeThreadSafe,
-        C_CompilerType, CSharp_CompilerType,
+        C_CompilerType, CSharp_CompilerType, SubdirSetting,
         ReuseStrategy, MaybeFeedback, HostEnvType, SystemEnvType,
         TargetEnvType, FileInstallCmd, LimitErrorContextsMap, Globals) :-
     Globals = globals(DefaultOptions, Options, OptTuple, OpMode, TraceSuppress,
         ReuseStrategy, MaybeFeedback, FileInstallCmd, LimitErrorContextsMap,
-        C_CompilerType, CSharp_CompilerType, Target, WordSize, GC_Method,
-        TerminationNorm, Termination2Norm, TraceLevel, SSTraceLevel,
+        C_CompilerType, CSharp_CompilerType, Target, SubdirSetting, WordSize,
+        GC_Method, TerminationNorm, Termination2Norm, TraceLevel, SSTraceLevel,
         MaybeThreadSafe, HostEnvType, SystemEnvType, TargetEnvType).
 
 get_default_options(Globals, Globals ^ g_default_options).
@@ -811,6 +829,7 @@ get_ssdb_trace_level(Globals, Globals ^ g_ssdb_trace_level).
 get_maybe_thread_safe(Globals, Globals ^ g_may_be_thread_safe).
 get_c_compiler_type(Globals, Globals ^ g_c_compiler_type).
 get_csharp_compiler_type(Globals, Globals ^ g_csharp_compiler_type).
+get_subdir_setting(Globals, Globals ^ g_subdir_setting).
 get_reuse_strategy(Globals, Globals ^ g_reuse_strategy).
 get_maybe_feedback_info(Globals, Globals ^ g_maybe_feedback).
 get_host_env_type(Globals, Globals ^ g_host_env_type).
@@ -836,6 +855,9 @@ set_opt_tuple(OptTuple, !Globals) :-
 
 set_op_mode(OpMode, !Globals) :-
     !Globals ^ g_op_mode := OpMode.
+
+set_subdir_setting(X, !Globals) :-
+    !Globals ^ g_subdir_setting := X.
 
 set_word_size(WordSize, !Globals) :-
     !Globals ^ g_word_size := WordSize.

compiler/make.program_target.m
@@ -573,10 +573,23 @@ build_linked_target_2(Globals, MainModuleName, FileType, OutputFileName,
             top_target_file(MainModuleName, linked_target(FileType)),
         linked_target_file_name_full_curdir(Globals, MainModuleName, FileType,
             FullMainModuleLinkedFileName, CurDirMainModuleLinkedFileName, !IO),
-        globals.lookup_bool_option(NoLinkObjsGlobals, use_grade_subdirs,
-            UseGradeSubdirs),
+        globals.get_subdir_setting(NoLinkObjsGlobals, SubdirSetting),
+        % XXX Consider making the choice between the two switch arms below
+        % using FullMainModuleLinkedFileName = CurDirMainModuleLinkedFileName,
+        % not using SubdirSetting. The equality test would work even if
+        % we wanted to copy a file from a NON-grade-specific directory.
         (
-            UseGradeSubdirs = yes,
+            ( SubdirSetting = use_cur_dir
+            ; SubdirSetting = use_cur_ngs_subdir
+            ),
+            maybe_warn_up_to_date_target_msg(NoLinkObjsGlobals,
+                MainModuleLinkedTarget, FullMainModuleLinkedFileName, !Info,
+                UpToDateMsg),
+            % XXX MAKE_STREAM
+            maybe_write_msg(UpToDateMsg, !IO),
+            Succeeded = succeeded
+        ;
+            SubdirSetting = use_cur_ngs_gs_subdir,
             post_link_maybe_make_symlink_or_copy(NoLinkObjsGlobals,
                 ProgressStream, ErrorStream,
                 FullMainModuleLinkedFileName, CurDirMainModuleLinkedFileName,
@@ -595,14 +608,6 @@ build_linked_target_2(Globals, MainModuleName, FileType, OutputFileName,
                 % XXX MAKE_STREAM
                 maybe_write_msg(UpToDateMsg, !IO)
             )
-        ;
-            UseGradeSubdirs = no,
-            maybe_warn_up_to_date_target_msg(NoLinkObjsGlobals,
-                MainModuleLinkedTarget, FullMainModuleLinkedFileName, !Info,
-                UpToDateMsg),
-            % XXX MAKE_STREAM
-            maybe_write_msg(UpToDateMsg, !IO),
-            Succeeded = succeeded
         )
     ;
         DepsResult = deps_out_of_date,
@@ -1175,16 +1180,18 @@ choose_analysis_cache_dir_name(Globals, DirName) :-
     % to the caller whether they want to create the directory path,
     % the directory path would (or at least, SHOULD) be exactly what
     % this predicate computes.
-    globals.lookup_bool_option(Globals, use_grade_subdirs, UseGradeSubdirs),
-    globals.lookup_string_option(Globals, target_arch, TargetArch),
+    globals.get_subdir_setting(Globals, SubdirSetting),
     (
-        UseGradeSubdirs = yes,
+        ( SubdirSetting = use_cur_dir
+        ; SubdirSetting = use_cur_ngs_subdir
+        ),
+        DirComponents = ["Mercury", "analysis_cache"]
+    ;
+        SubdirSetting = use_cur_ngs_gs_subdir,
         grade_directory_component(Globals, Grade),
+        globals.lookup_string_option(Globals, target_arch, TargetArch),
         DirComponents = ["Mercury", Grade, TargetArch, "Mercury",
             "analysis_cache"]
-    ;
-        UseGradeSubdirs = no,
-        DirComponents = ["Mercury", "analysis_cache"]
     ),
     DirName = dir.relative_path_name_from_components(DirComponents).
 
@@ -1643,8 +1650,16 @@ install_library_grade(LinkSucceeded0, ModuleName, AllModules, Globals, Grade,
         Succeeded, !Info, !IO) :-
     % Only remove grade-dependent files after installing if
     % --use-grade-subdirs is not specified by the user.
-    globals.lookup_bool_option(Globals, use_grade_subdirs, UseGradeSubdirs),
-    CleanAfter = not(UseGradeSubdirs),
+    globals.get_subdir_setting(Globals, SubdirSetting),
+    (
+        ( SubdirSetting = use_cur_dir
+        ; SubdirSetting = use_cur_ngs_subdir
+        ),
+        CleanAfter = yes
+    ;
+        SubdirSetting = use_cur_ngs_gs_subdir,
+        CleanAfter = no
+    ),
 
     % Set up so that grade-dependent files for the current grade
     % don't overwrite the files for the default grade.

compiler/module_cmds.m
@@ -1053,15 +1053,15 @@ get_mercury_std_libs_for_java(Globals, !:StdLibs) :-
 
 list_class_files_for_jar(Globals, MainClassFiles, ClassSubDir,
         ListClassFiles, !IO) :-
-    globals.lookup_bool_option(Globals, use_subdirs, UseSubdirs),
-    globals.lookup_bool_option(Globals, use_grade_subdirs, UseGradeSubdirs),
-    AnySubdirs = UseSubdirs `or` UseGradeSubdirs,
+    globals.get_subdir_setting(Globals, SubdirSetting),
     (
-        AnySubdirs = yes,
-        get_class_dir_name(Globals, ClassSubDir)
-    ;
-        AnySubdirs = no,
+        SubdirSetting = use_cur_dir,
         ClassSubDir = dir.this_directory
+    ;
+        ( SubdirSetting = use_cur_ngs_subdir
+        ; SubdirSetting = use_cur_ngs_gs_subdir
+        ),
+        get_class_dir_name(Globals, ClassSubDir)
     ),
 
     list.filter_map(make_nested_class_prefix, MainClassFiles,
@@ -1098,15 +1098,18 @@ list_class_files_for_jar(Globals, MainClassFiles, ClassSubDir,
     ).
 
 list_class_files_for_jar_mmake(Globals, ClassFiles, ListClassFiles) :-
-    globals.lookup_bool_option(Globals, use_subdirs, UseSubdirs),
-    globals.lookup_bool_option(Globals, use_grade_subdirs, UseGradeSubdirs),
-    AnySubdirs = UseSubdirs `or` UseGradeSubdirs,
+    globals.get_subdir_setting(Globals, SubdirSetting),
     (
-        AnySubdirs = yes,
+        SubdirSetting = use_cur_dir,
+        ListClassFiles = ClassFiles
+    ;
+        ( SubdirSetting = use_cur_ngs_subdir
+        ; SubdirSetting = use_cur_ngs_gs_subdir
+        ),
         get_class_dir_name(Globals, ClassSubdir),
         % Here we use the `-C' option of jar to change directory during
-        % execution, then use sed to strip away the Mercury/classs/
-        % prefix to the class files.
+        % execution, then use sed to strip away the Mercury/classs/ prefix
+        % to the class files.
         % Otherwise, the class files would be stored as
         %   Mercury/classs/*.class
         % within the jar file, which is not what we want.
@@ -1114,9 +1117,6 @@ list_class_files_for_jar_mmake(Globals, ClassFiles, ListClassFiles) :-
         ListClassFiles = "-C " ++ ClassSubdir ++ " \\\n" ++
             "\t\t`echo "" " ++ ClassFiles ++ """" ++
             " | sed 's| '" ++ ClassSubdir ++ "/| |'`"
-    ;
-        AnySubdirs = no,
-        ListClassFiles = ClassFiles
     ).
 
 :- pred make_nested_class_prefix(string::in, string::out) is semidet.

commit 23d377b53265bf5d62200bedb52aac793d0a8f75

Fix mmc --make not implying --use-subdirs.
Commit 315fd9eb03b4afec6f6959fe600065e9a7295c70 introduced a regression
where mmc --make did not imply --use-subdirs in the compiler's global
options structure. In particular, this broke the --track-flags option
as it caused .track_flags files to be written to the current directory
instead of the Mercury/track_flagss subdirectory.

compiler/handle_options.m:
    When deciding the value for subdir_setting,
    look up the values of setting_only_use_subdirs and
    setting_only_use_grade_subdirs from the Globals variable
    (which include the implications of the --make option),
    not the original options table.

compiler/handle_options.m
@@ -1165,13 +1165,14 @@ convert_options_to_globals(ProgressStream, DefaultOptionTable, OptionTable0,
     postprocess_options_libgrades(!Globals, !Specs),
     globals_init_mutables(!.Globals, !IO),
 
-    lookup_bool_option(OptionTable0, setting_only_use_subdirs, UseSubdirs),
+    globals.lookup_bool_option(!.Globals, setting_only_use_subdirs,
+        UseSubdirs),
     (
         UseSubdirs = no,
         SubdirSetting = use_cur_dir
     ;
         UseSubdirs = yes,
-        lookup_bool_option(OptionTable0, setting_only_use_grade_subdirs,
+        globals.lookup_bool_option(!.Globals, setting_only_use_grade_subdirs,
             UseGradeSubdirs),
         (
             UseGradeSubdirs = no,