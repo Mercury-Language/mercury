457dba0c6cce4f9a482af108bed46e9772c748b8
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2023-10-05 00:32:47 +1100

    Fix the failure of invalid/multiply_star in debug grades.
    tests/invalid/multiply_star.err_exp3:
        Add a file for the expected error message in debug grades.
    
    tests/invalid/multiply_star.m:
        Add a description of the reason for the difference in expected
        error messages in debug grades.
    
    tests/invalid/Mercury.options:
        Specify verbose errors, which affects the .err_exp2 file.
    
    tests/invalid/multiply_star.err_exp:
    tests/invalid/multiply_star.err_exp2:
        Update the line numbers in the error messages.

--- tests/invalid/multiply_star.m
+++ tests/invalid/multiply_star.m
@@ -2,8 +2,17 @@
 % vim: ts=4 sw=4 et ft=mercury
 %---------------------------------------------------------------------------%
 %
-% The .err_exp file is for compiling without intermodule optimization.
-% The .err_exp2 file is for compiling with intermodule optimization.
+% The .err_exp file is for non-debug grades without --intermod-opt.
+% The .err_exp2 file is for all grades with --intermod-opt.
+% The .err_exp3 file is for debug grades without --intermod-opt.
+%
+% The presence of --intermod-opt yields a kind of error message
+% which makes the presence or absence of debugging irrelevant.
+% However, without --intermod-opt, debug grades affect the error message
+% by causing the implicit import of table_builtin.m (for I/O tabling),
+% which causes the implicit import of io.m, which causes the implicit import
+% of string.m.
+%
 
 :- module multiply_star.
 
