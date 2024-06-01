bf5d7b0ef81a04e773b8b4d83302549f0d68ee47
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2024-06-02 03:22:21 +1000

    Reenable two tests.
    tests/invalid/Mmakefile:
        Renenable the undef_type test, which got accidentally disabled
        in oct 2023.
    
        Execute the trailed_mutable test in both trailing and non-trailing grades.
        We used to enable it *only* in trailing grades, but had its only .err_exp
        file set to the contents expected in *non*-trailing grades.
    
    tests/invalid/undef_type.err_exp:
        Update this file for recent changes to diagnostics.
    
    tests/invalid/trailed_mutable.m:
        Modify the code of this test to ensure that we can expect
        at least one diagnostic in both trailing and non-trailing grades.
    
    tests/invalid/trailed_mutable.err_exp:
    tests/invalid/trailed_mutable.err_exp2:
        Add the .err_exp2 file, and fill each of these files with the
        expected output for non-trailing and trailing grades respectively.

--- tests/invalid/trailed_mutable.m
+++ tests/invalid/trailed_mutable.m
@@ -1,6 +1,9 @@
 %---------------------------------------------------------------------------%
 % vim: ts=4 sw=4 et ft=mercury
 %---------------------------------------------------------------------------%
+% The .err_exp file is for non-trailing grades.
+% The .err_exp2 file is for trailing grades.
+%---------------------------------------------------------------------------%
 
 :- module trailed_mutable.
 
@@ -13,3 +16,4 @@
 :- type foo == int.
 
 :- mutable(global, int, 42, ground, [trailed]).
+:- mutable(global2, int, 42.3, ground, [untrailed]).
