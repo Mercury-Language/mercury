56160a9806225932150cb7284a9b560670ea2d6c
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2024-12-07 15:19:52 +1100

    Fix copyright dates.

--- compiler/color_schemes.m
+++ compiler/color_schemes.m
@@ -1,8 +1,7 @@
 %---------------------------------------------------------------------------%
 % vim: ft=mercury ts=4 sw=4 et
 %---------------------------------------------------------------------------%
-% Copyright (C) 1994-2012 The University of Melbourne.
-% Copyright (C) 2013-2024 The Mercury Team.
+% Copyright (C) 2024 The Mercury Team.
 % This file may only be copied under the terms of the GNU General
 % Public License - see the file COPYING in the Mercury distribution.
 %---------------------------------------------------------------------------%
