d21de2c9816bc9799e50f2643642bf887cdf339d
Julien Fischer <juliensf@gmail.com>
2024-12-15 17:14:22 +1100

    Update and fix more copyright notices.
    compiler/*.m:
        As above.

--- compiler/mercury_compile.m
+++ compiler/mercury_compile.m
@@ -1,8 +1,8 @@
 %---------------------------------------------------------------------------%
 % vim: ft=mercury ts=4 sw=4 et
 %---------------------------------------------------------------------------%
-% Copyright (C) 2002-2009 The University of Melbourne.
-% Copyright (C) 2016 The Mercury team.
+% Copyright (C) 2002-2012 The University of Melbourne.
+% Copyright (C) 2013-2016, 2024 The Mercury team.
 % This file may only be copied under the terms of the GNU General
 % Public License - see the file COPYING in the Mercury distribution.
 %---------------------------------------------------------------------------%
