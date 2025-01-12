0326634b101267567913daa5e4d33179816df3a8
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2025-01-12 22:10:28 +1100

    Move pred markers and goal features to a new module.
    compiler/hlds_markers.m:
        This is that new module.
    
    compiler/hlds.m:
    compiler/notes/compiler_design.html:
        Include and document the new module.
    
    compiler/hlds_pred.m:
    compiler/hlds_goal.m:
        Delete the moved code.
    
    compiler/*.m:
        Conform to the changes above. Roughly a third of the modules
        that import hlds_pred.m or hlds_goal.m import the new module.

--- compiler/simplify_goal.m
+++ compiler/simplify_goal.m
@@ -1,7 +1,7 @@
 %----------------------------------------------------------------------------%
 % vim: ft=mercury ts=4 sw=4 et
 %----------------------------------------------------------------------------%
-% Copyright (C) 2014-2018, 2020-2024 The Mercury team.
+% Copyright (C) 2014-2018, 2020-2025 The Mercury team.
 % This file may only be copied under the terms of the GNU General
 % Public License - see the file COPYING in the Mercury distribution.
 %----------------------------------------------------------------------------%
