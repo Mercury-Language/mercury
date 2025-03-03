af093c2fad19679033115e8c28bb5efb9392ecfe
Julien Fischer <juliensf@gmail.com>
2025-03-03 15:42:59 +1100

    Minor fix for function description.
    library/set.m:
        As above.

--- library/set.m
+++ library/set.m
@@ -266,7 +266,7 @@
 :- func list_to_set(list(T)) = set(T).
 :- pred list_to_set(list(T)::in, set(T)::out) is det.
 
-    % Synonyms for list_to_set/1.
+    % Synonym for list_to_set/1.
     %
 :- func from_list(list(T)) = set(T).
 
