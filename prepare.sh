96bf455994114344f42c6ef8c82bab2e85c6289f
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2025-05-16 20:08:46 +1000

    Clarify a diagnostic from prepare.sh.

--- prepare.sh
+++ prepare.sh
@@ -3,14 +3,15 @@
 # the necessary git submodules and produce the configure script.
 set -e
 
-if [ ! -e boehm_gc/.git ]; then
+if test ! -e boehm_gc/.git; then
     echo "Setting up submodules"
     if git submodule --quiet init; then
         git submodule update --init --recursive
     else
-        echo "There was a problem configuring the submodules."
-        echo "If the repositories could not be found then edit .gitmodules"
-        echo "then run: git submodule update --recursive"
+        echo "There was a problem configuring the boehm_gc submodule."
+        echo "If its repository could not be found, then"
+        echo "first edit .gitmodules to specify its current location,"
+        echo "and then run \"git submodule update --recursive\"."
         exit 1
     fi
 fi
