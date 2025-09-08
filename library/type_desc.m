04a5200293de7986f083fdda9e19ee4ab95c6356
Peter Wang <novalazy@gmail.com>
2025-09-08 17:26:09 +1000

    Check for null TypeInfo.args array.
    library/type_desc.m:
        Check for TypeInfo.args == null to avoid a null pointer exception.
        This is necessary for the Java grade.
    
        Make the corresponding change for C# grade for consistency,
        though it does not appear to be necessary.

--- library/type_desc.m
+++ library/type_desc.m
@@ -682,7 +682,13 @@ type_ctor_arity(TypeCtor) = Arity :-
         case runtime.TypeCtorRep.MR_TYPECTOR_REP_PRED:
         case runtime.TypeCtorRep.MR_TYPECTOR_REP_FUNC:
         case runtime.TypeCtorRep.MR_TYPECTOR_REP_TUPLE:
-            tci = new runtime.TypeCtorInfo_Struct(tci, TypeInfo.args.Length);
+            int arity;
+            if (TypeInfo.args == null) {
+                arity = 0;
+            } else {
+                arity = TypeInfo.args.Length;
+            }
+            tci = new runtime.TypeCtorInfo_Struct(tci, arity);
             break;
         default:
             break;
@@ -702,7 +708,13 @@ type_ctor_arity(TypeCtor) = Arity :-
         case jmercury.runtime.TypeCtorRep.MR_TYPECTOR_REP_PRED:
         case jmercury.runtime.TypeCtorRep.MR_TYPECTOR_REP_FUNC:
         case jmercury.runtime.TypeCtorRep.MR_TYPECTOR_REP_TUPLE:
-            tci = new TypeCtorInfo_Struct(tci, TypeInfo.args.length);
+            int arity;
+            if (TypeInfo.args == null) {
+                arity = 0;
+            } else {
+                arity = TypeInfo.args.length;
+            }
+            tci = new TypeCtorInfo_Struct(tci, arity);
             break;
         default:
             break;
