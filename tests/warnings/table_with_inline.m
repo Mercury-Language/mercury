e8eadc61bdfba65152934d8934d38bb4852e0b19
Zoltan Somogyi <zoltan.somogyi@runbox.com>
2025-09-13 14:06:18 +1000

    Fix some causes of test case failures in java grade.
    tests/warnings/foreign_singleton.err_exp2:
    tests/warnings/singleton_test.err_exp2:
    tests/warnings/singleton_test.err_exp3:
        Update expected outputs for recent changes.
    
    tests/warnings/table_with_inline.m:
        Document the new .err_exp3 file.
    
    tests/warnings/table_with_inline.err_exp3:
        Add this expected output for non-C grades.
    
    tests/warnings/table_with_inline.err_exp:
    tests/warnings/table_with_inline.err_exp2:
        Update line numbers.

--- tests/warnings/table_with_inline.m
+++ tests/warnings/table_with_inline.m
@@ -1,8 +1,9 @@
 %---------------------------------------------------------------------------%
 % vim: ts=4 sw=4 et ft=mercury
 %---------------------------------------------------------------------------%
-% The .err_exp file is for non .par grades.
-% The .err_exp2 file is for .par grades.
+% The .err_exp file is for non .par C grades.
+% The .err_exp2 file is for .par C grades.
+% The .err_exp3 file is for non-C grades.
 %---------------------------------------------------------------------------%
 
 :- module table_with_inline.
